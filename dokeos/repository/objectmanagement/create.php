8497e1aefe894e5e0e26a0b3f721d3c4391c3c60
bmol <bmol@cosnics>
2006-03-06 11:20:02 +0000

    Initial import

---
+++ dokeos/repository/objectmanagement/create.php
@@ -0,0 +1,28 @@
+<?php
+require_once('../../claroline/inc/claro_init_global.inc.php');
+require_once(api_get_library_path().'/formvalidator/FormValidator.class.php');
+require_once('../lib/datamanager.class.php');
+require_once('../lib/learningobject_form.class.php');
+require_once('../lib/learning_object/announcement/form.class.php');
+Display::display_header('Create');
+if( !api_get_user_id())
+{
+	api_not_allowed();
+}
+if( isset($_GET['type']))
+{
+	$type = $_GET['type'];
+	$form = LearningObjectForm::factory($type,'create','post','create.php?type='.$type);
+	$form->build_create_form();
+	if($form->validate())
+	{
+		$object = $form->create_learning_object(api_get_user_id());
+		var_dump($object);
+	}
+	else
+	{
+		$form->display();
+	}
+}
+Display::display_footer();
+?>
