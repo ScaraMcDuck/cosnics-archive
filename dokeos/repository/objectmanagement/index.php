2a5191eafd45c1eb292ce71a67228a0539a677c8
bmol <bmol@cosnics>
2006-03-06 15:00:00 +0000

    adding simple search (only searching on title at the moment)

--- dokeos/repository/objectmanagement/index.php
+++ dokeos/repository/objectmanagement/index.php
@@ -11,7 +11,12 @@ function get_number_of_objects()
 {
 	$datamanager = DataManager::get_instance();
 	$properties['owner'] = api_get_user_id();
-	$objects = $datamanager->retrieve_learning_objects($properties);
+	if (isset ($_GET['keyword']))
+	{
+		$partial_properties['title'] = $_GET['keyword'];
+		// Should be completed to description property
+	}
+	$objects = $datamanager->retrieve_learning_objects($properties,$partial_properties);
 	return count($objects);
 }
 /**
@@ -22,9 +27,14 @@ function get_objects($from, $number_of_items, $column, $direction)
 	$table_columns = array('type','title','description','id');
 	$datamanager = DataManager::get_instance();
 	$properties['owner'] = api_get_user_id();
+	if (isset ($_GET['keyword']))
+	{
+		$partial_properties['title'] = $_GET['keyword'];
+		// Should be completed to description property
+	}
 	$order_by[] = $table_columns[$column];
 	$order_desc[] = $direction == SORT_ASC ? 1 : 0;
-	$objects = $datamanager->retrieve_learning_objects($properties,array(),$order_by,$order_desc);
+	$objects = $datamanager->retrieve_learning_objects($properties,$partial_properties,$order_by,$order_desc);
 	$table_data = array();
 	foreach($objects as $index => $object)
 	{
@@ -40,10 +50,19 @@ function get_objects($from, $number_of_items, $column, $direction)
 /*
  * Display page
  */
-Display::display_header('Object Management');
+// Display header
+$tool_name = get_lang('MyLearningObjects');
+Display::display_header($tool_name);
+api_display_tool_title($tool_name);
 $datamanager = DataManager::get_instance();
-$properties['owner'] = api_get_user_id();
-$objects = $datamanager->retrieve_learning_objects($properties);
+// Create a search-box
+$form = new FormValidator('search_simple','get','','',null,false);
+$renderer =& $form->defaultRenderer();
+$renderer->setElementTemplate('<span>{element}</span> ');
+$form->addElement('text','keyword',get_lang('keyword'));
+$form->addElement('submit','submit',get_lang('Search'));
+$form->display();
+// Create a sortable table to display the learning objects
 $table = new SortableTable('objects','get_number_of_objects','get_objects');
 $column = 0;
 $table->set_header($column++,get_lang('Type'));
@@ -51,5 +70,6 @@ $table->set_header($column++,get_lang('Title'));
 $table->set_header($column++,get_lang('Description'));
 $table->set_header($column++,get_lang('Modify'),false);
 $table->display();
+// Display footer
 Display::display_footer();
 ?>
