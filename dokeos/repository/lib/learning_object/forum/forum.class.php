c00e4c0d8c3afad296a1d21d19501c17c05b52e9
malchick <malchick@localhost>
2006-03-06 08:52:35 +0000

    additional types of learning objects

---
+++ dokeos/repository/lib/learning_object/forum/forum.class.php
@@ -0,0 +1,48 @@
+<?php
+require_once dirname(__FILE__) . '/../../learningobject.class.php';
+
+class Forum extends LearningObject {
+	function get_access () {
+		return $this->get_additional_property('access');
+	}
+	function set_access ($access) {
+		return $this->set_additional_property('access', $access);
+	}
+	function get_moderator () {
+		return $this->get_additional_property('moderator');
+	}
+	function set_moderator ($moderator) {
+		return $this->set_additional_property('moderator', $moderator);
+	}
+	function get_topics () {
+		return $this->get_additional_property('topics');
+	}
+	function set_topics ($topics) {
+		return $this->set_additional_property('topics', $topics);
+	}
+	function get_posts () {
+		return $this->get_additional_property('posts');
+	}
+	function set_posts ($posts) {
+		return $this->set_additional_property('posts', $posts);
+	}
+	function get_last_post_id () {
+		return $this->get_additional_property('last_post_id');
+	}
+	function set_last_post_id ($last_post_id) {
+		return $this->set_additional_property('last_post_id', $last_post_id);
+	}
+	function get_cat_id () {
+		return $this->get_additional_property('cat_id');
+	}
+	function set_cat_id ($cat_id) {
+		return $this->set_additional_property('cat_id', $cat_id);
+	}
+	function get_forum_type () {
+		return $this->get_additional_property('forum_type');
+	}
+	function set_forum_type ($forum_type) {
+		return $this->set_additional_property('forum_type', $forum_type);
+	}
+}
+?>
