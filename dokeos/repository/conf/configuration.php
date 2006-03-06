c243d4b3ac45d02152d831e9367f13a041d76530
ceetee <ceetee@cosnics>
2006-03-06 10:50:23 +0000

    Moving from INI file to PHP script for configuration for security reasons.

---
+++ dokeos/repository/conf/configuration.php
@@ -0,0 +1,12 @@
+<?php
+$configuration = array();
+
+$configuration['general']['data_manager']
+	= 'Database';
+
+$configuration['database']['connection_string']
+	= 'mysql://root:moo@localhost/dokeoslcms';
+
+$configuration['database']['table_name_prefix']
+	= 'dokeos_';
+?>
