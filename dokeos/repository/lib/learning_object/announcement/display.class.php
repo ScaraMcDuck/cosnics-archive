cf305f8137f0e9d61399595c26b41a6e4b292c4a
bmol <bmol@cosnics>
2006-03-07 13:43:01 +0000

    Display class for announcement

---
+++ dokeos/repository/lib/learning_object/announcement/display.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package learningobject.announcement
+ */
+class AnnouncementDisplay extends LearningObjectDisplay
+{
+	public function AnnouncementDisplay(&$object)
+	{
+		parent :: LearningObjectDisplay($object);
+	}
+	public function get_full_html()
+	{
+		$object = $this->get_learning_object();
+		$html[] = '<div class="learning_object">';
+		$html[] = '<div class="icon"><img src="'.api_get_path(WEB_CODE_PATH).'img/'.$object->get_type().'.gif" alt="'.$object->get_type().'"/></div>';
+		$html[] = '<div class="title">'.$object->get_title().'</div>';
+		$html[] = '<div class="description">'.$object->get_description();
+		$html[] = '</div>';
+		$html[] = '</div>';
+		return implode("\n",$html);
+	}
+}
+?>
