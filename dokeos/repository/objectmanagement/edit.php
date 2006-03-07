28e22dbf02b5aeb40199e0a5e3b78c20854f6363
bmol <bmol@localhost>
2006-03-07 11:49:12 +0000

    return to overview page after update pass the learning object as parameter to the update-function & form build function

--- dokeos/repository/objectmanagement/edit.php
+++ dokeos/repository/objectmanagement/edit.php
@@ -3,7 +3,6 @@ require_once('../../claroline/inc/claro_init_global.inc.php');
 require_once(api_get_library_path().'/formvalidator/FormValidator.class.php');
 require_once('../lib/datamanager.class.php');
 require_once('../lib/learningobject_form.class.php');
-Display::display_header('Create');
 if( !api_get_user_id())
 {
 	api_not_allowed();
@@ -13,15 +12,17 @@ if( isset($_GET['id']))
 	$datamanager = DataManager::get_instance();
 	$object = $datamanager->retrieve_learning_object($_GET['id']);
 	$form = LearningObjectForm::factory($object->get_type(),'edit','post','edit.php?id='.$object->get_id());
-	$form->build_edit_form();
+	$form->build_edit_form($object);
 	if($form->validate())
 	{
-		$form->update_learning_object();
+		$form->update_learning_object($object);
+		header('Location: index.php');
 	}
 	else
 	{
+		Display::display_header('Create');
 		$form->display();
+		Display::display_footer();
 	}
 }
-Display::display_footer();
 ?>
