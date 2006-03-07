1d2b9f96260e51df11f483b97b2d1691fdc13487
bmol <bmol@cosnics>
2006-03-07 13:40:43 +0000

    added link to edit page added breadcrumb to index page

--- dokeos/repository/objectmanagement/view.php
+++ dokeos/repository/objectmanagement/view.php
@@ -11,7 +11,12 @@ if( isset($_GET['id']))
 	$datamanager = DataManager::get_instance();
 	$object = $datamanager->retrieve_learning_object($_GET['id']);
 	$display = LearningObjectDisplay::factory($object);
+	$interbredcrump[] = array ("url" => "index.php", "name" => get_lang('MyLearningObjects'));
 	Display::display_header($object->get_title());
+	api_display_tool_title('LearningObjectDetails');
+	echo '<p>';
+	echo '<a href="edit.php?id='.$object->get_id().'" title="'.get_lang('Edit').'"><img src="'.api_get_path(WEB_CODE_PATH).'img/edit.gif" alt="'.get_lang('Edit').'"/> '.get_lang('Edit').'</a>';
+	echo '</p>';
 	echo $display->get_full_html();
 	Display::display_footer();
 }
