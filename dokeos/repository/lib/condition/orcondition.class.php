38de89ad91b14bd8fa0492e5aa25283d7f5eeccc
ceetee <ceetee@cosnics>
2006-03-07 09:42:39 +0000

    Relax constructor for AND/OR Condition objects.

--- dokeos/repository/lib/condition/orcondition.class.php
+++ dokeos/repository/lib/condition/orcondition.class.php
@@ -1,28 +1,16 @@
 <?php
-require_once dirname(__FILE__).'/aggregatecondition.class.php';
+require_once dirname(__FILE__).'/multipleaggregatecondition.class.php';
 
 /**
 ==============================================================================
  *	This type of condition requires that one or more of its aggregated
- *	conditions be met. The aggregated conditions must be passed to the
- *	constructor as an array.
+ *	conditions be met.
  *
  *	@author Tim De Pauw
 ==============================================================================
  */
 
-class OrCondition extends AggregateCondition
+class OrCondition extends MultipleAggregateCondition
 {
-	private $conditions;
-
-	function OrCondition($conditions)
-	{
-		$this->conditions = $conditions;
-	}
-
-	function get_conditions()
-	{
-		return $this->conditions;
-	}
 }
 ?>
