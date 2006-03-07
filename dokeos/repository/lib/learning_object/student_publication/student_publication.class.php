fbbdb81d9137c36ab54884e7a55863b33841014d
ceetee <ceetee@cosnics>
2006-03-07 12:39:00 +0000

    Adding types to table filler; tweaking student publication table.

--- dokeos/repository/lib/learning_object/student_publication/student_publication.class.php
+++ dokeos/repository/lib/learning_object/student_publication/student_publication.class.php
@@ -1,30 +1,39 @@
 <?php
-require_once dirname(__FILE__) . '/../../learningobject.class.php';
+require_once dirname(__FILE__).'/../../learningobject.class.php';
 
-class StudentPublication extends LearningObject {
-	function get_url () {
+class StudentPublication extends LearningObject
+{
+	function get_url()
+	{
 		return $this->get_additional_property('url');
 	}
-	function set_url ($url) {
+	function set_url($url)
+	{
 		return $this->set_additional_property('url', $url);
 	}
-	function get_author () {
+	function get_author()
+	{
 		return $this->get_additional_property('author');
 	}
-	function set_author ($author) {
+	function set_author($author)
+	{
 		return $this->set_additional_property('author', $author);
 	}
-	function get_active () {
-		return $this->get_additional_property('active');
+	function get_active()
+	{
+		return ($this->get_additional_property('active') ? true : false);
 	}
-	function set_active ($active) {
-		return $this->set_additional_property('active', $active);
+	function set_active($active)
+	{
+		return $this->set_additional_property('active', ($active ? 1 : 0));
 	}
-	function get_accepted () {
-		return $this->get_additional_property('accepted');
+	function get_accepted()
+	{
+		return ($this->get_additional_property('accepted') ? true : false);
 	}
-	function set_accepted ($accepted) {
-		return $this->set_additional_property('accepted', $accepted);
+	function set_accepted($accepted)
+	{
+		return $this->set_additional_property('accepted', ($accepted ? 1 : 0));
 	}
 }
 ?>
--- dokeos/repository/test/fill_tables.php
+++ dokeos/repository/test/fill_tables.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
 ==============================================================================
  *	This is a simple test script that fills the LCMS data source with garbage
@@ -12,31 +13,93 @@
 
 require_once dirname(__FILE__).'/../lib/datamanager.class.php';
 
-$links = 1000;
-$documents = 1000;
+$users = 1;
+
+$announcements = rand(100, 500);
+$calendar_events = rand(100, 500);
+$documents = rand(100, 500);
+$links = rand(100, 500);
+$student_publications = rand(100, 500);
+
+// TODO
+/*
+$forums = rand(100,500);
+$forum_posts = rand(100,500);
+$forum_topics = rand(100,500);
+$learnpaths = rand(100,500);
+*/
 
 $dataManager = DataManager :: get_instance();
 
-for ($i = 0; $i < $links; $i ++)
+for ($i = 0; $i < $announcements; $i ++)
 {
-	$link = new Link();
-	$link->set_owner_id(1);
-	$link->set_title(random_string(2));
-	$link->set_description(random_string(8));
-	$link->set_url('http://www.google.com/');
-	$link->create();
+	$announcement = new Announcement();
+	$announcement->set_owner_id(random_user());
+	$announcement->set_title(random_string(2));
+	$announcement->set_description(random_string(8));
+	$announcement->create();
+}
+
+for ($i = 0; $i < $calendar_events; $i ++)
+{
+	$event = new CalendarEvent();
+	$event->set_owner_id(random_user());
+	$event->set_title(random_string(2));
+	$event->set_description(random_string(8));
+	$event->create();
 }
 
 for ($i = 0; $i < $documents; $i ++)
 {
 	$document = new Document();
-	$document->set_owner_id(1);
+	$document->set_owner_id(random_user());
 	$document->set_title(random_string(2));
 	$document->set_description(random_string(8));
 	$document->set_path('/'.random_word().'/'.random_word());
 	$document->set_filename(random_word());
 	$document->set_filesize(rand(1000, 10000));
 	$document->create();
+}
+
+for ($i = 0; $i < $forums; $i ++)
+{
+	$document = new Forum();
+	$document->set_owner_id(random_user());
+	$document->set_title(random_string(2));
+	$document->set_description(random_string(8));
+	$document->set_path('/'.random_word().'/'.random_word());
+	$document->set_filename(random_word());
+	$document->set_filesize(rand(1000, 10000));
+	$document->create();
+}
+
+for ($i = 0; $i < $links; $i ++)
+{
+	$link = new Link();
+	$link->set_owner_id(random_user());
+	$link->set_title(random_string(2));
+	$link->set_description(random_string(8));
+	$link->set_url('http://www.google.com/');
+	$link->create();
+}
+
+for ($i = 0; $i < $student_publications; $i ++)
+{
+	$student_publication = new StudentPublication();
+	$student_publication->set_owner_id(random_user());
+	$student_publication->set_title(random_string(2));
+	$student_publication->set_description(random_string(8));
+	$student_publication->set_author(random_user());
+	$student_publication->set_url('http://webs.hogent.be/~'.random_string(8).'/'.random_string(8).'.'.random_string(3));
+	$student_publication->set_active(true);
+	$student_publication->set_accepted(true);
+	$student_publication->create();
+}
+
+function random_user()
+{
+	global $users;
+	return rand(1, $users);
 }
 
 function random_string($length)
