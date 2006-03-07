2055406122ea4735371a11e184bf0866c7312cc4
ceetee <ceetee@localhost>
2006-03-07 12:39:00 +0000

    Adding types to table filler; tweaking student publication table.

--- dokeos/repository/lib/learning_object/student_publication/student_publication.sql
+++ dokeos/repository/lib/learning_object/student_publication/student_publication.sql
@@ -2,7 +2,7 @@ CREATE TABLE `%prefix%student_publication` (
   `id` int(10) unsigned NOT NULL,
   `url` varchar(200) default NULL,
   `author` varchar(200) default NULL,
-  `active` tinyint(4) default NULL,
-  `accepted` tinyint(4) default '0',
+  `active` tinyint(1) default NULL,
+  `accepted` tinyint(1) default '0',
   PRIMARY KEY  (`id`)
 );
