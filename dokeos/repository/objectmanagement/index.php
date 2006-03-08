54af1f33c8a42a328b87cdb707cd4a2f5766c243
bmol <bmol@cosnics>
2006-03-08 14:01:31 +0000

    allow user to move a single object or a set of selected objects between categories

--- dokeos/repository/objectmanagement/index.php
+++ dokeos/repository/objectmanagement/index.php
@@ -66,6 +66,8 @@ function get_objects($from, $number_of_items, $column, $direction)
 		$row[] = date('Y-m-d, H:i', is_null($object->get_modification_date()) ? $object->get_creation_date() : $object->get_modification_date());
 		$modify = '<a href="edit.php?id='.$object->get_id().'" title="'.get_lang('Edit').'"><img src="'.api_get_path(WEB_CODE_PATH).'img/edit.gif" alt="'.get_lang('Edit').'"/></a>';
  		$modify .= '<a href="index.php?action=delete&amp;id='.$object->get_id().'" title="'.get_lang('Delete').'"  onclick="javascript:if(!confirm(\''.addslashes(htmlentities(get_lang("ConfirmYourChoice"))).'\')) return false;"><img src="'.api_get_path(WEB_CODE_PATH).'img/delete.gif" alt="'.get_lang('Delete').'"/></a>';
+ 		$modify .= '<a href="index.php?action=move&amp;id='.$object->get_id().'" title="'.get_lang('Move').'"><img src="'.api_get_path(WEB_CODE_PATH).'img/move.gif" alt="'.get_lang('Move').'"/></a>';
+
  		$row[] = $modify;
  		$table_data[] = $row;
 	}
@@ -81,50 +83,19 @@ if( is_null($current_category_id))
 	$current_category_id = $root_category->get_id();
 }
 
-// Perform actions if needed
-if(isset($_GET['action']))
-{
-	switch($_GET['action'])
-	{
-		case 'delete':
-			$object = $datamanager->retrieve_learning_object($_GET['id']);
-			$object->delete();
-			break;
-	}
-}
-if(isset($_POST['action']))
-{
-	switch($_POST['action'])
-	{
-		case 'delete_selected':
-			foreach($_POST['id'] as $index => $object_id)
-			{
-				$object = $datamanager->retrieve_learning_object($object_id);
-				$object->delete();
-			}
-			break;
-	}
-}
-/*
- * Display page
- */
-// Display header
 $tool_name = get_lang('MyLearningObjects');
-Display::display_header($tool_name);
-api_display_tool_title($tool_name);
 // Create a search-box
 $form = new FormValidator('search_simple','get','','',null,false);
 $renderer =& $form->defaultRenderer();
 $renderer->setElementTemplate('<span>{element}</span> ');
 $form->addElement('text','keyword',get_lang('keyword'));
 $form->addElement('submit','submit',get_lang('Search'));
-$form->display();
 // Create a navigation menu to browse through the categories
 $menu = new CategoryMenu(api_get_user_id(),$current_category_id);
-$menu->show();
 // Create a sortable table to display the learning objects
 $table = new SortableTable('objects','get_number_of_objects','get_objects');
 $parameters = array();
+$parameters['category'] = $current_category_id;
 if (isset ($_GET['keyword']))
 {
 	$parameters = array ('keyword' => $_GET['keyword']);
@@ -138,8 +109,117 @@ $table->set_header($column++,get_lang('Description'));
 $table->set_header($column++,get_lang('LastModified'));
 $table->set_header($column++,get_lang('Modify'),false);
 $actions['delete_selected'] = get_lang('Delete');
+$actions['move_selected'] = get_lang('Move');
 $table->set_form_actions($actions);
+
+// Perform actions if needed
+if(isset($_GET['action']))
+{
+	switch($_GET['action'])
+	{
+		case 'delete':
+			$object = $datamanager->retrieve_learning_object($_GET['id']);
+			$object->delete();
+			$message = get_lang('ObjectDeleted');
+			break;
+		case 'move':
+			$condition = new ExactMatchCondition('owner',api_get_user_id());
+			$categories = $datamanager->retrieve_learning_objects('category',$condition);
+			$category_choices = array();
+			foreach($categories as $index => $category)
+			{
+				$category_choices[$category->get_id()] = $category->get_title();
+			}
+			$popup_form = new FormValidator('move_form','get');
+			$popup_form->addElement('hidden','id',$_GET['id']);
+			$popup_form->addElement('hidden','action','move');
+			$popup_form->addElement('select','new_category',get_lang('Category'),$category_choices);
+			$popup_form->addElement('submit','submit',get_lang('Ok'));
+			if($popup_form->validate())
+			{
+				$values = $popup_form->exportValues();
+				$object = $datamanager->retrieve_learning_object($_GET['id']);
+				$object->set_category_id($values['new_category']);
+				$object->update(false);
+				$message = get_lang('ObjectMoved');
+			}
+			else
+			{
+				$message = get_lang('SelectCategory');
+				$message .= $popup_form->toHtml();
+			}
+			break;
+	}
+}
+if(isset($_POST['action']))
+{
+	switch($_POST['action'])
+	{
+		case 'delete_selected':
+			foreach($_POST['id'] as $index => $object_id)
+			{
+				$object = $datamanager->retrieve_learning_object($object_id);
+				$object->delete();
+			}
+			$message = get_lang('ObjectsDeleted');
+			break;
+		case 'move_selected':
+			$condition = new ExactMatchCondition('owner',api_get_user_id());
+			$categories = $datamanager->retrieve_learning_objects('category',$condition);
+			$category_choices = array();
+			foreach($categories as $index => $category)
+			{
+				$category_choices[$category->get_id()] = $category->get_title();
+			}
+			$popup_form = new FormValidator('move_form','post');
+			foreach($_POST['id'] as $index => $object_id)
+			{
+				$popup_form->addElement('hidden','id[]',$object_id);
+			}
+			$popup_form->addElement('hidden','action','move_selected');
+			$popup_form->addElement('select','category',get_lang('Category'),$category_choices);
+			$popup_form->addElement('submit','submit',get_lang('Ok'));
+			if($popup_form->validate())
+			{
+				$values = $popup_form->exportValues();
+				foreach($_POST['id'] as $index => $object_id)
+				{
+					$object = $datamanager->retrieve_learning_object($object_id);
+					$object->set_category_id($values['category']);
+					$object->update(false);
+				}
+				$message = get_lang('ObjectsMoved');
+			}
+			else
+			{
+				$message = get_lang('SelectCategory');
+				$message .= $popup_form->toHtml();
+			}
+			break;
+	}
+}
+
+/*
+ * Display page
+ */
+// Display header
+Display::display_header($tool_name);
+api_display_tool_title($tool_name);
+// Display search form
+$form->display();
+// Display message if needed
+if(isset($message))
+{
+	Display::display_normal_message($message);
+}
+//TODO: Get rid of table -> move to css styled layout
+echo '<table width="100%"><tr><td width="180px">';
+// Display menu
+$menu->show();
+echo '</td><td>';
+// Display table
 $table->display();
+echo '</td></tr></table>';
 // Display footer
 Display::display_footer();
 ?>
