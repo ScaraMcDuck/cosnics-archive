5db423ae188208d1db5c5191cffb185cb8b8fa56
bmol <bmol@cosnics>
2006-03-09 13:29:56 +0000

    reset course

--- dokeos/repository/objectmanagement/index.php
+++ dokeos/repository/objectmanagement/index.php
@@ -1,9 +1,9 @@
 <?php
+$cidReset = true;
 require_once('../../claroline/inc/claro_init_global.inc.php');
 require_once(api_get_library_path().'/formvalidator/FormValidator.class.php');
 require_once('../lib/repositorydatamanager.class.php');
 require_once('../lib/learningobject_form.class.php');
-require_once('../lib/learning_object/announcement/form.class.php');
 require_once('../lib/categorymenu.class.php');
 require_once(api_get_library_path().'/text.lib.php');
 require_once dirname(__FILE__).'/../lib/repositoryutilities.class.php';
