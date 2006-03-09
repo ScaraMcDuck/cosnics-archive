83b98ace13be29931235dc7a593e245dad0a3c83
bmol <bmol@cosnics>
2006-03-09 13:09:22 +0000

    added some functions to retrieve quota information

--- dokeos/repository/lib/quotamanager.class.php
+++ dokeos/repository/lib/quotamanager.class.php
@@ -34,6 +34,22 @@ class QuotaManager
 		return $datamanager->get_used_disk_space($this->owner);
 	}
 	/**
+	 * Get the used disk space
+	 * @return int The percentage of disk space used
+	 */
+	public function get_used_disk_space_percent()
+	{
+		return round(100*$this->get_used_disk_space()/$this->get_max_disk_space());
+	}
+	/**
+	 * Get the available disk space
+	 * @return int The number of bytes available on disk
+	 */
+	public function get_available_disk_space()
+	{
+		return $this->get_max_disk_space()-$this->get_used_disk_space();
+	}
+	/**
 	 * Get the used database space
 	 * @return int The number of learning objects in the repository the the
 	 * owner
@@ -43,6 +59,48 @@ class QuotaManager
 		$datamanager = RepositoryDatamanager::get_instance();
 		$condition = new ExactMatchCondition('owner',$this->owner);
 		return $datamanager->count_learning_objects(null,$condition);
+	}
+	/**
+	 * Get the used database space
+	 * @return int The percentage of database space used
+	 */
+	public function get_used_database_space_percent()
+	{
+		return round(100*$this->get_used_database_space()/$this->get_max_database_space());
+	}
+	/**
+	 * Get the available database space
+	 * @return int The number learning objects available in the database
+	 */
+	public function get_available_database_space()
+	{
+		return $this->get_max_database_space()-$this->get_used_database_space();
+	}
+	/**
+	 * Get the maximum allowed disk space
+	 * @return int The number of bytes the user is allowed to use
+	 */
+	public function get_max_disk_space()
+	{
+		// TODO : This code is here temporarily for testing pupuses. This should be moved to the main_api function api_get_user_info
+		$user_table = Database::get_main_table(MAIN_USER_TABLE);
+		$sql = "SELECT disk_quota FROM ".$user_table." WHERE user_id = '".$this->owner."'";
+		$res = api_sql_query($sql,__FILE__,__LINE__);
+		$quota = mysql_fetch_object($res);
+		return $quota->disk_quota;
+	}
+	/**
+	 * Get the maximum allowed database space
+	 * @return int The number of learning objects the user is allowed to have
+	 */
+	public function get_max_database_space()
+	{
+		// TODO : This code is here temporarily for testing pupuses. This should be moved to the main_api function api_get_user_info
+		$user_table = Database::get_main_table(MAIN_USER_TABLE);
+		$sql = "SELECT database_quota FROM ".$user_table." WHERE user_id = '".$this->owner."'";
+		$res = api_sql_query($sql,__FILE__,__LINE__);
+		$quota = mysql_fetch_object($res);
+		return $quota->database_quota;
 	}
 }
 ?>
