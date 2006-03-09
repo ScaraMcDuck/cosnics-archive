cb2b2e0d85ef9521a489422d4d823933e1b7ed5a
bmol <bmol@cosnics>
2006-03-09 13:09:46 +0000

    simple quota overview page

--- dokeos/repository/objectmanagement/quota.php
+++ dokeos/repository/objectmanagement/quota.php
@@ -8,17 +8,31 @@ if( !api_get_user_id())
 	api_not_allowed();
 }
 
+function get_bar($percent)
+{
+	$html = '<blockquote>';
+	$html .= '<img src="'.api_get_path(WEB_CODE_PATH).'/img/bar_1m.gif" height="12"/>';
+	$html .= '<img src="'.api_get_path(WEB_CODE_PATH).'/img/bar_1u.gif" width="'.(2*$percent).'" height="12"/>';
+	$html .= '<img src="'.api_get_path(WEB_CODE_PATH).'/img/bar_1r.gif" width="'.(2*(100-$percent)).'" height="12"/>';
+	$html .= '<img src="'.api_get_path(WEB_CODE_PATH).'/img/bar_1.gif" height="12"/>';
+	$html .= ' '.$percent.' %';
+	$html .= '</blockquote>';
+	return $html;
+}
+
 $quotamanager = new QuotaManager(api_get_user_id());
 
 // Display header
 Display::display_header(get_lang('Quota'));
 api_display_tool_title(get_lang('Quota'));
-
-echo 'DISK: ';
-echo $quotamanager->get_used_disk_space();
-
-echo '<br />DB: ';
-echo $quotamanager->get_used_database_space();
+$percent = $quotamanager->get_used_disk_space_percent();
+echo '<h3>'.get_lang('Disk').'</h3>';
+echo $quotamanager->get_used_disk_space().' / '.$quotamanager->get_max_disk_space();
+echo get_bar($percent);
+echo '<h3>'.get_lang('Database').'</h3>';
+echo $quotamanager->get_used_database_space().' / '.$quotamanager->get_max_database_space();
+$percent = $quotamanager->get_used_database_space_percent();
+echo get_bar($percent);
 
 
 // Display footer
