dd5bc14c4235612eed9abc0273af6c4599cb1cea
ceetee <ceetee@cosnics>
2006-03-09 09:36:05 +0000

    Sharing database connection.

--- dokeos/application/lib/weblcms/data_manager/database.class.php
+++ dokeos/application/lib/weblcms/data_manager/database.class.php
@@ -1,10 +1,18 @@
 <?php
 require_once dirname(__FILE__).'/../weblcmsdatamanager.class.php';
+require_once dirname(__FILE__).'/../../../../repository/lib/repositorydatamanager.class.php';
 
 class DatabaseWebLCMSDataManager extends WebLCMSDataManager
 {
-	function DatabaseWebLCMSDataManager()
+	private $connection;
+	
+	private $prefix;
+
+	function initialize()
 	{
+		$m = RepositoryDataManager :: get_instance();
+		$this->connection = $m->get_connection();
+		$this->prefix = $m->get_table_name_prefix();
 	}
 }
 ?>
