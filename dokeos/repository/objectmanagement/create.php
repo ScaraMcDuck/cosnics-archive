707eb73764960084a22b009731084ef3ce45f7ad
malchick <malchick@cosnics>
2006-03-10 12:53:38 +0000

    added header to display message

--- dokeos/repository/objectmanagement/create.php
+++ dokeos/repository/objectmanagement/create.php
@@ -4,7 +4,6 @@ require_once(api_get_library_path().'/formvalidator/FormValidator.class.php');
 require_once('../lib/repositorydatamanager.class.php');
 require_once('../lib/learningobject_form.class.php');
 require_once('../lib/learning_object/announcement/form.class.php');
-Display::display_header('Create');
 if( !api_get_user_id())
 {
 	api_not_allowed();
@@ -19,12 +18,13 @@ if( isset($_REQUEST['type']))
 	if($form->validate())
 	{
 		$object = $form->create_learning_object(api_get_user_id());
-		var_dump($object);	
+		header('Location: index.php?show_message&message='.get_lang(urlencode('ObjectCreated')));
 	}
 	else
 	{
+		Display::display_header('Create');
 		$form->display();
+		Display::display_footer();
 	}
 }
-Display::display_footer();
 ?>
