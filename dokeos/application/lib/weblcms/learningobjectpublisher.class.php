69e8ae6534802c9f42d869a1c5382bec2b56d61b
ceetee <ceetee@localhost>
2006-03-10 08:22:52 +0000

    Adding support for multiple types.

--- dokeos/application/lib/weblcms/learningobjectpublisher.class.php
+++ dokeos/application/lib/weblcms/learningobjectpublisher.class.php
@@ -3,12 +3,12 @@ class LearningObjectPublisher
 {
 	private $owner;
 	
-	private $type;
+	private $types;
 		
-    function LearningObjectPublisher($owner, $type)
+    function LearningObjectPublisher($owner, $types)
     {
     	$this->owner = $owner;
-    	$this->type = $type;
+    	$this->types = (is_array($types) ? $types : array($types));
     }
     
     function display()
@@ -24,12 +24,22 @@ class LearningObjectPublisher
     	echo '<div class="tabbed-pane-content">';
     	require_once dirname(__FILE__) . '/publisher/learningobject'.$action.'.class.php';
     	$class = 'LearningObject' . ucfirst($action);
-    	$f = new $class($this->owner, $this->type);
+    	$f = new $class($this->get_owner(), $this->get_types());
     	$f->set_additional_parameter('tool', $_GET['tool']);
     	$f->set_additional_parameter('publish_action', $action);
     	$f->display();
     	echo '</div>';
     	echo '</div>';
+    }
+    
+    function get_owner()
+	{
+		return $this->owner;
+	}
+    
+    function get_types()
+    {
+    	return $this->types;
     }
 }
 ?>
---
+++ dokeos/application/lib/weblcms/publisher/learningobjectcreator.class.php
@@ -0,0 +1,59 @@
+<?php
+require_once dirname(__FILE__).'/../learningobjectpublishercomponent.class.php';
+require_once dirname(__FILE__).'/../../../../repository/lib/learningobject_form.class.php';
+
+class LearningObjectCreator extends LearningObjectPublisherComponent
+{
+	function get_type()
+	{
+		$types = $this->get_types();
+		return (count($types) == 1 ? $types[0] : $_REQUEST['type']);
+	}
+
+	function display()
+	{
+		$type = $this->get_type();
+		if ($type)
+		{
+			$this->display_creation_form($type);
+		}
+		else
+		{
+			$this->display_type_selector();
+		}
+	}
+
+	private function display_type_selector()
+	{
+		$types = array ();
+		foreach ($this->get_types() as $t)
+		{
+			$types[$t] = $t;
+		}
+		$form = new FormValidator('selecttype', 'get');
+		$form->addElement('hidden', 'tool');
+		$form->addElement('hidden', 'publish_action');
+		$form->addElement('select', 'type', '', $types);
+		$form->addElement('submit', 'submit', get_lang('OK'));
+		$form->setDefaults(array ('tool' => $_GET['tool'], 'publish_action' => $_GET['publish_action']));
+		$form->display();
+	}
+
+	private function display_creation_form($type)
+	{
+		$form = LearningObjectForm :: factory($type, 'create', 'post', $_ENV['SCRIPT_NAME'].'?tool='.$_GET['tool'].'&publish_action='.$_GET['publish_action']);
+		$form->build_create_form($type);
+		$form->addElement('hidden', 'type');
+		$form->setDefaults(array ('type' => $type));
+		if ($form->validate())
+		{
+			$object = $form->create_learning_object(api_get_user_id());
+			var_dump($object);
+		}
+		else
+		{
+			$form->display();
+		}
+	}
+}
+?>
