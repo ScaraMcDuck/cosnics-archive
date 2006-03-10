57ba200cfc088b9257ac60b8dffc8c48b1895cca
ceetee <ceetee@localhost>
2006-03-10 09:40:32 +0000

    Setting up framework for LO publication management.

--- dokeos/application/lib/weblcms/data_manager/database.class.php
+++ dokeos/application/lib/weblcms/data_manager/database.class.php
@@ -14,5 +14,35 @@ class DatabaseWebLCMSDataManager extends WebLCMSDataManager
 		$this->connection = $m->get_connection();
 		$this->prefix = $m->get_table_name_prefix();
 	}
+	
+	function retrieve_learning_object_publication($pid)
+	{
+		// TODO
+	}
+
+	function retrieve_learning_object_publications($learningObjects, $courses, $conditions = null, $orderBy = array (), $orderDesc = array (), $firstIndex = 0, $maxObjects = -1)
+	{
+		// TODO
+	}
+
+	function count_learning_object_publications($learningObjects, $courses, $conditions = null)
+	{
+		// TODO
+	}
+
+	function create_learning_object_publication($publication)
+	{
+		// TODO
+	}
+	
+	function update_learning_object_publication($publication)
+	{
+		// TODO
+	}
+	
+	function delete_learning_object_publication($publication)
+	{
+		// TODO
+	}
 }
 ?>
