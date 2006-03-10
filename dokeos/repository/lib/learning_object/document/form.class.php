8ef54a4a01bc4b02f4aa971fdd387ae1fc41d731
malchick <malchick@cosnics>
2006-03-10 09:12:14 +0000

    document upload added

--- dokeos/repository/lib/learning_object/document/form.class.php
+++ dokeos/repository/lib/learning_object/document/form.class.php
@@ -34,12 +34,17 @@ class DocumentForm extends LearningObjectForm
 	public function create_learning_object($owner)
 	{
 		$values = $this->exportValues();
+		$file = $_FILES['filename'];
+		$filename = strtolower(ereg_replace('[^0-9a-zA-Z\.]','',$file['name']));
+		$path = api_get_user_id().'/'.$filename;
+		$test = is_uploaded_file($_FILES['filename']['tmp_name']);
+		move_uploaded_file($_FILES['filename']['tmp_name'], Configuration::get_instance()->get_parameter('general', 'upload_path').$path);
+		$filename_t = Configuration::get_instance()->get_parameter('general', 'upload_path').$path;
 		$dataManager = RepositoryDataManager::get_instance();
 		$document = new Document();
 		$document->set_owner_id($owner);
 		$document->set_title($values['title']);
 		$document->set_description($values['description']);
-		$document->set_path($values['path']);
 		$document->set_filename($values['filename']);
 		$document->set_category_id($values['category']);
 		$document->create();
@@ -47,7 +52,7 @@ class DocumentForm extends LearningObjectForm
 	}
 	public function update_learning_object(& $object)
 	{
-		$values = $this->exportValues();
+		$values = $this->exportValues();	
 		$object->set_title($values['title']);
 		$object->set_description($values['description']);
 		$object->set_path($values['path']);
