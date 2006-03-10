56a79e3586d7e4b83541efe38289b273718e54ac
malchick <malchick@localhost>
2006-03-10 15:56:34 +0000

    new learning object HtmlDocument created

---
+++ dokeos/repository/lib/learning_object/html_document/form.class.php
@@ -0,0 +1,75 @@
+<?php
+require_once dirname(__FILE__) . '/../../learningobject_form.class.php';
+class HtmlDocumentForm extends LearningObjectForm
+{
+	public function HtmlDocumentForm($formName, $method = 'post', $action = null)
+	{
+		parent :: __construct($formName, $method, $action);
+	}
+	public function build_create_form()
+	{
+		parent :: build_create_form();
+		$this->addElement('file', 'filename', 'Filename');
+		//$this->addRule('filename',get_lang('DiskQuotaExceeded'),'disk_quota');
+		$allowed_document_types = array ('html', 'htm');
+		$this->addRule('filename', get_lang('OnlyHtmlFilesAllowed'), 'filetype', $allowed_document_types);
+		$this->add_submit_button();
+	}
+	public function build_edit_form($object)
+	{
+		parent :: build_edit_form($object);
+		$this->setDefaults();
+		$this->addElement('text', 'path', 'Path');
+		$this->addElement('text', 'filename', 'Filename');
+		$this->add_submit_button();
+	}
+	public function setDefaults($defaults = array ())
+	{
+		$lo = $this->get_learning_object();
+		if (isset ($lo))
+		{
+			$defaults['path'] = $lo->get_path();
+			$defaults['filename'] = $lo->get_filename();
+		}
+		parent :: setDefaults($defaults);
+	}
+	public function create_learning_object($owner)
+	{
+		$values = $this->exportValues();
+		$file = $_FILES['filename'];
+		$filename = strtolower(ereg_replace('[^0-9a-zA-Z\.]','',$file['name']));
+		$path = api_get_user_id().'/'.$filename;
+		$main_upload_dir = Configuration::get_instance()->get_parameter('general', 'upload_path');
+		$dataManager = RepositoryDataManager::get_instance();
+		$document = new Document();
+		$document->set_owner_id($owner);
+		$document->set_title($values['title']);
+		$document->set_description($values['description']);
+		$document->set_path($path);
+		$document->set_filename($filename);
+		$document->set_category_id($values['category']);
+		$document->create();
+		$i = 1;
+		while (file_exists($main_upload_dir.'/'.$path))
+		{
+			$file_base = substr($filename, 0, strlen($filename)-4);
+			$file_ext = substr($filename, strlen($filename)-4, strlen($filename));
+			$filename = $file_base.$i.$file_ext;
+			$path = api_get_user_id().'/'.$filename;
+			$i++;
+		}
+		move_uploaded_file($_FILES['filename']['tmp_name'], $main_upload_dir.'/'.$path);
+		return $document;
+	}
+	public function update_learning_object(& $object)
+	{
+		$values = $this->exportValues();	
+		$object->set_title($values['title']);
+		$object->set_description($values['description']);
+		$object->set_path($values['path']);
+		$object->set_filename($values['filename']);
+		$object->set_category_id($values['category']);
+		$object->update();
+	}
+}
+?>
