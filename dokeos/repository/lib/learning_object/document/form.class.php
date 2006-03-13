b1ce48b0bef878dc2905a997c364bcc885cc8673
malchick <malchick@cosnics>
2006-03-13 14:22:11 +0000

    added support for html-documents

--- dokeos/repository/lib/learning_object/document/form.class.php
+++ dokeos/repository/lib/learning_object/document/form.class.php
@@ -9,16 +9,16 @@ class DocumentForm extends LearningObjectForm
 	public function build_create_form()
 	{
 		parent :: build_create_form();
-		$this->addElement('file', 'filename', 'Filename');
-		$this->addRule('filename',get_lang('DiskQuotaExceeded'),'disk_quota');
+		$this->addElement('file', 'filename', get_lang('Filename'));
+		//$this->addRule('filename',get_lang('DiskQuotaExceeded'),'disk_quota');
 		$this->add_submit_button();
 	}
 	public function build_edit_form($object)
 	{
 		parent :: build_edit_form($object);
 		$this->setDefaults();
-		$this->addElement('text', 'path', 'Path');
-		$this->addElement('text', 'filename', 'Filename');
+		$this->addElement('text', 'path', get_lang('Path'));
+		$this->addElement('text', 'filename', get_lang('Filename'));
 		$this->add_submit_button();
 	}
 	public function setDefaults($defaults = array ())
@@ -38,25 +38,38 @@ class DocumentForm extends LearningObjectForm
 		$filename = strtolower(ereg_replace('[^0-9a-zA-Z\.]','',$file['name']));
 		$path = api_get_user_id().'/'.$filename;
 		$main_upload_dir = Configuration::get_instance()->get_parameter('general', 'upload_path');
+		$i = 1;
+		$file_base = $filename;
+		$file_ext = '';
+		$dot_pos = strrpos($filename, '.');
+		if ($dot_pos != FALSE)
+		{
+			$file_base = substr($filename, 0, $dot_pos);
+			$file_ext = substr($filename, $dot_pos);
+		}			
+		while (file_exists($main_upload_dir.'/'.$path))
+		{	
+			$filename = $file_base.$i.$file_ext;
+			$path = api_get_user_id().'/'.$filename;
+			$i++;
+		}
+		if(preg_match('/^\.x?html?$/', $file_ext) === 1)
+			$document = new HtmlDocument();
+		else
+		{
+			$document = new Document();
+			var_dump(preg_match('/^\.x?html?$/', $file_ext));
+		}
+		move_uploaded_file($_FILES['filename']['tmp_name'], $main_upload_dir.'/'.$path);
 		$dataManager = RepositoryDataManager::get_instance();
-		$document = new Document();
 		$document->set_owner_id($owner);
 		$document->set_title($values['title']);
 		$document->set_description($values['description']);
 		$document->set_path($path);
 		$document->set_filename($filename);
+		$document->set_filesize(filesize($main_upload_dir.'/'.$path));
 		$document->set_category_id($values['category']);
 		$document->create();
-		$i = 1;
-		while (file_exists($main_upload_dir.'/'.$path))
-		{
-			$file_base = substr($filename, 0, strlen($filename)-4);
-			$file_ext = substr($filename, strlen($filename)-4, strlen($filename));
-			$filename = $file_base.$i.$file_ext;
-			$path = api_get_user_id().'/'.$filename;
-			$i++;
-		}
-		move_uploaded_file($_FILES['filename']['tmp_name'], $main_upload_dir.'/'.$path);
 		return $document;
 	}
 	public function update_learning_object(& $object)
