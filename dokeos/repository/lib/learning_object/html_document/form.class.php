e74048c10a7304975c5010b8988abbad4c4a29a1
malchick <malchick@cosnics>
2006-03-13 14:23:46 +0000

    updated create_learning_object function

--- dokeos/repository/lib/learning_object/html_document/form.class.php
+++ dokeos/repository/lib/learning_object/html_document/form.class.php
@@ -9,56 +9,65 @@ class HtmlDocumentForm extends LearningObjectForm
 	public function build_create_form()
 	{
 		parent :: build_create_form();
-		$this->addElement('file', 'filename', 'Filename');
-		//$this->addRule('filename',get_lang('DiskQuotaExceeded'),'disk_quota');
-		$allowed_document_types = array ('html', 'htm');
-		$this->addRule('filename', get_lang('OnlyHtmlFilesAllowed'), 'filetype', $allowed_document_types);
+		$this->addElement('text', 'filename', get_lang('Filename'));
+		$this->addElement('html_editor', 'htmldoc', get_lang('HtmlDocument'));
+		$this->applyFilter('htmldoc', 'html_filter');
 		$this->add_submit_button();
 	}
 	public function build_edit_form($object)
 	{
 		parent :: build_edit_form($object);
 		$this->setDefaults();
-		$this->addElement('text', 'path', 'Path');
-		$this->addElement('text', 'filename', 'Filename');
+		$this->addElement('text', 'path', get_lang('Path'));
+		$this->addElement('text', 'filename', get_lang('Filename'));
+		$this->addElement('html_editor', 'htmldoc', get_lang('HtmlDocument'));
+		$this->applyFilter('htmldoc', 'html_filter');
 		$this->add_submit_button();
 	}
 	public function setDefaults($defaults = array ())
 	{
 		$lo = $this->get_learning_object();
+		$main_upload_dir = Configuration::get_instance()->get_parameter('general', 'upload_path');
 		if (isset ($lo))
 		{
 			$defaults['path'] = $lo->get_path();
 			$defaults['filename'] = $lo->get_filename();
+			$defaults['htmldoc'] = file_get_contents($main_upload_dir.'/'.$lo->get_path());
 		}
 		parent :: setDefaults($defaults);
 	}
 	public function create_learning_object($owner)
 	{
+		
 		$values = $this->exportValues();
-		$file = $_FILES['filename'];
-		$filename = strtolower(ereg_replace('[^0-9a-zA-Z\.]','',$file['name']));
+		$file = $values['filename'];
+		$file_base = strtolower(ereg_replace('[^0-9a-zA-Z\.]','',$file));
+		$filename = $file_base.'.html';
 		$path = api_get_user_id().'/'.$filename;
 		$main_upload_dir = Configuration::get_instance()->get_parameter('general', 'upload_path');
+		$i = 1;
+		while (file_exists($main_upload_dir.'/'.$path))
+		{
+			$file_base = substr($filename, 0, strlen($filename)-5);
+			$file_ext = substr($filename, strlen($filename)-5, strlen($filename));
+			$filename = $file_base.$i.$file_ext;
+			$path = api_get_user_id().'/'.$filename;
+			$i++;
+		}
+		$create_file = fopen($main_upload_dir.'/'.$path, 'w') or die("can't open file");
+		fwrite ($create_file, $values['htmldoc']);
+		fclose($create_file);
+		$filesize = filesize($main_upload_dir.'/'.$path);
 		$dataManager = RepositoryDataManager::get_instance();
-		$document = new Document();
+		$document = new HtmlDocument();
 		$document->set_owner_id($owner);
 		$document->set_title($values['title']);
 		$document->set_description($values['description']);
 		$document->set_path($path);
 		$document->set_filename($filename);
+		$document->set_filesize($filesize);
 		$document->set_category_id($values['category']);
 		$document->create();
-		$i = 1;
-		while (file_exists($main_upload_dir.'/'.$path))
-		{
-			$file_base = substr($filename, 0, strlen($filename)-4);
-			$file_ext = substr($filename, strlen($filename)-4, strlen($filename));
-			$filename = $file_base.$i.$file_ext;
-			$path = api_get_user_id().'/'.$filename;
-			$i++;
-		}
-		move_uploaded_file($_FILES['filename']['tmp_name'], $main_upload_dir.'/'.$path);
 		return $document;
 	}
 	public function update_learning_object(& $object)
