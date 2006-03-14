b32a3cf34cbbf8af20228a10a7ff0922ac361839
malchick <malchick@cosnics>
2006-03-14 14:50:14 +0000

    updated

--- dokeos/repository/lib/learning_object/calendar_event/calendar_event.class.php
+++ dokeos/repository/lib/learning_object/calendar_event/calendar_event.class.php
@@ -1,6 +1,23 @@
 <?php
 require_once dirname(__FILE__) . '/../../learningobject.class.php';
 
-class CalendarEvent extends LearningObject {
+class CalendarEvent extends LearningObject
+{
+	function get_start_date ()
+	{
+		return $this->get_additional_property('start_date');
+	}
+	function set_start_date ($start_date) 
+	{
+		return $this->set_additional_property('start_date', $start_date);
+	}
+	function get_end_date ()
+	{
+		return $this->get_additional_property('end_date');
+	}
+	function set_end_date ($end_date) 
+	{
+		return $this->set_additional_property('end_date', $end_date);
+	}
 }
 ?>
