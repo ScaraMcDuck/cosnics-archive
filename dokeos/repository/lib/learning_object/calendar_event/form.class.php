2b7c48099c209163c564cf4ea56911c26d4f9c70
malchick <malchick@cosnics>
2006-03-14 14:54:52 +0000

    functions updated

--- dokeos/repository/lib/learning_object/calendar_event/form.class.php
+++ dokeos/repository/lib/learning_object/calendar_event/form.class.php
@@ -20,8 +20,23 @@ class CalendarEventForm extends LearningObjectForm
     public function build_edit_form($object)
     {
 		parent :: build_edit_form($object);
+		$this->addElement('datepicker', 'start', get_lang('StartTimeWindow'), array ('form_name' => $this->getAttribute('id')));
+		$this->addRule('start', 'Invalid date', 'date');
+		$this->addElement('datepicker', 'end', get_lang('EndTimeWindow'), array ('form_name' => $this->getAttribute('id')));
+		$this->addRule('end', 'Invalid date', 'date');
+		$this->addRule(array ('start', 'end'), get_lang('StartDateShouldBeBeforeEndDate'), 'date_compare', 'lte');
 		$this->setDefaults();
 		$this->add_submit_button();
+	}
+	public function setDefaults($defaults = array ())
+	{
+		$lo = $this->get_learning_object();
+		if (isset ($lo))
+		{
+			$defaults['start'] = $lo->get_start_date();
+			$defaults['end'] = $lo->get_end_date();
+		}
+		parent :: setDefaults($defaults);
 	}
 	public function create_learning_object($owner)
 	{
@@ -32,6 +47,8 @@ class CalendarEventForm extends LearningObjectForm
 		$calendarEvent->set_title($values['title']);
 		$calendarEvent->set_description($values['description']);
 		$calendarEvent->set_category_id($values['category']);
+		$calendarEvent->set_start_date($values['start']);
+		$calendarEvent->set_end_date($values['end']);
 		$calendarEvent->create();
 		return $calendarEvent;
 	}
@@ -41,6 +58,8 @@ class CalendarEventForm extends LearningObjectForm
 		$object->set_title($values['title']);
 		$object->set_description($values['description']);
 		$object->set_category_id($values['category']);
+		$object->set_start_date($values['start']);
+		$object->set_end_date($values['end']);
 		$object->update();
 	}
 }
