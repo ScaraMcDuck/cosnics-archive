3a8bd96087aef37c9d0bbdff38d372e1fd9eb80f
ceetee <ceetee@cosnics>
2006-03-14 12:40:22 +0000

    Making link tool's user mode and publisher mode easier to distinguish for testing.

--- dokeos/application/lib/weblcms/tool/link/linktool.class.php
+++ dokeos/application/lib/weblcms/tool/link/linktool.class.php
@@ -5,14 +5,24 @@ class LinkTool extends RepositoryTool
 {
 	function run()
 	{
-		// Temporary testing code.
-		require_once dirname(__FILE__).'/linkbrowser.class.php';
-		$browser = new LinkBrowser();
-		$browser->display();
-
-		require_once dirname(__FILE__).'/../../learningobjectpublisher.class.php';
-		$pub = new LearningObjectPublisher('link', api_get_course_id(), api_get_user_id());
-		$pub->display();
+		if (isset($_GET['linkadmin']))
+		{
+			$_SESSION['linkadmin'] = $_GET['linkadmin'];
+		}
+		if ($_SESSION['linkadmin'])
+		{
+			echo '<p>Go to <a href="?linkadmin=0&amp;tool=link">User Mode</a> &hellip;</p>';
+			require_once dirname(__FILE__).'/../../learningobjectpublisher.class.php';
+			$pub = new LearningObjectPublisher('link', api_get_course_id(), api_get_user_id());
+			$pub->display();
+		}
+		else
+		{
+			echo '<p>Go to <a href="?linkadmin=1&amp;tool=link">Publisher Mode</a> &hellip;</p>';
+			require_once dirname(__FILE__).'/linkbrowser.class.php';
+			$browser = new LinkBrowser();
+			$browser->display();
+		}
 	}
 }
 ?>
