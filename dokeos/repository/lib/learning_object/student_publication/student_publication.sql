5db980885e4715be7ff2eee65aaf56cfb43c1a6e
bmol <bmol@cosnics>
2006-03-15 13:10:18 +0000

    removing object type student_publication

--- dokeos/repository/lib/learning_object/student_publication/student_publication.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `%prefix%student_publication` (
-  `id` int(10) unsigned NOT NULL default '0',
-  `url` text NOT NULL,
-  `author` int(10) unsigned NOT NULL default '0',
-  `active` tinyint(1) unsigned NOT NULL default '0',
-  `accepted` tinyint(1) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-);
