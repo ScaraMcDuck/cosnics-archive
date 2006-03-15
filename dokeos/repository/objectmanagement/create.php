49d117e12b50e4623cd3d8f3dc8fb4f9568aee30
bmol <bmol@localhost>
2006-03-15 13:49:39 +0000

    added breadcrumbs

--- dokeos/repository/objectmanagement/create.php
+++ dokeos/repository/objectmanagement/create.php
@@ -11,18 +11,20 @@ if( !api_get_user_id())
 if( isset($_REQUEST['type']))
 {
 	$type = $_REQUEST['type'];
-	$current_category = $_REQUEST['category'];
+	$current_category_id = $_REQUEST['category'];
 	$form = LearningObjectForm::factory($type,'create','post','create.php?type='.$type);
-	$form->set_default_category($current_category);
+	$form->set_default_category($current_category_id);
 	$form->build_create_form();
 	if($form->validate())
 	{
 		$object = $form->create_learning_object(api_get_user_id());
-		header('Location: index.php?category='.$current_category.'&action=show_message&message='.urlencode(get_lang('ObjectCreated')));
+		header('Location: index.php?category='.$current_category_id.'&action=show_message&message='.urlencode(get_lang('ObjectCreated')));
 	}
 	else
 	{
-		Display::display_header('Create');
+		$menu = new CategoryMenu(api_get_user_id(),$current_category_id,'index.php?category=%s');
+		$interbredcrump = $menu->get_breadcrumbs();
+		Display::display_header(get_lang('Create'));
 		$form->display();
 		Display::display_footer();
 	}
