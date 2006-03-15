86d8a6e2d3082b1836a697a4ae24a70bd04fe421
malchick <malchick@localhost>
2006-03-15 18:53:59 +0000

    extracted method, created new function

--- dokeos/repository/lib/learning_object/document/form.class.php
+++ dokeos/repository/lib/learning_object/document/form.class.php
@@ -32,7 +32,47 @@ class DocumentForm extends LearningObjectForm
 	}
 	public function create_learning_object($owner)
 	{
+		global $path;
+		global $filename;
+		global $main_upload_dir;
+		$values = $this->exportValues();			
+		$this->upload_document();
+		if(preg_match('/\.x?html?$/', $filename) === 1)
+			$document = new HtmlDocument();
+		else
+			$document = new Document();			
+		$dataManager = RepositoryDataManager::get_instance();
+		$document->set_owner_id($owner);
+		$document->set_title($values['title']);
+		$document->set_description($values['description']);
+		$document->set_path($path);
+		$document->set_filename($filename);
+		$document->set_filesize(filesize($main_upload_dir.'/'.$path));
+		$document->set_category_id($values['category']);
+		$document->create();
+		return $document;
+	}
+	public function update_learning_object(& $object)
+	{
+		global $path;
+		global $filename;
+		global $main_upload_dir;
 		$values = $this->exportValues();
+		unlink(Configuration::get_instance()->get_parameter('general', 'upload_path').'/'.$values['path']);
+		$this->upload_document();		
+		$object->set_title($values['title']);
+		$object->set_description($values['description']);
+		$object->set_path($path);
+		$object->set_filename($filename);
+		$object->set_filesize(filesize($main_upload_dir.'/'.$path));
+		$object->set_category_id($values['category']);
+		$object->update();
+	}
+	public function upload_document()
+	{
+		global $path;
+		global $filename;
+		global $main_upload_dir;
 		$file = $_FILES['filename'];
 		$filename = strtolower(ereg_replace('[^0-9a-zA-Z\.]','',$file['name']));
 		$path = api_get_user_id().'/'.$filename;
@@ -52,53 +92,7 @@ class DocumentForm extends LearningObjectForm
 			$path = api_get_user_id().'/'.$filename;
 			$i++;
 		}
-		if(preg_match('/^\.x?html?$/', $file_ext) === 1)
-			$document = new HtmlDocument();
-		else
-			$document = new Document();
 		move_uploaded_file($_FILES['filename']['tmp_name'], $main_upload_dir.'/'.$path);
-		$dataManager = RepositoryDataManager::get_instance();
-		$document->set_owner_id($owner);
-		$document->set_title($values['title']);
-		$document->set_description($values['description']);
-		$document->set_path($path);
-		$document->set_filename($filename);
-		$document->set_filesize(filesize($main_upload_dir.'/'.$path));
-		$document->set_category_id($values['category']);
-		$document->create();
-		return $document;
-	}
-	public function update_learning_object(& $object)
-	{
-		$values = $this->exportValues();
-		$file = $_FILES['filename'];
-		$filename = strtolower(ereg_replace('[^0-9a-zA-Z\.]','',$file['name']));
-		$path = api_get_user_id().'/'.$filename;
-		$main_upload_dir = Configuration::get_instance()->get_parameter('general', 'upload_path');
-		unlink($main_upload_dir.'/'.$values['path']);
-		$i = 1;
-		$file_base = $filename;
-		$file_ext = '';
-		$dot_pos = strrpos($filename, '.');
-		if ($dot_pos != FALSE)
-		{
-			$file_base = substr($filename, 0, $dot_pos);
-			$file_ext = substr($filename, $dot_pos);
-		}				
-		while (file_exists($main_upload_dir.'/'.$path))
-		{	
-			$filename = $file_base.$i.$file_ext;
-			$path = api_get_user_id().'/'.$filename;
-			$i++;
-		}
-		move_uploaded_file($_FILES['filename']['tmp_name'], $main_upload_dir.'/'.$path);
-		$object->set_title($values['title']);
-		$object->set_description($values['description']);
-		$object->set_path($path);
-		$object->set_filename($filename);
-		$object->set_filesize(filesize($main_upload_dir.'/'.$path));
-		$object->set_category_id($values['category']);
-		$object->update();
 	}
 }
 ?>
