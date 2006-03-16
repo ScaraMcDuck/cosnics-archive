9866a264988f09a2a1a967627277176398087b5b
bmol <bmol@cosnics>
2006-03-16 12:48:10 +0000

    adding page to edit metadata of learning objects

---
+++ dokeos/repository/objectmanagement/metadata.php
@@ -0,0 +1,28 @@
+<?php
+require_once('../../claroline/inc/claro_init_global.inc.php');
+require_once(api_get_library_path().'/formvalidator/FormValidator.class.php');
+require_once('../lib/repositorydatamanager.class.php');
+require_once('../lib/learningobject_form.class.php');
+require_once('../lib/learningobject_display.class.php');
+if( !api_get_user_id())
+{
+	api_not_allowed();
+}
+if( isset($_GET['id']))
+{
+	$datamanager = RepositoryDataManager::get_instance();
+	$object = $datamanager->retrieve_learning_object($_GET['id']);
+	$display = LearningObjectDisplay::factory($object);
+	// Create a navigation menu to browse through the categories
+	$current_category_id = $object->get_category_id();
+	$menu = new CategoryMenu(api_get_user_id(),$current_category_id,'index.php?category=%s');
+	$interbredcrump = $menu->get_breadcrumbs();
+	$tool_name = get_lang('Metadata').': '.$object->get_title();
+	Display::display_header($tool_name);
+	api_display_tool_title($tool_name);
+	echo $display->get_full_html();
+	//TODO: connect current metadata implementation in Dokeos to this repository-system
+	echo '<p><b>TODO: Here you can edit the metadata of the selected object...</b></p>';
+	Display::display_footer();
+}
+?>
