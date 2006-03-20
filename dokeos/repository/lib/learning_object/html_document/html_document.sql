e9c23683ecebfe81ef4dfa99d191ad7c6a0c74b6
malchick <malchick@cosnics>
2006-03-20 12:01:06 +0000

    all types of documents are now handled by the learning-object document

--- dokeos/repository/lib/learning_object/html_document/html_document.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `%prefix%html_document` (
-  `id` int(11) unsigned NOT NULL default '0',
-  `path` text NOT NULL,
-  `filename` text NOT NULL,
-  `filesize` int(11) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-);
