41c84c3a4ac6223b875fbd2b7d47091c89f675a1
ceetee <ceetee@localhost>
2006-03-20 14:57:20 +0000

    Using int(10) unsigned instead of int(11).

--- dokeos/repository/lib/learning_object/calendar_event/calendar_event.sql
+++ dokeos/repository/lib/learning_object/calendar_event/calendar_event.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `%prefix%calendar_event` (
-  `id` int(11) unsigned NOT NULL default '0',
-  `start_date` unsigned NOT NULL default '0',
-  `end_date` unsigned NOT NULL default '0',
+  `id` int(10) unsigned NOT NULL default '0',
+  `start_date` int(10) unsigned NOT NULL default '0',
+  `end_date` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`)
 );
