d5e229d8cca3c5d42f9be90d44f82bcae66260dd
ceetee <ceetee@cosnics>
2006-03-20 12:21:16 +0000

    Adding icon for LO publication (using category icon for now).

--- dokeos/application/lib/weblcms/publisher/learningobjectviewer.class.php
+++ dokeos/application/lib/weblcms/publisher/learningobjectviewer.class.php
@@ -7,11 +7,11 @@ class LearningObjectViewer extends LearningObjectPublisherComponent
 {
 	function as_html()
 	{
-		if ($_GET['object']) {
-			$object = RepositoryDataManager::get_instance()->retrieve_learning_object($_GET['object']);
-			return LearningObjectDisplay::factory($object)->get_full_html()
-				. '<p>[ USE ]</p>';
-		}		
+		if ($_GET['object'])
+		{
+			$object = RepositoryDataManager :: get_instance()->retrieve_learning_object($_GET['object']);
+			return LearningObjectDisplay :: factory($object)->get_full_html().'<p><a href="'.$this->get_url(array ('publish_action' => 'publicationcreator', 'object' => $object->get_id())).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/publish.gif" alt="'.get_lang('Publish').'" style="vertical-align: middle"/> '.get_lang('PublishThisObject').'</a></p>';
+		}
 	}
 }
 ?>
