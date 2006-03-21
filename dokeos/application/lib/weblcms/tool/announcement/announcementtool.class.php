a2eb3804793d2a4b5c7d31efe6f5b9e3e774b089
bmol <bmol@cosnics>
2006-03-21 10:44:30 +0000

    further improvements of announcementTool

--- dokeos/application/lib/weblcms/tool/announcement/announcementtool.class.php
+++ dokeos/application/lib/weblcms/tool/announcement/announcementtool.class.php
@@ -1,8 +1,13 @@
 <?php
 require_once dirname(__FILE__).'/../repositorytool.class.php';
-
+/**
+ * This tool allows a user to publish announcements in his or her course.
+ */
 class AnnouncementTool extends RepositoryTool
 {
+	/**
+	 * inherited
+	 */
 	function run()
 	{
 		if (isset($_GET['announcementadmin']))
@@ -19,8 +24,38 @@ class AnnouncementTool extends RepositoryTool
 		else
 		{
 			echo '<p>Go to <a href="' . $this->get_url(array('announcementadmin' => 1)) . '">Publisher Mode</a> &hellip;</p>';
-			//TODO: code to display published announcements
+			$this->display();
 		}
+	}
+	/**
+	 * Display the list of announcements
+	 */
+	function display()
+	{
+		$announcement_publications = $this->get_announcement_publications();
+		foreach($announcement_publications as $index => $announcement_publication)
+		{
+			$announcement = $announcement_publication->get_learning_object();
+			$html = array();
+			$html[] = '<div class="learning_object">';
+			$html[] = '<div class="icon"><img src="'.api_get_path(WEB_CODE_PATH).'img/'.$announcement->get_type().'.gif" alt="'.$announcement->get_type().'"/></div>';
+			$html[] = '<div class="title">'.$announcement->get_title().'</div>';
+			$html[] = '<div class="description">'.$announcement->get_description();
+			$html[] = '</div>';
+			$html[] = '</div>';
+			$html[] = '<br /><br />';
+			echo implode("\n",$html);
+		}
+	}
+	/**
+	 * Get the list of published announcements
+	 * @return array An array with all publications of announcements
+	 */
+	function get_announcement_publications()
+	{
+		$datamanager = WebLCMSDataManager :: get_instance();
+		$announcement_publications = $datamanager->retrieve_learning_object_publications(api_get_course_id(), null, api_get_user_id(), $this->get_groups());
+		return $announcement_publications;
 	}
 }
 ?>
