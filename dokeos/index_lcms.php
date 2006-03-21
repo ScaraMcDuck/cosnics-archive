79decd139eb8013a198211e1b29da3d8d06fd691
ceetee <ceetee@localhost>
2006-03-21 09:05:15 +0000

    Adding a TODO.

--- dokeos/index_lcms.php
+++ dokeos/index_lcms.php
@@ -5,6 +5,7 @@ require_once dirname(__FILE__).'/application/lib/weblcms/weblcms.class.php';
 // TODO: Move this somewhere where it makes sense.
 api_protect_course_script();
 
+// TODO: Move this inside the application, so breadcrumbs can be set etc.
 Display::display_header(api_get_setting('siteName'));
 
 $app = new WebLCMS();
