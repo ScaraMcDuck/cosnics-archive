353f36288cc7073dfc1cf8d28015f8b2cfc350c6
bmol <bmol@cosnics>
2006-03-24 14:41:37 +0000

    some minor changes

--- dokeos/application/lib/weblcms/data_manager/database.class.php
+++ dokeos/application/lib/weblcms/data_manager/database.class.php
@@ -152,7 +152,8 @@ class DatabaseWebLCMSDataManager extends WebLCMSDataManager
 	function update_learning_object_publication($publication)
 	{
 		$where = $this->escape_column_name('id').'='.$publication->get_id();
-		$props = array ();
+		$props = array();
+		//TODO add other properties
 		$props['hidden'] = $publication->is_hidden();
 		return $this->connection->autoExecute($this->get_table_name('learning_object_publication'), $props, DB_AUTOQUERY_UPDATE, $where);
 	}
@@ -169,12 +170,12 @@ class DatabaseWebLCMSDataManager extends WebLCMSDataManager
 	{
 		if (!is_array($types))
 		{
-			$types = array ($types);
+			$types = array($types);
 		}
-		$query = 'SELECT * FROM '.$this->escape_table_name('learning_object_publication_category').' WHERE '.$this->escape_column_name('course').'=? AND '.$this->escape_column_name('tool').' IN ('.str_repeat('?,', count($types) - 1).'?)';
+		$query = 'SELECT * FROM '.$this->escape_table_name('learning_object_publication_category').' WHERE '.$this->escape_column_name('course').'=? AND '.$this->escape_column_name('tool').' IN ('.str_repeat('?,',count($types)-1).'?)';
 		$sth = $this->connection->prepare($query);
 		$params = $types;
-		array_unshift($params, $course);
+		array_unshift ($params, $course);
 		$res = & $this->connection->execute($sth, $params);
 		$cats = array ();
 		while ($record = $res->fetchRow(DB_FETCHMODE_ASSOC))
@@ -193,7 +194,7 @@ class DatabaseWebLCMSDataManager extends WebLCMSDataManager
 		if ($places < 0)
 		{
 			$places = abs($places);
-			for ($i = 0; $i < $places; $i ++)
+			for ($i = 0; $i < $places; $i++)
 			{
 				if (!$this->move_learning_object_publication_up($publication))
 				{
@@ -203,7 +204,7 @@ class DatabaseWebLCMSDataManager extends WebLCMSDataManager
 		}
 		else
 		{
-			for ($i = 0; $i < $places; $i ++)
+			for ($i = 0; $i < $places; $i++)
 			{
 				if (!$this->move_learning_object_publication_down($publication))
 				{
@@ -216,44 +217,54 @@ class DatabaseWebLCMSDataManager extends WebLCMSDataManager
 
 	private function move_learning_object_publication_up($publication)
 	{
-		$sql = 'SELECT pub1.'.$this->escape_column_name('id').' AS id1, pub2.'.$this->escape_column_name('id').' AS id2, pub1.'.$this->escape_column_name('display_order').' AS ord1, pub2.'.$this->escape_column_name('display_order').' AS ord2 FROM '.$this->escape_table_name('learning_object_publication').' AS pub1,'.$this->escape_table_name('learning_object_publication').' AS pub2 WHERE pub1.'.$this->escape_column_name('course').' = pub2.'.$this->escape_column_name('course').' AND pub1.'.$this->escape_column_name('tool').' = pub2.'.$this->escape_column_name('tool').' AND pub1.'.$this->escape_column_name('category').' = pub2.'.$this->escape_column_name('category').' AND pub1.'.$this->escape_column_name('id').' = ? AND pub1.'.$this->escape_column_name('id').' <> pub2.'.$this->escape_column_name('id').' AND pub2.'.$this->escape_column_name('display_order').' < pub1.'.$this->escape_column_name('display_order').' ORDER BY pub2.'.$this->escape_column_name('display_order').' DESC';
-		$result = & $this->connection->limitQuery($sql, 0, 1, array ($publication->get_id()));
+		// TODO: Escape table and column names, use limitQuery.
+		$sql = 'SELECT co1.id AS id1, co2.id AS id2, co1.display_order AS display_order1, co2.display_order AS display_order2 FROM '.$this->escape_table_name('learning_object_publication').' co1, '.$this->escape_table_name('learning_object_publication').' co2 WHERE co1.course = co2.course AND co1.tool = co2.tool AND co1.category = co2.category AND co1.id = ? AND co1.id <> co2.id AND co2.display_order < co1.display_order ORDER BY co2.display_order DESC LIMIT 1';
+		$statement = $this->connection->prepare($sql);
+		$result =& $this->connection->execute($statement, array ($publication->get_id()));
 		if ($result->numRows() == 0)
 		{
 			return false;
 		}
-		$obj = $result->fetchRow(DB_FETCHMODE_ASSOC);
+		$obj = $result->fetchRow(DB_FETCHMODE_OBJECT);
 		$sql = 'UPDATE '.$this->escape_table_name('learning_object_publication').' SET display_order = ? WHERE id = ?';
-		$this->connection->limitQuery($sql, 0, 1, array ($obj['ord1'], $obj['id2']));
-		$this->connection->limitQuery($sql, 0, 1, array ($obj['ord2'], $obj['id1']));
+		$statement = $this->connection->prepare($sql);
+		$this->connection->execute($statement, array ($obj->display_order1, $obj->id2));
+		$this->connection->execute($statement, array ($obj->display_order2, $obj->id1));
 		return true;
 	}
 
 	private function move_learning_object_publication_down($publication)
 	{
-		$sql = 'SELECT pub1.'.$this->escape_column_name('id').' AS id1, pub2.'.$this->escape_column_name('id').' AS id2, pub1.'.$this->escape_column_name('display_order').' AS ord1, pub2.'.$this->escape_column_name('display_order').' AS ord2 FROM '.$this->escape_table_name('learning_object_publication').' AS pub1,'.$this->escape_table_name('learning_object_publication').' AS pub2 WHERE pub1.'.$this->escape_column_name('course').' = pub2.'.$this->escape_column_name('course').' AND pub1.'.$this->escape_column_name('tool').' = pub2.'.$this->escape_column_name('tool').' AND pub1.'.$this->escape_column_name('category').' = pub2.'.$this->escape_column_name('category').' AND pub1.'.$this->escape_column_name('id').' = ? AND pub1.'.$this->escape_column_name('id').' <> pub2.'.$this->escape_column_name('id').' AND pub2.'.$this->escape_column_name('display_order').' > pub1.'.$this->escape_column_name('display_order').' ORDER BY pub2.'.$this->escape_column_name('display_order').' ASC';
-		$result = & $this->connection->limitQuery($sql, 0, 1, array ($publication->get_id()));
+		// TODO: Escape table and column names, use limitQuery.
+		$sql = 'SELECT co1.id AS id1, co2.id AS id2, co1.display_order AS display_order1, co2.display_order AS display_order2 FROM '.$this->escape_table_name('learning_object_publication').' co1, '.$this->escape_table_name('learning_object_publication').' co2 WHERE co1.course = co2.course AND co1.tool = co2.tool AND co1.category = co2.category AND co1.id = ? AND co1.id <> co2.id AND co2.display_order > co1.display_order ORDER BY co2.display_order ASC LIMIT 1';
+		$statement = $this->connection->prepare($sql);
+		$result =& $this->connection->execute($statement, array ($publication->get_id()));
 		if ($result->numRows() == 0)
 		{
 			return false;
 		}
-		$obj = $result->fetchRow(DB_FETCHMODE_ASSOC);
+		$obj = $result->fetchRow(DB_FETCHMODE_OBJECT);
 		$sql = 'UPDATE '.$this->escape_table_name('learning_object_publication').' SET display_order = ? WHERE id = ?';
-		$this->connection->limitQuery($sql, 0, 1, array ($obj['ord1'], $obj['id2']));
-		$this->connection->limitQuery($sql, 0, 1, array ($obj['ord2'], $obj['id1']));
+		$statement = $this->connection->prepare($sql);
+		$this->connection->execute($statement, array ($obj->display_order1, $obj->id2));
+		$this->connection->execute($statement, array ($obj->display_order2, $obj->id1));
 		return true;
 	}
 
-	function get_next_learning_object_publication_display_order_index($course, $tool, $category)
+	function get_next_learning_object_publication_display_order_index($course,$tool,$category)
 	{
-		$query = 'SELECT MAX('.$this->escape_column_name('display_order').') AS h FROM '.$this->escape_table_name('learning_object_publication').' WHERE '.$this->escape_column_name('course').'=? AND '.$this->escape_column_name('tool').'=?  AND '.$this->escape_column_name('category').'=?';
+		// TODO: Escape table and column names, limitQuery.
+		$query = 'SELECT MAX(display_order)+1 AS new_display_order FROM '.$this->escape_table_name('learning_object_publication').' WHERE '.$this->escape_column_name('course').'=? AND '.$this->escape_column_name('tool').'=?  AND '.$this->escape_column_name('category').'=?';
 		$statement = $this->connection->prepare($query);
-		$res = & $this->connection->execute($statement, array ($course, $tool, $category));
+		$params['course'] = $course;
+		$params['tool'] = $tool;
+		$params['category'] = $category;
+		$res = & $this->connection->execute($statement, $params);
 		$record = $res->fetchRow(DB_FETCHMODE_ASSOC);
-		$highest_index = $record['h'];
-		if (!is_null($highest_index))
+		$new_display_order = $record['new_display_order'];
+		if(!is_null($new_display_order))
 		{
-			return $highest_index +1;
+			return $new_display_order;
 		}
 		return 1;
 	}
@@ -280,7 +291,7 @@ class DatabaseWebLCMSDataManager extends WebLCMSDataManager
 	private function record_to_publication($record)
 	{
 		$obj = $this->repoDM->retrieve_learning_object($record['learning_object']);
-		return new LearningObjectPublication($record['id'], $obj, $record['course'], $record['category'], $record['target_users'], $record['target_groups'], self :: from_db_date($record['from_date']), self :: from_db_date($record['to_date']), $record['hidden'] != 0, $record['display_order']);
+		return new LearningObjectPublication($record['id'], $obj, $record['course'], $record['tool'], $record['category'], $record['target_users'], $record['target_groups'], self :: from_db_date($record['from_date']), self :: from_db_date($record['to_date']), $record['hidden'] != 0, $record['display_order']);
 	}
 
 	private function translate_condition($condition, & $params)
