6fd6935f44399a6b44567720c3b6f50a7cc624d3
malchick <malchick@cosnics>
2006-03-29 13:01:17 +0000

    updated properties for the LearningObjectTree

--- dokeos/repository/lib/learning_object/forum_topic/display.class.php
+++ dokeos/repository/lib/learning_object/forum_topic/display.class.php
@@ -1,4 +1,6 @@
 <?php
+require_once dirname(__FILE__).'/../../treemenurenderer.class.php';
+require_once dirname(__FILE__).'/../../learningobjecttree.class.php';
 class ForumTopicDisplay extends LearningObjectDisplay
 {
 	public function ForumTopicDisplay(&$object)
@@ -9,9 +11,15 @@ class ForumTopicDisplay extends LearningObjectDisplay
 	{
 		$object = $this->get_learning_object();
 		$html[] = '<div class="learning_object">';
-		$html[] = '<div class="icon"><img src="'.api_get_path(WEB_CODE_PATH).'img/'.$object->get_type().'.gif" alt="'.$object->get_type().'"/></div>';
+		$html[] = '<div style="float:right;width:80%;">';
+		$html[] = '<div class="icon"><img src="'.api_get_path(WEB_CODE_PATH).'img/'.$object->get_type().'.gif" alt="'.$object->get_type().'"/></div>';		
 		$html[] = '<div class="title">'.$object->get_title().'</div>';
-		$html[] = '<div class="description">'.$object->get_description().'</div>';
+		$html[] = '<div class="description">'.$object->get_description().'</div></div>';
+		$type_array = array('forum', 'forum_topic', 'forum_post');
+		$menu = new LearningObjectTree($object->get_parent_id(),$type_array);
+		$renderer =& new TreeMenuRenderer();
+		$menu->render($renderer,'sitemap');
+		$html[] = '<div>'.$renderer->toHtml().'</div>';
 		$html[] = '</div>';
 		return implode("\n",$html);
 	}
