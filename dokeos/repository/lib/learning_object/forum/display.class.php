7864c58fbe70b30d3b29e860b9ef42fda25ef0e0
malchick <malchick@cosnics>
2006-03-29 12:02:55 +0000

    added type_array

--- dokeos/repository/lib/learning_object/forum/display.class.php
+++ dokeos/repository/lib/learning_object/forum/display.class.php
@@ -11,18 +11,17 @@ class ForumDisplay extends LearningObjectDisplay
 	{
 		$object = $this->get_learning_object();
 		$html[] = '<div class="learning_object">';
-		$html[] = '<div class="icon"><img src="'.api_get_path(WEB_CODE_PATH).'img/'.$object->get_type().'.gif" alt="'.$object->get_type().'"/></div>';
+		$html[] = '<div style="float:right;width:80%;">';
+		$html[] = '<div class="icon"><img src="'.api_get_path(WEB_CODE_PATH).'img/'.$object->get_type().'.gif" alt="'.$object->get_type().'"/></div>';		
 		$html[] = '<div class="title">'.$object->get_title().'</div>';
-		$html[] = '<div class="description">'.$object->get_description().'</div>';
-		$html[] = '</div>';
-		//$html[] = '<a href="create.php?category='.$object->get_id().'&amp;type=forum_topic" title="'.get_lang('Move').'">Create Topic</a><br />';
-		$condition = new EqualityCondition('parent', $object->get_id());
-		$datamanager = RepositoryDataManager :: get_instance();
-		$objects = $datamanager->retrieve_learning_objects('forum_topic', $condition);
-		$menu = new LearningObjectTree($object->get_id());
+		$html[] = '<div class="description">'.$object->get_description().'</div></div>';
+		//$html[] = '<a href="create.php?category='.$object->get_id().'&amp;type=forum_topic" title="'.get_lang('Move').'">Create Topic</a><br />';	
+		$type_array = array('forum', 'forum_topic', 'forum_post');
+		$menu = new LearningObjectTree($object->get_parent_id(),$type_array);
 		$renderer =& new TreeMenuRenderer();
 		$menu->render($renderer,'sitemap');
-		$html[] = $renderer->toHtml();
+		$html[] = '<div>'.$renderer->toHtml().'</div>';
+		$html[] = '</div>';
 		return implode("\n",$html);
 	}
 }
