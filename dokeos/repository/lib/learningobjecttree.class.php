3a51f3dfc16b5e70588e452bdf19a1860ae3cf6a
malchick <malchick@cosnics>
2006-03-29 11:59:04 +0000

    added support for multiple learning object types

--- dokeos/repository/lib/learningobjecttree.class.php
+++ dokeos/repository/lib/learningobjecttree.class.php
@@ -17,16 +17,30 @@ class LearningObjectTree extends HTML_Menu
 	 *                           Passed to sprintf(). Defaults to the string
 	 *                           "?id=%s".
 	 */
-	public function LearningObjectTree($parent, $url_format = '?id=%s')
+	public function LearningObjectTree($parent, $type_array, $url_format = '?id=%s' )
 	{
 		$this->parent = $parent;
-		$this->urlFmt = $url_format;		
-		parent :: __construct($this->get_items($parent));	
+		$this->urlFmt = $url_format;
+		if (isset($type_array) && count($type_array))
+		{
+			$equality_array = array();
+			foreach($type_array as $condition)
+			{
+				$equality_array[]	= new EqualityCondition('type',$condition);
+			}
+		}
+		else
+			$equality_array = new EqualityCondition('type',null);			
+		parent :: __construct($this->get_items(1,$equality_array));
 	}
 
-	private function get_items($parent, $format)
+	private function get_items($parent, $equality_array)
 	{
-		$condition = new EqualityCondition('parent', $parent);
+		$cond2 = new EqualityCondition('type', null);
+		if($equality_array != null)
+			$cond2 = new OrCondition($equality_array);
+		$cond1 = new EqualityCondition('parent', $parent);
+		$condition = new AndCondition($cond1, $cond2);
 		$datamanager = RepositoryDataManager :: get_instance();
 		$objects = $datamanager->retrieve_learning_objects(null, $condition);
 		$items = array ();
@@ -40,8 +54,8 @@ class LearningObjectTree extends HTML_Menu
 			$menu_item['title'] = $item->get_title();
 			$menu_item['url'] = $this->get_id_url($item->get_id());
 			$menu_item['id'] = $item->get_id();
-			$items = $this->get_items($item->get_id());
-			if (count($items))
+			$items = $this->get_items($item->get_id(),$equality_array);
+			if(count($items))
 				$menu_item['sub'] = $items;
 			$sub_tree[$item->get_id()] = $menu_item;
 		}
