2d88d438906ee1124bfcd104e1da4f0381c0480a
malchick <malchick@localhost>
2006-04-05 10:40:55 +0000

    displays a table with the forumthreads

--- dokeos/repository/lib/learning_object/forum/display.class.php
+++ dokeos/repository/lib/learning_object/forum/display.class.php
@@ -1,6 +1,7 @@
 <?php
 require_once dirname(__FILE__).'/../../treemenurenderer.class.php';
 require_once dirname(__FILE__).'/../../learningobjecttree.class.php';
+require_once dirname(__FILE__).'/forumtable.class.php';
 class ForumDisplay extends LearningObjectDisplay
 {
 	public function ForumDisplay(&$object)
@@ -14,13 +15,15 @@ class ForumDisplay extends LearningObjectDisplay
 		$html[] = '<div style="float:right;width:80%;">';
 		$html[] = '<div class="icon"><img src="'.api_get_path(WEB_CODE_PATH).'img/'.$object->get_type().'.gif" alt="'.$object->get_type().'"/></div>';		
 		$html[] = '<div class="title">'.$object->get_title().'</div>';
-		$html[] = '<div class="description">'.$object->get_description().'</div></div>';
-		//$html[] = '<a href="create.php?category='.$object->get_id().'&amp;type=forum_topic" title="'.get_lang('Move').'">Create Topic</a><br />';	
+		$html[] = '<div class="description">'.$object->get_description().'</div></div>';	
 		$type_array = array('forum', 'forum_topic', 'forum_post');
-		$menu = new LearningObjectTree($object->get_parent_id(),$type_array);
+		$end_type = array('forum_post');
+		$menu = new LearningObjectTree($object->get_parent_id(),$type_array, $end_type);
 		$renderer =& new TreeMenuRenderer();
 		$menu->render($renderer,'sitemap');
-		$html[] = '<div>'.$renderer->toHtml().'</div>';
+		$html[] = '<div>'.$renderer->toHtml().'</div>';		
+		$table = new ForumTable($object);
+		$table->display();
 		$html[] = '</div>';
 		return implode("\n",$html);
 	}
