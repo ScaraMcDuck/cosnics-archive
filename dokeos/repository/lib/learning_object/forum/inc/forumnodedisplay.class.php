72e29fe3772e050224b405e3761036487ccd5215
ceetee <ceetee@localhost>
2006-04-13 10:57:01 +0000

    Centralizing forum and thread display; moving auxiliary classes for forums to separate path; fixing bug in fill_tables.php.

---
+++ dokeos/repository/lib/learning_object/forum/inc/forumnodedisplay.class.php
@@ -0,0 +1,24 @@
+<?php
+require_once dirname(__FILE__).'/../../../treemenurenderer.class.php';
+require_once dirname(__FILE__).'/forumtable.class.php';
+require_once dirname(__FILE__).'/forumtree.class.php';
+class ForumNodeDisplay extends LearningObjectDisplay
+{
+	function get_full_html()
+	{
+		$object = $this->get_learning_object();
+		$menu = new ForumTree($object->get_id(), $object->get_id());
+		$renderer =& new TreeMenuRenderer();
+		$menu->render($renderer,'sitemap');
+		$html[] = '<div class="forum_tree" style="float:left;width:20%;">';
+		$html[] = $renderer->toHtml();
+		$html[] = '</div>';		
+		$html[] = '<div class="forum_topics" style="float:right;width:80%;">';
+		$html[] = parent :: get_full_html();
+		$table = new ForumTable($object);
+		$html[] = $table->as_html();
+		$html[] = '</div>';
+		return implode("\n",$html);
+	}
+}
+?>
