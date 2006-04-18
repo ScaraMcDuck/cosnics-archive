02a3aa683193edbf43a31048ccddb07c6e068d1f
malchick <malchick@cosnics>
2006-04-18 09:55:11 +0000

    learning_path_item elements

---
+++ dokeos/repository/lib/learning_object/learning_path_item/learning_path_item_form.class.php
@@ -0,0 +1,39 @@
+<?php
+require_once dirname(__FILE__) . '/../../learningobjectform.class.php';
+class LearningPathItemForm extends LearningObjectForm
+{
+	public function LearningPathItemForm($formName, $method = 'post', $action = null)
+	{
+		parent :: __construct($formName, $method, $action);
+	}
+	public function build_creation_form()
+	{
+		parent :: build_creation_form();
+		$this->add_submit_button();
+	}
+	public function build_editing_form($object)
+	{
+		parent :: build_editing_form($object);
+		$this->setDefaults();
+		$this->add_submit_button();
+	}
+	public function create_learning_object($owner)
+	{
+		$values = $this->exportValues();
+		$dataManager = RepositoryDataManager::get_instance();
+		$learningPathItem = new LearningPathItem();
+		$learningPathItem->set_owner_id($owner);
+		$learningPathItem->set_title($values['title']);
+		$learningPathItem->set_description($values['description']);
+		$learningPathItem->create();
+		return $learningPathItem;
+	}
+	public function update_learning_object(& $object)
+	{
+		$values = $this->exportValues();
+		$object->set_title($values['title']);
+		$object->set_description($values['description']);
+		$object->update();
+	}
+}
+?>
