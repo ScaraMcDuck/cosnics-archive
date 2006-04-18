93dc91829ed6e2968b5ea9bc8e09584031cc8255
malchick <malchick@localhost>
2006-04-18 07:43:13 +0000

    created display class for learning paths

---
+++ dokeos/repository/lib/learning_object/learning_path/inc/learningpathnodedisplay.class.php
@@ -0,0 +1,24 @@
+<?php
+require_once dirname(__FILE__).'/../../../treemenurenderer.class.php';
+require_once dirname(__FILE__).'/learningpathtable.class.php';
+require_once dirname(__FILE__).'/learningpathtree.class.php';
+class LearningPathNodeDisplay extends LearningObjectDisplay
+{
+	function get_full_html()
+	{
+		$object = $this->get_learning_object();
+		$menu = new LearningPathTree($object->get_id(), $object->get_id());
+		$renderer =& new TreeMenuRenderer();
+		$menu->render($renderer,'sitemap');
+		$html[] = '<div class="forum_tree" style="float:left;width:20%;">';
+		$html[] = $renderer->toHtml();
+		$html[] = '</div>';		
+		$html[] = '<div class="forum_topics" style="float:right;width:80%;">';
+		$html[] = parent :: get_full_html();
+		$table = new LearningPathTable($object);
+		$html[] = $table->as_html();
+		$html[] = '</div>';
+		return implode("\n",$html);
+	}
+}
+?>
