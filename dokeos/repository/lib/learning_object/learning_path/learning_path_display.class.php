37df7156acb6343f009391cf798264764081f8e1
malchick <malchick@localhost>
2006-04-18 07:42:49 +0000

    created display class

---
+++ dokeos/repository/lib/learning_object/learning_path/learning_path_display.class.php
@@ -0,0 +1,6 @@
+<?php
+require_once dirname(__FILE__).'/inc/learningpathnodedisplay.class.php';
+class LearningPathDisplay extends LearningPathNodeDisplay
+{
+}
+?>
---
+++ dokeos/repository/lib/learning_object/learning_path_chapter/learning_path_chapter_display.class.php
@@ -0,0 +1,6 @@
+<?php
+require_once dirname(__FILE__).'/../learning_path/inc/learningpathnodedisplay.class.php';
+class LearningPathChapterDisplay extends LearningPathNodeDisplay
+{
+}
+?>
