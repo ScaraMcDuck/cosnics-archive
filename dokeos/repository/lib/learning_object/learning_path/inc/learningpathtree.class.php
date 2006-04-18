df1c23f30e93cd37e5742b71c71b8967c6a41315
malchick <malchick@cosnics>
2006-04-18 07:44:05 +0000

    created tree for learning paths

---
+++ dokeos/repository/lib/learning_object/learning_path/inc/learningpathtree.class.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(__FILE__).'/../../../learningobjecttree.class.php';
+class LearningPathTree extends LearningObjectTree
+{
+    function LearningPathTree($root, $active = 0)
+    {
+    	parent :: __construct($root, array('learning_path_chapter', 'learning_path_item'), array('learning_path_item'));
+    	$this->set_active_item($active);
+    }
+}
+?>
