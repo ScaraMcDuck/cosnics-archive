19f712bf07b3660af11b47ab119a8ca89cd6c4f5
malchick <malchick@localhost>
2006-04-19 14:36:28 +0000

    added repositorybrowsertable

--- dokeos/repository/objectmanagement/index.php
+++ dokeos/repository/objectmanagement/index.php
@@ -12,76 +12,16 @@ require_once dirname(__FILE__).'/../lib/treemenurenderer.class.php';
 require_once dirname(__FILE__).'/../lib/optionsmenurenderer.class.php';
 require_once api_get_library_path().'/text.lib.php';
 require_once dirname(__FILE__).'/../lib/repositoryutilities.class.php';
+require_once dirname(__FILE__).'/../lib/repositorybrowsertable.class.php';
+
 if( !api_get_user_id())
 {
 	api_not_allowed();
 }
-/**
- * Get the condition to use when retrieving objects from the datamanager
- */
-function get_condition()
-{
-	global $current_category_id;
-	$condition1 = new EqualityCondition(LearningObject :: PROPERTY_OWNER_ID,api_get_user_id());
-	$condition2 = new EqualityCondition(LearningObject :: PROPERTY_PARENT_ID,$current_category_id);
-	$condition = new AndCondition($condition1,$condition2);
-	if (isset ($_GET['keyword']))
-	{
-		$c = RepositoryUtilities :: query_to_condition($_GET['keyword']);
-		if (!is_null($c)) {
-			$condition = new AndCondition($condition, $c);
-		}
-	}
-	return $condition;
-}
-/**
- * Get the total number of objects
- */
-function get_number_of_objects()
-{
-	$datamanager = RepositoryDataManager::get_instance();
-	return $datamanager->count_learning_objects(null,get_condition());
-}
-/**
- * Get the objects to display in the table
- */
-function get_objects($from, $number_of_items, $column, $direction)
-{
-	global $current_category_id;
-	$table_columns = array('id','type','title','description','modified','id');
-	$datamanager = RepositoryDataManager::get_instance();
-	$order_by[] = $table_columns[$column];
-	$order_desc[] = $direction;
-	$objects = $datamanager->retrieve_learning_objects(null,get_condition(),$order_by,$order_desc,$from,$number_of_items);
-	$table_data = array();
-	foreach($objects as $index => $object)
-	{
-		$row = array();
-		$row[] = $object->get_id();
-		$row[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/'.$object->get_type().'.gif" alt="'.$object->get_type().'"/>';
-		if($object->get_type() == 'category')
-		{
-			$row[] = '<a href="index.php?category='.$object->get_id().'">'.$object->get_title().'</a>';
-		}
-		else
-		{
-			$row[] = '<a href="view.php?id='.$object->get_id().'">'.$object->get_title().'</a>';
-		}
-		$row[] = $object->get_description();
-		$row[] = date('Y-m-d, H:i', is_null($object->get_modification_date()) ? $object->get_creation_date() : $object->get_modification_date());
-		$modify = '<a href="edit.php?id='.$object->get_id().'" title="'.get_lang('Edit').'"><img src="'.api_get_path(WEB_CODE_PATH).'img/edit.gif" alt="'.get_lang('Edit').'"/></a>';
- 		$modify .= '<a href="index.php?category='.$current_category_id.'&amp;action=delete&amp;id='.$object->get_id().'" title="'.get_lang('Delete').'"  onclick="javascript:if(!confirm(\''.addslashes(htmlentities(get_lang("ConfirmYourChoice"))).'\')) return false;"><img src="'.api_get_path(WEB_CODE_PATH).'img/delete.gif" alt="'.get_lang('Delete').'"/></a>';
- 		$modify .= '<a href="index.php?category='.$current_category_id.'&amp;action=move&amp;id='.$object->get_id().'" title="'.get_lang('Move').'"><img src="'.api_get_path(WEB_CODE_PATH).'img/move.gif" alt="'.get_lang('Move').'"/></a>';
- 		$modify .= '<a href="metadata.php?id='.$object->get_id().'" title="'.get_lang('Metadata').'"><img src="'.api_get_path(WEB_CODE_PATH).'img/info_small.gif" alt="'.get_lang('Metadata').'"/></a>';
- 		$modify .= '<a href="rights.php?id='.$object->get_id().'" title="'.get_lang('Rights').'"><img src="'.api_get_path(WEB_CODE_PATH).'img/group_small.gif" alt="'.get_lang('Rights').'"/></a>';
 
- 		$row[] = $modify;
- 		$table_data[] = $row;
-	}
-	return $table_data;
-}
 // Load datamanager
 $datamanager = RepositoryDataManager::get_instance();
+
 // Load category
 $current_category_id = isset($_GET['category']) ? intval($_GET['category']) : NULL;
 if($current_category_id <= 0)
@@ -90,55 +30,24 @@ if($current_category_id <= 0)
 	$current_category_id = $root_category->get_id();
 }
 $object = $datamanager->retrieve_learning_object($current_category_id);
+
 if($object->get_owner_id() != api_get_user_id())
 {
 	api_not_allowed();
 }
 
 $tool_name = get_lang('MyLearningObjects');
-// Retrieve learning objecttypes
-$object_types = $datamanager->get_registered_types();
-$type_choices = array();
-foreach($object_types as $key => $type)
-{
-	$type_choices[$type] = get_lang($type);
-}
-$tool_name = get_lang('MyRepository');
-// Create a search-box
-$search_form = new FormValidator('search_simple','get','search.php','',null,false);
-$renderer =& $search_form->defaultRenderer();
-$renderer->setElementTemplate('<span>{element}</span> ');
-$search_form->addElement('hidden','action','simple_search');
-$search_form->addElement('text','keyword',get_lang('keyword'));
-$search_form->addElement('submit','submit',get_lang('Search'));
-// Create a dropdownlist with learning objecttypes
-$create_form = new FormValidator('type_list', 'post', 'create.php');
-$renderer =& $create_form->defaultRenderer();
-$renderer->setElementTemplate('<span>{element}</span> ');
-$create_form->addElement('hidden', 'category',$current_category_id);
-$create_form->addElement('select','type',null,$type_choices);
-$create_form->addElement('submit','submit',get_lang('Create'));
+
+//$tool_name = get_lang('MyRepository');
+
+
 // Create a navigation menu to browse through the categories
+
 create_category_menu();
+
 // Create a sortable table to display the learning objects
-$table = new SortableTable('objects','get_number_of_objects','get_objects');
-$parameters = array();
-$parameters['category'] = $current_category_id;
-if (isset ($_GET['keyword']))
-{
-	$parameters = array ('keyword' => $_GET['keyword']);
-}
-$table->set_additional_parameters($parameters);
-$column = 0;
-$table->set_header($column++,'');
-$table->set_header($column++,get_lang('Type'));
-$table->set_header($column++,get_lang('Title'));
-$table->set_header($column++,get_lang('Description'));
-$table->set_header($column++,get_lang('LastModified'));
-$table->set_header($column++,get_lang('Modify'),false);
-$actions['delete_selected'] = get_lang('Delete');
-$actions['move_selected'] = get_lang('Move');
-$table->set_form_actions($actions);
+
+
 
 // Perform actions if needed
 if(isset($_GET['action']))
@@ -245,28 +154,75 @@ if(isset($message))
 api_display_tool_title($current_location['name']);
 echo '<div style="float:left;width:40%;margin:5px;">';
 // Display create form
-$create_form->display();
+create_learning_object_list();
+
 echo '</div><div style="float:right;width:40%;text-align:right;margin:5px;">';
 // Display search form
-$search_form->display();
+create_search_form();
 // Display message if needed
 echo '</div><div style="float:left;width:20%;">';
 // Display menu
+
 $renderer =& new TreeMenuRenderer();
 $menu->render($renderer,'sitemap');
 echo $renderer->toHtml();
 echo '</div><div style="float:right;width:80%;">';
 // Display table
-$table->display();
+create_repository_table();
+
 // Link to quota-page
 echo '<a href="quota.php" style="float:right;"><img src="'.api_get_path(WEB_CODE_PATH).'/img/statistics.gif" style="vertical-align:middle;">'.get_lang('Quota').'</a>';
 echo '</div>';
 // Display footer
 Display::display_footer();
 
+// Create a search-box
+function create_search_form()
+{
+	$search_form = new FormValidator('search_simple','get','search.php','',null,false);
+	$renderer =& $search_form->defaultRenderer();
+	$renderer->setElementTemplate('<span>{element}</span> ');
+	$search_form->addElement('hidden','action','simple_search');
+	$search_form->addElement('text','keyword',get_lang('keyword'));
+	$search_form->addElement('submit','submit',get_lang('Search'));
+	$search_form->display();
+}
+// Create a dropdownlist with learning objecttypes
+function create_learning_object_list()
+{
+	global $current_category_id;
+	$create_form = new FormValidator('type_list', 'post', 'create.php');
+	$renderer =& $create_form->defaultRenderer();
+	$renderer->setElementTemplate('<span>{element}</span> ');
+	$create_form->addElement('hidden', 'category',$current_category_id);
+	$create_form->addElement('select','type',null,retrieve_learning_object_types());
+	$create_form->addElement('submit','submit',get_lang('Create'));
+	$create_form->display();
+}
+
+function create_repository_table()
+{
+	global $object;
+	$table = new RepositoryBrowserTable($object);
+	$table->set_additional_parameters(array('category' =>$object->get_id()));
+	$table->display();
+}
+
 function create_category_menu ()
 {
 	global $menu, $current_category_id;
 	$menu = new CategoryMenu(api_get_user_id(),$current_category_id,'?category=%s',true);
+}
+// Retrieve learning objecttypes
+function retrieve_learning_object_types()
+{
+	global $datamanager;
+	$object_types = $datamanager->get_registered_types();
+	$type_choices = array();
+	foreach($object_types as $key => $type)
+	{
+		$type_choices[$type] = get_lang($type);
+	}
+	return $type_choices;
 }
 ?>
--- dokeos/repository/objectmanagement/search.php
+++ dokeos/repository/objectmanagement/search.php
@@ -5,10 +5,11 @@ $langFile = 'admin';
 require_once dirname(__FILE__).'/../../claroline/inc/claro_init_global.inc.php';
 require_once api_get_library_path().'/formvalidator/FormValidator.class.php';
 require_once dirname(__FILE__).'/../lib/categorymenu.class.php';
-require_once api_get_library_path().'/fileDisplay.lib.php';
+//require_once api_get_library_path().'/fileDisplay.lib.php';
 require_once api_get_library_path().'/text.lib.php';
 require_once dirname(__FILE__).'/../lib/repositoryutilities.class.php';
 require_once dirname(__FILE__).'/../lib/repositorydatamanager.class.php';
+require_once dirname(__FILE__).'/../lib/repositorybrowsertable.class.php';
 if( !api_get_user_id())
 {
 	api_not_allowed();
@@ -19,99 +20,7 @@ if(!isset($_GET['action']))
 	$_GET['action'] = 'simple_search';
 }
 
-/**
- * Get the condition to use when retrieving objects from the datamanager
- */
-function get_condition()
-{
-	$cond_owner = new EqualityCondition(LearningObject :: PROPERTY_OWNER_ID,api_get_user_id());
-	if (isset ($_GET['action']))
-	{
-		switch($_GET['action'])
-		{
-			case 'advanced_search':
-				$cond_title = RepositoryUtilities::query_to_condition($_GET['title'],LearningObject :: PROPERTY_TITLE);
-				$cond_description = RepositoryUtilities::query_to_condition($_GET['description'], LearningObject :: PROPERTY_DESCRIPTION);
-				foreach($_GET['type'] as $index => $type)
-				{
-					$cond_type[] = new EqualityCondition(LearningObject :: PROPERTY_TYPE,$type);
-				}
-				$search_conditions = array();
-				if( !is_null($cond_title))
-				{
-					$search_conditions[] = $cond_title;
-				}
-				if( !is_null($cond_description))
-				{
-					$search_conditions[] = $cond_description;
-				}
-				if(count($cond_type)>0)
-				{
-					$search_conditions[] = new OrCondition($cond_type);
-				}
-				if(count($search_conditions) > 0)
-				{
-					$condition = new AndCondition($search_conditions);
-				}
-				else
-				{
-					$condition = null;
-				}
-				break;
-			default:
-				$cond_keyword = RepositoryUtilities :: query_to_condition($_GET['keyword']);
-				if (!is_null($cond_keyword)) {
-					$condition = new AndCondition($cond_owner, $cond_keyword);
-				}
-				else
-				{
-					$condition = null;
-				}
-				break;
-		}
-	}
-	return $condition;
-}
-/**
- * Get the total number of objects
- */
-function get_number_of_objects()
-{
-	$datamanager = RepositoryDataManager::get_instance();
-	$objects = $datamanager->retrieve_learning_objects(null,get_condition());
-	return count($objects);
-}
-/**
- * Get the objects to display in the table
- */
-function get_objects($from, $number_of_items, $column, $direction)
-{
-	$table_columns = array('id','type','title','description','modified','category');
-	$datamanager = RepositoryDataManager::get_instance();
-	$order_by[] = $table_columns[$column];
-	$order_desc[] = $direction;
-	$objects = $datamanager->retrieve_learning_objects(null,get_condition(),$order_by,$order_desc,$from,$number_of_items);
-	$table_data = array();
-	foreach($objects as $index => $object)
-	{
-		$row = array();
-		$row[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/'.$object->get_type().'.gif" alt="'.$object->get_type().'"/>';
-		if($object->get_type() == 'category')
-		{
-			$row[] = '<a href="index.php?category='.$object->get_id().'">'.$object->get_title().'</a>';
-		}
-		else
-		{
-			$row[] = '<a href="view.php?id='.$object->get_id().'">'.$object->get_title().'</a>';
-		}
-		$row[] = $object->get_description();
-		$row[] = date('Y-m-d, H:i', is_null($object->get_modification_date()) ? $object->get_creation_date() : $object->get_modification_date());
-		$modify = '<a href="edit.php?id='.$object->get_id().'" title="'.get_lang('Edit').'"><img src="'.api_get_path(WEB_CODE_PATH).'img/edit.gif" alt="'.get_lang('Edit').'"/></a>';
-		$row[] = $modify;
- 		$table_data[] = $row;
-	}
-	return $table_data;
-}
+
 // Load datamanager
 $datamanager = RepositoryDataManager::get_instance();
 
@@ -133,9 +42,10 @@ if(isset($_GET['action']))
 			$search_form->addElement('text','description',get_lang('Description'),'size="50"');
 			$search_form->addElement('select','type',get_lang('Type'),$type_choices,'multiple="multiple" size="5"');
 			$search_form->addElement('submit','submit',get_lang('Search'));
+			$search_form->addElement('static','simple','','<a href="search.php?action=simple_search">'.get_lang('SimpleSearch').'</a>');
 			break;
 		// Create a simple search-box
-		default:
+		case 'simple_search':
 			$search_form = new FormValidator('search_simple','get','search.php','',null,false);
 			$renderer =& $search_form->defaultRenderer();
 			$renderer->setElementTemplate('<span>{element}</span> ');
@@ -146,20 +56,23 @@ if(isset($_GET['action']))
 			break;
 	}
 }
-if(isset($_GET['action']) && !is_null(get_condition()))
-{
-	// Create a sortable table to display the learning objects
-	$table = new SortableTable('objects','get_number_of_objects','get_objects');
-	$parameters = $search_form->exportValues();
-	$table->set_additional_parameters($parameters);
-	$column = 0;
-	$table->set_header($column++,get_lang('Type'));
-	$table->set_header($column++,get_lang('Title'));
-	$table->set_header($column++,get_lang('Description'));
-	$table->set_header($column++,get_lang('LastModified'));
-	$table->set_header($column++,'');
-}
 
+// Create a sortable table to display the learning objects
+function create_repository_table()
+{
+	global $search_form;
+	
+	if(isset($_GET['action']))
+	{
+		$table = new RepositoryBrowserTable();
+		$parameters = $search_form->exportValues();
+		$table->set_additional_parameters($parameters);
+		if(!is_null($table))
+		{
+			$table->display();
+		}
+	}
+}
 $tool_name = get_lang('Search');
 
 $root_category = $datamanager->retrieve_root_category(api_get_user_id());
@@ -179,11 +92,7 @@ echo '<div style="text-align:center;margin:20px;">';
 $search_form->display();
 echo '</div>';
 
-if(!is_null($table))
-{
-	// Display search results
-	$table->display();
-}
+create_repository_table();
 
 // Display footer
 Display::display_footer();
