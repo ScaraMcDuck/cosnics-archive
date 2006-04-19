2f39f2a5342725ef8ce998e739e0e8715a3405e2
ceetee <ceetee@localhost>
2006-04-19 14:40:01 +0000

    Supporting multiple users.

--- dokeos/repository/test/repository_hammer_test.php
+++ dokeos/repository/test/repository_hammer_test.php
@@ -1,9 +1,13 @@
 <?php
 header('Content-Type: text/plain');
+require_once dirname(__FILE__).'/../lib/learningobject.class.php';
 require_once dirname(__FILE__).'/../lib/repositorydatamanager.class.php';
 $dataManager = RepositoryDataManager :: get_instance();
-$objects = $dataManager->retrieve_learning_objects();
-foreach ($objects as $o) {
-	echo $o->get_id() . "\n";
+$connection = $dataManager->get_connection();
+$query = 'SELECT '.$dataManager->escape_column_name(LearningObject :: PROPERTY_ID).' FROM '.$dataManager->escape_table_name('learning_object').' WHERE '.$dataManager->escape_column_name(LearningObject :: PROPERTY_OWNER_ID).'='.$_GET['owner'];
+$sth = $connection->prepare($query);
+$res = & $connection->execute($sth, $params);
+while ($record = $res->fetchRow(DB_FETCHMODE_ORDERED)) {
+	echo $record[0] . "\n";
 }
 ?>
