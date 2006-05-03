274aa7d1bd9fb1887b2fe1d14bb087408b386a74
bmol <bmol@cosnics>
2006-05-03 14:28:15 +0000

    adding function to set the default ordering column

--- dokeos/repository/lib/learning_object_table/learningobjecttablecolumnmodel.class.php
+++ dokeos/repository/lib/learning_object_table/learningobjecttablecolumnmodel.class.php
@@ -2,38 +2,42 @@
 class LearningObjectTableColumnModel
 {
 	private $columns;
-	
+
 	private $order_column;
-	
+
 	private $order_direction;
-	
+
 	function LearningObjectTableColumnModel($columns, $default_order_column = 0, $default_order_direction = SORT_ASC)
 	{
 		$this->columns = $columns;
 		$this->order_column = $default_order_column;
 		$this->order_direction = $default_order_direction;
 	}
-	
+
 	function get_column_count()
 	{
 		return count($this->columns);
 	}
-	
+
 	function get_column($index)
 	{
 		return $this->columns[$index];
 	}
-	
+
 	function add_column($column)
 	{
 		$this->columns[] = $column;
 	}
-	
+
 	function get_default_order_column()
 	{
 		return $this->order_column;
 	}
-	
+	function set_default_order_column($column)
+	{
+		$this->order_column = $column;
+	}
+
 	function get_default_order_direction()
 	{
 		return $this->order_direction;
