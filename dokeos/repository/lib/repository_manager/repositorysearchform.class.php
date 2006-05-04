5534c6f4cde4b4d05641fbba6eebfe7ac82e866b
ceetee <ceetee@cosnics>
2006-05-04 09:54:43 +0000

    Readability is where it's at.

--- dokeos/repository/lib/repository_manager/repositorysearchform.class.php
+++ dokeos/repository/lib/repository_manager/repositorysearchform.class.php
@@ -66,7 +66,12 @@ class RepositorySearchForm extends FormValidator
 
 	function is_full_repository_search()
 	{
-		return ($this->validate() && (!$this->advanced || $this->frozen_elements[3]->getValue() == self :: SEARCH_SCOPE_REPOSITORY)) || (is_array($_GET[RepositoryManager :: PARAM_LEARNING_OBJECT_TYPE]) && count($_GET[RepositoryManager :: PARAM_LEARNING_OBJECT_TYPE]));
+		if ($this->validate())
+		{
+			return (!$this->advanced || $this->frozen_elements[3]->getValue() == self :: SEARCH_SCOPE_REPOSITORY);
+		}
+		$types = $_GET[RepositoryManager :: PARAM_LEARNING_OBJECT_TYPE];
+		return (is_array($types) && count($types));
 	}
 
 	private function autofreeze()
