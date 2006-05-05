636f5ff74a4d128b14606205ccce0c997de0d061
bmol <bmol@cosnics>
2006-05-05 11:39:30 +0000

    including dutch language file for repository

--- dokeos/index_repository_manager.php
+++ dokeos/index_repository_manager.php
@@ -1,5 +1,6 @@
 <?php
 $cidReset = true;
+$langFile = 'repository';
 require_once dirname(__FILE__).'/claroline/inc/claro_init_global.inc.php';
 require_once dirname(__FILE__).'/repository/lib/repository_manager/repositorymanager.class.php';
 
