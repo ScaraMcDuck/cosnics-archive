01d1c3de2287bccfb9231fb5f5a6f4768da33c39
bmol <bmol@localhost>
2006-05-05 11:39:30 +0000

    including dutch language file for repository

---
+++ dokeos/claroline/lang/dutch/repository.inc.php
@@ -0,0 +1,12 @@
+<?php
+$AdvancedSearch='Geavanceerd zoeken';
+$Create='Aanmaken';
+$DeleteSelected='Geslecteerde items verwijderen';
+$MoveSelected='Geselecteerde items verplaatsen';
+$OriginalLocation='Oorspronkelijke locatie';
+$Quota='Quota';
+$RecycleBin='Prullenbak';
+$Restore='Herstel';
+$ToAdvancedSearch='Naar geavanceerd zoeken';
+$ToSimpleSearch='Naar eenvoudig zoeken';
+?>
