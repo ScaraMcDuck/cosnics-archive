2d605d469d89ce5bfe7c979893502fee04a7d1d1
ceetee <ceetee@localhost>
2006-05-08 13:39:38 +0000

    More API docs.

--- dokeos/repository/lib/learning_object_table/learningobjecttablecolumn.class.php
+++ dokeos/repository/lib/learning_object_table/learningobjecttablecolumn.class.php
@@ -1,31 +1,44 @@
 <?php
 /**
- * A column in a table of learning objects
  * @package repository.learningobjecttable
+ */
+/**
+ * This class represents a single column in a table of learning objects.
+ * 
+ * @see LearningObjectTable
+ * @see LearningObjectTableColumnModel
+ * @author Tim De Pauw
  */
 class LearningObjectTableColumn
 {
 	/**
 	 * The property of the learning object which will be displayed in this
-	 * column
+	 * column.
 	 */
 	private $learning_object_property;
 	/**
-	 * The title of this column
+	 * The title of the column.
 	 */
 	private $title;
 	/**
-	 * Is sorting on this column allowed?
+	 * Whether or not sorting by this column is allowed.
 	 */
 	private $sortable;
 	/**
-	 * Create a new column
-	 * @param string $property_name_or_column_title If this column will display
-	 * the value of a learning object property, this parameter should contain
-	 * the name of this property. Else this parameter should contain the title
-	 * of the column.
-	 * @param boolean $contains_learning_object_property True if the parameter
-	 * $property_name_or_column contains a property, false otherwise.
+	 * Constructor. Either defines a column that displays a default property
+	 * of learning objects, or arbitrary content. 
+	 * @param string $property_name_or_column_title If the column contains
+	 *                                              arbitrary content, the
+	 *                                              title of the column. If
+	 *                                              it displays a learning
+	 *                                              object property, that
+	 *                                              particular property, a
+	 *                                              LearningObject::PROPERTY_*
+	 *                                              constant. 
+	 * @param boolean $contains_learning_object_property True if the column
+	 *                                                   displays a learning
+	 *                                                   object property, false
+	 *                                                   otherwise.
 	 */
 	function LearningObjectTableColumn($property_name_or_column_title, $contains_learning_object_property = false)
 	{
@@ -42,40 +55,43 @@ class LearningObjectTableColumn
 		}
 	}
 	/**
-	 * Retrieve learning object property
-	 * @return string The property name
+	 * Gets the learning object property that this column displays.
+	 * @return string The property name, or null if the column contains
+	 *                arbitrary content.
 	 */
 	function get_learning_object_property()
 	{
 		return $this->learning_object_property;
 	}
 	/**
-	 * Retrieve the title of this column
-	 * @return string The title
+	 * Gets the title of this column.
+	 * @return string The title.
 	 */
 	function get_title()
 	{
 		return $this->title;
 	}
 	/**
-	 * Determine if the table can be sorted on this column
-	 * @return boolean True if table can be sorted on this column
+	 * Determine if the table's contents may be sorted by this column.
+	 * @return boolean True if sorting by this column is allowed, false
+	 *                 otherwise.
 	 */
 	function is_sortable()
 	{
 		return $this->sortable;
 	}
 	/**
-	 * Set this columns title
-	 * @param string $title The new title
+	 * Sets the title of this column.
+	 * @param string $title The new title.
 	 */
 	function set_title($title)
 	{
 		$this->title = $title;
 	}
 	/**
-	 * Set if the table containing this column may be sorted on this column
-	 * @param boolean $sortable True if table can be sorted on this column
+	 * Sets whether or not the table's contents may be sorted by this column.
+	 * @param boolean $sortable True if sorting by this column should be
+	 *                          allowed, false otherwise.
 	 */
 	function set_sortable($sortable)
 	{
