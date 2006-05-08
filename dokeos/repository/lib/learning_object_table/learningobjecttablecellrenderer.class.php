41a5bb3634a851f09673ebabde01b0b4d5a0f2e7
bmol <bmol@localhost>
2006-05-08 12:47:23 +0000

    adding phpdoc

--- dokeos/repository/lib/learning_object_table/learningobjecttablecellrenderer.class.php
+++ dokeos/repository/lib/learning_object_table/learningobjecttablecellrenderer.class.php
@@ -1,6 +1,18 @@
 <?php
+/**
+ * A cell renderer which can be used for rendering a table of learning objects.
+ * @package repository
+ */
 interface LearningObjectTableCellRenderer
 {
+	/**
+	 * Render a table cell
+	 * @param LearningObjectTableColumn $column The column in which this cell
+	 * will be displayed
+	 * @param LearningObject $learning_object The learning object which will be
+	 * displayed in this cell
+	 * @return string The rendered cell content
+	 */
 	function render_cell($column, $learning_object);
 }
 ?>
