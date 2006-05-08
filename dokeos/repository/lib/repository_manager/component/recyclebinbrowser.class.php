9171d51557d8eb78b335a04786254a60333aba22
ceetee <ceetee@cosnics>
2006-05-08 11:50:39 +0000

    Adding option to empty recycle bin.

--- dokeos/repository/lib/repository_manager/component/recyclebinbrowser.class.php
+++ dokeos/repository/lib/repository_manager/component/recyclebinbrowser.class.php
@@ -14,7 +14,16 @@ class RepositoryManagerRecycleBinBrowserComponent extends RepositoryManagerCompo
 	function run()
 	{
 		$this->display_header(array(array('url' => $this->get_url(), 'name' => get_lang('RecycleBin'))));
-		$this->display_learning_objects();
+		if ($_GET[RepositoryManager :: PARAM_EMPTY_RECYCLE_BIN])
+		{
+			$this->empty_recycle_bin();
+			$this->display_message(get_lang('RecycleBinEmptied'));
+		}
+		$count = $this->display_learning_objects();
+		if ($count)
+		{
+			echo '<div class="empty_recycle_bin" style="margin-top: 1em; text-align: right;"><a href="'.htmlentities($this->get_url(array(RepositoryManager :: PARAM_EMPTY_RECYCLE_BIN => 1))).'" style="background: url('.$this->get_web_code_path().'img/recycle.gif) no-repeat 0 50%; padding: 10px 0 10px 31px;">'.get_lang('EmptyRecycleBin').'</a></div>';
+		}
 		$this->display_footer();
 	}
 	private function display_learning_objects()
@@ -23,6 +32,18 @@ class RepositoryManagerRecycleBinBrowserComponent extends RepositoryManagerCompo
 		$parameters = $this->get_parameters(true);
 		$table = new RepositoryRecycleBinBrowserTable($this, null, $parameters, $condition);
 		echo $table->as_html();
+		return $table->get_learning_object_count();
+	}
+	private function empty_recycle_bin()
+	{
+		$trashed_objects = $this->retrieve_learning_objects(null, new EqualityCondition(LearningObject :: PROPERTY_OWNER_ID, $this->get_user_id()), array(), array(), 0, -1, LearningObject :: STATE_RECYCLED);
+		$count = 0;
+		while ($object = $trashed_objects->next_result())
+		{
+			$object->delete();
+			$count++;
+		}
+		return $count;
 	}
 }
 ?>
