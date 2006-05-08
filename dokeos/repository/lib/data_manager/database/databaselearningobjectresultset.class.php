2044f036e371f5001e2f4c978b33e2b65323465a
bmol <bmol@localhost>
2006-05-08 11:58:19 +0000

    adding phpdoc

--- dokeos/repository/lib/data_manager/database/databaselearningobjectresultset.class.php
+++ dokeos/repository/lib/data_manager/database/databaselearningobjectresultset.class.php
@@ -7,16 +7,25 @@ require_once dirname(__FILE__).'/../../resultset.class.php';
  * Resultset to hold a set of learning objects
  */
 class DatabaseLearningObjectResultSet extends ResultSet {
+	/**
+	 * The datamanager used to retrieve objects from the repository
+	 */
 	private $data_manager;
-
+	/**
+	 * An instance of DB_result
+	 */
 	private $handle;
-
+	/**
+	 * Flag to know if the $handle contains all properties of the learning
+	 * object
+	 */
 	private $single_type;
 	/**
 	 * Create a new resultset for handling a set of learning objects
 	 * @param RepositoryDataManager $data_manager The datamanager used to
-	 * retreve objects from the repository
-	 * @param $handle
+	 * retrieve objects from the repository
+	 * @param DB_result $handle The handle to retrieve records from a database
+	 * resultset
 	 * @param boolean $single_type True if the handle holds all properties of
 	 * the learning objects (so when retrieving the learning objects, the
 	 * datamanager shouldn't perform additional queries)
