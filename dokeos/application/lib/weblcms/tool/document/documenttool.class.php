d008848d621606a91923e2abe12ee8b390838ee3
bmol <bmol@localhost>
2006-05-22 09:12:56 +0000

    reorganized code so publisher component can use header(..) function to redirect user

--- dokeos/application/lib/weblcms/tool/document/documenttool.class.php
+++ dokeos/application/lib/weblcms/tool/document/documenttool.class.php
@@ -15,42 +15,53 @@ class DocumentTool extends RepositoryTool
 	 */
 	function run()
 	{
-		$this->display_header();
 		if (isset($_GET['documenttoolmode']))
 		{
 			$_SESSION['documenttoolmode'] = $_GET['documenttoolmode'];
 		}
-		echo '<ul style="list-style: none; padding: 0; margin: 0 0 1em 0">';
+		if( isset($_GET['admin']) && $_GET['admin'] == 0)
+		{
+			$_SESSION['documenttoolmode'] = 0;
+		}
+		$html[] = '<ul style="list-style: none; padding: 0; margin: 0 0 1em 0">';
 		$i = 0;
 		foreach (array('Browser Mode', 'Publisher Mode', 'Category Manager Mode') as $title)
 		{
 			$current = ($_SESSION['documenttoolmode'] == $i);
-			echo '<li style="display: inline; margin: 0 1ex 0 0; padding: 0">';
-			if (!$current) echo '<a href="' . $this->get_url(array('documenttoolmode' => $i), true) . '">';
-			echo '[' . $title . ']';
-			if (!$current) echo '</a>';
-			echo '</li>';
+			$html[] =  '<li style="display: inline; margin: 0 1ex 0 0; padding: 0">';
+			if (!$current)
+			{
+				$html[] = '<a href="' . $this->get_url(array('documenttoolmode' => $i), true) . '">';
+			}
+			$html[] =  '[' . $title . ']';
+			if (!$current)
+			{
+				$html[] =  '</a>';
+			}
+			$html[] =  '</li>';
 			$i++;
 		}
-		echo '</ul>';
-		$this->perform_requested_actions();
+		$html[] =  '</ul>';
+		$html[] = $this->perform_requested_actions();
 		switch ($_SESSION['documenttoolmode'])
 		{
 			case 2:
 				require_once dirname(__FILE__).'/../../learningobjectpublicationcategorymanager.class.php';
 				$catman = new LearningObjectPublicationCategoryManager($this, 'document');
-				echo $catman->as_html();
+				$html[] =  $catman->as_html();
 				break;
 			case 1:
 				require_once dirname(__FILE__).'/../../learningobjectpublisher.class.php';
 				$pub = new LearningObjectPublisher($this, 'document');
-				echo $pub->as_html();
+				$html[] =  $pub->as_html();
 				break;
 			default:
 				require_once dirname(__FILE__).'/documentbrowser.class.php';
 				$browser = new DocumentBrowser($this);
-				echo $browser->as_html();
+				$html[] =  $browser->as_html();
 		}
+		$this->display_header();
+		echo implode("\n",$html);
 		$this->display_footer();
 	}
 }
