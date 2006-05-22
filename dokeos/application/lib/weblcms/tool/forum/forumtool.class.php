03672621554824983ae8c76733befccbc59c6c71
bmol <bmol@localhost>
2006-05-22 09:06:51 +0000

    reorganized code so publisher component can use header(..) function to redirect user

--- dokeos/application/lib/weblcms/tool/forum/forumtool.class.php
+++ dokeos/application/lib/weblcms/tool/forum/forumtool.class.php
@@ -16,35 +16,39 @@ class ForumTool extends RepositoryTool
 	 */
 	function run()
 	{
-		$this->display_header();
 		if(!$this->is_allowed(VIEW_RIGHT))
 		{
+			$this->display_header();
 			api_not_allowed();
 			$this->display_footer();
 			return;
 		}
-		if (isset($_GET['forumadmin']))
+		if (isset($_GET['admin']))
 		{
-			$_SESSION['forumadmin'] = $_GET['forumadmin'];
+			$_SESSION['forumadmin'] = $_GET['admin'];
 		}
 		if ($_SESSION['forumadmin'] && $this->is_allowed(ADD_RIGHT))
 		{
-			echo '<p>Go to <a href="' . $this->get_url(array('forumadmin' => 0), true) . '">User Mode</a> &hellip;</p>';
+			$html[] = '<p>Go to <a href="' . $this->get_url(array('admin' => 0), true) . '">User Mode</a> &hellip;</p>';
 			require_once dirname(__FILE__).'/../../learningobjectpublisher.class.php';
 			$pub = new LearningObjectPublisher($this, 'forum');
-			echo $pub->as_html();
+			$html[] =  $pub->as_html();
+			$this->display_header();
+			echo implode("\n",$html);
+			$this->display_footer();
 		}
 		else
 		{
+			$this->display_header();
 			if($this->is_allowed(ADD_RIGHT))
 			{
-				echo '<p>Go to <a href="' . $this->get_url(array('forumadmin' => 1), true) . '">Publisher Mode</a> &hellip;</p>';
+				echo '<p>Go to <a href="' . $this->get_url(array('admin' => 1), true) . '">Publisher Mode</a> &hellip;</p>';
 			}
-			$this->perform_requested_actions();
+			echo $this->perform_requested_actions();
 			$browser = new ForumBrowser($this);
 			echo $browser->as_html();
+			$this->display_footer();
 		}
-		$this->display_footer();
 	}
 }
 ?>
