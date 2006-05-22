e8845861210c28fefb36803c11acd89a24d6d635
bmol <bmol@localhost>
2006-05-22 08:31:44 +0000

    reorganized code so publisher component can use header(..) function to redirect user

--- dokeos/application/lib/weblcms/tool/announcement/announcementtool.class.php
+++ dokeos/application/lib/weblcms/tool/announcement/announcementtool.class.php
@@ -16,35 +16,39 @@ class AnnouncementTool extends RepositoryTool
 	 */
 	function run()
 	{
-		$this->display_header();
 		if(!$this->is_allowed(VIEW_RIGHT))
 		{
+			$this->display_header();
 			api_not_allowed();
 			$this->display_footer();
 			return;
 		}
-		if (isset($_GET['announcementadmin']))
+		if (isset($_GET['admin']))
 		{
-			$_SESSION['announcementadmin'] = $_GET['announcementadmin'];
+			$_SESSION['announcementadmin'] = $_GET['admin'];
 		}
 		if ($_SESSION['announcementadmin'] && $this->is_allowed(ADD_RIGHT))
 		{
-			echo '<p>Go to <a href="' . $this->get_url(array('announcementadmin' => 0), true) . '">User Mode</a> &hellip;</p>';
 			require_once dirname(__FILE__).'/../../learningobjectpublisher.class.php';
 			$pub = new LearningObjectPublisher($this, 'announcement');
-			echo $pub->as_html();
+			$html[] = '<p>Go to <a href="' . $this->get_url(array('admin' => 0), true) . '">User Mode</a> &hellip;</p>';
+			$html[] =  $pub->as_html();
+			$this->display_header();
+			echo implode("\n",$html);
+			$this->display_footer();
 		}
 		else
 		{
+			$this->display_header();
 			if($this->is_allowed(ADD_RIGHT))
 			{
-				echo '<p>Go to <a href="' . $this->get_url(array('announcementadmin' => 1), true) . '">Publisher Mode</a> &hellip;</p>';
+				echo '<p>Go to <a href="' . $this->get_url(array('admin' => 1), true) . '">Publisher Mode</a> &hellip;</p>';
 			}
 			$this->perform_requested_actions();
 			$browser = new AnnouncementBrowser($this);
 			echo $browser->as_html();
+			$this->display_footer();
 		}
-		$this->display_footer();
 	}
 }
 ?>
