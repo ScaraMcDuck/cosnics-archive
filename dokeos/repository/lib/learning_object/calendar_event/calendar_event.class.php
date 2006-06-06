08d2db99c457a116d70940aa12ac1af8bb541f9d
bmol <bmol@cosnics>
2006-06-06 11:25:42 +0000

    allow attachments on calendar events

--- dokeos/repository/lib/learning_object/calendar_event/calendar_event.class.php
+++ dokeos/repository/lib/learning_object/calendar_event/calendar_event.class.php
@@ -6,24 +6,52 @@ require_once dirname(__FILE__) . '/../../learningobject.class.php';
  */
 class CalendarEvent extends LearningObject
 {
+	/**
+	 * The start date of the calendar event
+	 */
 	const PROPERTY_START_DATE = 'start_date';
+	/**
+	 * The end date of the calendar event
+	 */
 	const PROPERTY_END_DATE = 'end_date';
-
+	/**
+	 * Gets the start date of this calendar event
+	 * @return int The start date
+	 */
 	function get_start_date ()
 	{
 		return $this->get_additional_property(self :: PROPERTY_START_DATE);
 	}
+	/**
+	 * Sets the start date of this calendar event
+	 * @param int The start date
+	 */
 	function set_start_date ($start_date)
 	{
 		return $this->set_additional_property(self :: PROPERTY_START_DATE, $start_date);
 	}
+	/**
+	 * Gets the end date of this calendar event
+	 * @return int The end date
+	 */
 	function get_end_date ()
 	{
 		return $this->get_additional_property(self :: PROPERTY_END_DATE);
 	}
+	/**
+	 * Sets the end date of this calendar event
+	 * @param int The end date
+	 */
 	function set_end_date ($end_date)
 	{
 		return $this->set_additional_property(self :: PROPERTY_END_DATE, $end_date);
+	}
+	/**
+	 * @return boolean Always true
+	 */
+	function supports_attachments()
+	{
+		return true;
 	}
 }
 ?>
