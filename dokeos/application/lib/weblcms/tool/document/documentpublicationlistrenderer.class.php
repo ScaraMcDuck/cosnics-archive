b6ea6b6c4307faf579d516466e95a3cc1e633309
bmol <bmol@cosnics>
2006-06-07 08:40:45 +0000

    some document tool improvements

--- dokeos/application/lib/weblcms/tool/document/documentpublicationlistrenderer.class.php
+++ dokeos/application/lib/weblcms/tool/document/documentpublicationlistrenderer.class.php
@@ -7,17 +7,25 @@
 require_once dirname(__FILE__).'/../../browser/list_renderer/tablelearningobjectpublicationlistrenderer.class.php';
 class DocumentPublicationListRenderer extends TableLearningObjectPublicationListRenderer
 {
-    function DocumentPublicationListRenderer($browser) 
+    function DocumentPublicationListRenderer($browser)
     {
     	parent :: __construct($browser);
-    	$this->set_header(0, get_lang('Title'), false);
-		$this->set_header(1, get_lang('Description'), false);
-		$this->set_header(2, get_lang('PublishedOn'), false);
-		$this->set_header(3, get_lang('PublishedBy'), false);
-		$this->set_header(4, get_lang('PublishedFor'), false);
-		$this->set_header(5, '', false);
+    	$column = 0;
+    	if($browser->is_allowed(EDIT_RIGHT) || $browser->is_allowed(DELETE_RIGHT))
+    	{
+    		$this->set_header($column++, '', false);
+    	}
+    	$this->set_header($column++, get_lang('Title'), false);
+		$this->set_header($column++, get_lang('Description'), false);
+		$this->set_header($column++, get_lang('PublishedOn'), false);
+		$this->set_header($column++, get_lang('PublishedBy'), false);
+		$this->set_header($column++, get_lang('PublishedFor'), false);
+    	if($browser->is_allowed(EDIT_RIGHT) || $browser->is_allowed(DELETE_RIGHT))
+    	{
+    		$this->set_header($column++, '', false);
+    	}
     }
-    
+
     function render_title($publication) {
     	return '<a href="'.htmlentities($publication->get_learning_object()->get_url()).'">'.parent::render_title($publication).'</a>';
     }
