b95705d931a88fe6dee9f3901afb360a15448713
bmol <bmol@localhost>
2006-08-21 12:25:54 +0000

    first repository-based learning path tool

---
+++ dokeos/application/lib/weblcms/tool/learning_path/learning_pathpublicationlistrenderer.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Learning path tool - list renderer
+ * @package application.weblcms.tool
+ * @subpackage learning_path
+ */
+require_once dirname(__FILE__).'/../../browser/list_renderer/listlearningobjectpublicationlistrenderer.class.php';
+
+class LearningPathPublicationListRenderer extends ListLearningObjectPublicationListRenderer
+{
+
+}
+?>
