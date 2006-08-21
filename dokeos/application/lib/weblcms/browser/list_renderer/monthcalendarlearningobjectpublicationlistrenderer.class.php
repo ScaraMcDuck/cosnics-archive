4a5624d6698ec5bd82896dc1e554dfc6bcca4e6c
bmol <bmol@localhost>
2006-08-21 11:57:48 +0000

    adding PHPDOC

--- dokeos/application/lib/weblcms/browser/list_renderer/monthcalendarlearningobjectpublicationlistrenderer.class.php
+++ dokeos/application/lib/weblcms/browser/list_renderer/monthcalendarlearningobjectpublicationlistrenderer.class.php
@@ -1,25 +1,29 @@
 <?php
 /**
+ * $Id$
  * @package application.weblcms
  * @subpackage browser.listrenderer
  */
 require_once dirname(__FILE__).'/../learningobjectpublicationlistrenderer.class.php';
-
+/**
+ * Renderer to display events in a month calendar
+ */
 class MonthCalendarLearningObjectPublicationListRenderer extends LearningObjectPublicationListRenderer
 {
 	/**
-	 *
+	 * The current time displayed in the calendar
 	 */
 	private $display_time;
 	/**
-	 *
+	 * Sets the current display time.
+	 * @param int $time The current display time.
 	 */
 	function set_display_time($time)
 	{
 		$this->display_time = $time;
 	}
 	/**
-	 *
+	 * Returns the HTML output of this renderer.
 	 */
 	function as_html()
 	{
@@ -81,20 +85,30 @@ class MonthCalendarLearningObjectPublicationListRenderer extends LearningObjectP
 		$html[] = $calendar_table->toHtml();
 		return implode("\n",$html);
 	}
+	/**
+	 * Renders a publication
+	 * @param LearningObjectPublication $publication The publication to render
+	 * @param int $table_date The current date displayed in the table.
+	 */
 	function render_publication($publication,$table_date)
 	{
+		static $color_cache;
 		$event = $publication->get_learning_object();
 		$event_url = $this->get_url(array('pid'=>$publication->get_id()), true);
 		$start_date = $event->get_start_date();
 		$end_date = $event->get_end_date();
+		if(!isset($color_cache[$event->get_id()]))
+		{
+			$color_cache[$event->get_id()] = 'rgb('.rand(0,255).','.rand(0,255).','.rand(0,255).')';
+		}
+		$html[] = '';
+		$html[] = '<div class="event" style="border-right: 4px solid '.$color_cache[$event->get_id()].';">';
 		if($start_date > $table_date && $start_date <= strtotime('+1 Day',$table_date))
 		{
-			$html[] = '<div class="event">';
 			$html[] = date('H:i',$start_date);
 		}
 		else
 		{
-			$html[] = '<div class="event">';
 			$html[] = '&rarr;';
 		}
 		$html[] = '<a href="'.$event_url.'">'.htmlentities($event->get_title()).'</a>';
