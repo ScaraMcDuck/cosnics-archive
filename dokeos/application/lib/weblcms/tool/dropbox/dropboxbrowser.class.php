0e67475f5b30c6e688bae1e5a33e19e128c0359e
bmol <bmol@localhost>
2006-08-21 12:25:08 +0000

    first repository-based dropbox tool

---
+++ dokeos/application/lib/weblcms/tool/dropbox/dropboxbrowser.class.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Dropbox tool - browser
+ * @package application.weblcms.tool
+ * @subpackage dropbox
+ */
+require_once dirname(__FILE__).'/../../weblcmsdatamanager.class.php';
+require_once dirname(__FILE__).'/../../learningobjectpublicationbrowser.class.php';
+require_once dirname(__FILE__).'/dropboxpublicationlistrenderer.class.php';
+
+class DropboxBrowser extends LearningObjectPublicationBrowser
+{
+	function DropboxBrowser($parent, $types)
+	{
+		parent :: __construct($parent, 'dropbox');
+		$renderer = new AnnouncementPublicationListRenderer($this);
+		$this->set_publication_list_renderer($renderer);
+	}
+
+	function get_publications($from, $count, $column, $direction)
+	{
+		$datamanager = WeblcmsDataManager :: get_instance();
+		$tool_condition = new EqualityCondition(LearningObjectPublication :: PROPERTY_TOOL, 'dropbox');
+		$condition = $tool_condition;
+		$user_id = $this->get_user_id();
+		$groups = $this->get_groups();
+		$publications = $datamanager->retrieve_learning_object_publications($this->get_course_id(), null, $user_id, $groups, $condition, false, array (Announcement :: PROPERTY_DISPLAY_ORDER_INDEX), array (SORT_DESC));
+		$visible_publications = array ();
+		while ($publication = $publications->next_result())
+		{
+			// If the publication is hidden and the user is not allowed to DELETE or EDIT, don't show this publication
+			if (!$publication->is_visible_for_target_users() && !($this->is_allowed(DELETE_RIGHT) || $this->is_allowed(EDIT_RIGHT)))
+			{
+				continue;
+			}
+			$visible_publications[] = $publication;
+		}
+		return $visible_publications;
+	}
+
+	function get_publication_count()
+	{
+		return count($this->get_publications());
+	}
+}
+?>
---
+++ dokeos/application/lib/weblcms/tool/dropbox/dropboxpublicationlistrenderer.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Dropbox tool - list renderer
+ * @package application.weblcms.tool
+ * @subpackage dropbox
+ */
+require_once dirname(__FILE__).'/../../browser/list_renderer/listlearningobjectpublicationlistrenderer.class.php';
+
+class DropboxPublicationListRenderer extends ListLearningObjectPublicationListRenderer
+{
+
+}
+?>
