b1bfaf761868dc3075172d094772d621b0ad1087
bmol <bmol@cosnics>
2006-08-29 13:01:59 +0000

    display auto-generated metadata

--- dokeos/repository/lib/repository_manager/component/metadataeditor.class.php
+++ dokeos/repository/lib/repository_manager/component/metadataeditor.class.php
@@ -3,6 +3,8 @@
  * @package repository.repositorymanager
  */
 require_once dirname(__FILE__).'/../repositorymanagercomponent.class.php';
+require_once dirname(__FILE__).'/../repositorymanager.class.php';
+require_once dirname(__FILE__).'/../../metadata/ieee_lom/ieee_lom_generator.class.php';
 /**
  * Repository manager component to edit the metadata of a learning object.
  */
@@ -12,8 +14,13 @@ class RepositoryManagerMetadataEditorComponent extends RepositoryManagerComponen
 	{
 		$breadcrumbs = array(array('url' => $this->get_url(), 'name' => get_lang('EditMetadata')));
 		$this->display_header($breadcrumbs);
-		// TODO: Implementation (connect with existing metadata code)
-		echo '<p>'.htmlentities(get_lang('NotAvailable')).'</p>';
+		$id = $_GET[RepositoryManager :: PARAM_LEARNING_OBJECT_ID];
+		if ($id)
+		{
+			$object = $this->retrieve_learning_object($id);
+			$lom = IeeeLomGenerator::generate($object);
+			$lom->display();
+		}
 		$this->display_footer();
 	}
 }
