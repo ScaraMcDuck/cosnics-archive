94599920a0864a651646cea665c7b4986a0e224e
bmol <bmol@localhost>
2006-09-01 12:17:40 +0000

    adding PHPDoc

--- dokeos/repository/lib/condition/andcondition.class.php
+++ dokeos/repository/lib/condition/andcondition.class.php
@@ -1,20 +1,19 @@
 <?php
 /**
+ * $Id$
  * @package repository.condition
  */
 require_once dirname(__FILE__).'/multipleaggregatecondition.class.php';
-
 /**
-==============================================================================
- *	This type of condition requires that all of its aggregated conditions be
- *	met.
- *
- *	@author Tim De Pauw
-==============================================================================
+ * This type of condition requires that all of its aggregated conditions be met.
+ * @author Tim De Pauw
  */
-
 class AndCondition extends MultipleAggregateCondition
 {
+	/**
+	 * Gets a string representation of this condition
+	 * @return string
+	 */
 	function __toString()
 	{
 		$conditions = $this->get_conditions();
--- dokeos/repository/lib/condition/orcondition.class.php
+++ dokeos/repository/lib/condition/orcondition.class.php
@@ -1,20 +1,21 @@
 <?php
 /**
+ * $Id$
  * @package repository.condition
  */
 require_once dirname(__FILE__).'/multipleaggregatecondition.class.php';
-
 /**
-==============================================================================
  *	This type of condition requires that one or more of its aggregated
  *	conditions be met.
  *
  *	@author Tim De Pauw
-==============================================================================
  */
-
 class OrCondition extends MultipleAggregateCondition
 {
+	/**
+	 * Gets a string representation of this condition
+	 * @return string
+	 */
 	function __toString()
 	{
 		$conditions = $this->get_conditions();
