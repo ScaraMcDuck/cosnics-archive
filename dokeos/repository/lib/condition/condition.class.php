b6b3eb9b7f98b2f95765b133b018a14f7441b4d1
bmol <bmol@cosnics>
2006-09-01 12:17:40 +0000

    adding PHPDoc

--- dokeos/repository/lib/condition/condition.class.php
+++ dokeos/repository/lib/condition/condition.class.php
@@ -1,17 +1,13 @@
 <?php
 /**
+ * $Id$
  * @package repository.condition
  */
-
 /**
-==============================================================================
  *	All conditions used for learning object selection in the data source must
  *	implement this interface.
- *
  *	@author Tim De Pauw
-==============================================================================
  */
-
 interface Condition
 {
 }
