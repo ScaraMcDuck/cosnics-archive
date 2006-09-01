1bd01a6b9432742cbb65b25bbcd80e936112c66b
bmol <bmol@localhost>
2006-09-01 11:42:13 +0000

    adding PHPDoc

--- dokeos/repository/lib/abstractlearningobject.class.php
+++ dokeos/repository/lib/abstractlearningobject.class.php
@@ -4,13 +4,25 @@
  * @package repository
  */
 require_once dirname(__FILE__).'/learningobject.class.php';
-
+/**
+ * An abstract learning object
+ */
 class AbstractLearningObject extends LearningObject
 {
+	/**
+	 * The type of the learning object
+	 */
 	private $type;
-
+	/**
+	 * Are attachments supported
+	 */
 	private $attachments_supported;
-
+	/**
+	 * Constructor
+	 * @param string $type
+	 * @param int $owner
+	 * @param int $parent
+	 */
 	function AbstractLearningObject($type, $owner, $parent = 0)
 	{
 		parent :: __construct();
@@ -19,12 +31,18 @@ class AbstractLearningObject extends LearningObject
 		$this->set_owner_id($owner);
 		$this->set_parent_id($parent);
 	}
-
+	/**
+	 * Gets the type of this abstract learning object
+	 * @return string
+	 */
 	function get_type()
 	{
 		return $this->type;
 	}
-
+	/**
+	 * Determines if this object supports attachments
+	 * @return boolean
+	 */
 	function supports_attachments()
 	{
 		$class = LearningObject :: type_to_class($this->get_type());
--- dokeos/repository/lib/arrayresultset.class.php
+++ dokeos/repository/lib/arrayresultset.class.php
@@ -12,16 +12,24 @@ require_once dirname(__FILE__).'/resultset.class.php';
  */
 class ArrayResultSet extends ResultSet
 {
+	/**
+	 * The data in this set
+	 */
 	private $data;
-
+	/**
+	 * A pointer to the current element in the set
+	 */
 	private $pointer;
-
+	/**
+	 * Constructor
+	 * @param array $array
+	 */
 	function ArrayResultSet(& $array)
 	{
 		$this->data = & $array;
 		$this->pointer = 0;
 	}
-
+	// Inherited
 	function next_result()
 	{
 		if ($this->pointer < count($this->data))
@@ -30,17 +38,17 @@ class ArrayResultSet extends ResultSet
 		}
 		return null;
 	}
-
+	// Inherited
 	function as_array()
 	{
 		return $this->data;
 	}
-
+	// Inherited
 	function size()
 	{
 		return count($this->data);
 	}
-
+	// Inherited
 	function skip ($count)
 	{
 		$this->pointer += $count;
