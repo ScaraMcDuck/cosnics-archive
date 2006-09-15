fbb6d8a33973ae9548924ca9cc79cfea23ad20f0
bmol <bmol@localhost>
2006-09-15 09:18:21 +0000

    Renderers to display course homepage

---
+++ dokeos/application/lib/weblcms/toollistrenderer.class.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * $Id$
+ * @package application.weblcms
+ */
+/**
+ * Renderer to display a set of tools
+ */
+abstract class ToolListRenderer
+{
+	/**
+	 * The parent application
+	 */
+	private $parent;
+	/**
+	 * Constructor
+	 * @param WebLcms $parent The parent application
+	 */
+	function ToolListRenderer($parent)
+	{
+		$this->parent = $parent;
+	}
+	/**
+	 * Create a new tool list renderer
+	 * @param string $class The implementation of this abstract class to load
+	 * @param WebLcms $parent The parent application
+	 */
+	static function factory($class,$parent)
+	{
+		require_once(dirname(__FILE__).'/tool_list_renderer/'.strtolower($class).'.class.php');
+		return new $class($parent);
+	}
+	/**
+	 * Gets the parent application
+	 * @return WebLcms
+	 */
+	function get_parent()
+	{
+		return $this->parent;
+	}
+	/**
+	 * Displays the tool list.
+	 */
+	abstract function display();
+}
+?>
