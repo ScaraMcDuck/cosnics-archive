8553d6a46db12413518e8569903816a7f7d37ae4
evie_em <evie_em@localhost>
2006-11-07 13:36:17 +0000

    renamed 'claroline' folder to 'main'

--- dokeos/claroline/lang/greek/coursebackup.inc.php
+++
@@ -1,8 +0,0 @@
-<?php /*
-for more information: see languages.txt in the lang folder. 
-*/
-$langAnnouncements = "Ανακοινώσεις";
-$langLinks = "Σύνδεσμοι";
-$langForums = "Περιοχή συζητήσεων";
-$langCourseDescription = "περιγραφή στο κατάλογο μαθημάτων";
-?>
