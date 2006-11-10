c0b467b09474a99e8c2d61276056fc095d348e24
evie_em <evie_em@localhost>
2006-11-10 13:20:11 +0000

    refactoring database installation code - broken now, has to be structured for quickform controller/action input

--- dokeos/main/install/install_db.inc.php
+++ dokeos/main/install/install_db.inc.php
@@ -35,9 +35,264 @@ require_once("install_upgrade.lib.php");
 
 /*
 ==============================================================================
+		FUNCTIONS
+==============================================================================
+*/
+
+
+/**
+* creating the tables of the main database
+*/
+function create_main_database_tables($main_database)
+{
+	mysql_select_db($main_database) or die(mysql_error());
+	
+	$installation_settings['{ORGANISATIONNAME}'] = $institutionForm;
+	$installation_settings['{ORGANISATIONURL}'] = $institutionUrlForm;
+	$installation_settings['{CAMPUSNAME}'] = $campusForm;
+	$installation_settings['{PLATFORMLANGUAGE}'] = $languageForm;
+	$installation_settings['{ALLOWSELFREGISTRATION}'] = trueFalse($allowSelfReg);
+	$installation_settings['{ALLOWTEACHERSELFREGISTRATION}'] = trueFalse($allowSelfRegProf);
+	$installation_settings['{ADMINLASTNAME}'] = $adminLastName;
+	$installation_settings['{ADMINFIRSTNAME}'] = $adminFirstName;
+	$installation_settings['{ADMINLOGIN}'] = $loginForm;
+	$installation_settings['{ADMINPASSWORD}'] = $passToStore;
+	$installation_settings['{ADMINEMAIL}'] = $emailForm;
+	$installation_settings['{ADMINPHONE}'] = $adminPhoneForm;
+	$installation_settings['{PLATFORM_AUTH_SOURCE}'] = PLATFORM_AUTH_SOURCE;
+	
+	load_main_database($installation_settings);
+}
+
+/**
+* creating the tables of the tracking database
+*/
+function create_tracking_database_tables($statistics_database)
+{
+	mysql_select_db($statistics_database) or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_c_browsers` (
+				`id` int(11) NOT NULL auto_increment,
+				`browser` varchar(255) NOT NULL default '',
+				`counter` int(11) NOT NULL default '0',
+				PRIMARY KEY  (`id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_c_countries` (
+				`id` int(11) NOT NULL auto_increment,
+				`code` varchar(40) NOT NULL default '',
+				`country` varchar(50) NOT NULL default '',
+				`counter` int(11) NOT NULL default '0',
+				PRIMARY KEY  (`id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_c_os` (
+				`id` int(11) NOT NULL auto_increment,
+				`os` varchar(255) NOT NULL default '',
+				`counter` int(11) NOT NULL default '0',
+				PRIMARY KEY  (`id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_c_providers` (
+				`id` int(11) NOT NULL auto_increment,
+				`provider` varchar(255) NOT NULL default '',
+				`counter` int(11) NOT NULL default '0',
+				PRIMARY KEY  (`id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_c_referers` (
+				`id` int(11) NOT NULL auto_increment,
+				`referer` varchar(255) NOT NULL default '',
+				`counter` int(11) NOT NULL default '0',
+				PRIMARY KEY  (`id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_access` (
+				`access_id` int(11) NOT NULL auto_increment,
+				`access_user_id` int unsigned default NULL,
+				`access_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				`access_cours_code` varchar(40) NOT NULL default '0',
+				`access_tool` varchar(30) default NULL,
+				PRIMARY KEY  (`access_id`),
+				KEY `access_user_id` (`access_user_id`),
+				KEY `access_cours_code` (`access_cours_code`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_lastaccess` (
+				`access_id` bigint(20) NOT NULL auto_increment,
+				`access_user_id` int unsigned default NULL,
+				`access_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				`access_cours_code` varchar(40) NOT NULL default '0',
+				`access_tool` varchar(30) default NULL,
+				PRIMARY KEY  (`access_id`),
+				KEY `access_user_id` (`access_user_id`),
+				KEY `access_cours_code` (`access_cours_code`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_default` (
+				`default_id` int(11) NOT NULL auto_increment,
+				`default_user_id` int unsigned NOT NULL default '0',
+				`default_cours_code` varchar(40) NOT NULL default '',
+				`default_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				`default_event_type` varchar(20) NOT NULL default '',
+				`default_value_type` varchar(20) NOT NULL default '',
+				`default_value` tinytext NOT NULL,
+				PRIMARY KEY  (`default_id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_downloads` (
+				`down_id` int(11) NOT NULL auto_increment,
+				`down_user_id` int unsigned default NULL,
+				`down_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				`down_cours_id` varchar(20) NOT NULL default '0',
+				`down_doc_path` varchar(255) NOT NULL default '0',
+				PRIMARY KEY  (`down_id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_exercices` (
+				`exe_id` int(11) NOT NULL auto_increment,
+				`exe_user_id` int unsigned default NULL,
+				`exe_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				`exe_cours_id` varchar(40) NOT NULL default '',
+				`exe_exo_id` mediumint unsigned NOT NULL default '0',
+				`exe_result` smallint NOT NULL default '0',
+				`exe_weighting` smallint NOT NULL default '0',
+				PRIMARY KEY  (`exe_id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_hotpotatoes` (
+				`exe_name` VARCHAR( 255 ) NOT NULL ,
+				`exe_user_id` int unsigned DEFAULT NULL ,
+				`exe_date` DATETIME DEFAULT '0000-00-00 00:00:00' NOT NULL ,
+				`exe_cours_id` varchar(40) NOT NULL ,
+				`exe_result` smallint DEFAULT '0' NOT NULL ,
+				`exe_weighting` smallint DEFAULT '0' NOT NULL
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_links` (
+				`links_id` int NOT NULL auto_increment,
+				`links_user_id` int unsigned default NULL,
+				`links_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				`links_cours_id` varchar(20) NOT NULL default '0',
+				`links_link_id` int(11) NOT NULL default '0',
+				PRIMARY KEY  (`links_id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_login` (
+				`login_id` int NOT NULL auto_increment,
+				`login_user_id` int unsigned NOT NULL default '0',
+				`login_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				`login_ip` varchar(39) NOT NULL default '',
+				PRIMARY KEY  (`login_id`),
+				KEY `login_user_id` (`login_user_id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_online` (
+				`login_id` int NOT NULL auto_increment,
+				`login_user_id` int unsigned NOT NULL default '0',
+				`login_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				`login_ip` varchar(39) NOT NULL default '',
+				`course` varchar(40) default NULL, 
+				PRIMARY KEY  (`login_id`),
+				KEY `login_user_id` (`login_user_id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_open` (
+				`open_id` int(11) NOT NULL auto_increment,
+				`open_remote_host` tinytext NOT NULL,
+				`open_agent` tinytext NOT NULL,
+				`open_referer` tinytext NOT NULL,
+				`open_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				PRIMARY KEY  (`open_id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `track_e_uploads` (
+				`upload_id` int NOT NULL auto_increment,
+				`upload_user_id` int unsigned default NULL,
+				`upload_date` datetime NOT NULL default '0000-00-00 00:00:00',
+				`upload_cours_id` varchar(20) NOT NULL default '0',
+				`upload_work_id` int(11) NOT NULL default '0',
+				PRIMARY KEY  (`upload_id`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	$track_countries_table = "track_c_countries";
+	fill_track_countries_table($track_countries_table);
+}
+
+/**
+* creating the tables of the SCORM database
+*/
+function create_scorm_database_tables($scorm_database)
+{
+	mysql_select_db($scorm_database) or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `scorm_main` (
+				`contentId` int(5) unsigned NOT NULL auto_increment,
+				`contentTitle` varchar(100) default NULL,
+				`dokeosCourse` varchar(100) default NULL,
+				PRIMARY KEY  (`contentId`)
+				) TYPE=MyISAM") or die(mysql_error());
+	
+	mysql_query("CREATE TABLE `scorm_sco_data` (
+				`contentId` int(5) default NULL,
+				`scoId` int(5) NOT NULL auto_increment,
+				`scoIdentifier` varchar(100) default NULL,
+				`scoTitle` varchar(100) default NULL,
+				`status` varchar(100) default NULL,
+				`studentId` int(10) default NULL,
+				`score` int(10) default NULL,
+				`time` varchar(20) default NULL,
+				KEY `scoId` (`scoId`)
+				) TYPE=MyISAM") or die(mysql_error());
+}
+
+/**
+* creating the tables of the USER database
+* this database stores
+* - the personal agenda items are stored
+* - the user defined course categories (sorting of my courses)
+*/
+function create_user_database_tables($user_database)
+{
+	mysql_select_db($user_database) or die(mysql_error());
+	
+	// creating the table where the personal agenda items are stored
+	mysql_query("CREATE TABLE `personal_agenda` (
+				`id` int NOT NULL auto_increment,
+				`user` int unsigned,
+				`title` text,
+				`text` text,
+				`date` datetime default NULL,
+				`enddate` datetime default NULL,
+				`course` varchar(255),
+				UNIQUE KEY `id` (`id`))
+				TYPE=MyISAM") or die(mysql_error());
+	
+	// creating the table that is used for the user defined course categories
+	mysql_query("CREATE TABLE `user_course_category` (
+				`id` int unsigned NOT NULL auto_increment,
+				`user_id` int unsigned NOT NULL default '0',
+				`title` text NOT NULL,
+				PRIMARY KEY  (`id`)
+				) TYPE=MyISAM") or die(mysql_error());
+}
+
+/**
+* Create all tables of the default databases.
+*/
+function full_install($main_database, $statistics_database, $scorm_database, $user_database)
+{
+	create_main_database_tables($main_database);
+	create_tracking_database_tables($statistics_database);
+	create_scorm_database_tables($scorm_database);
+	create_user_database_tables($user_database);
+}
+
+/*
+==============================================================================
 		MAIN CODE
 ==============================================================================
-*/ 
+*/
 
 //this page can only be access through including from the install script.
 
@@ -212,229 +467,5 @@ if($languageForm != 'english')
 	include("../lang/$languageForm/create_course.inc.php");
 }
 
-/**
-* creating the tables of the main database
-*/
-mysql_select_db($mysqlMainDb) or die(mysql_error());
-
-$installation_settings['{ORGANISATIONNAME}'] = $institutionForm;
-$installation_settings['{ORGANISATIONURL}'] = $institutionUrlForm;
-$installation_settings['{CAMPUSNAME}'] = $campusForm;
-$installation_settings['{PLATFORMLANGUAGE}'] = $languageForm;
-$installation_settings['{ALLOWSELFREGISTRATION}'] = trueFalse($allowSelfReg);
-$installation_settings['{ALLOWTEACHERSELFREGISTRATION}'] = trueFalse($allowSelfRegProf);
-$installation_settings['{ADMINLASTNAME}'] = $adminLastName;
-$installation_settings['{ADMINFIRSTNAME}'] = $adminFirstName;
-$installation_settings['{ADMINLOGIN}'] = $loginForm;
-$installation_settings['{ADMINPASSWORD}'] = $passToStore;
-$installation_settings['{ADMINEMAIL}'] = $emailForm;
-$installation_settings['{ADMINPHONE}'] = $adminPhoneForm;
-$installation_settings['{PLATFORM_AUTH_SOURCE}'] = PLATFORM_AUTH_SOURCE;
-
-load_main_database($installation_settings);
-
-/**
-* creating the tables of the tracking database
-*/
-
-mysql_select_db($mysqlStatsDb) or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_c_browsers` (
-			`id` int(11) NOT NULL auto_increment,
-			`browser` varchar(255) NOT NULL default '',
-			`counter` int(11) NOT NULL default '0',
-			PRIMARY KEY  (`id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_c_countries` (
-			`id` int(11) NOT NULL auto_increment,
-			`code` varchar(40) NOT NULL default '',
-			`country` varchar(50) NOT NULL default '',
-			`counter` int(11) NOT NULL default '0',
-			PRIMARY KEY  (`id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_c_os` (
-			`id` int(11) NOT NULL auto_increment,
-			`os` varchar(255) NOT NULL default '',
-			`counter` int(11) NOT NULL default '0',
-			PRIMARY KEY  (`id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_c_providers` (
-			`id` int(11) NOT NULL auto_increment,
-			`provider` varchar(255) NOT NULL default '',
-			`counter` int(11) NOT NULL default '0',
-			PRIMARY KEY  (`id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_c_referers` (
-			`id` int(11) NOT NULL auto_increment,
-			`referer` varchar(255) NOT NULL default '',
-			`counter` int(11) NOT NULL default '0',
-			PRIMARY KEY  (`id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_access` (
-			`access_id` int(11) NOT NULL auto_increment,
-			`access_user_id` int unsigned default NULL,
-			`access_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			`access_cours_code` varchar(40) NOT NULL default '0',
-			`access_tool` varchar(30) default NULL,
-			PRIMARY KEY  (`access_id`),
-			KEY `access_user_id` (`access_user_id`),
-			KEY `access_cours_code` (`access_cours_code`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_lastaccess` (
-			`access_id` bigint(20) NOT NULL auto_increment,
-			`access_user_id` int unsigned default NULL,
-			`access_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			`access_cours_code` varchar(40) NOT NULL default '0',
-			`access_tool` varchar(30) default NULL,
-			PRIMARY KEY  (`access_id`),
-			KEY `access_user_id` (`access_user_id`),
-			KEY `access_cours_code` (`access_cours_code`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_default` (
-			`default_id` int(11) NOT NULL auto_increment,
-			`default_user_id` int unsigned NOT NULL default '0',
-			`default_cours_code` varchar(40) NOT NULL default '',
-			`default_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			`default_event_type` varchar(20) NOT NULL default '',
-			`default_value_type` varchar(20) NOT NULL default '',
-			`default_value` tinytext NOT NULL,
-			PRIMARY KEY  (`default_id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_downloads` (
-			`down_id` int(11) NOT NULL auto_increment,
-			`down_user_id` int unsigned default NULL,
-			`down_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			`down_cours_id` varchar(20) NOT NULL default '0',
-			`down_doc_path` varchar(255) NOT NULL default '0',
-			PRIMARY KEY  (`down_id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_exercices` (
-			`exe_id` int(11) NOT NULL auto_increment,
-			`exe_user_id` int unsigned default NULL,
-			`exe_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			`exe_cours_id` varchar(40) NOT NULL default '',
-			`exe_exo_id` mediumint unsigned NOT NULL default '0',
-			`exe_result` smallint NOT NULL default '0',
-			`exe_weighting` smallint NOT NULL default '0',
-			PRIMARY KEY  (`exe_id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_hotpotatoes` (
-			`exe_name` VARCHAR( 255 ) NOT NULL ,
-			`exe_user_id` int unsigned DEFAULT NULL ,
-			`exe_date` DATETIME DEFAULT '0000-00-00 00:00:00' NOT NULL ,
-			`exe_cours_id` varchar(40) NOT NULL ,
-			`exe_result` smallint DEFAULT '0' NOT NULL ,
-			`exe_weighting` smallint DEFAULT '0' NOT NULL
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_links` (
-			`links_id` int NOT NULL auto_increment,
-			`links_user_id` int unsigned default NULL,
-			`links_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			`links_cours_id` varchar(20) NOT NULL default '0',
-			`links_link_id` int(11) NOT NULL default '0',
-			PRIMARY KEY  (`links_id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_login` (
-			`login_id` int NOT NULL auto_increment,
-			`login_user_id` int unsigned NOT NULL default '0',
-			`login_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			`login_ip` varchar(39) NOT NULL default '',
-			PRIMARY KEY  (`login_id`),
-			KEY `login_user_id` (`login_user_id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_online` (
-			`login_id` int NOT NULL auto_increment,
-			`login_user_id` int unsigned NOT NULL default '0',
-			`login_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			`login_ip` varchar(39) NOT NULL default '',
-			`course` varchar(40) default NULL, 
-			PRIMARY KEY  (`login_id`),
-			KEY `login_user_id` (`login_user_id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_open` (
-			`open_id` int(11) NOT NULL auto_increment,
-			`open_remote_host` tinytext NOT NULL,
-			`open_agent` tinytext NOT NULL,
-			`open_referer` tinytext NOT NULL,
-			`open_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			PRIMARY KEY  (`open_id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `track_e_uploads` (
-			`upload_id` int NOT NULL auto_increment,
-			`upload_user_id` int unsigned default NULL,
-			`upload_date` datetime NOT NULL default '0000-00-00 00:00:00',
-			`upload_cours_id` varchar(20) NOT NULL default '0',
-			`upload_work_id` int(11) NOT NULL default '0',
-			PRIMARY KEY  (`upload_id`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-$track_countries_table = "track_c_countries";
-fill_track_countries_table($track_countries_table);
-
-/**
-* creating the tables of the SCORM database
-*/
-
-mysql_select_db($mysqlScormDb) or die(mysql_error());
-
-mysql_query("CREATE TABLE `scorm_main` (
-			`contentId` int(5) unsigned NOT NULL auto_increment,
-			`contentTitle` varchar(100) default NULL,
-			`dokeosCourse` varchar(100) default NULL,
-			PRIMARY KEY  (`contentId`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-mysql_query("CREATE TABLE `scorm_sco_data` (
-			`contentId` int(5) default NULL,
-			`scoId` int(5) NOT NULL auto_increment,
-			`scoIdentifier` varchar(100) default NULL,
-			`scoTitle` varchar(100) default NULL,
-			`status` varchar(100) default NULL,
-			`studentId` int(10) default NULL,
-			`score` int(10) default NULL,
-			`time` varchar(20) default NULL,
-			KEY `scoId` (`scoId`)
-			) TYPE=MyISAM") or die(mysql_error());
-
-/**
-* creating the tables of the USER database
-* this is where the personal agenda items are storen, the user defined course categories (sorting of my courses)
-*/
-
-mysql_select_db($mysqlUserDb) or die(mysql_error());
-
-// creating the table where the personal agenda items are stored
-mysql_query("CREATE TABLE `personal_agenda` (
-			`id` int NOT NULL auto_increment,
-			`user` int unsigned,
-			`title` text,
-			`text` text,
-			`date` datetime default NULL,
-			`enddate` datetime default NULL,
-			`course` varchar(255),
-			UNIQUE KEY `id` (`id`))
-			TYPE=MyISAM") or die(mysql_error());
-
-// creating the table that is used for the user defined course categories
-mysql_query("CREATE TABLE `user_course_category` (
-			`id` int unsigned NOT NULL auto_increment,
-			`user_id` int unsigned NOT NULL default '0',
-			`title` text NOT NULL,
-			PRIMARY KEY  (`id`)
-			) TYPE=MyISAM") or die(mysql_error());
+full_install($mysqlMainDb, $mysqlStatsDb, $mysqlScormDb, $mysqlUserDb);
 ?>
