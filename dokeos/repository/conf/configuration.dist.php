02f06ac92624fd9cc8bd7ae930d637058b2db6c3
bmol <bmol@cosnics>
2006-12-06 11:56:55 +0000

    template for repository configuration

---
+++ dokeos/repository/conf/configuration.dist.php
@@ -0,0 +1,26 @@
+<?php
+/**
+==============================================================================
+ * This is the configuration file. You'll probably want to modify the values.
+ * @package repository
+==============================================================================
+ */
+
+$configuration = array();
+
+$configuration['general']['data_manager']
+	= 'Database';
+
+$configuration['database']['connection_string']
+	= 'mysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_REPOSITORY}';
+
+$configuration['database']['table_name_prefix']
+	= 'dokeos_';
+
+$configuration['general']['upload_path']
+	= dirname(__FILE__).'/../../main/upload';
+
+$configuration['general']['upload_url']
+	= api_get_path(WEB_PATH).'main/upload';
+
+?>
