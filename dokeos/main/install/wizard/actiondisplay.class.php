405ab536dd2c6e898fdcc29967b46dfe6edd8768
bmol <bmol@cosnics>
2006-12-08 09:57:30 +0000

    - show overview of all steps - show current step number - style

--- dokeos/main/install/wizard/actiondisplay.class.php
+++ dokeos/main/install/wizard/actiondisplay.class.php
@@ -12,17 +12,17 @@ class ActionDisplay extends HTML_QuickForm_Action_Display
 	 * Displays the HTML-code of a page in the wizard
 	 * @param HTML_Quickform_Page $page The page to display.
 	 */
-	function _renderForm(& $page)
+	function _renderForm(& $current_page)
 	{
 		global $dokeos_version, $installType, $updateFromVersion;
-		$renderer = & $page->defaultRenderer();
-		$page->setRequiredNote('<font color="#FF0000">*</font> '.get_lang('ThisFieldIsRequired'));
+		$renderer = & $current_page->defaultRenderer();
+		$current_page->setRequiredNote('<font color="#FF0000">*</font> '.get_lang('ThisFieldIsRequired'));
 		$element_template = "\n\t<tr>\n\t\t<td align=\"right\" valign=\"top\"><!-- BEGIN required --><span style=\"color: #ff0000\">*</span> <!-- END required -->{label}</td>\n\t\t<td valign=\"top\" align=\"left\"><!-- BEGIN error --><span style=\"color: #ff0000;font-size:x-small;margin:2px;\">{error}</span><br /><!-- END error -->\t{element}</td>\n\t</tr>";
 		$renderer->setElementTemplate($element_template);
 		$header_template = "\n\t<tr>\n\t\t<td align=\"left\" valign=\"top\" colspan=\"2\">{header}</td>\n\t</tr>";
 		$renderer->setHeaderTemplate($header_template);
 		HTML_QuickForm :: setRequiredNote('<font color="red">*</font> <small>'.get_lang('ThisFieldIsRequired').'</small>');
-		$page->accept($renderer);
+		$current_page->accept($renderer);
 ?>
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
@@ -34,14 +34,34 @@ class ActionDisplay extends HTML_QuickForm_Action_Display
 		<div style="background-color:#4171B5;color:white;font-size:x-large;">
 			Dokeos installation - version <?php echo $dokeos_version; ?><?php if($installType == 'new') echo ' - New installation'; else if($installType == 'update') echo ' - Update from Dokeos '.implode('|',$updateFromVersion); ?>
 		</div>
-		<div style="margin:50px;">
-			<img src="../img/bluelogo.gif" alt="logo" align="right"/>
+		<div style="float: left; background-color:#EFEFEF;margin-right: 20px;padding: 10px;">
+			<img src="../img/bluelogo.gif" alt="logo"/>
 			<?php
 
-
-		echo '<h2>'.$page->get_title().'</h2>';
+		$all_pages = $current_page->controller->_pages;
+		$total_number_of_pages = count($all_pages);
+		$current_page_number = 0;
+		$page_number = 0;
+		echo '<ol>';
+		foreach($all_pages as $index => $page)
+		{
+			$page_number++;
+			if($page->get_title() == $current_page->get_title())
+			{
+				$current_page_number = $page_number;
+				echo '<li style="font-weight: bold;">'.$page->get_title().'</li>';
+			}
+			else
+			{
+				echo '<li>'.$page->get_title().'</li>';
+			}
+		}
+		echo '</ol>';
+		echo '</div>';
+		echo '<div style="margin: 10px;">';
+		echo '<h2>'.get_lang('Step').' '.$current_page_number.' '.get_lang('of').' '.$total_number_of_pages.' &ndash; '.$current_page->get_title().'</h2>';
 		echo '<div>';
-		echo $page->get_info();
+		echo $current_page->get_info();
 		echo '</div>';
 		echo $renderer->toHtml();
 		?>
