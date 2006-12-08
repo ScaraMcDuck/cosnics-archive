7801e0972ad82cfba383e9ffea763e5fa9ee0c62
bmol <bmol@cosnics>
2006-12-08 09:11:24 +0000

    using some langVars

--- dokeos/main/install/wizard/page_language.class.php
+++ dokeos/main/install/wizard/page_language.class.php
@@ -11,7 +11,7 @@ class Page_Language extends HTML_QuickForm_Page
 {
 	function get_title()
 	{
-		return 'Welcome to the Dokeos installer!';
+		return get_lang('WelcomeToDokeosInstaller');
 	}
 	function get_info()
 	{
@@ -20,7 +20,7 @@ class Page_Language extends HTML_QuickForm_Page
 	function buildForm()
 	{
 		$this->_formBuilt = true;
-		$this->addElement('select', 'install_language', 'Language', get_language_folder_list());
+		$this->addElement('select', 'install_language', get_lang('InstallationLanguage'), get_language_folder_list());
 		$buttons[0] = & HTML_QuickForm :: createElement('submit', $this->getButtonName('next'), get_lang('Next').' >>');
 		$this->addGroup($buttons, 'buttons', '', '&nbsp', false);
 		$this->setDefaultAction('next');
