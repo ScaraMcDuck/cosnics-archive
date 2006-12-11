55a2cd8a12bef5f8d45ec058727e4020ea5b75f2
bmol <bmol@localhost>
2006-12-11 11:53:32 +0000

    Remove old SQL-files (no longer needed)

--- dokeos/repository/lib/learning_object/forum_post/forum_post.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `%prefix%forum_post` (
-  `id` int(10) unsigned NOT NULL default '0',
-  `parent_post` int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-)CHARACTER SET utf8 COLLATE utf8_unicode_ci;
--- dokeos/repository/lib/learning_object/link/link.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `%prefix%link` (
-  `id` int(10) unsigned NOT NULL default '0',
-  `url` text NOT NULL,
-  PRIMARY KEY  (`id`)
-) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
