cb81d5623c0843569194db5b76ffe79a1df9f031
bmol <bmol@cosnics>
2006-12-11 11:53:32 +0000

    Remove old SQL-files (no longer needed)

--- dokeos/repository/lib/learning_object/calendar_event/calendar_event.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `%prefix%calendar_event` (
-  `id` int(10) unsigned NOT NULL default '0',
-  `start_date` int(10) unsigned NOT NULL default '0',
-  `end_date` int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-)CHARACTER SET utf8 COLLATE utf8_unicode_ci;
--- dokeos/repository/lib/learning_object/document/document.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `%prefix%document` (
-  `id` int(10) unsigned NOT NULL default '0',
-  `path` text NOT NULL,
-  `filename` text NOT NULL,
-  `filesize` int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-)CHARACTER SET utf8 COLLATE utf8_unicode_ci;
