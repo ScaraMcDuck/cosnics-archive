1d55f53a8903d38d14fac8f4a40157a1206e8f78
bmol <bmol@localhost>
2006-12-12 08:56:03 +0000

    use icon name

--- dokeos/repository/lib/learningobjectdisplay.class.php
+++ dokeos/repository/lib/learningobjectdisplay.class.php
@@ -68,7 +68,7 @@ abstract class LearningObjectDisplay
 	{
 		$object = $this->get_learning_object();
 		$html = array();
-		$html[] = '<div class="learning_object" style="background-image: url('.api_get_path(WEB_CODE_PATH).'img/'.$object->get_type().'.gif);">';
+		$html[] = '<div class="learning_object" style="background-image: url('.api_get_path(WEB_CODE_PATH).'img/'.$object->get_icon_name().'.gif);">';
 		$html[] = '<div class="title">'.htmlspecialchars($object->get_title()).'</div>';
 		$html[] = '<div class="description">'.$object->get_description().'</div>';
 		$html[] = '</div>';
