f20d9b5d2e9113727d56f2e2bc0ba1725beb97ed
bmol <bmol@localhost>
2006-12-12 10:21:58 +0000

    date i8n

--- dokeos/index_repository_manager.php
+++ dokeos/index_repository_manager.php
@@ -1,8 +1,9 @@
 <?php
 $cidReset = true;
-$langFile = 'repository';
+$langFile = array('repository','agenda');
 $this_section = 'myrepository';
 require_once dirname(__FILE__).'/main/inc/claro_init_global.inc.php';
+require_once dirname(__FILE__).'/main/inc/lib/text.lib.php';
 require_once dirname(__FILE__).'/repository/lib/repository_manager/repositorymanager.class.php';
 
 if (!api_get_user_id())
--- dokeos/repository/lib/learning_object/calendar_event/calendar_event_display.class.php
+++ dokeos/repository/lib/learning_object/calendar_event/calendar_event_display.class.php
@@ -14,7 +14,8 @@ class CalendarEventDisplay extends LearningObjectDisplay
 	{
 		$html = parent :: get_full_html();
 		$object = $this->get_learning_object();
-		$append = '<div class="calendar_event_range" style="margin-top: 1em;"">'.htmlentities(get_lang('From').' '.date('r',$object->get_start_date()).' '.get_lang('Until').' '.date('r',$object->get_end_date())).'</div>';
+		$date_format = get_lang('dateTimeFormatLong');
+		$append = '<div class="calendar_event_range" style="margin-top: 1em;"">'.htmlentities(get_lang('From').' '.format_locale_date($date_format,$object->get_start_date()).' '.get_lang('Until').' '.format_locale_date($date_format,$object->get_end_date())).'</div>';
 		return preg_replace('|</div>\s*$|s', $append.'</div>', $html);
 	}
 }
