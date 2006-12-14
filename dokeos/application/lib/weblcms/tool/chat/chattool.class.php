568bde67b64b5dabdbfef5393653451ee59e2b79
bmol <bmol@localhost>
2006-12-14 09:18:19 +0000

    Adding chat tool

---
+++ dokeos/application/lib/weblcms/tool/chat/chattool.class.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * $Id: wikitool.class.php 9206 2006-09-05 10:12:59Z bmol $
+ * Chat tool
+ * @package application.weblcms.tool
+ * @subpackage chat
+ */
+require_once dirname(__FILE__).'/../repositorytool.class.php';
+require_once dirname(__FILE__).'/chatbrowser.class.php';
+/**
+ * This tool allows a user to publish chatboxes in his or her course.
+ */
+class ChatTool extends RepositoryTool
+{
+	// Inherited.
+	function run()
+	{
+		if(!$this->is_allowed(VIEW_RIGHT))
+		{
+			$this->display_header();
+			api_not_allowed();
+			$this->display_footer();
+			return;
+		}
+		if (isset($_GET['admin']))
+		{
+			$_SESSION['chatadmin'] = $_GET['admin'];
+		}
+		if ($_SESSION['chatadmin'] && $this->is_allowed(ADD_RIGHT))
+		{
+			require_once dirname(__FILE__).'/../../learningobjectpublisher.class.php';
+			$pub = new LearningObjectPublisher($this, 'chatbox');
+			$html[] = '<p><a href="' . $this->get_url(array('admin' => 0), true) . '"><img src="'.api_get_path(WEB_CODE_PATH).'/img/browser.gif" alt="'.get_lang('BrowserTitle').'" style="vertical-align:middle;"/> '.get_lang('BrowserTitle').'</a></p>';
+			$html[] =  $pub->as_html();
+			$this->display_header();
+			echo implode("\n",$html);
+			$this->display_footer();
+		}
+		else
+		{
+			$this->display_header();
+			if($this->is_allowed(ADD_RIGHT))
+			{
+				echo '<p><a href="' . $this->get_url(array('admin' => 1), true) . '"><img src="'.api_get_path(WEB_CODE_PATH).'/img/publish.gif" alt="'.get_lang('Publish').'" style="vertical-align:middle;"/> '.get_lang('Publish').'</a></p>';
+			}
+			echo $this->perform_requested_actions();
+			$browser = new ChatBrowser($this);
+			echo $browser->as_html();
+			$this->display_footer();
+		}
+	}
+}
+?>
