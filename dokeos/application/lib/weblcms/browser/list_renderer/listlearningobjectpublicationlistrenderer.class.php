4278a05e999ed250c80dfbe2e194da4e06e90b35
bmol <bmol@localhost>
2006-12-14 09:28:01 +0000

    use _na icon when publication is hidden

--- dokeos/application/lib/weblcms/browser/list_renderer/listlearningobjectpublicationlistrenderer.class.php
+++ dokeos/application/lib/weblcms/browser/list_renderer/listlearningobjectpublicationlistrenderer.class.php
@@ -39,12 +39,16 @@ class ListLearningObjectPublicationListRenderer extends LearningObjectPublicatio
 	{
 		$html = array ();
 		$last_visit_date = $this->browser->get_last_visit_date();
-		$new = '';
-		if( $publication->get_publication_date() >= $last_visit_date)
+		$icon_suffix = '';
+		if($publication->is_hidden())
 		{
-			$new = '_new';
+			$icon_suffix = '_na';
 		}
-		$html[] = '<div class="learning_object" style="background-image: url('.api_get_path(WEB_CODE_PATH).'img/'.$publication->get_learning_object()->get_type().$new.'.gif);">';
+		elseif( $publication->get_publication_date() >= $last_visit_date)
+		{
+			$icon_suffix = '_new';
+		}
+		$html[] = '<div class="learning_object" style="background-image: url('.api_get_path(WEB_CODE_PATH).'img/'.$publication->get_learning_object()->get_icon_name().$icon_suffix.'.gif);">';
 		$html[] = '<div class="title'. ($publication->is_visible_for_target_users() ? '' : ' invisible').'">';
 		$html[] = $this->render_title($publication);
 		$html[] = '</div>';
