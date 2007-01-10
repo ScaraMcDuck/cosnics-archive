36808846e90c2ef1d0cdd7a7384935657c3e2cf1
bmol <bmol@cosnics>
2007-01-10 12:19:36 +0000

    Renamed DateTime to IeeeLomDateTime to avoid conflicts with other defined DateTime classes

---
+++ dokeos/repository/lib/metadata/ieee_lom/ieee_lom_datetime.class.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * $Id: ieee_lom_datetime.class.php 9169 2006-08-29 13:01:29Z bmol $
+ * @package repository.metadata
+ * @subpackage ieee_lom
+ */
+/**
+ * A DateTime field used in IEEE LOM. This object contains a date & time value
+ * and a description
+ */
+class IeeeLomDateTime {
+	/**
+	 * The date & time value
+	 */
+	private $datetime;
+	/**
+	 * The description
+	 */
+	private $description;
+	/**
+	 * Constructor
+	 * @param string|null $datetime
+	 * @param LangString|null $description
+	 */
+    function IeeeLomDateTime($datetime = null,$description = null) {
+    	$this->datetime = $datetime;
+    	$this->description = $description;
+    }
+    /**
+     * Gets the date & time value
+     * @return string|null
+     */
+    function get_datetime()
+    {
+    	return $this->datetime;
+    }
+    /**
+     * Gets the description
+     * @return LangString|null
+     */
+    function get_description()
+    {
+    	return $this->description;
+    }
+}
+?>
