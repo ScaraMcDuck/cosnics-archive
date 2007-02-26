70023088b1b7520037013a1387cf879ee57c1eb6
bmol <bmol@cosnics>
2007-02-26 15:35:29 +0000

    Display of mathing questions

--- dokeos/repository/lib/learning_object/matching_question/matching_question_display.class.php
+++ dokeos/repository/lib/learning_object/matching_question/matching_question_display.class.php
@@ -14,12 +14,19 @@ class MatchingQuestionDisplay extends LearningObjectDisplay
 	{
 		$html = parent::get_description();
 		$options = $this->get_learning_object()->get_options();
-		$html .= '<ul>';
+		$html .= '<table><tr><td><ol>';
 		foreach($options as $index => $option)
 		{
 			$html .= '<li>'.$option->get_value().'</li>';
 		}
-		$html .= '</ul>';
+		$html .= '</ol></td>';
+		$matches = $this->get_learning_object()->get_matches();
+		$html .= '<td><ol type="A">';
+		foreach($matches as $index => $match)
+		{
+			$html .= '<li>'.$match.'</li>';
+		}
+		$html .= '</ol></td></tr></table>';
 		return $html;
 	}
 }
