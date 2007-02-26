9100e833b4c42d1493f7f3a8f08b7f25b94472c4
bmol <bmol@localhost>
2007-02-26 10:18:41 +0000

    First draft implementation of multiple choice question learning object

---
+++ dokeos/repository/lib/learning_object/multiple_choice_question/multiple_choice_question.class.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(__FILE__).'/../../learningobject.class.php';
+/**
+ * @package repository.learningobject
+ * @subpackage exercise
+ */
+class MultipleChoiceQuestion extends LearningObject
+{
+	const PROPERTY_OPTIONS = 'options';
+}
+?>
