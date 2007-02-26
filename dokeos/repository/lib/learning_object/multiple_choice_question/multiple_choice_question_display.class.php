55b56f8cd4834b588c35eea33d864d9d32379d2d
bmol <bmol@localhost>
2007-02-26 10:36:32 +0000

    First draft implementation of multiple choice question learning object display class

---
+++ dokeos/repository/lib/learning_object/multiple_choice_question/multiple_choice_question_display.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package repository.learningobject
+ * @subpackage exercise
+ */
+class MultipleChoiceQuestionDisplay extends LearningObjectDisplay
+{
+	function get_full_html()
+	{
+		$html = parent :: get_full_html();
+		return $html;
+	}
+}
+?>
