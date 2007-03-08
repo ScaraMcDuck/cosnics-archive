8a284159a36e8a41f27e3760ec985f72602f673c
bmol <bmol@cosnics>
2007-03-08 08:21:01 +0000

    Adding some phpdoc

--- dokeos/application/lib/weblcms/tool/statistics/datarenderer/barchartdatarenderer.class.php
+++ dokeos/application/lib/weblcms/tool/statistics/datarenderer/barchartdatarenderer.class.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * $Id: statisticstool.class.php 10644 2007-01-10 13:13:43Z bmol $
+ * Statistics tool: Bar chart renderer
+ * @package application.weblcms.tool
+ * @subpackage statistics
+ */
 require_once dirname(__FILE__).'/../datarenderer.class.php';
 class BarChartDataRenderer extends DataRenderer
 {
