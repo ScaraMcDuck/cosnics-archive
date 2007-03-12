440ac08b9c05659663a5fd8076b69ab06a1691fc
bmol <bmol@localhost>
2007-03-12 14:03:49 +0000

    Updated wizard workflow

--- dokeos/application/lib/weblcms/tool/maintenance/inc/wizard/maintenancewizarddisplay.class.php
+++ dokeos/application/lib/weblcms/tool/maintenance/inc/wizard/maintenancewizarddisplay.class.php
@@ -13,6 +13,16 @@ class MaintenanceWizardDisplay extends HTML_QuickForm_Action_Display
 	function _renderForm($current_page)
 	{
 		$this->parent->display_header();
+		if(isset($_SESSION['maintenance_message']))
+		{
+			Display::display_normal_message($_SESSION['maintenance_message']);
+			unset($_SESSION['maintenance_message']);
+		}
+		if(isset($_SESSION['maintenance_error_message']))
+		{
+			Display::display_error_message($_SESSION['maintenance_error_message']);
+			unset($_SESSION['maintenance_error_message']);
+		}
 		parent::_renderForm($current_page);
 		$this->parent->display_footer();
 	}
