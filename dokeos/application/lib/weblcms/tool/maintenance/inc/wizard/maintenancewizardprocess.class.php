a38cedbb070ae468dfe8082d70476700fe680a11
bmol <bmol@localhost>
2007-03-13 08:23:01 +0000

    Implemented copy tool

--- dokeos/application/lib/weblcms/tool/maintenance/inc/wizard/maintenancewizardprocess.class.php
+++ dokeos/application/lib/weblcms/tool/maintenance/inc/wizard/maintenancewizardprocess.class.php
@@ -16,6 +16,7 @@ class MaintenanceWizardProcess extends HTML_QuickForm_Action
 	function perform(& $page, $actionName)
 	{
 		$values = $page->controller->exportValues();
+		//Todo: Split this up in several form-processing classes depending on selected action
 		switch ($values['action'])
 		{
 			case ActionSelectionMaintenanceWizardPage :: ACTION_EMPTY :
@@ -41,7 +42,36 @@ class MaintenanceWizardProcess extends HTML_QuickForm_Action
 				}
 				break;
 			case ActionSelectionMaintenanceWizardPage :: ACTION_COPY :
-				$_SESSION['maintenance_error_message'] = 'COPY: TODO';
+				$publication_ids = array_keys($values['publications']);
+				$dm = WeblcmsDataManager :: get_instance();
+				$number_of_publications_to_copy = count($publication_ids);
+				$number_of_copied_publications = 0;
+				foreach ($publication_ids as $index => $id)
+				{
+					$publication = $dm->retrieve_learning_object_publication($id);
+					$courses = $values['course'];
+					foreach($courses as $index => $course_code)
+					{
+						$pub = new LearningObjectPublication(
+							null,
+							$publication->get_learning_object(),
+							$course_code,
+							$publication->get_tool(),
+							0,
+							null,
+							null,
+							$publication->get_from_date(),
+							$publication->get_to_date(),
+							$publication->get_publisher_id(),
+							time(),
+							$publication->is_hidden(),
+							$dm->get_next_learning_object_publication_display_order_index($course_code,$publication->get_tool(),0),
+							false
+						);
+						$pub->create();
+					}
+				}
+				$_SESSION['maintenance_message'] = get_lang('CopyFinished');
 				break;
 			case ActionSelectionMaintenanceWizardPage :: ACTION_BACKUP :
 				$_SESSION['maintenance_error_message'] = 'BACKUP: TODO';
