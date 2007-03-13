bdf9a3c3a2ec27c1b2483b434cfb505cc79aacd8
bmol <bmol@localhost>
2007-03-13 12:18:14 +0000

    Count learning object publications for each tool

--- dokeos/application/lib/weblcms/tool/statistics/statisticstool.class.php
+++ dokeos/application/lib/weblcms/tool/statistics/statisticstool.class.php
@@ -18,7 +18,8 @@ class StatisticsTool extends Tool
 		$parent = $this->get_parent();
 		foreach ($parent->get_registered_tools() as $tool)
 		{
-			$data[htmlspecialchars(get_lang(Tool :: type_to_class($tool->name).'Title'))] = rand(0,100);
+			$number_of_publications = $dm->count_learning_object_publications($this->get_course_id(),null,null,null,new EqualityCondition('tool',$tool->name));
+			$data[htmlspecialchars(get_lang(Tool :: type_to_class($tool->name).'Title'))] = $number_of_publications;
 		}
 		$renderer = new BarChartDataRenderer($this,$data);
 		$renderer->display();
