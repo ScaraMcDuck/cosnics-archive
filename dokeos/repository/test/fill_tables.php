dca174d0662df3b6926e1f46833ea3d4dcc70509
bmol <bmol@localhost>
2007-03-15 08:37:16 +0000

    Protecting this script by only allowing logged in admin

--- dokeos/repository/test/fill_tables.php
+++ dokeos/repository/test/fill_tables.php
@@ -2,247 +2,265 @@
 $langFile = 'repository';
 require_once dirname(__FILE__).'/../../main/inc/claro_init_global.inc.php';
 require_once dirname(__FILE__).'/../lib/repositorydatamanager.class.php';
-
+include_once (api_get_library_path()."/formvalidator/FormValidator.class.php");
+api_protect_admin_script();
+Display :: display_header();
 set_time_limit(0);
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php
-/**
- * This is a simple test script that removes all learning objects from the LCMS
- * data source and fills it with garbage data. For testing purposes only.
- *
- * @author Tim De Pauw
- * @package repository
- */
+
+$form = new FormValidator('create_random');
+$form->addElement('submit','go','go');
 
 
-$users = 2;
-
-$max_categories = array (3,2);
-
-$announcements = rand(2, 10);
-$calendar_events = rand(2, 10);
-$documents = rand(2, 10);
-$links = rand(5, 1);
-$forums = rand(1,5);
-$forum_topics = rand(1,5);
-$forum_posts = rand(6,12);
-$questions_fill_in_blanks = rand(2,10);
-$questions_multiple_choice = rand(2,10);
-
-// TODO
-//$learning_paths = rand(100,500);
-
-$randomTexts[] = 'Lorem ipsum dolor sit amet consectetuer adipiscing elit Cras vel erat Phasellus est Curabitur nunc leo laoreet eu varius sit amet faucibus faucibus magna Quisque venenatis ante quis dictum sodales orci velit molestie';
-$randomTexts[] = '我能吞下 玻璃而 不伤身体';
-$randomTexts[] = 'Μπορώ να φάω σπασμένα γυαλιά χωρίς να πάθω τίποτα.';
-$randomTexts[] = 'काचं शक्नोम्यत्तुम् । नोपहिनस्ति मा';
-$randomTexts[] = 'Jeg kan spise glas det gør ikke ondt på mig';
-$randomTexts[] = 'ღმერთსი შემვედრე, ნუთუ კვლა დამხსნას სოფლისა შრომასა, ცეცხლს, წყალსა და მიწასა, ჰაერთა';
-$randomTexts[] = 'தேமதுரத் தமிழோசை உலகமெலாம் பரவும்வகை செய்தல் வேண்டும';
-$randomTexts[] = 'Я могу есть стекло, оно мне не вредит';
-$words = array();
-foreach($randomTexts as $index => $randomText)
+if (!$form->isSubmitted())
 {
-	$words = array_merge($words,explode(' ', $randomText));
+	Display::display_warning_message('By running this script, all existing learning objects will be deleted and replaced by randomly created learning objects. Only use this for testing purpuses.');
+	echo 'If you\'re sure to continue, click the button below';
+	$form->display();
 }
-$dataManager = RepositoryDataManager :: get_instance();
+else
+{
+	/**
+	 * This is a simple test script that removes all learning objects from the LCMS
+	 * data source and fills it with garbage data. For testing purposes only.
+	 *
+	 * @author Tim De Pauw
+	 * @package repository
+	 */
 
-$dataManager->delete_all_learning_objects();
-title('Categories');
-for ($u = 1; $u <= $users; $u ++)
-{
-	create_category($u);
-}
-title('Announcements');
-for ($i = 0; $i < $announcements; $i ++)
-{
-	$user = random_user();
-	$announcement = new Announcement();
-	$announcement->set_owner_id($user);
-	$announcement->set_title(random_string(2));
-	$announcement->set_description(random_string(8));
-	$announcement->set_parent_id(random_category($user));
-	$announcement->create();
-	for ($j = 0; $j < rand(1,5); $j++)
+	$users = 2;
+
+	$max_categories = array (3, 2);
+
+	$announcements = rand(2, 10);
+	$calendar_events = rand(2, 10);
+	$documents = rand(2, 10);
+	$links = rand(5, 1);
+	$forums = rand(1, 5);
+	$forum_topics = rand(1, 5);
+	$forum_posts = rand(6, 12);
+	$questions_fill_in_blanks = rand(2, 10);
+	$questions_multiple_choice = rand(2, 10);
+
+	// TODO
+	//$learning_paths = rand(100,500);
+
+	$randomTexts[] = 'Lorem ipsum dolor sit amet consectetuer adipiscing elit Cras vel erat Phasellus est Curabitur nunc leo laoreet eu varius sit amet faucibus faucibus magna Quisque venenatis ante quis dictum sodales orci velit molestie';
+	$randomTexts[] = '我能吞下 玻璃而 不伤身体';
+	$randomTexts[] = 'Μπορώ να φάω σπασμένα γυαλιά χωρίς να πάθω τίποτα.';
+	$randomTexts[] = 'काचं शक्नोम्यत्तुम् । नोपहिनस्ति मा';
+	$randomTexts[] = 'Jeg kan spise glas det gør ikke ondt på mig';
+	$randomTexts[] = 'ღმერთსი შემვედრე, ნუთუ კვლა დამხსნას სოფლისა შრომასა, ცეცხლს, წყალსა და მიწასა, ჰაერთა';
+	$randomTexts[] = 'தேமதுரத் தமிழோசை உலகமெலாம் பரவும்வகை செய்தல் வேண்டும';
+	$randomTexts[] = 'Я могу есть стекло, оно мне не вредит';
+	$words = array ();
+	foreach ($randomTexts as $index => $randomText)
 	{
-		$announcementobjectnumber = $announcement->get_object_number();
-		$announcement->set_object_number($announcementobjectnumber);
+		$words = array_merge($words, explode(' ', $randomText));
+	}
+	$dataManager = RepositoryDataManager :: get_instance();
+
+	$dataManager->delete_all_learning_objects();
+	title('Categories');
+	for ($u = 1; $u <= $users; $u ++)
+	{
+		create_category($u);
+	}
+	title('Announcements');
+	for ($i = 0; $i < $announcements; $i ++)
+	{
+		$user = random_user();
+		$announcement = new Announcement();
+		$announcement->set_owner_id($user);
 		$announcement->set_title(random_string(2));
 		$announcement->set_description(random_string(8));
-		$announcement->version();
+		$announcement->set_parent_id(random_category($user));
+		$announcement->create();
+		for ($j = 0; $j < rand(1, 5); $j ++)
+		{
+			$announcementobjectnumber = $announcement->get_object_number();
+			$announcement->set_object_number($announcementobjectnumber);
+			$announcement->set_title(random_string(2));
+			$announcement->set_description(random_string(8));
+			$announcement->version();
+		}
+		progress();
 	}
-	progress();
-}
-title('Calendar Events');
-for ($i = 0; $i < $calendar_events; $i ++)
-{
-	$user = random_user();
-	$event = new CalendarEvent();
-	$event->set_owner_id($user);
-	$event->set_title(random_string(2));
-	$event->set_description(random_string(8));
-	$event->set_parent_id(random_category($user));
-	$start_date = rand(strtotime('-1 Month',time()),strtotime('+1 Month',time()));
-	$end_date = rand($start_date+1,strtotime('+1 Month',$start_date));
-	$event->set_start_date($start_date);
-	$event->set_end_date($end_date);
-	$event->create();
-	for ($j = 0; $j < rand(2,5); $j++)
+	title('Calendar Events');
+	for ($i = 0; $i < $calendar_events; $i ++)
 	{
-		$eventobjectnumber = $event->get_object_number();
-		$event->set_object_number($eventobjectnumber);
+		$user = random_user();
+		$event = new CalendarEvent();
+		$event->set_owner_id($user);
 		$event->set_title(random_string(2));
 		$event->set_description(random_string(8));
-		$event->version();
+		$event->set_parent_id(random_category($user));
+		$start_date = rand(strtotime('-1 Month', time()), strtotime('+1 Month', time()));
+		$end_date = rand($start_date +1, strtotime('+1 Month', $start_date));
+		$event->set_start_date($start_date);
+		$event->set_end_date($end_date);
+		$event->create();
+		for ($j = 0; $j < rand(2, 5); $j ++)
+		{
+			$eventobjectnumber = $event->get_object_number();
+			$event->set_object_number($eventobjectnumber);
+			$event->set_title(random_string(2));
+			$event->set_description(random_string(8));
+			$event->version();
+		}
+		progress();
 	}
-	progress();
-}
-title('Documents');
-for ($i = 0; $i < $documents; $i ++)
-{
-	$user = random_user();
-	$document = new Document();
-	$document->set_owner_id($user);
-	$document->set_title(random_string(2));
-	$document->set_description(random_string(8));
-	$document->set_path('/'.random_word().'/'.random_word());
-	$document->set_filename(random_word());
-	$document->set_filesize(rand(1000, 10000));
-	$document->set_parent_id(random_category($user));
-	$document->create();
-	for ($j = 0; $j < rand(2,5); $j++)
+	title('Documents');
+	for ($i = 0; $i < $documents; $i ++)
 	{
-		$docobjectnumber = $document->get_object_number();
-		$document->set_object_number($docobjectnumber);
+		$user = random_user();
+		$document = new Document();
+		$document->set_owner_id($user);
 		$document->set_title(random_string(2));
 		$document->set_description(random_string(8));
-		$document->set_path('/'.random_word().'/'.random_word());
-		$document->set_filename(random_word());
-		$document->set_filesize(rand(1000, 10000));
-		$document->version();
+		$filename = random_filename();
+		$path = $user.'/'.$filename;
+		$filesize = create_random_file_content($path);
+		$document->set_path($path);
+		$document->set_filename($filename);
+		$document->set_filesize($filesize);
+		$document->set_parent_id(random_category($user));
+		$document->create();
+		for ($j = 0; $j < rand(2, 5); $j ++)
+		{
+			$docobjectnumber = $document->get_object_number();
+			$document->set_object_number($docobjectnumber);
+			$document->set_title(random_string(2));
+			$document->set_description(random_string(8));
+			$filename = random_filename();
+			$path = $user.'/'.$filename;
+			$filesize = create_random_file_content($path);
+			$document->set_path($path);
+			$document->set_filename($filename);
+			$document->set_filesize($filesize);
+			;
+			$document->version();
+		}
+		progress();
 	}
-	progress();
-}
-title('Links');
-for ($i = 0; $i < $links; $i ++)
-{
-	$user = random_user();
-	$link = new Link();
-	$link->set_owner_id($user);
-	$link->set_title(random_string(2));
-	$link->set_description(random_string(8));
-	$link->set_url(random_url());
-	$link->set_parent_id(random_category($user));
-	$link->create();
-	for ($j = 0; $j < rand(2,5); $j++)
+	title('Links');
+	for ($i = 0; $i < $links; $i ++)
 	{
-		$linkobjectnumber = $link->get_object_number();
-		$link->set_object_number($linkobjectnumber);
+		$user = random_user();
+		$link = new Link();
+		$link->set_owner_id($user);
 		$link->set_title(random_string(2));
 		$link->set_description(random_string(8));
 		$link->set_url(random_url());
-		$link->version();
+		$link->set_parent_id(random_category($user));
+		$link->create();
+		for ($j = 0; $j < rand(2, 5); $j ++)
+		{
+			$linkobjectnumber = $link->get_object_number();
+			$link->set_object_number($linkobjectnumber);
+			$link->set_title(random_string(2));
+			$link->set_description(random_string(8));
+			$link->set_url(random_url());
+			$link->version();
+		}
+		progress();
 	}
-	progress();
-}
-title('Forums');
-$created_forums = array();
-for ($i = 0; $i < $forums; $i++)
-{
-	$user = random_user();
-	$forum = new Forum();
-	$forum->set_owner_id($user);
-	$forum->set_title(random_string(2));
-	$forum->set_description(random_string(8));
-	$forum->set_parent_id(random_category($user));
-	$forum->create();
-	$created_forums[] = $forum;
-	progress();
-}
-title('Forum Topics');
-$created_forum_topics = array();
-$topic_to_forum = array();
-for ($i = 0; $i < $forum_topics; $i++)
-{
-	$forum = random_forum();
-	$user = random_user();
-	$topic = new ForumTopic();
-	$topic->set_owner_id($user);
-	$topic->set_title(random_string(2));
-	$topic->set_description(random_string(8));
-	$topic->set_parent_id($forum->get_id());
-	$topic->create();
-	$created_forum_topics[] = $topic;
-	// Map topic to its forum object, for convenience.
-	$topic_to_forum[$topic->get_id()] = $forum;
-	// Every topic needs at least one post.
-	$post = new ForumPost();
-	$post->set_owner_id($user);
-	$post->set_title(random_string(2));
-	$post->set_description(random_string(8));
-	$post->set_parent_id($topic->get_id());
-	$post->create();
-	progress();
-}
-title('Forum Posts');
-for ($i = 0; $i < $forum_posts - $forum_topics; $i++)
-{
-	$user = random_user();
-	$topic = random_forum_topic();
-	$post = new ForumPost();
-	$post->set_owner_id($user);
-	$post->set_title(random_string(2));
-	$post->set_description(random_string(8));
-	$post->set_parent_id($topic->get_id());
-	$post->create();
-	progress();
-}
-
-foreach ($created_forum_topics as $topic)
-{
-	$topic->update();
-}
-
-foreach ($created_forums as $forum)
-{
-	$forum->update();
-}
-title('Questions: Fill in blanks');
-for($i = 0; $i< $questions_fill_in_blanks; $i++)
-{
-	$user = random_user();
-	$question = new FillInBlanksQuestion();
-	$question->set_owner_id($user);
-	$question->set_title(random_string(2));
-	$question->set_description(random_string(8));
-	$question->set_parent_id(random_category($user));
-	$question->set_answer(random_string(2).'['.random_word.']'.random_string(3).'['.random_word().']');
-	$question->create();
-	progress();
-}
-title('Questions: Multiple Choice');
-for($i = 0; $i< $questions_multiple_choice; $i++)
-{
-	$user = random_user();
-	$question = new MultipleChoiceQuestion();
-	$question->set_owner_id($user);
-	$question->set_title(random_string(2));
-	$question->set_description(random_string(8));
-	$question->set_parent_id(random_category($user));
-	$question->set_answer_type('checkbox');
-	$options = array();
-	for($j = 0; $j< 3; $j++)
+	title('Forums');
+	$created_forums = array ();
+	for ($i = 0; $i < $forums; $i ++)
 	{
-		$options[] = new MultipleChoiceQuestionOption(random_word(),rand(0,1),rand(0,5));
+		$user = random_user();
+		$forum = new Forum();
+		$forum->set_owner_id($user);
+		$forum->set_title(random_string(2));
+		$forum->set_description(random_string(8));
+		$forum->set_parent_id(random_category($user));
+		$forum->create();
+		$created_forums[] = $forum;
+		progress();
 	}
-	$question->set_options($options);
-	$question->create();
-	progress();
+	title('Forum Topics');
+	$created_forum_topics = array ();
+	$topic_to_forum = array ();
+	for ($i = 0; $i < $forum_topics; $i ++)
+	{
+		$forum = random_forum();
+		$user = random_user();
+		$topic = new ForumTopic();
+		$topic->set_owner_id($user);
+		$topic->set_title(random_string(2));
+		$topic->set_description(random_string(8));
+		$topic->set_parent_id($forum->get_id());
+		$topic->create();
+		$created_forum_topics[] = $topic;
+		// Map topic to its forum object, for convenience.
+		$topic_to_forum[$topic->get_id()] = $forum;
+		// Every topic needs at least one post.
+		$post = new ForumPost();
+		$post->set_owner_id($user);
+		$post->set_title(random_string(2));
+		$post->set_description(random_string(8));
+		$post->set_parent_id($topic->get_id());
+		$post->create();
+		progress();
+	}
+	title('Forum Posts');
+	for ($i = 0; $i < $forum_posts - $forum_topics; $i ++)
+	{
+		$user = random_user();
+		$topic = random_forum_topic();
+		$post = new ForumPost();
+		$post->set_owner_id($user);
+		$post->set_title(random_string(2));
+		$post->set_description(random_string(8));
+		$post->set_parent_id($topic->get_id());
+		$post->create();
+		progress();
+	}
+
+	foreach ($created_forum_topics as $topic)
+	{
+		$topic->update();
+	}
+
+	foreach ($created_forums as $forum)
+	{
+		$forum->update();
+	}
+	title('Questions: Fill in blanks');
+	for ($i = 0; $i < $questions_fill_in_blanks; $i ++)
+	{
+		$user = random_user();
+		$question = new FillInBlanksQuestion();
+		$question->set_owner_id($user);
+		$question->set_title(random_string(2));
+		$question->set_description(random_string(8));
+		$question->set_parent_id(random_category($user));
+		$question->set_answer(random_string(2).'['.random_word.']'.random_string(3).'['.random_word().']');
+		$question->create();
+		progress();
+	}
+	title('Questions: Multiple Choice');
+	for ($i = 0; $i < $questions_multiple_choice; $i ++)
+	{
+		$user = random_user();
+		$question = new MultipleChoiceQuestion();
+		$question->set_owner_id($user);
+		$question->set_title(random_string(2));
+		$question->set_description(random_string(8));
+		$question->set_parent_id(random_category($user));
+		$question->set_answer_type('checkbox');
+		$options = array ();
+		for ($j = 0; $j < 3; $j ++)
+		{
+			$options[] = new MultipleChoiceQuestionOption(random_word(), rand(0, 1), rand(0, 5));
+		}
+		$question->set_options($options);
+		$question->create();
+		progress();
+	}
 }
+Display :: display_footer();
+
 function random_url()
 {
 	return 'http://www.example.com/~'.totally_random_word(8).'/'.str_replace(' ', '%20', random_string(2)).'.'.totally_random_word(3);
@@ -264,6 +282,16 @@ function random_string($length)
 	return implode(' ', $words);
 }
 
+function create_random_file_content($path)
+{
+	$string = random_string(50);
+	$path = api_get_path(SYS_CODE_PATH).'/upload/'.$path;
+	$handle = fopen($path, 'w+');
+	fwrite($handle, $string);
+	fclose($handle);
+	return filesize($path);
+}
+
 function totally_random_word($length)
 {
 	$str = '';
@@ -278,6 +306,11 @@ function random_word()
 {
 	global $words;
 	return random_array_element(& $words);
+}
+
+function random_filename()
+{
+	return substr(md5(uniqid()), 0, rand(5, 10)).'.txt';
 }
 
 function random_category($owner)
