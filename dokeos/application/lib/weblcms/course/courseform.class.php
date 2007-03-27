dd00d455c58e8b11bdfee462e1afce32a2966a56
Hans De Bisschop <hans.de.bisschop@ehb.be>
2007-03-27 13:28:43 +0000

    Furher implementation of weblcms components.

---
+++ dokeos/application/lib/weblcms/course/courseform.class.php
@@ -0,0 +1,124 @@
+<?php
+require_once dirname(__FILE__).'/course.class.php';
+
+class CourseForm extends FormValidator {
+	
+	const TYPE_CREATE = 1;
+	const TYPE_EDIT = 2;
+	const RESULT_SUCCESS = 'ObjectUpdated';
+	const RESULT_ERROR = 'ObjectUpdateFailed';
+	
+	private $parent;
+	private $course;
+
+    function CourseForm($form_type, $parent) {
+    	parent :: __construct('course_settings', 'post', $parent->get_url());
+    	
+    	$this->course = $parent->get_course();
+    	$this->parent = $parent;
+    	
+		$this->form_type = $form_type;
+		if ($this->form_type == self :: TYPE_EDIT)
+		{
+			$this->build_editing_form();
+		}
+		elseif ($this->form_type == self :: TYPE_CREATE)
+		{
+			$this->build_creation_form();
+		}
+    }
+    
+    function build_editing_form()
+    {
+    	$course = $this->course;
+    	$parent = $this->parent;
+    	
+		$this->addElement('text', Course :: PROPERTY_VISUAL, get_lang('VisualCode'));
+		$this->addRule(Course :: PROPERTY_VISUAL, get_lang('ThisFieldIsRequired'), 'required');
+		$this->addElement('text', Course :: PROPERTY_TITULAR, get_lang('Teacher'));
+		$this->addRule(Course :: PROPERTY_TITULAR, get_lang('ThisFieldIsRequired'), 'required');
+		$this->addElement('text', Course :: PROPERTY_NAME, get_lang('Title'));
+		$this->addRule(Course :: PROPERTY_NAME, get_lang('ThisFieldIsRequired'), 'required');
+		
+		$cat_options = array();
+		$categories =  $parent->get_parent()->retrieve_course_categories();
+		while ($category = $categories->next_result())
+		{
+			$cat_options[$category->get_code()] = $category->get_name();
+		}
+		
+		$this->addElement('select', Course :: PROPERTY_CATEGORY, get_lang('Category'), $cat_options);
+		 
+		$this->addElement('text', Course :: PROPERTY_EXTLINK_NAME, get_lang('Department'));
+		$this->addElement('text', Course :: PROPERTY_EXTLINK_URL, get_lang('DepartmentUrl'));
+		
+		/*
+		 * TODO: SCARA - It's probably better to have a general language class somewhere ?
+		 */
+		
+		$languages = api_get_languages();
+		$lang_options = array();
+		foreach ($languages['name'] as $index => $name)
+		{
+			$lang_options[$languages['folder'][$index]] = $name;
+		}
+		
+		$this->addElement('select', Course :: PROPERTY_LANGUAGE, get_lang('Language'), $lang_options);
+		 
+		$course_access = array();
+		$course_access[] =& $this->createElement('radio', null, null, get_lang('CourseAccessOpenWorld'), COURSE_VISIBILITY_OPEN_WORLD);
+		$course_access[] =& $this->createElement('radio', null, null, get_lang('CourseAccessOpenRegistered'), COURSE_VISIBILITY_OPEN_PLATFORM);
+		$course_access[] =& $this->createElement('radio', null, null, get_lang('CourseAccessPrivate'), COURSE_VISIBILITY_REGISTERED);
+		$course_access[] =& $this->createElement('radio', null, null, get_lang('CourseAccessClosed'), COURSE_VISIBILITY_CLOSED);
+		$course_access[] =& $this->createElement('radio', null, null, get_lang('CourseAccessModified'), COURSE_VISIBILITY_MODIFIED);
+		$this->addGroup($course_access, Course :: PROPERTY_VISIBILITY, get_lang('CourseAccess'), '<br />');
+		
+		$subscribe_allowed = array();
+		$subscribe_allowed[] =& $this->createElement('radio', null, null, get_lang('SubscribeAllowed'), 1);
+		$subscribe_allowed[] =& $this->createElement('radio', null, null, get_lang('SubscribeNotAllowed'), 0);
+		$this->addGroup($subscribe_allowed, Course :: PROPERTY_SUBSCRIBE_ALLOWED, get_lang('Subscribe'), '<br />');
+		
+		$unsubscribe_allowed = array();
+		$unsubscribe_allowed[] =& $this->createElement('radio', null, null, get_lang('UnsubscribeAllowed'), 1);
+		$unsubscribe_allowed[] =& $this->createElement('radio', null, null, get_lang('UnsubscribeNotAllowed'), 0);
+		$this->addGroup($unsubscribe_allowed, Course :: PROPERTY_UNSUBSCRIBE_ALLOWED, get_lang('Unsubscribe'), '<br />');		
+		
+		$this->addElement('submit', 'course_settings', get_lang('Ok'));
+    	$defaults = array();
+		$defaults[Course :: PROPERTY_VISUAL] = $course->get_visual();
+		$defaults[Course :: PROPERTY_TITULAR] = $course->get_titular();
+		$defaults[Course :: PROPERTY_NAME] = $course->get_name();
+		$defaults[Course :: PROPERTY_CATEGORY] = $course->get_category()->get_code();
+		$defaults[Course :: PROPERTY_EXTLINK_NAME] = $course->get_extlink_name();
+		$defaults[Course :: PROPERTY_EXTLINK_URL] = $course->get_extlink_url();
+		$defaults[Course :: PROPERTY_LANGUAGE] = $course->get_language();
+		$defaults[Course :: PROPERTY_VISIBILITY] = $course->get_visibility();
+		$defaults[Course :: PROPERTY_SUBSCRIBE_ALLOWED] = $course->get_subscribe_allowed();
+		$defaults[Course :: PROPERTY_UNSUBSCRIBE_ALLOWED] = $course->get_unsubscribe_allowed();
+    	$this->setDefaults($defaults);
+    }
+    
+    function build_creation_form()
+    {
+    }
+    
+    function update_course()
+    {
+    	$course = $this->course;
+    	$values = $this->exportValues();
+    	
+    	$course->set_visual($values[Course :: PROPERTY_VISUAL]);
+    	$course->set_name($values[Course :: PROPERTY_NAME]);
+    	$course->set_category($values[Course :: PROPERTY_CATEGORY]);
+    	$course->set_titular($values[Course :: PROPERTY_TITULAR]);
+    	$course->set_extlink_name($values[Course :: PROPERTY_EXTLINK_NAME]);
+    	$course->set_extlink_url($values[Course :: PROPERTY_EXTLINK_URL]);
+    	$course->set_language($values[Course :: PROPERTY_LANGUAGE]);
+    	$course->set_visibility($values[Course :: PROPERTY_VISIBILITY]);
+    	$course->set_subscribe_allowed($values[Course :: PROPERTY_SUBSCRIBE_ALLOWED]);
+    	$course->set_unsubscribe_allowed($values[Course :: PROPERTY_UNSUBSCRIBE_ALLOWED]);
+    	
+    	return $course->update();
+    }
+}
+?>
---
+++ dokeos/application/lib/weblcms/course/courseusercategory.class.php
@@ -0,0 +1,72 @@
+<?php
+
+class CourseUserCategory {
+
+	const PROPERTY_ID = 'id';
+	const PROPERTY_USER = 'user_id';
+	const PROPERTY_TITLE = 'title';
+	
+	private $id;
+	private $defaultProperties;
+
+    function CourseUserCategory($id = null, $defaultProperties = array ())
+    {
+    	$this->id = $id;
+		$this->defaultProperties = $defaultProperties;
+    }
+    
+	function get_default_property($name)
+	{
+		return $this->defaultProperties[$name];
+	}
+	
+	function get_default_properties()
+	{
+		return $this->defaultProperties;
+	}
+
+	function set_default_property($name, $value)
+	{
+		$this->defaultProperties[$name] = $value;
+	}
+	
+	/**
+	 * Get the default properties of all user course categories.
+	 * @return array The property names.
+	 */
+	static function get_default_property_names()
+	{
+		return array (self :: PROPERTY_ID, self :: PROPERTY_USER, self :: PROPERTY_TITLE);
+	}
+    
+    function get_id()
+    {
+    	return $this->id;
+    }
+    
+    function set_id($id)
+	{
+		$this->id = $id;
+	}
+	
+    function get_user()
+    {
+    	return $this->get_default_property(self :: PROPERTY_USER);
+    }
+	
+	function set_user($user)
+	{
+		$this->set_default_property(self :: PROPERTY_USER, $user);
+	}
+	
+    function get_title()
+    {
+    	return $this->get_default_property(self :: PROPERTY_TITLE);
+    }
+	
+	function set_title($title)
+	{
+		$this->set_default_property(self :: PROPERTY_TITLE, $title);
+	}
+}
+?>
