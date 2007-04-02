b5e848e8db6b2350e289b64f0a318a4a024416b7
Scara84 <Scara84@localhost>
2007-04-02 13:44:06 +0000

    Further development of the WebLCMS Sorter component.

--- dokeos/application/lib/weblcms/weblcmsdatamanager.class.php
+++ dokeos/application/lib/weblcms/weblcmsdatamanager.class.php
@@ -387,7 +387,7 @@ abstract class WeblcmsDataManager
 	
 	abstract function retrieve_course_user_relation($course_code, $user_id);
 	
-	abstract function retrieve_course_user_relation_at_sort($user_id, $category_id, $sort);
+	abstract function retrieve_course_user_relation_at_sort($user_id, $category_id, $sort, $direction);
 	
 	abstract function retrieve_course_user_relations($user_id, $course_user_category);
 	
