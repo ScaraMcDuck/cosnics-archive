266e2484debe7f11ed8c6ff34cdc2d8b93e871c8
bmol <bmol@cosnics>
2007-04-10 13:40:58 +0000

    Blocking anonymous users

--- dokeos/index_personal_calendar.php
+++ dokeos/index_personal_calendar.php
@@ -5,7 +5,10 @@ $this_section = 'myagenda';
 require_once dirname(__FILE__).'/main/inc/claro_init_global.inc.php';
 require_once dirname(__FILE__).'/main/inc/lib/text.lib.php';
 require_once dirname(__FILE__).'/application/lib/personal_calendar/personal_calendar_manager/personal_calendar.class.php';
-
+if (!api_get_user_id())
+{
+	api_not_allowed();
+}
 $app = new PersonalCalendar(api_get_user_id());
 $app->run();
 ?>
