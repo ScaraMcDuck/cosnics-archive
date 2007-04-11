e58acfdca25dc4de20f318e296b32ec4bc2ce3c3
bmol <bmol@localhost>
2007-04-11 07:54:51 +0000

    starting implementation of the personal calendar publisher to allow users to publish events in their personal calendar

---
+++ dokeos/application/lib/personal_calendar/publisher/personal_calendar_publisher.class.php
@@ -0,0 +1,46 @@
+<?php
+class PersonalCalendarPublisher
+{
+	const PARAM_ACTION = 'publish_action';
+	private $personal_calendar;
+	function PersonalCalendarPublisher($personal_calendar)
+	{
+		$this->personal_calendar = $personal_calendar;
+	}
+	public function as_html()
+	{
+		$out = '<div class="tabbed-pane"><ul class="tabbed-pane-tabs">';
+		foreach (array ('publicationcreator', 'browser', 'finder') as $action)
+		{
+			$out .= '<li><a';
+			if ($this->get_action() == $action)
+				$out .= ' class="current"';
+			$out .= ' href="'.$this->get_url(array (PersonalCalendarPublisher :: PARAM_ACTION => $action), true).'">'.htmlentities(get_lang(ucfirst($action).'Title')).'</a></li>';
+		}
+		$out .= '</ul><div class="tabbed-pane-content">';
+
+//		$action = $this->get_action();
+//		require_once dirname(__FILE__).'/publisher/learningobject'.$action.'.class.php';
+//		$class = 'LearningObject'.ucfirst($action);
+//		$component = new $class ($this);
+//		$out .= $component->as_html();
+		$out .= '</div></div>';
+		return $out;
+	}
+	/**
+	 * @see PersonalCalendar::get_url()
+	 */
+	function get_url($parameters = array(), $encode = false)
+	{
+		return $this->personal_calendar->get_url($parameters, $encode);
+	}
+	/**
+	 * Returns the action that the user selected, or "publicationcreator" if none.
+	 * @return string The action.
+	 */
+	function get_action()
+	{
+		return ($_GET[PersonalCalendarPublisher :: PARAM_ACTION] ? $_GET[PersonalCalendarPublisher :: PARAM_ACTION] : 'publicationcreator');
+	}
+}
+?>
