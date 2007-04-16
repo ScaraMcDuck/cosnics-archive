e69f77294204842b47ec7075a1982d2cbf931faf
xistance85 <xistance85@cosnics>
2007-04-16 13:56:48 +0000

    added user functionality (when a user is able to register him/herself to the portal)

---
+++ dokeos/users/lib/usermanager/component/register.class.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @package user.usermanager
+ */
+require_once dirname(__FILE__).'/../usermanager.class.php';
+require_once dirname(__FILE__).'/../usermanagercomponent.class.php';
+require_once dirname(__FILE__).'/../registerform.class.php';
+require_once dirname(__FILE__).'/../../usersdatamanager.class.php';
+
+class UserManagerRegisterComponent extends UserManagerComponent
+{
+	/**
+	 * Runs this component and displays its output.
+	 */
+	function run()
+	{	
+		if (get_setting('allow_registration') == 'false')
+		{
+			api_not_allowed();
+		}	
+		$breadcrumbs = array();
+		$breadcrumbs[] = array ('url' => $this->get_url(), 'name' => get_lang('UserRegister'));
+		if (isset($user_id)) 
+		{
+			$this->display_header($breadcrumbs);
+			Display :: display_warning_message(get_lang('AlreadyRegistered'));
+			$this->display_footer();
+			exit;
+		}
+		$user = new User();
+		$user->set_platformadmin(0);
+		$user->set_password(1);
+		//$user->set_creator_id($user_info['user_id']);
+		
+		$form = new RegisterForm($user, $this->get_url());
+		
+
+		
+		if($form->validate())
+		{
+			$success = $form->create_user();
+			echo 'redirect';
+			//$this->redirect(User :: ACTION_CREATE_USER, get_lang($success ? 'UserCreated' : 'UserNotCreated'), ($success ? false : true));
+		}
+		else
+		{
+			$this->display_header($breadcrumbs);
+			$form->display();
+			$this->display_footer();
+		}
+	}
+}
+?>
--- dokeos/users/lib/usermanager/registerform.class.php
+++ dokeos/users/lib/usermanager/registerform.class.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__).'/../../../main/inc/lib/fileUpload.lib.php';
 require_once dirname(__FILE__).'/../user.class.php';
 require_once dirname(__FILE__).'/../usersdatamanager.class.php';
 
-class UserForm extends FormValidator {
+class RegisterForm extends FormValidator {
 	
 	const TYPE_CREATE = 1;
 	const TYPE_EDIT = 2;
@@ -14,25 +14,13 @@ class UserForm extends FormValidator {
 	
 	private $parent;
 	private $user;
-	private $form_user;
 	private $unencryptedpass;
 
-    function UserForm($form_type, $user, $form_user, $action) {
+    function RegisterForm($user, $action) {
     	parent :: __construct('user_settings', 'post', $action);
     	
     	$this->user = $user;
-    	$this->form_user = $form_user;
-    	
-		$this->form_type = $form_type;
-		if ($this->form_type == self :: TYPE_EDIT)
-		{
-			$this->build_editing_form();
-		}
-		elseif ($this->form_type == self :: TYPE_CREATE)
-		{
-			$this->build_creation_form();
-		}
-		
+		$this->build_creation_form();
 		$this->setDefaults();
     }
     
@@ -53,10 +41,6 @@ class UserForm extends FormValidator {
 		$this->addRule(User :: PROPERTY_USERNAME, get_lang('ThisFieldIsRequired'), 'required');
 		//pw
 		$group = array();
-		if ($this->form_type == self :: TYPE_EDIT)
-		{
-			$group[] =& $this->createElement('radio', 'pass', null,get_lang('KeepPassword').'<br />',2);
-		}
 		$group[] =& $this->createElement('radio', 'pass', null,get_lang('AutoGeneratePassword').'<br />',1);
 		$group[] =& $this->createElement('radio', 'pass', null,null,0);
 		$group[] =& $this->createElement('password', User :: PROPERTY_PASSWORD,null,null);
@@ -77,30 +61,14 @@ class UserForm extends FormValidator {
 			$lang_options[$languages['folder'][$index]] = $name;
 		}
 		$this->addElement('select', User :: PROPERTY_LANGUAGE, get_lang('Language'), $lang_options);
-		// Disk Quota
-		$this->addElement('text', User :: PROPERTY_DISK_QUOTA, get_lang('DiskQuota'));
-		$this->addRule(User :: PROPERTY_DISK_QUOTA, get_lang('FieldMustBeNumeric'), 'numeric', null, 'server');
-		// Database Quota
-		$this->addElement('text', User :: PROPERTY_DATABASE_QUOTA, get_lang('DatabaseQuota'));
-		$this->addRule(User :: PROPERTY_DATABASE_QUOTA, get_lang('FieldMustBeNumeric'), 'numeric', null, 'server');
-		// Version quota
-		$this->addElement('text', User :: PROPERTY_VERSION_QUOTA, get_lang('VersionQuota'));
-		$this->addRule(User :: PROPERTY_VERSION_QUOTA, get_lang('FieldMustBeNumeric'), 'numeric', null, 'server');
-		
 		// Status
-		$status = array();  
-		$status[STUDENT] = get_lang('Student');  
-		$status[COURSEMANAGER]  = get_lang('CourseAdmin');
-		$this->addElement('select',User :: PROPERTY_STATUS,get_lang('Status'),$status);
-		// Platform admin
-		if ($this->user->is_platform_admin() && $this->user->get_user_id() == $this->form_user->get_user_id() && $this->form_type == self :: TYPE_EDIT)
+		if (get_setting('allow_registration_as_teacher') == 'true')
 		{
-		$this->add_warning_message(null, get_lang('LockOutWarningMessage'));
+			$status = array();  
+			$status[STUDENT] = get_lang('Student');  
+			$status[COURSEMANAGER]  = get_lang('CourseAdmin');
+			$this->addElement('select',User :: PROPERTY_STATUS,get_lang('Status'),$status);
 		}
-		$group = array();
-		$group[] =& $this->createElement('radio', User :: PROPERTY_PLATFORMADMIN,null,get_lang('Yes'),1);
-		$group[] =& $this->createElement('radio', User :: PROPERTY_PLATFORMADMIN,null,get_lang('No'),0);
-		$this->addGroup($group, 'admin', get_lang('PlatformAdmin'), '&nbsp;');
 		//  Send email 
 		$group = array();
 		$group[] =& $this->createElement('radio', 'send_mail',null,get_lang('Yes'),1);
@@ -110,68 +78,10 @@ class UserForm extends FormValidator {
 		$this->addElement('submit', 'user_settings', 'OK');
     }
     
-    function build_editing_form()
-    {
-    	$user = $this->user;
-    	$parent = $this->parent;
-    	
-    	$this->build_basic_form();
-    	
-    	$this->addElement('hidden', User :: PROPERTY_USER_ID);
-    }
-    
     function build_creation_form()
     {		
     	$this->build_basic_form();
     }
-    
-    function update_user()
-    {
-    	$user = $this->user;
-    	$values = $this->exportValues();
-    	
-    	$password = $values['pw']['pass'] == '1' ? api_generate_password() : $values['pw'][User :: PROPERTY_PASSWORD];
-    	
-    	if ($_FILES[User :: PROPERTY_PICTURE_URI])
-    	{
-			$temp_picture_location = $_FILES[User :: PROPERTY_PICTURE_URI]['tmp_name'];
-			$picture_name = $_FILES[User :: PROPERTY_PICTURE_URI]['name'];
-			$picture_uri = uniqid('').'_'.replace_dangerous_char($picture_name);
-			$picture_location = api_get_path(SYS_CODE_PATH).'upload/users/'.$picture_uri;
-			$user->set_picture_uri($picture_location);
-			move_uploaded_file($temp_picture_location, $picture_location);
-    	}
-		$udm = UsersDataManager :: get_instance();
-    	if ($udm->is_username_available($values[User :: PROPERTY_USERNAME], $values[User :: PROPERTY_USER_ID]))
-    	{
-    		$user->set_lastname($values[User :: PROPERTY_LASTNAME]);
-    		$user->set_firstname($values[User :: PROPERTY_FIRSTNAME]);
-    		$user->set_email($values[User :: PROPERTY_EMAIL]);
-	    	$user->set_username($values[User :: PROPERTY_USERNAME]);
-	 	   	$user->set_password(md5($password));
-	 	   	$this->unencryptedpass = $password;
-    		$user->set_official_code($values[User :: PROPERTY_OFFICIAL_CODE]);
-			$user->set_picture_uri($picture_uri);
-  		  	$user->set_phone($values[User :: PROPERTY_PHONE]);
-  		  	$user->set_status(intval($values[User :: PROPERTY_STATUS]));
- 		   	$user->set_version_quota(intval($values[User :: PROPERTY_VERSION_QUOTA]));
- 		   	$user->set_language($values[User :: PROPERTY_LANGUAGE]);
-			$user->set_platformadmin(intval($values['admin'][User :: PROPERTY_PLATFORMADMIN]));
-    		$send_mail = intval($values['mail']['send_mail']);
-    		if ($send_mail)
-    		{
-    			$this->send_email($user); 
-    		}
-
-    		return $user->update();
-    	}
-    	else 
-    	{
-    		return false;
-    	}
-    }
-    
-    
     
     function create_user()
     {
@@ -202,22 +112,33 @@ class UserForm extends FormValidator {
     		$user->set_official_code($values[User :: PROPERTY_OFFICIAL_CODE]);
 			$user->set_picture_uri($picture_uri);
   		  	$user->set_phone($values[User :: PROPERTY_PHONE]);
-  		  	$user->set_status(intval($values[User :: PROPERTY_STATUS]));
-  		  	if ($values[User :: PROPERTY_VERSION_QUOTA] != '')
- 		   	$user->set_version_quota(intval($values[User :: PROPERTY_VERSION_QUOTA]));
- 		   	if ($values[User :: PROPERTY_DATABASE_QUOTA] != '')
- 		   	$user->set_database_quota(intval($values[User :: PROPERTY_DATABASE_QUOTA]));
- 		   	if ($values[User :: PROPERTY_DISK_QUOTA] != '')
- 		   	$user->set_disk_quota(intval($values[User :: PROPERTY_DISK_QUOTA]));
+  		  	if (get_setting('allow_registration_as_teacher') == 'false')
+			{
+				$values[User :: PROPERTY_STATUS] = STUDENT;
+			}
+			$user->set_status(intval($values[User :: PROPERTY_STATUS]));
  		   	$user->set_language($values[User :: PROPERTY_LANGUAGE]);
- 		   	$user->set_platformadmin(intval($values['admin'][User :: PROPERTY_PLATFORMADMIN]));
     		$send_mail = intval($values['mail']['send_mail']);
     		if ($send_mail)
     		{
     			$this->send_email($user); 
     		}
-
-    		return $user->create();
+			if ($user->create())
+			{
+				$_uid = intval($user->get_user_id());
+				api_session_register('_uid');
+				echo $_uid;
+				//stats
+				include (api_get_library_path()."/events.lib.inc.php");
+				event_login($_uid);
+				// last user login date is now
+				$user_last_login_datetime = 0; // used as a unix timestamp it will correspond to : 1 1 1970
+				return true;
+			}
+			else
+			{
+				return false;
+			}
     	}
     	else 
     	{
