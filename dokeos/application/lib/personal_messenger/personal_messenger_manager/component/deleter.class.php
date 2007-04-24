3b5d3ecf4b72b69fb7528520f6fc6eae72159909
Hans De Bisschop <hans.de.bisschop@ehb.be>
2007-04-24 13:09:48 +0000

    Further implementation of Personal Messenger

---
+++ dokeos/application/lib/personal_messenger/personal_messenger_manager/component/deleter.class.php
@@ -0,0 +1,63 @@
+<?php
+require_once dirname(__FILE__).'/../personal_messenger.class.php';
+require_once dirname(__FILE__).'/../personalmessengercomponent.class.php';
+
+class PersonalMessengerDeleterComponent extends PersonalMessengerComponent
+{
+	/**
+	 * Runs this component and displays its output.
+	 */
+	function run()
+	{
+		$ids = $_GET[PersonalMessenger :: PARAM_PERSONAL_MESSAGE_ID];
+		$failures = 0;
+		
+		if (!empty ($ids))
+		{
+			if (!is_array($ids))
+			{
+				$ids = array ($ids);
+			}
+			
+			foreach ($ids as $id)
+			{
+				$publication = $this->get_parent()->retrieve_personal_message_publication($id);
+				
+				if (!$publication->delete())
+				{
+					$failures++;
+				}
+			}
+			
+			if ($failures)
+			{
+				if (count($ids) == 1)
+				{
+					$message = 'SelectedPublicationNotDeleted';
+				}
+				else
+				{
+					$message = 'SelectedPublicationsNotDeleted';
+				}
+			}
+			else
+			{
+				if (count($ids) == 1)
+				{
+					$message = 'SelectedPublicationDeleted';
+				}
+				else
+				{
+					$message = 'SelectedPublicationsDeleted';
+				}
+			}
+			
+			$this->redirect(null, get_lang($message), ($failures ? true : false), array(PersonalMessenger :: PARAM_ACTION => PersonalMessenger :: ACTION_BROWSE_MESSAGES));
+		}
+		else
+		{
+			$this->display_error_page(htmlentities(get_lang('NoPublicationSelected')));
+		}
+	}
+}
+?>
