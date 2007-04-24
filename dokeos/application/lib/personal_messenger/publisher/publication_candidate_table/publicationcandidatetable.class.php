d51d41baf6219b90c26b8cd986b215f5cc572b75
xistance85 <xistance85@cosnics>
2007-04-24 13:50:15 +0000

    publishing functionality

---
+++ dokeos/application/lib/personal_messenger/publisher/publication_candidate_table/publicationcandidatetable.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * $Id: publicationcandidatetable.class.php 9141 2006-08-21 12:55:22Z bmol $
+ * @package application.weblcms
+ * @subpackage publisher
+ */
+require_once dirname(__FILE__).'/publicationcandidatetabledataprovider.class.php';
+require_once dirname(__FILE__).'/publicationcandidatetablecolumnmodel.class.php';
+require_once dirname(__FILE__).'/publicationcandidatetablecellrenderer.class.php';
+require_once dirname(__FILE__).'/../../../../../repository/lib/learning_object_table/learningobjecttable.class.php';
+/**
+ * This class represents a table with learning objects which are candidates for
+ * publication.
+ */
+class PublicationCandidateTable extends LearningObjectTable
+{
+	/**
+	 * Constructor.
+	 * @param int $owner The id of the current user.
+	 * @param array $types The types of objects that can be published in current
+	 * location.
+	 * @param string $query The search query, or null if none.
+	 * @param string $publish_url_format URL for publishing the selected
+	 * learning object.
+	 * @param string $edit_and_publish_url_format URL for editing and publishing
+	 * the selected learning object.
+	 * @see PublicationCandidateTableCellRenderer::PublicationCandidateTableCellRenderer()
+	 */
+	function PublicationCandidateTable($owner, $types, $query, $publish_url_format, $edit_and_publish_url_format)
+	{
+		$name = 'pubcand';
+		$data_provider = new PublicationCandidateTableDataProvider($owner, $types, $query);
+		$column_model = new PublicationCandidateTableColumnModel();
+		$cell_renderer = new PublicationCandidateTableCellRenderer($publish_url_format, $edit_and_publish_url_format);
+		parent :: __construct($data_provider, $name, $column_model, $cell_renderer);
+	}
+}
+?>
