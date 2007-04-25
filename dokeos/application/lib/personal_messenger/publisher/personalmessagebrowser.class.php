884f2428ef6b019c603b1d5b493ef5cbc9f38d8e
xistance85 <xistance85@cosnics>
2007-04-25 07:06:29 +0000

    publishing functionality

---
+++ dokeos/application/lib/personal_messenger/publisher/personalmessagebrowser.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * $Id: learningobjectbrowser.class.php 9141 2006-08-21 12:55:22Z bmol $
+ * @package application.weblcms
+ * @subpackage publisher
+ */
+require_once dirname(__FILE__).'/../personalmessagepublisher.class.php';
+require_once dirname(__FILE__).'/../personalmessagepublishercomponent.class.php';
+require_once dirname(__FILE__).'/publication_candidate_table/publicationcandidatetable.class.php';
+/**
+ * This class represents a learning object publisher component which can be used
+ * to browse through the possible learning objects to publish.
+ */
+class PersonalMessageBrowser extends PersonalMessagePublisherComponent
+{
+	/*
+	 * Inherited
+	 */
+	function as_html()
+	{
+		$publish_url_format = $this->get_url(array (PersonalMessagePublisher :: PARAM_ACTION => 'publicationcreator', PersonalMessagePublisher :: PARAM_LEARNING_OBJECT_ID => '__ID__'),false);
+		$edit_and_publish_url_format = $this->get_url(array (PersonalMessagePublisher :: PARAM_ACTION => 'publicationcreator', PersonalMessagePublisher :: PARAM_LEARNING_OBJECT_ID => '__ID__', PersonalMessagePublisher :: PARAM_EDIT => 1));
+		$publish_url_format = str_replace('__ID__', '%d', $publish_url_format);
+		$edit_and_publish_url_format = str_replace('__ID__', '%d', $edit_and_publish_url_format);
+		$table = new PublicationCandidateTable($this->get_user_id(), $this->get_types(), $this->get_query(), $publish_url_format, $edit_and_publish_url_format);
+		return $table->as_html();
+	}
+
+	/**
+	 * Returns the search query.
+	 * @return string|null The query, or null if none.
+	 */
+	protected function get_query()
+	{
+		return null;
+	}
+}
+?>
---
+++ dokeos/application/lib/personal_messenger/publisher/personalmessagefinder.class.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * $Id: learningobjectfinder.class.php 9197 2006-09-04 12:02:29Z bmol $
+ * @package application.weblcms
+ * @subpackage publisher
+ */
+require_once dirname(__FILE__).'/personalmessagebrowser.class.php';
+require_once dirname(__FILE__).'/../../../../repository/lib/condition/andcondition.class.php';
+require_once dirname(__FILE__).'/../../../../repository/lib/condition/orcondition.class.php';
+require_once dirname(__FILE__).'/../../../../repository/lib/condition/patternmatchcondition.class.php';
+require_once api_get_library_path().'/formvalidator/FormValidator.class.php';
+/**
+ * This class represents a learning object publisher component which can be used
+ * to search for a certain learning object.
+ */
+class PersonalMessageFinder extends PesonalMessageBrowser
+{
+	/**
+	 * The search form
+	 */
+	private $form;
+	/**
+	 * The renderer for the search form
+	 */
+	private $renderer;
+	/**
+	 * Constructor.
+	 * @param LearningObjectPublisher $parent The creator of this object.
+	 */
+	function PersonalMessageFinder($parent)
+	{
+		parent :: __construct($parent);
+		$this->form = new FormValidator('search', 'get','','',null,false);
+		$this->form->addElement('hidden', 'tool');
+		$this->form->addElement('hidden', PersonalMessagePublisher :: PARAM_ACTION);
+		$this->form->addElement('text', 'query', get_lang('Find'), 'size="40" class="search_query"');
+		$this->form->addElement('submit', 'submit', get_lang('Ok'));
+		$this->renderer = clone $this->form->defaultRenderer();
+		$this->renderer->setElementTemplate('<span>{element}</span> ');
+		$this->form->accept($this->renderer);
+	}
+
+	/*
+	 * Inherited
+	 */
+	function as_html()
+	{
+		$html = array();
+		$html[] = '<div class="lofinder_search_form" style="margin: 0 0 1em 0;">';
+		$html[] = $this->renderer->toHTML();
+		$html[] = '</div>';
+		if(strlen(trim($this->get_query())) > 0)
+		{
+			$html[] = parent::as_html();
+		}
+		return implode("\n",$html);
+	}
+	/*
+	 * Overriding
+	 */
+	protected function get_query()
+	{
+		if ($this->form->validate())
+		{
+			return $this->form->exportValue('query');
+		}
+		if ($_GET['query'])
+		{
+			return $_GET['query'];
+		}
+		return null;
+	}
+}
+?>
