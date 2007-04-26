987fae3b2f432bed167afcce8287d11862125657
Scara84 <Scara84@localhost>
2007-04-26 13:46:15 +0000

    Profile object introduced.

---
+++ dokeos/repository/lib/learning_object/profile/profile_difference.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package repository.learningobject
+ * @subpackage calendarevent
+ */
+/**
+ * This class can be used to get the difference between profiles
+ */
+class ProfileDifference extends LearningObjectDifference
+{
+}
+?>
---
+++ dokeos/repository/lib/learning_object/profile/profile_differencedisplay.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package repository.learningobject
+ * @subpackage calendarevent
+ */
+/**
+ * This class can be used to display the difference between profiles
+ */
+class ProfileDifferenceDisplay extends LearningObjectDifferenceDisplay
+{
+}
+?>
