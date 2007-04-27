f8125dddca5d8d05e9d00fd95c09332421fac63c
Hans De Bisschop <hans.de.bisschop@ehb.be>
2007-04-27 11:49:13 +0000

    Language strings added.

---
+++ dokeos/main/lang/english/profiler.inc.php
@@ -0,0 +1,34 @@
+<?php
+$MyProfiler='User Profiles';
+$Username='Username';
+$Lastname='Last Name';
+$Firstname='First Name';
+$Profile='Profile';
+$Publish='Publish';
+$Home='Home';
+$RemoveSelected='Remove selected';
+$ViewProfile='View Profile';
+$Address='Address';
+$Description='Description';
+$Contact='Contact';
+$Competences='Competences';
+$Diplomas='Diplomas';
+$Teaching='Teaching';
+$Open='Open Area';
+$TelShort='Tel.';
+$FaxShort='Fax';
+$Skype='Skype';
+$Msn='MSN';
+$Yim='YIM';
+$Aim='AIM';
+$Icq='ICQ';
+$PublishProfile='Publish Profile';
+$PublicationcreatorTitle='Create a Profile';
+$CategoryTypeName='Category';
+$Phone='Phone';
+$Fax='Fax';
+$Mail='E-Mail';
+$BrowserTitle='Browse';
+$FinderTitle='Find';
+
+?>
