d441c257edca39b0a78e81ba7fb3c7b4e1e58ba0
Scara84 <Scara84@localhost>
2007-05-03 08:50:55 +0000

    Various additions & fixes.

--- dokeos/repository/lib/learning_object/profile/profile_form.class.php
+++ dokeos/repository/lib/learning_object/profile/profile_form.class.php
@@ -11,36 +11,36 @@ class ProfileForm extends LearningObjectForm
 	protected function build_creation_form()
 	{
 		parent :: build_creation_form();
-		$this->add_html_editor(Profile :: PROPERTY_ADDRESS, get_lang('Address'));
-		$this->add_textfield(Profile :: PROPERTY_PHONE, get_lang('Phone'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_FAX, get_lang('Fax'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_MAIL, get_lang('Mail'), true,'size="40" style="width: 100%;"');
-		$this->add_html_editor(Profile :: PROPERTY_COMPETENCES, get_lang('Competences'));
-		$this->add_html_editor(Profile :: PROPERTY_DIPLOMAS, get_lang('Diplomas'));
-		$this->add_html_editor(Profile :: PROPERTY_TEACHING, get_lang('Teaching'));
-		$this->add_html_editor(Profile :: PROPERTY_OPEN, get_lang('Open'));
-		$this->add_textfield(Profile :: PROPERTY_SKYPE, get_lang('Skype'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_MSN, get_lang('Msn'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_AIM, get_lang('Aim'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_YIM, get_lang('Yim'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_ICQ, get_lang('Icq'), true,'size="40" style="width: 100%;"');
+		$this->add_html_editor(Profile :: PROPERTY_ADDRESS, get_lang('Address'), false);
+		$this->add_textfield(Profile :: PROPERTY_PHONE, get_lang('Phone'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_FAX, get_lang('Fax'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_MAIL, get_lang('Mail'), false,'size="40" style="width: 100%;"');
+		$this->add_html_editor(Profile :: PROPERTY_COMPETENCES, get_lang('Competences'), false);
+		$this->add_html_editor(Profile :: PROPERTY_DIPLOMAS, get_lang('Diplomas'), false);
+		$this->add_html_editor(Profile :: PROPERTY_TEACHING, get_lang('Teaching'), false);
+		$this->add_html_editor(Profile :: PROPERTY_OPEN, get_lang('Open'), false);
+		$this->add_textfield(Profile :: PROPERTY_SKYPE, get_lang('Skype'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_MSN, get_lang('Msn'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_AIM, get_lang('Aim'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_YIM, get_lang('Yim'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_ICQ, get_lang('Icq'), false,'size="40" style="width: 100%;"');
 	}
 	protected function build_editing_form()
 	{
 		parent :: build_editing_form();
-		$this->add_html_editor(Profile :: PROPERTY_ADDRESS, get_lang('Address'));
-		$this->add_textfield(Profile :: PROPERTY_PHONE, get_lang('Phone'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_FAX, get_lang('Fax'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_MAIL, get_lang('Mail'), true,'size="40" style="width: 100%;"');
-		$this->add_html_editor(Profile :: PROPERTY_COMPETENCES, get_lang('Competences'));
-		$this->add_html_editor(Profile :: PROPERTY_DIPLOMAS, get_lang('Diplomas'));
-		$this->add_html_editor(Profile :: PROPERTY_TEACHING, get_lang('Teaching'));
-		$this->add_html_editor(Profile :: PROPERTY_OPEN, get_lang('Open'));
-		$this->add_textfield(Profile :: PROPERTY_SKYPE, get_lang('Skype'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_MSN, get_lang('Msn'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_AIM, get_lang('Aim'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_YIM, get_lang('Yim'), true,'size="40" style="width: 100%;"');
-		$this->add_textfield(Profile :: PROPERTY_ICQ, get_lang('Icq'), true,'size="40" style="width: 100%;"');
+		$this->add_html_editor(Profile :: PROPERTY_ADDRESS, get_lang('Address'), false);
+		$this->add_textfield(Profile :: PROPERTY_PHONE, get_lang('Phone'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_FAX, get_lang('Fax'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_MAIL, get_lang('Mail'), false,'size="40" style="width: 100%;"');
+		$this->add_html_editor(Profile :: PROPERTY_COMPETENCES, get_lang('Competences'), false);
+		$this->add_html_editor(Profile :: PROPERTY_DIPLOMAS, get_lang('Diplomas'), false);
+		$this->add_html_editor(Profile :: PROPERTY_TEACHING, get_lang('Teaching'), false);
+		$this->add_html_editor(Profile :: PROPERTY_OPEN, get_lang('Open'), false);
+		$this->add_textfield(Profile :: PROPERTY_SKYPE, get_lang('Skype'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_MSN, get_lang('Msn'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_AIM, get_lang('Aim'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_YIM, get_lang('Yim'), false,'size="40" style="width: 100%;"');
+		$this->add_textfield(Profile :: PROPERTY_ICQ, get_lang('Icq'), false,'size="40" style="width: 100%;"');
 	}
 	function setDefaults($defaults = array ())
 	{
