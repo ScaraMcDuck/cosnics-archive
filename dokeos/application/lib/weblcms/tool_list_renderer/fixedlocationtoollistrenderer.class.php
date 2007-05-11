e7c270a101fb82dd5e0cfd50b25cb4a130c965bd
bmol <bmol@cosnics>
2007-05-11 09:13:41 +0000

    Hide eye icon if not course admin Remove link to tool if not course admin and tool not visible

--- dokeos/application/lib/weblcms/tool_list_renderer/fixedlocationtoollistrenderer.class.php
+++ dokeos/application/lib/weblcms/tool_list_renderer/fixedlocationtoollistrenderer.class.php
@@ -16,12 +16,17 @@ class FixedLocationToolListRenderer extends ToolListRenderer
 	 */
 	const NUMBER_OF_COLUMNS = 3;
 	/**
+	 *
+	 */
+	private $is_course_admin;
+	/**
 	 * Constructor
 	 * @param  WebLcms $parent The parent application
 	 */
 	function FixedLocationToolListRenderer($parent)
 	{
 		parent::ToolListRenderer($parent);
+		$this->is_course_admin = $this->get_parent()->get_course()->is_course_admin($this->get_parent()->get_user_id());
 	}
 	// Inherited
 	function display()
@@ -34,7 +39,7 @@ class FixedLocationToolListRenderer extends ToolListRenderer
 		}
 		$this->show_tools('basic',$tools);
 
-		if ($this->get_parent()->get_course()->is_course_admin($this->get_parent()->get_user_id()))
+		if ($this->is_course_admin)
 		{
 			echo '<h4>'.get_lang('CourseAdministration').'</h4>';
 			$this->show_tools('course_admin',$tools);
@@ -77,11 +82,17 @@ class FixedLocationToolListRenderer extends ToolListRenderer
 			$row = $count/FixedLocationToolListRenderer::NUMBER_OF_COLUMNS;
 			$col = $count%FixedLocationToolListRenderer::NUMBER_OF_COLUMNS;
 			$html = array();
-			$html[] = '<a href="'.$parent->get_url(array (WebLcms :: PARAM_COMPONENT_ACTION=>null,WebLcms :: PARAM_TOOL => $tool->name), true).'" '.$link_class.'>';
+			if($this->is_course_admin || $tool->visible)
+			{
+				$html[] = '<a href="'.$parent->get_url(array (WebLcms :: PARAM_COMPONENT_ACTION=>null,WebLcms :: PARAM_TOOL => $tool->name), true).'" '.$link_class.'>';
+			}
 			$html[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/'.$tool_image.'" style="vertical-align: middle;" alt="'.$title.'"/>';
 			$html[] = $title;
-			$html[] = '</a>';
-			if($section!= 'course_admin')
+			if($this->is_course_admin || $tool->visible)
+			{
+				$html[] = '</a>';
+			}
+			if($this->is_course_admin && $section!= 'course_admin')
 			{
 				$html[] = '<a href="'.$parent->get_url(array(WebLcms :: PARAM_COMPONENT_ACTION=>$lcms_action,WebLcms :: PARAM_TOOL=>$tool->name)).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/'.$visible_image.'" alt=""/></a>';
 			}
