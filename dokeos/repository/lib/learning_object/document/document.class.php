cb2289c124f6e585eff2274dc2d54f741195b440
bmol <bmol@localhost>
2007-05-14 11:30:07 +0000

    Adding download icon to document tool

--- dokeos/repository/lib/learning_object/document/document.class.php
+++ dokeos/repository/lib/learning_object/document/document.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * $Id$
  * @package repository.learningobject
@@ -46,32 +47,32 @@ class Document extends LearningObject
 		unlink($path);
 		parent :: delete();
 	}
-	
+
 	function delete_version()
 	{
 		$path = Configuration :: get_instance()->get_parameter('general', 'upload_path');
 		$path = $path.'/'.$this->get_path();
-		if(RepositoryDataManager :: get_instance()->is_only_document_occurence($this->get_path()))
+		if (RepositoryDataManager :: get_instance()->is_only_document_occurence($this->get_path()))
 		{
 			unlink($path);
 		}
 		parent :: delete_version();
 	}
-	
+
 	function get_url()
 	{
-		return Configuration::get_instance()->get_parameter('general', 'upload_url').'/'.$this->get_path();
+		return Configuration :: get_instance()->get_parameter('general', 'upload_url').'/'.$this->get_path();
 	}
 	function get_full_path()
 	{
-		return realpath(Configuration::get_instance()->get_parameter('general', 'upload_path').'/'.$this->get_path());
+		return realpath(Configuration :: get_instance()->get_parameter('general', 'upload_path').'/'.$this->get_path());
 	}
 	function get_icon_name()
 	{
 		$filename = $this->get_filename();
-		$parts = explode('.',$filename);
-		$icon_name = $parts[count($parts)-1];
-		if( !file_exists(api_get_path(SYS_CODE_PATH).'/img/'.$icon_name.'.gif'))
+		$parts = explode('.', $filename);
+		$icon_name = $parts[count($parts) - 1];
+		if (!file_exists(api_get_path(SYS_CODE_PATH).'/img/'.$icon_name.'.gif'))
 		{
 			return 'document';
 		}
@@ -80,6 +81,34 @@ class Document extends LearningObject
 	static function get_disk_space_properties()
 	{
 		return 'filesize';
+	}
+	function send_as_download()
+	{
+		header('Expires: Wed, 01 Jan 1990 00:00:00 GMT');
+		header('Cache-Control: public');
+		header('Pragma: no-cache');
+		header('Content-type: application/octet-stream');
+		//header('Content-Type: application/force-download');
+		header('Content-length: '.$this->get_filesize());
+		if (preg_match("/MSIE 5.5/", $_SERVER['HTTP_USER_AGENT']))
+		{
+			header('Content-Disposition: filename= '.$this->get_filename());
+		}
+		else
+		{
+			header('Content-Disposition: attachment; filename= '.$this->get_filename());
+		}
+		if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE'))
+		{
+			header('Pragma: ');
+			header('Cache-Control: ');
+			header('Cache-Control: public'); // IE cannot download from sessions without a cache
+		}
+		header('Content-Description: '.$this->get_filename());
+		header('Content-transfer-encoding: binary');
+		$fp = fopen($this->get_full_path(), 'r');
+		fpassthru($fp);
+		return true;
 	}
 }
 ?>
