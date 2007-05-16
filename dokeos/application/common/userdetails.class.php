d169094884cd1c097108e7b8af5718b95386efe7
bmol <bmol@cosnics>
2007-05-16 13:41:03 +0000

    Update who is online list. Use sortable table (recovered from other Dokeos version). New look for user details

--- dokeos/application/common/userdetails.class.php
+++ dokeos/application/common/userdetails.class.php
@@ -23,11 +23,17 @@ class UserDetails
 	 */
 	public function toHtml()
 	{
-		$html[] = '<img src="'.$this->user->get_full_picture_url().'" alt="'.$this->user->get_fullname().'" style="max-width: 150px; border:1px solid black;"/>';
+		$html[] = '<div class="user_details" style="background-image: url('.api_get_path(WEB_CODE_PATH).'/img/profile.gif);">';
+		$html[] = '<img src="'.$this->user->get_full_picture_url().'" alt="'.$this->user->get_fullname().'" style="margin: 10px;max-height: 150px; border:1px solid black;float: right; display: inline;"/>';
+		$html[] = '<div class="title">';
 		$html[] = $this->user->get_fullname();
+		$html[] = '</div>';
+		$html[] = '<div class="description">';
 		$html[] = '<a href="mailto:'.$this->user->get_email().'">'.$this->user->get_email().'</a>';
-		$html[] = $this->user->get_username();
-		return implode("<br />\n",$html);
+		$html[] = '<br />'.$this->user->get_username();
+		$html[] = '</div>';
+		$html[] = '</div>';
+		return implode("\n",$html);
 	}
 }
 ?>
