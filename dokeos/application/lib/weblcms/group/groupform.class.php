1cbe298c50a71545fbce94a9f3507018b19d84db
bmol <bmol@localhost>
2007-05-31 09:48:35 +0000

    Adding some extra properties to group objects

--- dokeos/application/lib/weblcms/group/groupform.class.php
+++ dokeos/application/lib/weblcms/group/groupform.class.php
@@ -42,6 +42,8 @@ class GroupForm extends FormValidator {
 		$this->add_html_editor(Group :: PROPERTY_DESCRIPTION, get_lang('Description'));
 		$this->addElement('text', Group::PROPERTY_MAX_NUMBER_OF_MEMBERS,get_lang('MaxNumberOfMembers'),'size="4"');
 		$this->addRule(Group::PROPERTY_MAX_NUMBER_OF_MEMBERS, get_lang('ThisFieldShouldBeNumeric'), 'regex','/^[0-9]*$/');
+		$this->addElement('checkbox',Group::PROPERTY_SELF_REG,get_lang('Registration'),get_lang('SelfRegAllowed'));
+		$this->addElement('checkbox',Group::PROPERTY_SELF_UNREG,null,get_lang('SelfUnRegAllowed'));
 		$this->addElement('submit', 'group_settings', get_lang('Ok'));
     }
 
@@ -66,6 +68,8 @@ class GroupForm extends FormValidator {
     	$group->set_name($values[Group :: PROPERTY_NAME]);
     	$group->set_description($values[Group :: PROPERTY_DESCRIPTION]);
     	$group->set_max_number_of_members($values[Group :: PROPERTY_MAX_NUMBER_OF_MEMBERS]);
+   		$group->set_self_registration_allowed($values[Group :: PROPERTY_SELF_REG]);
+		$group->set_self_unregistration_allowed($values[Group :: PROPERTY_SELF_UNREG]);
     	return $group->update();
     }
 
@@ -77,6 +81,8 @@ class GroupForm extends FormValidator {
     	$group->set_name($values[Group :: PROPERTY_NAME]);
     	$group->set_description($values[Group :: PROPERTY_DESCRIPTION]);
 		$group->set_max_number_of_members($values[Group :: PROPERTY_MAX_NUMBER_OF_MEMBERS]);
+		$group->set_self_registration_allowed($values[Group :: PROPERTY_SELF_REG]);
+		$group->set_self_unregistration_allowed($values[Group :: PROPERTY_SELF_UNREG]);
     	if ($group->create())
     	{
    			return true;
@@ -99,6 +105,8 @@ class GroupForm extends FormValidator {
 		$defaults[Group :: PROPERTY_NAME] = $group->get_name();
 		$$defaults[Group :: PROPERTY_DESCRIPTION] = $group->get_description();
 		$defaults[Group :: PROPERTY_MAX_NUMBER_OF_MEMBERS] = $group->get_max_number_of_members();
+		$defaults[Group :: PROPERTY_SELF_REG]= $group->is_self_registration_allowed();;
+		$defaults[Group :: PROPERTY_SELF_UNREG]= $group->is_self_unregistration_allowed();;
 		parent :: setDefaults($defaults);
 	}
 }
