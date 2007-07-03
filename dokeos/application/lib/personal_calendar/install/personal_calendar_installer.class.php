3add95c8e22222a3e9b8ac4ea7515eb682b686d7
bmol <bmol@localhost>
2007-07-03 11:32:57 +0000

    Reorganized install-code

--- dokeos/application/lib/personal_calendar/install/personal_calendar_installer.class.php
+++ dokeos/application/lib/personal_calendar/install/personal_calendar_installer.class.php
@@ -4,11 +4,12 @@
  * @package application.personal_calendar
  */
 require_once dirname(__FILE__).'/../personalcalendardatamanager.class.php';
+require_once dirname(__FILE__).'/../../../../common/installer.class.php';
 /**
  *	This installer can be used to create the storage structure for the
  * personal calendar application.
  */
-class PersonalCalendarInstaller {
+class PersonalCalendarInstaller extends Installer{
 	/**
 	 * Constructor
 	 */
@@ -16,47 +17,17 @@ class PersonalCalendarInstaller {
     }
 	/**
 	 * Runs the install-script.
-	 * @todo This function now uses the function of the RepositoryInstaller
-	 * class. These shared functions should be available in a common base class.
 	 */
 	function install()
 	{
-		$this->parse_xml_file(dirname(__FILE__).'/personal_calendar.xml');
+		$this->create_storage_unit(dirname(__FILE__).'/personal_calendar.xml');
 	}
-	function parse_xml_file($path)
+	function create_storage_unit($path)
 	{
-		$doc = new DOMDocument();
-		$doc->load($path);
-		$object = $doc->getElementsByTagname('object')->item(0);
-		$name = $object->getAttribute('name');
-		$xml_properties = $doc->getElementsByTagname('property');
-		foreach($xml_properties as $index => $property)
-		{
-			 $property_info = array();
-			 $property_info['type'] = $property->getAttribute('type');
-			 $property_info['length'] = $property->getAttribute('length');
-			 $property_info['unsigned'] = $property->getAttribute('unsigned');
-			 $property_info['notnull'] = $property->getAttribute('notnull');
-			 $property_info['default'] = $property->getAttribute('default');
-			 $property_info['autoincrement'] = $property->getAttribute('autoincrement');
-			 $property_info['fixed'] = $property->getAttribute('fixed');
-			 $properties[$property->getAttribute('name')] = $property_info;
-		}
-		$xml_indexes = $doc->getElementsByTagname('index');
-		foreach($xml_indexes as $key => $index)
-		{
-			 $index_info = array();
-			 $index_info['type'] = $index->getAttribute('type');
-			 $index_properties = $index->getElementsByTagname('indexproperty');
-			 foreach($index_properties as $subkey => $index_property)
-			 {
-			 	$index_info['fields'][$index_property->getAttribute('name')] = array();
-			 }
-			 $indexes[$index->getAttribute('name')] = $index_info;
-		}
+		$storage_unit_info = parent::parse_xml_file($path);
 		$dm = PersonalCalendarDataManager :: get_instance();
-		echo '<pre>Creating PersonalCalendar Storage Unit: '.$name.'</pre>';flush();
-		$dm->create_storage_unit($name,$properties,$indexes);
+		echo '<pre>Creating PersonalCalendar Storage Unit: '.$storage_unit_info['name'].'</pre>';flush();
+		$dm->create_storage_unit($storage_unit_info['name'],$storage_unit_info['properties'],$storage_unit_info['indexes']);
 	}
 }
 ?>
--- dokeos/application/lib/profiler/install/profiler_installer.class.php
+++ dokeos/application/lib/profiler/install/profiler_installer.class.php
@@ -3,12 +3,13 @@
  * @package application.lib.profiler.install
  */
 require_once dirname(__FILE__).'/../profilerdatamanager.class.php';
+require_once dirname(__FILE__).'/../../../../common/installer.class.php';
 /**
  * This installer can be used to create the storage structure for the
  * profiler application.
  */
-class ProfilerInstaller {
-	
+class ProfilerInstaller extends Installer {
+
 	private $pdm;
 	/**
 	 * Constructor
@@ -18,88 +19,16 @@ class ProfilerInstaller {
     }
 	/**
 	 * Runs the install-script.
-	 * @todo This function now uses the function of the RepositoryInstaller
-	 * class. These shared functions should be available in a common base class.
 	 */
 	function install()
 	{
-		$sqlfiles = array();
-		$dir = dirname(__FILE__);
-		$handle = opendir($dir);		
-		while (false !== ($type = readdir($handle)))
-		{
-			$path = $dir.'/'.$type;
-			if (file_exists($path) && (substr($path, -3) == 'xml'))
-			{
-				$this->parse_xml_file($path);
-			}
-			elseif (file_exists($path) && (substr($path, -3) == 'sql'))
-			{
-				$sqlfiles[] = $type;
-			}
-		}
-		for ($i = 0; $i < count($sqlfiles); $i++)
-		{
-			$this->parse_sql_file($dir , $sqlfiles[$i]);
-		}
-		closedir($handle);
+		$this->create_storage_unit(dirname(__FILE__).'/profiler_publication.xml');
 	}
-	
-	/**
-	 * Parses an sql file and sends the request to the database manager
-	 * @param String $directory
-	 * @param String $filename
-	 */
-	function parse_sql_file($directory, $sqlfilename)
+	function create_storage_unit($path)
 	{
-		$pdm = $this->pdm;
-		$path = $directory.'/'.$sqlfilename;
-		$filecontent = file_get_contents($path);
-		$sqlstring = explode("\n", $filecontent);
-		echo '<pre>Executing additional Profiler SQL statement(s)</pre>';flush();
-		foreach($sqlstring as $sqlstatement)
-		{
-			echo $sqlstatement. '<br />';
-			$pdm->ExecuteQuery($sqlstatement);
-		}
-		
-	}
-	
-	function parse_xml_file($path)
-	{
-		$doc = new DOMDocument();
-		$doc->load($path);
-		$object = $doc->getElementsByTagname('object')->item(0);
-		$name = $object->getAttribute('name');
-		$xml_properties = $doc->getElementsByTagname('property');
-		foreach($xml_properties as $index => $property)
-		{
-			 $property_info = array();
-			 $property_info['type'] = $property->getAttribute('type');
-			 $property_info['length'] = $property->getAttribute('length');
-			 $property_info['unsigned'] = $property->getAttribute('unsigned');
-			 $property_info['notnull'] = $property->getAttribute('notnull');
-			 $property_info['default'] = $property->getAttribute('default');
-			 $property_info['autoincrement'] = $property->getAttribute('autoincrement');
-			 $property_info['fixed'] = $property->getAttribute('fixed');
-			 $properties[$property->getAttribute('name')] = $property_info;
-		}
-		$xml_indexes = $doc->getElementsByTagname('index');
-		foreach($xml_indexes as $key => $index)
-		{
-			 $index_info = array();
-			 $index_info['type'] = $index->getAttribute('type');
-			 $index_properties = $index->getElementsByTagname('indexproperty');
-			 foreach($index_properties as $subkey => $index_property)
-			 {
-			 	$index_info['fields'][$index_property->getAttribute('name')] = array();
-			 }
-			 $indexes[$index->getAttribute('name')] = $index_info;
-		}
-		$pdm = $this->pdm;
-
-		echo '<pre>Creating Profiler Storage Unit: '.$name.'</pre>';flush();
-		$pdm->create_storage_unit($name,$properties,$indexes);
+		$storage_unit_info = parent::parse_xml_file($path);
+		echo '<pre>Creating Profiler Storage Unit: '.$storage_unit_info['name'].'</pre>';flush();
+		$this->pdm->create_storage_unit($storage_unit_info['name'],$storage_unit_info['properties'],$storage_unit_info['indexes']);
 	}
 }
 ?>
