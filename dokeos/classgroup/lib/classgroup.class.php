74d4d188a56a61cb2dbacead8faba4f1236df7a7
bmol <bmol@localhost>
2007-07-03 11:31:29 +0000

    First drafts for the classgroup object

---
+++ dokeos/classgroup/lib/classgroup.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class ClassGroup {
+	const PROPERTY_CLASSGROUP_ID = 'id';
+	const PROPERTY_NAME = 'name';
+    function ClassGroup() {
+    }
+}
+?>
---
+++ dokeos/classgroup/lib/data_manager/database.class.php
@@ -0,0 +1,578 @@
+<?php
+/**
+ * @package classgroup
+ * @subpackage datamanager
+ */
+require_once dirname(__FILE__).'/database/databaseuserresultset.class.php';
+require_once dirname(__FILE__).'/../classgroupdatamanager.class.php';
+require_once dirname(__FILE__).'/../classgroup.class.php';
+require_once 'MDB2.php';
+
+/**
+==============================================================================
+ *	This is a data manager that uses a database for storage. It was written
+ *	for MySQL, but should be compatible with most SQL flavors.
+ *
+ *	@author Tim De Pauw
+ *	@author Bart Mollet
+==============================================================================
+ */
+
+class DatabaseClassGroupDataManager extends ClassGroupDataManager
+{
+	const ALIAS_CLASS_GROUP_TABLE = 'cg';
+
+	/**
+	 * The database connection.
+	 */
+	private $connection;
+
+	/**
+	 * The table name prefix, if any.
+	 */
+	private $prefix;
+	private $repoDM;
+
+	/**
+	 * Initializes the connection
+	 */
+	function initialize()
+	{
+		$this->repoDM = & RepositoryDataManager :: get_instance();
+		$conf = Configuration :: get_instance();
+		$this->connection = MDB2 :: connect($conf->get_parameter('database', 'connection_string_user'),array('debug'=>3,'debug_handler'=>array('ClassGroupDatamanager','debug')));
+		$this->prefix = $conf->get_parameter('database', 'table_name_prefix');
+		$this->connection->query('SET NAMES utf8');
+	}
+
+	function debug()
+	{
+		$args = func_get_args();
+		// Do something with the arguments
+		if($args[1] == 'query')
+		{
+			//echo '<pre>';
+		 	//echo($args[2]);
+		 	//echo '</pre>';
+		}
+	}
+
+	/**
+	 * Escapes a column name in accordance with the database type.
+	 * @param string $name The column name.
+	 * @param boolean $prefix_learning_object_properties Whether or not to
+	 *                                                   prefix learning
+	 *                                                   object properties
+	 *                                                   to avoid collisions.
+	 * @return string The escaped column name.
+	 */
+	function escape_column_name($name, $prefix_user_object_properties = false)
+	{
+		list($table, $column) = explode('.', $name, 2);
+		$prefix = '';
+		if (isset($column))
+		{
+			$prefix = $table.'.';
+			$name = $column;
+		}
+		elseif ($prefix_user_object_properties && self :: is_user_column($name))
+		{
+			$prefix = self :: ALIAS_USER_TABLE.'.';
+		}
+		return $prefix.$this->connection->quoteIdentifier($name);
+	}
+
+	/**
+	 * Escapes a table name in accordance with the database type.
+	 * @param string $name The table identifier.
+	 * @return string The escaped table name.
+	 */
+	function escape_table_name($name)
+	{
+		global $user_database;
+		$database_name = $this->connection->quoteIdentifier($user_database);
+		return $database_name.'.'.$this->connection->quoteIdentifier($this->prefix.$name);
+	}
+
+	/**
+	 * Checks whether the given name is a user column
+	 * @param String $name The column name
+	 */
+	private static function is_user_column($name)
+	{
+		return User :: is_default_property_name($name); //|| $name == User :: PROPERTY_TYPE || $name == User :: PROPERTY_DISPLAY_ORDER_INDEX || $name == User :: PROPERTY_USER_ID;
+	}
+
+	/**
+	 * Checks whether the given column name is the name of a column that
+	 * contains a date value, and hence should be formatted as such.
+	 * @param string $name The column name.
+	 * @return boolean True if the column is a date column, false otherwise.
+	 */
+	static function is_date_column($name)
+	{
+		return ($name == LearningObject :: PROPERTY_CREATION_DATE || $name == LearningObject :: PROPERTY_MODIFICATION_DATE);
+	}
+
+	// Inherited.
+	function update_user($user)
+	{
+		$where = $this->escape_column_name(User :: PROPERTY_USER_ID).'='.$user->get_user_id();
+		$props = array();
+		foreach ($user->get_default_properties() as $key => $value)
+		{
+			$props[$this->escape_column_name($key)] = $value;
+		}
+		$this->connection->loadModule('Extended');
+		$this->connection->extended->autoExecute($this->get_table_name('user'), $props, MDB2_AUTOQUERY_UPDATE, $where);
+
+		return true;
+	}
+
+	//Inherited.
+	function update_user_quota($user_quota)
+	{
+		$where = $this->escape_column_name(Userquota :: PROPERTY_USER_ID).'='.$user_quota->get_user_id(). ' AND '. $this->escape_column_name(Userquota :: PROPERTY_LEARNING_OBJECT_TYPE).'="'.$user_quota->get_learning_object_type(). '"';
+		$props = array();
+		foreach ($user_quota->get_default_properties() as $key => $value)
+		{
+			$props[$this->escape_column_name($key)] = $value;
+		}
+		$props[Userquota :: PROPERTY_USER_ID] = $user_quota->get_user_id();
+		$this->connection->loadModule('Extended');
+		$quota_type = $this->retrieve_version_type_quota($this->retrieve_user($user_quota->get_user_id()), $user_quota->get_learning_object_type());
+		if ($quota_type)
+		{
+			$this->connection->extended->autoExecute($this->get_table_name('user_quota'), $props, MDB2_AUTOQUERY_UPDATE, $where);
+		}
+		else
+		{
+			$this->connection->extended->autoExecute($this->get_table_name('user_quota'), $props, MDB2_AUTOQUERY_INSERT);
+		}
+
+	return true;
+	}
+
+	// Inherited.
+	function get_next_user_id()
+	{
+		$id = $this->connection->nextID($this->get_table_name('user'));
+		return $id;
+	}
+
+	// Inherited.
+	function delete_user($user)
+	{
+		if(!$this->user_deletion_allowed($user))
+		{
+			return false;
+		}
+
+		$this->repoDM->delete_learning_object_by_user($user->get_user_id());
+		// Delete the user from the database
+		$query = 'DELETE FROM '.$this->escape_table_name('user').' WHERE '.$this->escape_column_name('user_id').'=?';
+		$sth = $this->connection->prepare($query);
+		$res = $sth->execute($user->get_user_id());
+
+		return true;
+	}
+
+	// Inherited.
+	function delete_all_users()
+	{
+		$users = $this->retrieve_users()->as_array();
+		foreach($users as $index => $user)
+		{
+			$this->delete_user($user);
+		}
+	}
+
+	// Inherited.
+	function create_user($user)
+	{
+		$props = array();
+		foreach ($user->get_default_properties() as $key => $value)
+		{
+			$props[$this->escape_column_name($key)] = $value;
+		}
+		$props[$this->escape_column_name(User :: PROPERTY_USER_ID)] = $user->get_user_id();
+		$this->connection->loadModule('Extended');
+		$this->connection->extended->autoExecute($this->get_table_name('user'), $props, MDB2_AUTOQUERY_INSERT);
+
+		// Create the user's root category for the repository
+		$this->repoDM->create_root_category($user->get_user_id());
+		return true;
+	}
+
+	// Inherited.
+	function create_storage_unit($name,$properties,$indexes)
+	{
+		$name = $this->get_table_name($name);
+		$this->connection->loadModule('Manager');
+		$manager = $this->connection->manager;
+		// If table allready exists -> drop it
+		// @todo This should change: no automatic table drop but warning to user
+		$tables = $manager->listTables();
+		if( in_array($name,$tables))
+		{
+			$manager->dropTable($name);
+		}
+		$options['charset'] = 'utf8';
+		$options['collate'] = 'utf8_unicode_ci';
+		$manager->createTable($name,$properties,$options);
+		foreach($indexes as $index_name => $index_info)
+		{
+			if($index_info['type'] == 'primary')
+			{
+				$index_info['primary'] = 1;
+				$manager->createConstraint($name,$index_name,$index_info);
+			}
+			else
+			{
+				$manager->createIndex($name,$index_name,$index_info);
+			}
+		}
+
+	}
+
+	/**
+	 * Expands a table identifier to the real table name. Currently, this
+	 * method prefixes the given table name with the user-defined prefix, if
+	 * any.
+	 * @param string $name The table identifier.
+	 * @return string The actual table name.
+	 */
+	function get_table_name($name)
+	{
+		global $user_database;
+		return $user_database.'.'.$this->prefix.$name;
+	}
+
+	//Inherited.
+	function retrieve_user($id)
+	{
+		$query = 'SELECT * FROM '.$this->escape_table_name('user').' AS '.self :: ALIAS_USER_TABLE.' WHERE '.$this->escape_column_name(User :: PROPERTY_USER_ID).'=?';
+		$this->connection->setLimit(1);
+		$statement = $this->connection->prepare($query);
+		$res = $statement->execute($id);
+		$record = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
+		$res->free();
+		return self :: record_to_user($record);
+	}
+
+	//Inherited.
+	function retrieve_user_by_username($username)
+	{
+		$query = 'SELECT * FROM '.$this->escape_table_name('user').' AS '.self :: ALIAS_USER_TABLE.' WHERE '.$this->escape_column_name(User :: PROPERTY_USERNAME).'=?';
+		$this->connection->setLimit(1);
+		$statement = $this->connection->prepare($query);
+		$res = $statement->execute($username);
+		$record = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
+		$res->free();
+		return self :: record_to_user($record);
+	}
+
+	/**
+	 * Parses a database record fetched as an associative array into an user.
+	 * @param array $record The associative array.
+	 * @return LearningObject The learning object.
+	 */
+	function record_to_user($record)
+	{
+		if (!is_array($record) || !count($record))
+		{
+			throw new Exception(get_lang('InvalidDataRetrievedFromDatabase'));
+		}
+		$defaultProp = array ();
+		foreach (User :: get_default_property_names() as $prop)
+		{
+			$defaultProp[$prop] = $record[$prop];
+		}
+		return new User($record[User :: PROPERTY_USER_ID], $defaultProp);
+	}
+
+	//Inherited.
+	function is_username_available($username, $user_id = null)
+	{
+		$params = array();
+		$query = 'SELECT username FROM '.$this->escape_table_name('user').' WHERE '.$this->escape_column_name(User :: PROPERTY_USERNAME).'=?';
+		$params[] = $username;
+		if ($user_id)
+		{
+			$query .=  ' AND '.$this->escape_column_name(User :: PROPERTY_USER_ID).' !=?';
+			$params[] = $user_id;
+		}
+		$statement = $this->connection->prepare($query);
+		$result = $statement->execute($params);
+		if ($result->numRows() == 1)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	//Inherited
+	function count_users($condition = null)
+	{
+		$params = array ();
+		$query = 'SELECT COUNT('.$this->escape_column_name(User :: PROPERTY_USER_ID).') FROM '.$this->escape_table_name('user').' AS '. self :: ALIAS_USER_TABLE;
+		if (isset ($condition))
+		{
+			$query .= ' WHERE '.$this->translate_condition($condition, & $params, true);
+		}
+		$sth = $this->connection->prepare($query);
+		$res = $sth->execute($params);
+		$record = $res->fetchRow(MDB2_FETCHMODE_ORDERED);
+		return $record[0];
+	}
+
+	//Inherited.
+	function retrieve_users($condition = null, $offset = null, $maxObjects = null, $orderBy = null, $orderDir = null)
+	{
+		$query = 'SELECT * FROM '. $this->escape_table_name('user'). ' AS '. self :: ALIAS_USER_TABLE;
+
+		$params = array ();
+		if (isset ($condition))
+		{
+			$query .= ' WHERE '.$this->translate_condition($condition, & $params, true);
+		}
+		/*
+		 * Always respect display order as a last resort.
+		 */
+		$orderBy[] = User :: PROPERTY_LASTNAME;
+		$orderDir[] = SORT_ASC;
+		$order = array ();
+
+		for ($i = 0; $i < count($orderBy); $i ++)
+		{
+			$order[] = $this->escape_column_name($orderBy[$i], true).' '. ($orderDir[$i] == SORT_DESC ? 'DESC' : 'ASC');
+		}
+		if (count($order))
+		{
+			$query .= ' ORDER BY '.implode(', ', $order);
+		}
+		if ($maxObjects < 0)
+		{
+			$maxObjects = null;
+		}
+
+		$this->connection->setLimit(intval($maxObjects),intval($offset));
+		$statement = $this->connection->prepare($query);
+		$res = $statement->execute($params);
+		return new DatabaseUserResultSet($this, $res);
+	}
+
+	//Inherited.
+	function retrieve_version_type_quota($user, $type)
+	{
+		$query = 'SELECT * FROM '.$this->escape_table_name('user_quota').' WHERE '.$this->escape_column_name(User :: PROPERTY_USER_ID).'=? AND '.$this->escape_column_name('learning_object_type').'=?';
+		$this->connection->setLimit(1);
+		$statement = $this->connection->prepare($query);
+		$res = $statement->execute(array($user->get_user_id(), $type));
+
+		if ($res->numRows() >= 1)
+		{
+			$record = $res->fetchRow(MDB2_FETCHMODE_ASSOC);
+			$res->free();
+			return $record['user_quota'];
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * Translates any type of condition to a SQL WHERE clause.
+	 * @param Condition $condition The Condition object.
+	 * @param array $params A reference to the query's parameter list.
+	 * @param boolean $prefix_learning_object_properties Whether or not to
+	 *                                                   prefix learning
+	 *                                                   object properties
+	 *                                                   to avoid collisions.
+	 * @return string The WHERE clause.
+	 */
+	function translate_condition($condition, & $params, $prefix_learning_object_properties = false)
+	{
+		if(is_array($condition))
+		{
+			if(count($condition) == 1)
+			{
+				$condition = $condition[0];
+			}
+			else
+			{
+				$condition = new AndCondition($condition);
+			}
+		}
+		if ($condition instanceof AggregateCondition)
+		{
+			return $this->translate_aggregate_condition($condition, & $params, $prefix_learning_object_properties);
+		}
+		elseif ($condition instanceof InCondition)
+		{
+			return $this->translate_in_condition($condition, & $params, $prefix_learning_object_properties);
+		}
+		elseif ($condition instanceof Condition)
+		{
+			return $this->translate_simple_condition($condition, & $params, $prefix_learning_object_properties);
+		}
+		else
+		{
+			die('Need a Condition instance');
+		}
+	}
+
+	/**
+	 * Translates an aggregate condition to a SQL WHERE clause.
+	 * @param AggregateCondition $condition The AggregateCondition object.
+	 * @param array $params A reference to the query's parameter list.
+	 * @param boolean $prefix_learning_object_properties Whether or not to
+	 *                                                   prefix learning
+	 *                                                   object properties
+	 *                                                   to avoid collisions.
+	 * @return string The WHERE clause.
+	 */
+	function translate_aggregate_condition($condition, & $params, $prefix_learning_object_properties = false)
+	{
+		if ($condition instanceof AndCondition)
+		{
+			$cond = array ();
+			foreach ($condition->get_conditions() as $c)
+			{
+				$cond[] = $this->translate_condition($c, & $params, $prefix_learning_object_properties);
+			}
+			return '('.implode(' AND ', $cond).')';
+		}
+		elseif ($condition instanceof OrCondition)
+		{
+			$cond = array ();
+			foreach ($condition->get_conditions() as $c)
+			{
+				$cond[] = $this->translate_condition($c, & $params, $prefix_learning_object_properties);
+			}
+			return '('.implode(' OR ', $cond).')';
+		}
+		elseif ($condition instanceof NotCondition)
+		{
+			return 'NOT ('.$this->translate_condition($condition->get_condition(), & $params, $prefix_learning_object_properties) . ')';
+		}
+		else
+		{
+			die('Cannot translate aggregate condition');
+		}
+	}
+
+	/**
+	 * Translates an in condition to a SQL WHERE clause.
+	 * @param InCondition $condition The InCondition object.
+	 * @param array $params A reference to the query's parameter list.
+	 * @param boolean $prefix_learning_object_properties Whether or not to
+	 *                                                   prefix learning
+	 *                                                   object properties
+	 *                                                   to avoid collisions.
+	 * @return string The WHERE clause.
+	 */
+	function translate_in_condition($condition, & $params, $prefix_learning_object_properties = false)
+	{
+		if ($condition instanceof InCondition)
+		{
+			$name = $condition->get_name();
+			$where_clause = $this->escape_column_name($name).' IN (';
+			$values = $condition->get_values();
+			$placeholders = array();
+			foreach($values as $index => $value)
+			{
+				$placeholders[] = '?';
+				$params[] = $value;
+			}
+			$where_clause .= implode(',',$placeholders).')';
+			return $where_clause;
+		}
+		else
+		{
+			die('Cannot translate in condition');
+		}
+	}
+
+	/**
+	 * Translates a simple condition to a SQL WHERE clause.
+	 * @param Condition $condition The Condition object.
+	 * @param array $params A reference to the query's parameter list.
+	 * @param boolean $prefix_learning_object_properties Whether or not to
+	 *                                                   prefix learning
+	 *                                                   object properties
+	 *                                                   to avoid collisions.
+	 * @return string The WHERE clause.
+	 */
+	function translate_simple_condition($condition, & $params, $prefix_learning_object_properties = false)
+	{
+		if ($condition instanceof EqualityCondition)
+		{
+			$name = $condition->get_name();
+			$value = $condition->get_value();
+			if (self :: is_date_column($name))
+			{
+				$value = self :: to_db_date($value);
+			}
+			if (is_null($value))
+			{
+				return $this->escape_column_name($name).' IS NULL';
+			}
+			$params[] = $value;
+			return $this->escape_column_name($name, $prefix_learning_object_properties).' = ?';
+		}
+		elseif ($condition instanceof LikeCondition)
+		{
+			$name = $condition->get_name();
+			$value = $condition->get_value();
+			if (is_null($value))
+			{
+				return $this->escape_column_name($name).' IS NULL';
+			}
+			$params[] = $value;
+			return $this->escape_column_name($name, $prefix_learning_object_properties).' LIKE ?';
+		}
+		elseif ($condition instanceof InequalityCondition)
+		{
+			$name = $condition->get_name();
+			$value = $condition->get_value();
+			if (self :: is_date_column($name))
+			{
+				$value = self :: to_db_date($value);
+			}
+			$params[] = $value;
+			switch ($condition->get_operator())
+			{
+				case InequalityCondition :: GREATER_THAN :
+					$operator = '>';
+					break;
+				case InequalityCondition :: GREATER_THAN_OR_EQUAL :
+					$operator = '>=';
+					break;
+				case InequalityCondition :: LESS_THAN :
+					$operator = '<';
+					break;
+				case InequalityCondition :: LESS_THAN_OR_EQUAL :
+					$operator = '<=';
+					break;
+				default :
+					die('Unknown operator for inequality condition');
+			}
+			return $this->escape_column_name($name, $prefix_learning_object_properties).' '.$operator.' ?';
+		}
+		elseif ($condition instanceof PatternMatchCondition)
+		{
+			$params[] = $this->translate_search_string($condition->get_pattern());
+			return $this->escape_column_name($condition->get_name(), $prefix_learning_object_properties).' LIKE ?';
+		}
+		else
+		{
+			die('Cannot translate condition');
+		}
+	}
+}
+?>
