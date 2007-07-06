067309c9a9e03e069d32bd8525a6e1cf073fb56d
bmol <bmol@cosnics>
2007-07-06 10:50:00 +0000

    some code cleaning of header & footer

---
+++ dokeos/common/footer.class.php
@@ -0,0 +1,32 @@
+<?php
+class Footer
+{
+	private $version;
+	function Footer($version)
+	{
+		$this->version = $version;
+	}
+	function display()
+	{
+		$output[] = '    <div class="clear">&nbsp;</div> <!-- "clearing" div to make sure that footer stays below the main and right column sections -->';
+		$output[] = '   </div> <!-- end of #main" started at the end of claro_init_banner.inc.php -->';
+		$output[] = '   <div id="footer"> <!-- start of #footer section -->';
+		$output[] = '    <div class="copyright">';
+		$output[] = '     '.get_lang('Platform').'&nbsp;<a href="http://www.dokeos.com">'.$this->version.'</a>&nbsp;&copy;&nbsp;'.date('Y');
+		$output[] = '    </div>';
+		$admin_data = '';
+		if (get_setting('show_administrator_data') == "true")
+		{
+			$admin_data .= get_lang('Manager');
+			$admin_data .= '&nbsp;:&nbsp;';
+			$admin_data .= Display :: encrypted_mailto_link(get_setting('emailAdministrator'), get_setting('administratorSurname').' '.get_setting('administratorName'));
+		}
+		$output[] = '    '.$admin_data.'&nbsp;';
+		$output[] = '   </div> <!-- end of #footer -->';
+		$output[] = '  </div> <!-- end of #outerframe opened in header -->';
+		$output[] = ' </body>';
+		$output[] = '</html>';
+		echo implode("\n",$output);
+	}
+}
+?>
--- dokeos/main/inc/claro_init_header.inc.php
+++ dokeos/main/inc/claro_init_header.inc.php
@@ -1,38 +1,9 @@
 <?php // $Id$
 /**
-==============================================================================
-*	This script displays the Dokeos header.
-*
-*	@package dokeos.include
-==============================================================================
-*/
-
-/*----------------------------------------
-              HEADERS SECTION
-  --------------------------------------*/
-
-/*
- * HTTP HEADER
+ *	This script displays the Dokeos header.
+ *	@package include
  */
-
-//Give a default value to $charset. Should change to UTF-8 some time in the future.
-//This parameter should be set in the platform configuration interface in time.
-if(empty($charset))
-{
-	$charset = 'ISO-8859-15';
-}
-$charset = 'UTF-8';
-//header('Content-Type: text/html; charset='. $charset)
-//	or die ("WARNING : it remains some characters before &lt;?php bracket or after ?&gt end");
-
-header('Content-Type: text/html; charset='. $charset);
-if ( isset($httpHeadXtra) && $httpHeadXtra )
-{
-	foreach($httpHeadXtra as $thisHttpHead)
-	{
-		header($thisHttpHead);
-	}
-}
+require_once(dirname(__FILE__).'/../../common/header.class.php');
 
 // Get language iso-code for this page - ignore errors
 // The error ignorance is due to the non compatibility of function_exists()
@@ -44,63 +15,32 @@ if(empty($document_language))
   $document_language = 'en';
 }
 
-/*
- * HTML HEADER
- */
 
-echo '<!DOCTYPE html
-     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
-echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="'. $document_language .'" lang="'. $document_language .'">'."\n";
-echo '<head>'."\n";
-echo '<title>';
-
-if(!empty($nameTools))
+$header = new Header($document_language);
+$header->add_default_headers();
+$header->set_page_title((empty($nameTools) ? '' : $nameTools.' - ').get_setting('siteName'));
+if ( isset($httpHeadXtra) && $httpHeadXtra )
 {
-	echo $nameTools.' - ';
+	foreach($httpHeadXtra as $thisHttpHead)
+	{
+		$header->add_http_header($thisHttpHead);
+	}
 }
-
-echo get_setting('siteName');
-
-echo '</title>'."\n";
-echo '<style type="text/css" media="screen, projection">'."\n";
-echo '/*<![CDATA[*/'."\n";
-echo '@import "'. api_get_path(WEB_CODE_PATH) .'css/default.css";'."\n";
-echo '/*]]>*/'."\n";
-echo '</style>'."\n";
-echo '<style type="text/css" media="print">'."\n";
-echo '/*<![CDATA[*/'."\n";
-echo '@import "'. api_get_path(WEB_CODE_PATH) .'>css/print.css";'."\n";
-echo '/*]]>*/'."\n";
-echo '</style>'."\n";
-
 if(api_get_setting('stylesheets')<>'')
 {
-	echo '<style type="text/css" media="screen, projection">'."\n";
-	echo '/*<![CDATA[*/'."\n";
-	echo '@import "'. api_get_path(WEB_CODE_PATH). 'css/'. api_get_setting('stylesheets') .'/default.css";'."\n";
-	echo '/*]]>*/'."\n";
-	echo '</style>'."\n";
+	$header->add_css_file_header(api_get_path(WEB_CODE_PATH). 'css/'. api_get_setting('stylesheets') .'/default.css');
 }
-
-echo '<link rel="top" href="'. api_get_path(WEB_PATH). 'index.php" title="" />'."\n";
-echo '<link rel="courses" href="'. api_get_path(WEB_CODE_PATH). 'auth/courses.php" title="'. htmlentities(get_lang('OtherCourses')). '" />'."\n";
-echo '<link rel="profil" href="'. api_get_path(WEB_CODE_PATH). 'auth/profile.php" title="'. htmlentities(get_lang('ModifyProfile')). '" />'."\n";
-echo '<link href="http://www.dokeos.com/documentation.php" rel="Help" />'."\n";
-echo '<link href="http://www.dokeos.com/team.php" rel="Author" />'."\n";
-echo '<link href="http://www.dokeos.com" rel="Copyright" />'."\n";
-echo '<link rel="shortcut icon" href="'. api_get_path(WEB_PATH). 'favicon.ico" type="image/x-icon" />'."\n";
-echo '<meta http-equiv="Content-Type" content="text/html; charset='. $charset .'" />'."\n";
-
 if ( isset($htmlHeadXtra) && $htmlHeadXtra )
 {
 	foreach($htmlHeadXtra as $this_html_head)
 	{
-		echo($this_html_head);
+		$header->add_html_header($this_html_head);
 	}
 }
+$header->display();
 
-echo '</head>'."\n";
+
+
 echo '<body dir="'. $text_dir.'"';
 if(defined('DOKEOS_HOMEPAGE') && DOKEOS_HOMEPAGE)
 {
