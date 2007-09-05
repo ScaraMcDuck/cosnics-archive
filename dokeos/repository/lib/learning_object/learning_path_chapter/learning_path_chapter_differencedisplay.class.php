9f7d2a72bd011554ad6a5e982de5225c48b4deef
ceetee <ceetee@localhost>
2007-09-05 08:31:52 +0000

    Correcting filename.

--- dokeos/repository/lib/learning_object/learning_path_chapter/learning_path_chapter_differencedisplay.class.php
+++ dokeos/repository/lib/learning_object/learning_path_chapter/learning_path_chapter_differencedisplay.class.php
@@ -6,7 +6,7 @@
 /**
  * This class can be used to display the difference between learning paths
  */
-class LearningPathDifferenceDisplay extends LearningObjectDifferenceDisplay
+class LearningPathChapterDifferenceDisplay extends LearningObjectDifferenceDisplay
 {
 }
 ?>
