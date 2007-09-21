7a30608c0d992e6a97fcaa0ed2f2f9425f51a42c
bmol <bmol@localhost>
2007-09-21 11:12:20 +0000

    first include main api, then get lang files...

--- dokeos/main/help/allowed_html_tags.php
+++ dokeos/main/help/allowed_html_tags.php
@@ -4,34 +4,34 @@
 /*
 ==============================================================================
 	Dokeos - elearning and course management software
-	
+
 	Copyright (c) 2004-2005 Dokeos S.A.
 	Copyright (c) Bart Mollet, Hogeschool Gent
-	
+
 	For a full list of contributors, see "credits.txt".
 	The full license can be read in "license.txt".
-	
+
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
 	as published by the Free Software Foundation; either version 2
 	of the License, or (at your option) any later version.
-	
+
 	See the GNU General Public License for more details.
-	
+
 	Contact address: Dokeos, 44 rue des palais, B-1030 Brussels, Belgium
 	Mail: info@dokeos.com
 ==============================================================================
 */
 /**
-============================================================================== 
+==============================================================================
 *	This	 script displays a help window with an overview of the allowed HTML-
 *   tags  and their attributes.
 *
 *	@package dokeos.help
-============================================================================== 
+==============================================================================
 */
+require_once ('../inc/claro_init_global.inc.php');
 api_use_lang_files('help');
-include ('../inc/claro_init_global.inc.php');
 require_once (api_get_library_path().'/formvalidator/FormValidator.class.php');
 require_once (api_get_library_path().'/formvalidator/Rule/HTML.php');
 $language_code = Database :: get_language_isocode($language_interface);
