67eb4571f6e44db941e62cef0da5e8d2f55fb4e4
bmol <bmol@localhost>
2007-09-25 14:17:26 +0000

    added application parameter

--- dokeos/application/lib/weblcms/weblcms_manager/weblcms.class.php
+++ dokeos/application/lib/weblcms/weblcms_manager/weblcms.class.php
@@ -394,7 +394,7 @@ class Weblcms extends WebApplication
 			echo '<form method="get" action="'.$this->get_url().'" style="display: inline;">';
 			echo '<input type="hidden" name="'.self :: PARAM_ACTION.'" value="courseviewer" />';
 			echo '<input type="hidden" name="'.self :: PARAM_COURSE.'" value="'. $this->get_course_id() .'" />';
-			echo '<input type="hidden" name="application" value="'.$this->get_parameter('application').'"/>';
+			echo '<input type="hidden" name="'.WebApplication :: PARAM_APPLICATION.'" value="'.$this->get_parameter('application').'"/>';
 			echo '<select name="'.self :: PARAM_TOOL.'" onchange="submit();">';
 			$tools = array ();
 			foreach ($this->get_registered_tools() as $t)
