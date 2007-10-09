35ae058748699354598d1663668e0fe8bee5d602
bmol <bmol@cosnics>
2007-10-09 10:45:17 +0000

    move soap server file to the search_portal application

--- dokeos/soap_server.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/main/inc/claro_init_global.inc.php';
-require_once dirname(__FILE__).'/application/lib/search_portal/search_source/web_service/learningobjectsoapsearchserver.class.php';
-
-$server = new LearningObjectSoapSearchServer();
-$server->run();
-?>
