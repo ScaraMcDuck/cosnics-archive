611ca0c154f12023a4a723c24ab4ec87639ecaa0
bmol <bmol@cosnics>
2007-10-10 12:24:30 +0000

    implemented file extraction

--- dokeos/common/filecompression/pclzip/pclzipfilecompression.class.php
+++ dokeos/common/filecompression/pclzip/pclzipfilecompression.class.php
@@ -3,12 +3,22 @@
  * $Id$
  * @package filecompression
  */
+require_once api_get_path(SYS_PATH).'plugin/pclzip-2-6/pclzip.lib.php';
 /**
  * This class implements file compression and extraction using the PclZip
  * library
  */
-class PclzipFilecompression
+class PclzipFilecompression extends Filecompression
 {
-
+	function extract_file($file)
+	{
+		$dir = $this->create_temporary_directory();
+		$pclzip = new PclZip($file);
+		if($pclzip->extract(PCLZIP_OPT_PATH, $dir) == 0)
+		{
+			return false;
+		}
+		return $dir;
+	}
 }
 ?>
