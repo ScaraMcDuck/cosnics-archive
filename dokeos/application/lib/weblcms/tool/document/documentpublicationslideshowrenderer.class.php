44943755e2056418c38c0883eb9406f06602e1c3
bmol <bmol@localhost>
2007-10-26 09:00:22 +0000

    Adding thumbnail view for slideshow

--- dokeos/application/lib/weblcms/tool/document/documentpublicationslideshowrenderer.class.php
+++ dokeos/application/lib/weblcms/tool/document/documentpublicationslideshowrenderer.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * $Id: documentpublicationlistrenderer.class.php 12389 2007-05-14 11:30:07Z bmol $
  * Document tool - slideshow renderer
@@ -10,11 +11,11 @@ class DocumentPublicationSlideshowRenderer extends ListLearningObjectPublication
 	function as_html()
 	{
 		$publications = $this->get_publications();
-		if(count($publications) == 0)
+		if (count($publications) == 0)
 		{
-			$html[] = Display::display_normal_message(get_lang('NoPublicationsAvailable'),true);
+			$html[] = Display :: display_normal_message(get_lang('NoPublicationsAvailable'), true);
 		}
-		if(!isset($_GET['slideshow_index']))
+		if (!isset ($_GET['slideshow_index']))
 		{
 			$slideshow_index = 0;
 		}
@@ -22,40 +23,59 @@ class DocumentPublicationSlideshowRenderer extends ListLearningObjectPublication
 		{
 			$slideshow_index = $_GET['slideshow_index'];
 		}
-		$first = ($slideshow_index == 0);
-		$last = ($slideshow_index == count($publications) - 1);
-		$navigation[] = '<div style="text-align: center;">';
-		$navigation[] = ($slideshow_index+1).' / '.count($publications);
-		$navigation[] = '<div style="width=30%;text-align:left;float:left;">';
-		if(!$first)
+		if (isset ($_GET['thumbnails']))
 		{
-			$navigation[] = '<a href="'.$this->get_url(array('slideshow_index'=>0)).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/first.png" alt="'.get_lang('First').'"/></a>';
-			$navigation[] = '<a href="'.$this->get_url(array('slideshow_index'=>$slideshow_index-1)).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/prev.png" alt="'.get_lang('Previous').'"/></a>';
+			$toolbar_data[] = array(
+				'img'=>api_get_path(WEB_CODE_PATH).'img/slideshow.gif',
+				'label'=>get_lang('Slideshow'),
+				'href' => $this->get_url(array('thumbnails'=>null)),
+				'display' => RepositoryUtilities :: TOOLBAR_DISPLAY_ICON_AND_LABEL);
+			$html[] = RepositoryUtilities::build_toolbar($toolbar_data);
+			$html[] = $this->render_thumbnails($publications);
 		}
 		else
 		{
-			$navigation[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/first_na.png" alt="'.get_lang('First').'"/>';
-			$navigation[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/prev_na.png" alt="'.get_lang('Previous').'"/>';
-		}
-		$navigation[] = '</div>';
-		$navigation[] = '<div style="width=30%;text-align:right;float:right;">';
-		if(!$last)
-		{
-			$navigation[] = '<a href="'.$this->get_url(array('slideshow_index'=>$slideshow_index+1)).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/next.png" alt="'.get_lang('Next').'"/></a>';
-			$navigation[] = '<a href="'.$this->get_url(array('slideshow_index'=>count($publications)-1)).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/last.png" alt="'.get_lang('Last').'"/></a>';
-		}
-		else
-		{
-			$navigation[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/next_na.png" alt="'.get_lang('Next').'"/>';
-			$navigation[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/last_na.png" alt="'.get_lang('Last').'"/>';
+			$first = ($slideshow_index == 0);
+			$last = ($slideshow_index == count($publications) - 1);
+			$toolbar_data[] = array(
+				'img'=>api_get_path(WEB_CODE_PATH).'img/slideshow_thumbnails.gif',
+				'label'=>get_lang('Thumbnails'),
+				'href' => $this->get_url(array('thumbnails'=>1)),
+				'display' => RepositoryUtilities :: TOOLBAR_DISPLAY_ICON_AND_LABEL);
+			$html[] = RepositoryUtilities::build_toolbar($toolbar_data);
+			$navigation[] = '<div style="text-align: center;">';
+			$navigation[] = ($slideshow_index +1).' / '.count($publications);
+			$navigation[] = '<div style="width=30%;text-align:left;float:left;">';
+			if (!$first)
+			{
+				$navigation[] = '<a href="'.$this->get_url(array ('slideshow_index' => 0)).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/first.png" alt="'.get_lang('First').'"/></a>';
+				$navigation[] = '<a href="'.$this->get_url(array ('slideshow_index' => $slideshow_index -1)).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/prev.png" alt="'.get_lang('Previous').'"/></a>';
+			}
+			else
+			{
+				$navigation[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/first_na.png" alt="'.get_lang('First').'"/>';
+				$navigation[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/prev_na.png" alt="'.get_lang('Previous').'"/>';
+			}
+			$navigation[] = '</div>';
+			$navigation[] = '<div style="width=30%;text-align:right;float:right;">';
+			if (!$last)
+			{
+				$navigation[] = '<a href="'.$this->get_url(array ('slideshow_index' => $slideshow_index +1)).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/next.png" alt="'.get_lang('Next').'"/></a>';
+				$navigation[] = '<a href="'.$this->get_url(array ('slideshow_index' => count($publications) - 1)).'"><img src="'.api_get_path(WEB_CODE_PATH).'img/last.png" alt="'.get_lang('Last').'"/></a>';
+			}
+			else
+			{
+				$navigation[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/next_na.png" alt="'.get_lang('Next').'"/>';
+				$navigation[] = '<img src="'.api_get_path(WEB_CODE_PATH).'img/last_na.png" alt="'.get_lang('Last').'"/>';
 
+			}
+			$navigation[] = '</div>';
+			$navigation[] = '<div style="clear:both;"></div>';
+			$navigation[] = '</div>';
+			$html[] = implode("\n", $navigation);
+			$html[] = $this->render_publication($publications[$slideshow_index]);
+			$html[] = implode("\n", $navigation);
 		}
-		$navigation[] = '</div>';
-		$navigation[] = '<div style="clear:both;"></div>';
-		$navigation[] = '</div>';
-		$html[] = implode("\n",$navigation);
-		$html[] = $this->render_publication($publications[$slideshow_index]);
-		$html[] = implode("\n",$navigation);
 		return implode("\n", $html);
 	}
 	function render_publication($publication, $first = false, $last = false)
@@ -74,7 +94,36 @@ class DocumentPublicationSlideshowRenderer extends ListLearningObjectPublication
 		$html[] = '</div>';
 		$html[] = '</div>';
 		$html[] = '</div>';
-		return implode("\n",$html);
+		return implode("\n", $html);
+	}
+	function render_thumbnails($publications)
+	{
+		$thumbnail_size = 120;
+		foreach ($publications as $index => $publication)
+		{
+			$document = $publication->get_learning_object();
+			$img_url = $document->get_url();
+			$path = $document->get_full_path();
+			$imagesize = getimagesize($path);
+			$width = $imagesize[0];
+			$height = $imagesize[1];
+			if ($width <= $thumbnail_size && $height <= $thumbnail_size)
+			{
+				$style = '';
+			}
+			elseif ($width >= $height)
+			{
+				$style = 'width: 100px;';
+			}
+			elseif ($height > $width)
+			{
+				$style = 'height: 100px;';
+			}
+			$html[] = '<a href="'.$this->get_url(array ('slideshow_index' => $index)).'" style="border:1px solid #F0F0F0;margin: 2px;text-align: center;width: '.$thumbnail_size.'px;height:'.$thumbnail_size.'px;padding:5px;float:left;">';
+			$html[] = '<img src="'.$img_url.'" style="margin: auto;'.$style.'"/>';
+			$html[] = '</a>';
+		}
+		return implode("\n", $html);
 	}
 }
 ?>
