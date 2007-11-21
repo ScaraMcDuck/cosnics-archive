7c9f221fcc82824668923e89030af9be0702b105
bmol <bmol@cosnics>
2007-11-21 13:35:47 +0000

    Restoring feedback/description conflicts

---
+++ dokeos/repository/lib/learning_object/feedback/feedback_difference.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package repository.learningobject
+ * @subpackage feedback
+ */
+
+/**
+ * This class can be used to get the difference between feedbacks
+ */
+class FeedbackDifference extends LearningObjectDifference
+{
+}
+?>
---
+++ dokeos/repository/lib/learning_object/feedback/feedback_differencedisplay.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package repository.learningobject
+ * @subpackage feedback
+ */
+/**
+ * This class can be used to display the difference between feedbacks
+ */
+class FeedbackDifferenceDisplay extends LearningObjectDifferenceDisplay
+{
+}
+?>
