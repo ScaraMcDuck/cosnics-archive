0125f62ab5bf65e786e9a8863a7aae2cf5d4784c
bmol <bmol@localhost>
2007-11-21 13:44:02 +0000

    Restoring feedback/description conflicts

---
+++ dokeos/repository/lib/learning_object/description/description_form.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * $Id: description_form.class.php 9191 2006-09-01 11:48:41Z bmol $
+ * @package repository.learningobject
+ * @subpackage description
+ */
+require_once dirname(__FILE__).'/../../learningobjectform.class.php';
+require_once dirname(__FILE__).'/description.class.php';
+/**
+ * A form to create/update a description
+ */
+class DescriptionForm extends LearningObjectForm
+{
+	// Inherited
+	function create_learning_object()
+	{
+		$object = new Description();
+		$this->set_learning_object($object);
+		return parent :: create_learning_object();
+	}
+}
+
+?>
