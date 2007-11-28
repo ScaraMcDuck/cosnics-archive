8889c1b300b7de814cdc60d79f49fe60a49de03f
sbil136 <sbil136@cosnics>
2007-11-28 13:59:17 +0000

    added new Import class

---
+++ dokeos/common/import/import.class.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* This class provides some functions which can be used when importing data from
+* external files into Dokeos
+* @package	import
+*/
+class Import
+{
+	/**
+	 * Reads a CSV-file into an array. The first line of the CSV-file should
+	 * contain the array-keys. 
+	 * Example:
+	 *   FirstName;LastName;Email
+	 *   John;Doe;john.doe@mail.com
+	 *   Adam;Adams;adam@mail.com
+	 *  returns
+	 *   $result [0]['FirstName'] = 'John';
+	 *   $result [0]['LastName'] = 'Doe'; 
+	 *   $result [0]['Email'] = 'john.doe@mail. com';
+	 *   $result [1]['FirstName'] = 'Adam';
+	 *   ...
+	 * @param string $filename Path to the CSV-file which should be imported
+	 * @return array An array with all data from the CSV-file
+	 */
+	function csv_to_array($filename)
+	{
+		$result = array ();
+		$handle = fopen($filename, "r");
+		$keys = fgetcsv($handle, 1000, ";");
+		while (($row_tmp = fgetcsv($handle, 1000, ";")) !== FALSE)
+		{
+			
+			$row = array ();
+			foreach ($row_tmp as $index => $value)
+			{
+				$row[$keys[$index]] = $value;
+			}
+			$result[] = $row;
+		}
+		fclose($handle);
+		return $result;
+	}
+}
+?>
