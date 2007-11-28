84d81d1ada7324ad27163bc815eb824ea4fecb85
bmol <bmol@cosnics>
2007-11-28 08:49:59 +0000

    removing old code

--- dokeos/main/inc/lib/database.lib.php
+++ dokeos/main/inc/lib/database.lib.php
@@ -1,43 +1,43 @@
 <?php
 
-// $Id$ 
+// $Id$
 /*
-============================================================================== 
+==============================================================================
 	Dokeos - elearning and course management software
-	
+
 	Copyright (c) 2004-2005 Dokeos S.A.
 	Copyright (c) Roan Embrechts, Vrije Universiteit Brussel
 	Copyright (c) Patrick Cool, Ghent University
 	Copyright (c) Yannick Warnier, Dokeos S.A.
 	Copyright (c) Bart Mollet, Hogeschool Gent
-	
+
 	For a full list of contributors, see "credits.txt".
 	The full license can be read in "license.txt".
-	
+
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
 	as published by the Free Software Foundation; either version 2
 	of the License, or (at your option) any later version.
-	
+
 	See the GNU General Public License for more details.
-	
+
 	Contact address: Dokeos, 44 rue des palais, B-1030 Brussels, Belgium
 	Mail: info@dokeos.com
-============================================================================== 
+==============================================================================
 */
 /**
-============================================================================== 
+==============================================================================
 *	This is the main database library for Dokeos.
 *	Include/require it in your code to use its functionality.
 *
 *	@package dokeos.library
-============================================================================== 
+==============================================================================
 */
 
 /*
-============================================================================== 
+==============================================================================
 		CONSTANTS
-============================================================================== 
+==============================================================================
 */
 
 //main database tables
@@ -123,10 +123,10 @@ define("PERSONAL_AGENDA", "personal_agenda");
 define("USER_COURSE_CATEGORY_TABLE", "user_course_category");
 
 /*
-============================================================================== 
+==============================================================================
 		DATABASE CLASS
 		the class and its functions
-============================================================================== 
+==============================================================================
 */
 
 /**
@@ -175,24 +175,6 @@ class Database
 	}
 
 	/**
-	*	Returns the name of the main Dokeos database.
-	*/
-	function get_current_course_database()
-	{
-		$course_info = api_get_course_info();
-		return $course_info["dbName"];
-	}
-
-	/**
-	*	Returns the glued name of the current course database.
-	*/
-	function get_current_course_glued_database()
-	{
-		$course_info = api_get_course_info();
-		return $course_info["dbNameGlu"];
-	}
-
-	/**
 	*	The glue is the string needed between database and table.
 	*	The trick is: in multiple databases, this is a period (with backticks)
 	*	In single database, this can be e.g. an underscore so we just fake
@@ -202,18 +184,6 @@ class Database
 	function get_database_glue()
 	{
 		return $GLOBALS["dbGlu"];
-	}
-
-	/**
-	*	Returns the database prefix.
-	*	All created COURSE databases are prefixed with this string.
-	*	
-	*	TIP: this can be convenient e.g. if you have multiple Dokeos installations
-	*	on the same physical server.
-	*/
-	function get_database_name_prefix()
-	{
-		return $GLOBALS["dbNamePrefix"];
 	}
 
 	/**
@@ -230,13 +200,13 @@ class Database
 	/*
 	-----------------------------------------------------------------------------
 		Table Name functions
-		use these functions to get a table name for queries, 
+		use these functions to get a table name for queries,
 		instead of constructing them yourself.
-		
+
 		Backticks automatically surround the result,
 		e.g. `COURSE_NAME`.`link`
 		so the queries can look cleaner.
-		
+
 		Example:
 		$table = Database::get_course_document_table();
 		$sql_query = "SELECT * FROM $table WHERE $condition";
@@ -279,7 +249,7 @@ class Database
 
 	/**
 	 * This generic function returns the correct and complete name of any statistic table
-	 * of which you pass the short name as a parameter. 
+	 * of which you pass the short name as a parameter.
 	 * Please define table names as constants in this library and use them
 	 * instead of directly using magic words in your tool code.
 	 *
@@ -353,15 +323,6 @@ class Database
 	}
 
 	/**
-	* Returns the name of the data table of the SCORM database.
-	* @deprecated use Database :: get_scorm_table(SCORM_SCO_DATA_TABLE);
-	*/
-	function get_scorm_sco_data_table()
-	{
-		return Database :: get_scorm_table(SCORM_SCO_DATA_TABLE);
-	}
-
-	/**
 	* Returns the name of the document table a course. If no database parameter
 	* is present, the function works on the current course.
 	* @deprecated use Database::get_course_table(DOCUMENT_TABLE,$database_name);
@@ -382,36 +343,6 @@ class Database
 	}
 
 	/**
-	* Returns the name of the announcement table of a course. 	If no database
-	* parameter is present, the function works on 	the current course.
-	* @deprecated use Database::get_course_table(ANNOUNCEMENT_TABLE,$database_name);
-	*/
-	function get_course_announcement_table($database_name = '')
-	{
-		return Database :: get_course_table(ANNOUNCEMENT_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the chat-connected table of a course. 	If no
-	* database parameter is present, the function works on 	the current course.
-	* @deprecated use Database::get_course_table(CHAT_CONNECTED_TABLE,$database_name);
-	*/
-	function get_course_chat_connected_table($database_name = '')
-	{
-		return Database :: get_course_table(CHAT_CONNECTED_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the calendar_event table of a course. 	If no
-	* database parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(AGENDA_TABLE,$database_name);
-	*/
-	function get_course_agenda_table($database_name = '')
-	{
-		return Database :: get_course_table(AGENDA_TABLE, $database_name);
-	}
-
-	/**
 	* Returns the name of the tool table of a course. 	If no database parameter
 	* is present, the function works on the current course.
 	* @deprecated use Database::get_course_table(TOOL_LIST_TABLE,$database_name);
@@ -420,17 +351,6 @@ class Database
 	{
 		return Database :: get_course_table(TOOL_LIST_TABLE, $database_name);
 	}
-
-	/**
-	* Returns the name of the tool_intro table of a course. If no database
-	* parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(TOOL_INTRO_TABLE,$database_name);
-	*/
-	function get_course_tool_intro_table($database_name = '')
-	{
-		return Database :: get_course_table(TOOL_INTRO_TABLE, $database_name);
-	}
-
 	/**
 	* Returns the name of the scormdocument table of a course. 	If no database
 	* parameter is present, the function works on the current course.
@@ -439,67 +359,6 @@ class Database
 	function get_course_scormdocument_table($database_name = '')
 	{
 		return Database :: get_course_table(SCORMDOC_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the bb_forums table of a course. 	If no database
-	* parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(FORUM_TABLE,$database_name);
-	*/
-	function get_course_forum_table($database_name = '')
-	{
-		return Database :: get_course_table(FORUM_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the bb_categories table of a course. 	If no database
-	* parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(FORUM_CATEGORY_TABLE,$database_name);
-	*/
-	function get_course_forum_category_table($database_name = '')
-	{
-		return Database :: get_course_table(FORUM_CATEGORY_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the bb_posts table of a course. 	If no database
-	* parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(FORUM_POST_TABLE,$database_name);
-	*/
-	function get_course_forum_post_table($database_name = '')
-	{
-		return Database :: get_course_table(FORUM_POST_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the post text table of a course. 	If no database
-	* parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(FORUM_POST_TEXT_TABLE,$database_name);
-	*/
-	function get_course_forum_post_text_table($database_name = '')
-	{
-		return Database :: get_course_table(FORUM_POST_TEXT_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the forum topics table of a course. 	If no database
-	* parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(FORUM_TOPIC_TABLE,$database_name);
-	*/
-	function get_course_forum_topic_table($database_name = '')
-	{
-		return Database :: get_course_table(FORUM_TOPIC_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the item_property (previously last_tooledit) 	table of
-	* a course. If no database parameter is present, the 	function works on
-	* the current course.
-	* @deprecated use Database::get_course_table(LAST_TOOL_EDIT_TABLE,$database_name);
-	*/
-	function get_course_last_tool_edit_table($database_name = '')
-	{
-		return Database :: get_course_table(LAST_TOOL_EDIT_TABLE, $database_name);
 	}
 
 	/**
@@ -522,62 +381,6 @@ class Database
 		return Database :: get_course_table(GROUP_USER_TABLE, $database_name);
 	}
 
-	/**
-	* Returns the name of the course_description table of a course. 	If no
-	* database parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(COURSE_DESCRIPTION_TABLE,$database_name);
-	*/
-	function get_course_description_table($database_name = '')
-	{
-		return Database :: get_course_table(COURSE_DESCRIPTION_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the learnpath_main table of a course. 	If no
-	* database parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(LEARNPATH_MAIN_TABLE,$database_name);
-	*/
-	function get_course_learnpath_main_table($database_name = '')
-	{
-		return Database :: get_course_table(LEARNPATH_MAIN_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the learnpath_item table of a course. 	If no
-	* database parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(LEARNPATH_ITEM_TABLE,$database_name);
-	*/
-	function get_course_learnpath_item_table($database_name = '')
-	{
-		return Database :: get_course_table(LEARNPATH_ITEM_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the learnpath_chapter table of a course. 	If no
-	* database parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(LEARNPATH_CHAPTER_TABLE,$database_name);
-	*/
-	function get_course_learnpath_chapter_table($database_name = '')
-	{
-		return Database :: get_course_table(LEARNPATH_CHAPTER_TABLE, $database_name);
-	}
-
-	/**
-	* Returns the name of the learnpath_user table of a course. 	If no
-	* database parameter is present, the function works on the current course.
-	* @deprecated use Database::get_course_table(LEARNPATH_USER_TABLE,$database_name);
-	*/
-	function get_course_learnpath_user_table($database_name = '')
-	{
-		return Database :: get_course_table(LEARNPATH_USER_TABLE, $database_name);
-	}
-
-	/*
-	-----------------------------------------------------------------------------
-		Query Functions
-		these execute a query and return the result(s).
-	-----------------------------------------------------------------------------
-	*/
 
 	/**
 	*	@return a list (array) of all courses.
@@ -680,7 +483,7 @@ class Database
 
 		/*
 		still to do: (info taken from claro_init_local.inc.php)
-		
+
 		$_course['id'          ]         = $cData['cours_id'         ]; //auto-assigned integer
 		$_course['name'        ]         = $cData['title'            ];
 		$_course['official_code']        = $cData['visual_code'        ]; // use in echo
@@ -694,7 +497,7 @@ class Database
 		$_course['extLink'     ]['name'] = $cData['department_name'];
 		$_course['categoryCode']         = $cData['faCode'           ];
 		$_course['categoryName']         = $cData['faName'           ];
-		
+
 		$_course['visibility'  ]         = (bool) ($cData['visibility'] == 2 || $cData['visibility'] == 3);
 		$_course['registrationAllowed']  = (bool) ($cData['visibility'] == 1 || $cData['visibility'] == 2);
 		*/
@@ -722,17 +525,6 @@ class Database
 		#$result_array ['user_id'  ] = $result_array['user_id'   ];
 
 		return $result_array;
-	}
-
-	/**
-	* Changes the title of a course.
-	* @deprecated Function not in use
-	*/
-	function update_course_title($course_code, $new_title)
-	{
-		$table = Database :: get_main_table(MAIN_COURSE_TABLE);
-		$sql_query = "UPDATE $table SET `title` = '$new_title' WHERE `code` = '$course_code' LIMIT 1";
-		api_sql_query($sql_query, __FILE__, __LINE__);
 	}
 
 	/*
