b07ee345231ca0d5e73e35e437f31f1f78d6f605
bmol <bmol@localhost>
2007-12-04 13:28:33 +0000

    adding phpdoc

--- dokeos/common/export/export.class.php
+++ dokeos/common/export/export.class.php
@@ -10,17 +10,37 @@ require_once (api_get_library_path().'/document.lib.php');
  */
 abstract class Export
 {
+	/**
+	 * The filename which will be used for the export file.
+	 */
 	private $filename;
+	/**
+	 * Constructor
+	 * @param string $filename
+	 */
 	public function Export($filename)
 	{
 		$this->filename = $filename;
 		Export::get_supported_filetypes();
 	}
+	/**
+	 * Gets the filename
+	 * @return string
+	 */
 	protected function get_filename()
 	{
 		return $this->filename;
 	}
+	/**
+	 * Writes the given data to a file
+	 * @param array $data
+	 */
 	abstract function write_to_file($data);
+	/**
+	 * Gets the supported filetypes for export
+	 * @return array Array containig all supported filetypes (keys and values
+	 * are the same)
+	 */
 	public static function get_supported_filetypes()
 	{
 		$directories = FileSystem::get_directory_content(dirname(__FILE__),FileSystem::LIST_DIRECTORIES,false);
@@ -34,6 +54,13 @@ abstract class Export
 		}
 		return $types;
 	}
+	/**
+	 * Factory function to create an instance of an export class
+	 * @param string $type One of the supported file types returned by the
+	 * get_supported_filetypes function.
+	 * @param string $filename The desired filename for the export file
+	 * (extension will be automatically added depending on the given $type)
+	 */
 	public static function factory($type, $filename = 'export')
 	{
 		$file = dirname(__FILE__).'/'.$type.'/'.$type.'export.class.php';
