90417bd2591c62841f0dac330b724638a80cdd6c
bmol <bmol@cosnics>
2007-12-04 13:14:14 +0000

    Adding XML export

--- dokeos/common/export/export.class.php
+++ dokeos/common/export/export.class.php
@@ -44,36 +44,5 @@ abstract class Export
 			return new $class($filename.'.'.$type);
 		}
 	}
-	/**
-	 * Export tabular data to XML-file
-	 * @param array $data
-	 * @param string $filename
-	 */
-	function export_table_xml($data, $filename = 'export', $item_tagname = 'item', $wrapper_tagname = null)
-	{
-		$file = Filesystem::create_unique_name(api_get_path(SYS_ARCHIVE_PATH),'export.xml');
-		$handle = fopen($file, 'a+');
-		fwrite($handle, '<?xml version="1.0" encoding="ISO-8859-1"?>'."\n");
-		if (!is_null($wrapper_tagname))
-		{
-			fwrite($handle, "\t".'<'.$wrapper_tagname.'>'."\n");
-		}
-		foreach ($data as $index => $row)
-		{
-			fwrite($handle, '<'.$item_tagname.'>'."\n");
-			foreach ($row as $key => $value)
-			{
-				fwrite($handle, "\t\t".'<'.$key.'>'.$value.'</'.$key.'>'."\n");
-			}
-			fwrite($handle, "\t".'</'.$item_tagname.'>'."\n");
-		}
-		if (!is_null($wrapper_tagname))
-		{
-			fwrite($handle, '</'.$wrapper_tagname.'>'."\n");
-		}
-		fclose($handle);
-		DocumentManager :: file_send_for_download($file, true, $filename.'.xml');
-		exit;
-	}
 }
 ?>
