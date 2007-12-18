cd40e469ff7ae59e86a46af0e1f2b3008381cf90
iceman_be <iceman_be@cosnics>
2007-12-18 10:56:33 +0000

    latest modifications for the csv import functions added by Maarten

--- dokeos/repository/lib/repository_manager/component/csvcreator.class.php
+++ dokeos/repository/lib/repository_manager/component/csvcreator.class.php
@@ -20,7 +20,15 @@ class CSVCreator extends RepositoryManagerComponent
 		else 
 			{return false;}
 	}
+	//function to check 
+	function parent_split($parent)
+	{
+	echo $parent;
+	$aparent = explode('#',$parent);
+	$bparent = explode(' ',$aparent[1]);
+	return $bparent[0];
 
+	}
 	/*
 	 * Deze functie zal een array teruggeven
 	 * Indien er fouten voorkwamen in het csvbestand, zal deze functie een 
@@ -28,9 +36,9 @@ class CSVCreator extends RepositoryManagerComponent
 	 */
 	function csv_validate($typearray, $csvarray)
 	{
-		$foutenarray= array();
+		$errorarray= array();
 		$objectarray= array();
-		array_push($foutenarray, 0);
+		$errorarray[0]=''; 
 		
 		//elke regel in het csvbestand aflopen
 		for ($i = 0 ; $i < count($csvarray); $i++)
@@ -38,66 +46,62 @@ class CSVCreator extends RepositoryManagerComponent
 			//Kijken of het object in csv wel bestaat.
 			if (in_array($csvarray[$i][0],$typearray))
 			{	
+				$dataManager = RepositoryDataManager :: get_instance();
 				//Het eerste element van de regel is het type van het te importeren object.
-				$type = $csvarray[$i][0];					
-				$category = $_GET[RepositoryManager :: PARAM_CATEGORY_ID];
+				$type = $csvarray[$i][0];	
+				//retrieve the root category (this is for now , can be modded later on so users can include 					the category where they want everything to be added
 				$user = api_get_user_id();
+				$categorystring= $dataManager->retrieve_root_category($user);
+				$category= $this->parent_split($categorystring);
+				//create the abstract learning object			
 				$object = new AbstractLearningObject($type, $user, $category);
 				$message = '';
-
-				//Formulier voor dit object aanmaken
+				//Create a form for the Learning object
 				$lo_form = LearningObjectForm :: factory(LearningObjectForm :: TYPE_CREATE, $object, 'create');	
 				$valuearray= array();
-
-
-				//valuearray opvullen met de waarden van de regel en formulier invullen
+				//Fill up the valuearray with values , retrieved from the csv , id is automatically put on 					1 (root)
 				for ( $j=1; $j<count($csvarray[$i]); $j++)
 				{
+					if ($j==2)
+					{	//second value needs to be the category (predefined as root category here)
+						array_push($valuearray, $category);
+					}
 					array_push($valuearray, $csvarray[$i][$j]);
 				}
 				
-				if($lo_form->setCsvValues($valuearray))
+				$lo_form->setCsvValues($valuearray);				
+				
+				if ($lo_form->validatecsv($valuearray))
 				{
-					//Elk element in regel valideren.
-					for ( $h=0; $h<count($valuearray); $h++)
-					{
-						if (!$lo_form->validatecsv($valuearray[$h]))
-						{
-							echo 'ik ben verkeerd<br />';
-							$message = 'a'.$message;
-						}
-					}
-					//Message is leeg indien geen fouten gebeurt zijn.		
-					if (empty($message))
-					{
-						array_push($objectarray, $lo_form);
-					}
-					else 
-					{
-						array_push($foutenarray, $i);
-					}								
+					
+					array_push($objectarray, $lo_form);
 				}
-				else
+				else 
 				{
-					array_push($foutenarray, $i);
-				}
+						$errorarray[0]='faultyarrayreturn';
+						array_push($errorarray, ($i+1));
+				}								
+			
 			}
-			//Type zit er niet tussen -> fout
+			//Type not found in our list
 			else 
 			{
-				array_push($foutenarray, $i);
+				$errorarray[0]='faultyarrayreturn';
+				array_push($errorarray, ($i+1));
 			}
 				
-		}//Einde For
-
-		if (count($foutenarray)>1)
-		{			
-			return $foutenarray;
+		}
+		//return the errorarray if its filled
+		if (!empty($errorarray[0]))
+		{		
+			return $errorarray;
 		}
 		else 
 		{
 			return $objectarray;
 		}
+
 	}	
+
 	
 }
