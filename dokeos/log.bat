342fb61632bed19ed1d47f3e34059094b7d18e36
Scara84 <Scara84@localhost>
2008-02-15 10:35:29 +0000

    Small updates

--- dokeos/log.bat
+++
@@ -1,1 +0,0 @@
-java -jar "E:\Documents and Settings\hadebi\Mijn documenten\workspace\LCMS\statsvn.jar" "E:\Documents and Settings\hadebi\Mijn documenten\workspace\LCMS\logfile.log" "E:\Documents and Settings\hadebi\Mijn documenten\workspace\LCMS"
