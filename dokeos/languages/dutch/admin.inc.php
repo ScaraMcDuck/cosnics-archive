080e720e809ba7fbf95b2122de91ff7af5ab4679
Hans De Bisschop <hans.de.bisschop@ehb.be>
2008-02-27 08:55:27 +0000

    Added dummy files to avoid errors for now. (let's get it to work first and handle errors later)

---
+++ dokeos/languages/dutch/admin.inc.php
@@ -0,0 +1,3 @@
+<?php
+$lang['admin']['PlatformAdmin'] = 'Platform Administration';
+?>
---
+++ dokeos/languages/dutch/general.inc.php
@@ -0,0 +1,10 @@
+<?php
+$lang['general']['Ok'] = 'OK';
+$lang['general']['PersonalCalendar'] = 'Personal Calendar';
+$lang['general']['PersonalMessenger'] = 'Personal Messenger';
+$lang['general']['Profiler'] = 'Profiler';
+$lang['general']['SearchPortal'] = 'Search Portal';
+$lang['general']['Weblcms'] = 'WebLcms';
+$lang['general']['Repository'] = 'Repository';
+$lang['general']['InvalidDataRetrievedFromDatabase'] = 'Invalid data retrieved from the database.';
+?>
