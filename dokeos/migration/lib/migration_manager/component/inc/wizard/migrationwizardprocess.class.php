a9db0531cc06e8c0e461d993bb07e1e9084013ff
vanpouckesven <vanpouckesven@localhost>
2008-02-27 13:54:19 +0000

    Migration tool: * In wizard execution code for converting users * In dokeos185_user code for converting to new user format * Import bugfixed

--- dokeos/migration/lib/migration_manager/component/inc/wizard/migrationwizardprocess.class.php
+++ dokeos/migration/lib/migration_manager/component/inc/wizard/migrationwizardprocess.class.php
@@ -3,9 +3,12 @@
  * @package migration.lib.migration_manager.component.inc.wizard
  */
  
+ require_once(dirname(__FILE__) . '/../../../../import.class.php');
 /**
  * This class implements the action to take after the user has completed a
  * course maintenance wizard
+ * 
+ * @author Sven Vanpoucke
  */
 class MigrationWizardProcess extends HTML_QuickForm_Action
 {
@@ -26,8 +29,21 @@ class MigrationWizardProcess extends HTML_QuickForm_Action
 	
 	function perform($page, $actionName)
 	{
+		$exportvalues = $page->controller->exportValues();
 		$this->parent->display_header();
-		echo('test');
+		
+		echo('Migrating users<br />');
+		$userclass = Import :: factory($exportvalues['old_system'], 'user');
+		$users = array();
+		$users = $userclass->getAllUsers();
+		
+		foreach($users as $user)
+		{
+			$lcms_user = $user->convertToNewUser();
+		}
+		
+		echo('All users done<br />');
+		
 		$this->parent->display_footer();
 	}
 }
