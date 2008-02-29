d9c66cec91c652f8dffbac22b35cd984068314d9
vanwayenbergh <vanwayenbergh@localhost>
2008-02-29 12:36:34 +0000

    migration tool: * added logging class

--- dokeos/migration/lib/migration_manager/component/inc/wizard/migrationwizardprocess.class.php
+++ dokeos/migration/lib/migration_manager/component/inc/wizard/migrationwizardprocess.class.php
@@ -4,6 +4,7 @@
  */
  
  require_once(dirname(__FILE__) . '/../../../../import.class.php');
+ require_once(dirname(__FILE__) . '/../../../../logger.class.php');
 /**
  * This class implements the action to take after the user has completed a
  * course maintenance wizard
@@ -38,6 +39,9 @@ class MigrationWizardProcess extends HTML_QuickForm_Action
 		echo(Translation :: get_lang('migrating') . ' ' . 
 			 Translation :: get_lang('users') . '<br />');
 			 
+		$logfile = new Logger('user');
+		$logfile->set_start_time();
+		$logfile->add_message('started migrating users');
 		$userclass = Import :: factory($exportvalues['old_system'], 'user');
 		$users = array();
 		$users = $userclass->get_all_users();
@@ -47,9 +51,12 @@ class MigrationWizardProcess extends HTML_QuickForm_Action
 			$lcms_user = $user->convert_to_new_user();
 		}
 		
+
+		$logfile->add_message('users migrated');
 		echo( Translation :: get_lang('users') . ' ' .
 			  Translation :: get_lang('done') . '<br />');
-		
+			  
+		$logfile->write_passed_time();
 		$this->parent->display_footer();
 	}
 }
