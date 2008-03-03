b4d7185b8a2f851ac8fe62b6868a99d8604174e7
Scara84 <Scara84@localhost>
2008-03-03 11:25:23 +0000

    Backup of old language files in new language folder.

---
+++ dokeos/languages/oldlang/finnish/tracking.inc.php
@@ -0,0 +1,10 @@
+<?php /*
+for more information: see languages.txt in the lang folder. 
+*/
+$langShowAll = "Näytä kaikki";
+$langToolName = "Käytön seuranta";
+$TrackingDisabled = "Seuranta on kytketty pois päältä ylläpitäjän toimesta.";
+$langShowNone = "Piilota kaikki";
+$langCourseStats = "Kurssin tilastot";
+$langToolsAccess = "Työkalujen käyttö";
+?>
