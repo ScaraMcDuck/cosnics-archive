b890d6d21bfa5594a75e5b3ec229cd84451e41e5
vanpouckesven <vanpouckesven@users.noreply.github.com>
2008-03-10 11:19:05 +0000

    Migration Tool: * Bugfixes * Forgot some files last commit

--- dokeos/common/javascript/migration.js
+++ dokeos/common/javascript/migration.js
@@ -25,13 +25,49 @@ function courses_clicked()
 {
 	if(document.page_settings.migrate_courses.checked == false)
 	{
-		document.page_settings.migrate_announcements.checked = false
+		document.page_settings.migrate_groups.checked = false;
+		document.page_settings.migrate_announcements.checked = false;
+		document.page_settings.migrate_calendar_events.checked = false;
+		document.page_settings.migrate_documents.checked = false;
+		document.page_settings.migrate_links.checked = false;
+	}
+}
+
+function groups_clicked()
+{
+	if(document.page_settings.migrate_groups.checked == true)
+	{
+		document.page_settings.migrate_courses.checked = true;
 	}
 }
 
 function announcements_clicked()
 {
 	if(document.page_settings.migrate_announcements.checked == true)
+	{
+		document.page_settings.migrate_courses.checked = true;
+	}
+}
+
+function calendar_events_clicked()
+{
+	if(document.page_settings.migrate_calendar_events.checked == true)
+	{
+		document.page_settings.migrate_courses.checked = true;
+	}
+}
+
+function documents_clicked()
+{
+	if(document.page_settings.migrate_documents.checked == true)
+	{
+		document.page_settings.migrate_courses.checked = true;
+	}
+}
+
+function links_clicked()
+{
+	if(document.page_settings.migrate_links.checked == true)
 	{
 		document.page_settings.migrate_courses.checked = true;
 	}
