36dfac1c670cf9cc662f1759c141530b2bfeb078
vanpouckesven <vanpouckesven@localhost>
2008-04-03 10:48:03 +0000

    Migration Tool: * Created migration userinfo definition

---
+++ dokeos/migration/delete_empty_directories.php
@@ -0,0 +1,21 @@
+<?php
+require_once(dirname(__FILE__) . '/../common/configuration/configuration.class.php');
+ini_set('include_path',realpath(dirname(__FILE__).'/../plugin/pear'));
+require_once dirname(__FILE__).'/../common/global.inc.php';
+
+$conf = Configuration :: get_instance();
+
+$path = '/home/svennie/sites/dokeos-lcms/migration/testmap/';
+$directories = Filesystem :: get_directory_content($path, Filesystem :: LIST_DIRECTORIES, true);
+rsort($directories);
+foreach($directories as $directory)
+{
+	$files = Filesystem :: get_directory_content($directory, Filesystem :: LIST_FILES_AND_DIRECTORIES, true);
+	if(count($files) == 0)
+	{
+		Filesystem :: remove($directory);
+		echo($directory . '<br />');
+	}
+}
+
+?>
--- dokeos/migration/platform/dokeos185/dokeos185userinfodef.class.php
+++ dokeos/migration/platform/dokeos185/dokeos185userinfodef.class.php
@@ -2,7 +2,8 @@
 /**
  * migration.lib.platform.dokeos185
  */
-
+require_once dirname(__FILE__) . '/../../lib/import/importuserinfodef.class.php';
+require_once dirname(__FILE__) . '/../../../repository/lib/learning_object/userinfo_def/userinfo_def.class.php';
 /**
  * This class presents a Dokeos185 userinfo_def
  *
@@ -123,6 +124,78 @@ class Dokeos185UserinfoDef
 		return $this->get_default_property(self :: PROPERTY_RANK);
 	}
 
+	function is_valid($parameters)
+	{
+		$course = $parameters['course'];
+		
+		if(!$this->get_title())
+		{
+			self :: $mgdm->add_failed_element($this->get_id(),
+				$course->get_db_name() . '.userinfodef');
+			return false;		
+		}
+		
+		return true;
+	}
+	
+	function convert_to_lcms($parameters)
+	{	
+		$new_course_code = self :: $mgdm->get_id_reference($course->get_code(),'weblcms_course');
+		$new_user_id = self :: $mgdm->get_owner($new_course_code);
+		
+		//userinfodef parameters
+		$lcms_userinfodef = new UserinfoDef();
+		
+		// Category for userinfo already exists?
+		$lcms_category_id = self :: $mgdm->get_parent_id($new_user_id, 'category',
+			Translation :: get_lang('userinfos'));
+		if(!$lcms_category_id)
+		{
+			//Create category for tool in lcms
+			$lcms_repository_category = new Category();
+			$lcms_repository_category->set_owner_id($new_user_id);
+			$lcms_repository_category->set_title(Translation :: get_lang('userinfos'));
+			$lcms_repository_category->set_description('...');
+	
+			//Retrieve repository id from course
+			$repository_id = self :: $mgdm->get_parent_id($new_user_id, 
+				'category', Translation :: get_lang('MyRepository'));
+			$lcms_repository_category->set_parent_id($repository_id);
+			
+			//Create category in database
+			$lcms_repository_category->create();
+			
+			$lcms_userinfodef->set_parent_id($lcms_repository_category->get_id());
+		}
+		else
+		{
+			$lcms_userinfodef->set_parent_id($lcms_category_id);	
+		}
+
+		$lcms_userinfodef->set_title($this->get_title());
+		$lcms_userinfodef->set_description($this->get_title());
+		$lcms_userinfodef->set_owner_id($new_user_id);
+		
+		if($this->get_comment())
+			$lcms_userinfodef->set_comment($this->get_comment());
+		
+		//create userinfodef in database
+		$lcms_userinfodef->create_all();
+		
+		
+		return $lcms_userinfodef;
+	}
+
+	static function get_all($parameters)
+	{
+		self :: $mgdm = $parameters['mgdm'];
+		
+		$coursedb = $parameters['course']->get_db_name();
+		$tablename = 'userinfo_def';
+		$classname = 'Dokeos185UserinfoDef';
+			
+		return self :: $mgdm->get_all($coursedb, $tablename, $classname, $tool_name);	
+	}
 
 }
 
