7ca9f5d849831d9b726403bf7fdea47e9f981310
thxbb12 <thxbb12@cosnics>
2008-04-21 11:40:12 +0000

    Added a bunch of translations for the English language

--- dokeos/languages/english/admin.inc.php
+++ dokeos/languages/english/admin.inc.php
@@ -1,3 +1,7 @@
 <?php
 $lang['admin']['PlatformAdmin'] = 'Platform Administration';
+$lang['admin']['UserList'] = 'Users';
+$lang['admin']['UserCreate'] = 'Add User';
+$lang['admin']['UserExport'] = 'Export User';
+$lang['admin']['UserImport'] = 'Import User';
 ?>
--- dokeos/languages/english/user.inc.php
+++ dokeos/languages/english/user.inc.php
@@ -5,4 +5,21 @@
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
+
+$lang['user']['UserCreate'] = 'New user creation';
+$lang['user']['UserCreated'] = 'User created!';
+$lang['user']['Yes'] = 'Yes';
+$lang['user']['No'] = 'No';
+$lang['user']['Email'] = 'Email';
+$lang['user']['Username'] = 'Username';
+$lang['user']['Password'] = 'Password';
+$lang['user']['OfficialCode'] = 'Official code';
+$lang['user']['AddPicture'] = 'Add picture';
+$lang['user']['PhoneNumber'] = 'Phone number';
+$lang['user']['DiskQuota'] = 'Disk quota';
+$lang['user']['DatabaseQuota'] = 'Database quota';
+$lang['user']['VersionQuota'] = 'Version quota';
+$lang['user']['AutoGeneratePassword'] = 'Automatically generated password';
+$lang['user']['SendMailToNewUser'] = 'Send mail to new user';
+
 ?>
