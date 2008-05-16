99c4a85336c512d0ef6203f20e38a636c222cd9c
Scara84 <Scara84@localhost>
2008-05-16 13:30:25 +0000

    Moved register_trackers() from individual installers to common installer-class.

--- dokeos/classgroup/install/classgroup_installer.class.php
+++ dokeos/classgroup/install/classgroup_installer.class.php
@@ -8,12 +8,12 @@ require_once Path :: get_library_path().'installer.class.php';
  * This installer can be used to create the storage structure for the
  * classgroup application.
  */
-class ClassGroupInstaller extends Installer
+class ClassgroupInstaller extends Installer
 {
 	/**
 	 * Constructor
 	 */
-    function ClassGroupInstaller($values)
+    function ClassgroupInstaller($values)
     {
     	parent :: __construct($values, ClassGroupDataManager :: get_instance());
     }
@@ -36,10 +36,10 @@ class ClassGroupInstaller extends Installer
 			}
 		}
 		
-//		if(!$this->register_trackers())
-//		{
-//			return array('success' => false, 'message' => $this->retrieve_message());
-//		}
+		if(!$this->register_trackers())
+		{
+			return array('success' => false, 'message' => $this->retrieve_message());
+		}
 		
 		$success_message = '<span style="color: green; font-weight: bold;">' . Translation :: get('ApplicationInstallSuccess') . '</span>';
 		$this->add_message($success_message);
@@ -49,71 +49,5 @@ class ClassGroupInstaller extends Installer
 	/**
 	 * Registers the trackers, events and creates the storage units for the trackers
 	 */
-	function register_trackers()
-	{
-		$dir = dirname(__FILE__) . '/../trackers/tracker_tables';
-		$files = FileSystem :: get_directory_content($dir, FileSystem :: LIST_FILES);
-		
-		$trkinstaller = new TrackingInstaller();
-		
-		foreach($files as $file)
-		{
-			if ((substr($file, -3) == 'xml'))
-			{
-				if (!$trkinstaller->create_storage_unit($file))
-				{
-					return false;
-				}
-			}
-		}
-		
-		$classgroupevents = array();
-		$classgroupevents[] = Events :: create_event('create', 'classgroup');
-		$classgroupevents[] = Events :: create_event('update', 'classgroup');
-		$classgroupevents[] = Events :: create_event('delete', 'classgroup');
-		$classgroupevents[] = Events :: create_event('empty', 'classgroup');
-		$classgroupevents[] = Events :: create_event('subscribe_user', 'classgroup');
-		$classgroupevents[] = Events :: create_event('unsubscribe_user', 'classgroup');
-		
-		
-		$path = '/classgroup/trackers/';
-		
-		$dir = dirname(__FILE__) . '/../trackers/';
-		$files = FileSystem :: get_directory_content($dir, FileSystem :: LIST_FILES);
-		
-		foreach($files as $file)
-		{
-			if ((substr($file, -3) == 'php'))
-			{
-				$filename = basename($file);
-				$filename = substr($filename, 0, strlen($filename) - strlen('.class.php'));
-				
-				$tracker = $trkinstaller->register_tracker($path, $filename);
-				if (!$tracker)
-				{
-					return false;
-				}
-				else
-				{
-					if($tracker->get_class() == 'ClassGroupChangesTracker')
-					{
-						foreach($classgroupevents as $event)
-						{
-							if(!$trkinstaller->register_tracker_to_event($tracker, $event)) return false;
-						}
-						
-						$this->add_message(Translation :: get('TrackersRegistered') . ': ' . $filename);
-						continue;
-					}
-					else
-						echo($tracker->get_class());
-				}
-				
-				
-			}
-		}
-		
-		return true;
-	}
 }
 ?>
--- dokeos/rights/install/rights_installer.class.php
+++ dokeos/rights/install/rights_installer.class.php
@@ -41,89 +41,14 @@ class RightsInstaller extends Installer
 			}
 		}
 		
-//		if(!$this->register_trackers())
-//		{
-//			return array('success' => false, 'message' => $this->retrieve_message());
-//		}
+		if(!$this->register_trackers())
+		{
+			return array('success' => false, 'message' => $this->retrieve_message());
+		}
 		
 		$success_message = '<span style="color: green; font-weight: bold;">' . Translation :: get('ApplicationInstallSuccess') . '</span>';
 		$this->add_message($success_message);
 		return array('success' => true, 'message' => $this->retrieve_message());
-	}
-	
-	
-	/**
-	 * Registers the trackers, events and creates the storage units for the trackers
-	 */
-	function register_trackers()
-	{
-		$dir = dirname(__FILE__) . '/../trackers/tracker_tables';
-		$files = FileSystem :: get_directory_content($dir, FileSystem :: LIST_FILES);
-		
-		$trkinstaller = new TrackingInstaller();
-		
-		foreach($files as $file)
-		{
-			if ((substr($file, -3) == 'xml'))
-			{
-				if (!$trkinstaller->create_storage_unit($file))
-				{
-					return false;
-				}
-			}
-		}
-		
-		$rolesrightschangesevents = array();
-		$rolesrightschangesevents[] = Events :: create_event('role_create', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('right_create', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('location_create', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('role_right_location_create', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('role_update', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('right_update', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('location_update', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('role_right_location_update', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('role_delete', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('right_delete', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('location_delete', 'roles_rights');
-		$rolesrightschangesevents[] = Events :: create_event('role_right_location_delete', 'roles_rights');
-		
-		
-		$path = '/rights/trackers/';
-		
-		$dir = dirname(__FILE__) . '/../trackers/';
-		$files = FileSystem :: get_directory_content($dir, FileSystem :: LIST_FILES);
-		
-		foreach($files as $file)
-		{
-			if ((substr($file, -3) == 'php'))
-			{
-				$filename = basename($file);
-				$filename = substr($filename, 0, strlen($filename) - strlen('.class.php'));
-				
-				$tracker = $trkinstaller->register_tracker($path, $filename);
-				if (!$tracker)
-				{
-					return false;
-				}
-				else
-				{
-					if($tracker->get_class() == 'RolesRightChangesTracker')
-					{
-						foreach($rolesrightschangesevents as $event)
-						{
-							if(!$trkinstaller->register_tracker_to_event($tracker, $event)) return false;
-						}
-						
-						$this->add_message(Translation :: get('TrackersRegistered') . ': ' . $filename);
-						continue;
-					}
-				}
-				
-				
-			}
-		}
-		
-		return true;
 	}
 }
 ?>
