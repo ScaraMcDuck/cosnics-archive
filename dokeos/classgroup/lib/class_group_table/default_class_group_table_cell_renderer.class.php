9e1dd995921bcf1b532375ad739cc05143ff3608
Hans De Bisschop <hans.de.bisschop@ehb.be>
2008-05-26 18:10:01 +0000

    Next round of standardizations: more class group.

--- dokeos/classgroup/lib/class_group_table/default_class_group_table_cell_renderer.class.php
+++ dokeos/classgroup/lib/class_group_table/default_class_group_table_cell_renderer.class.php
@@ -3,7 +3,7 @@
  * @package repository.usertable
  */
 
-require_once dirname(__FILE__).'/classgrouptablecellrenderer.class.php';
+require_once dirname(__FILE__).'/class_group_table_cell_renderer.class.php';
 require_once dirname(__FILE__).'/../class_group.class.php';
 /**
  * TODO: Add comment
