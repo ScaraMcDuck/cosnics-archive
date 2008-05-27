e8ad1dc51dbf27a963285dff7f974aeb2428a69b
Hans De Bisschop <hans.de.bisschop@ehb.be>
2008-05-27 10:44:09 +0000

    Continued standardization: weblcms.

--- dokeos/application/lib/weblcms/tool/user/usertool.class.php
+++ dokeos/application/lib/weblcms/tool/user/usertool.class.php
@@ -8,7 +8,7 @@
 require_once dirname(__FILE__).'/../tool.class.php';
 require_once dirname(__FILE__).'/usertoolsearchform.class.php';
 require_once Path :: get_application_library_path().'user_details.class.php';
-require_once dirname(__FILE__).'/../../weblcms_manager/component/subscribeduserbrowser/subscribeduserbrowsertable.class.php';
+require_once dirname(__FILE__).'/../../weblcms_manager/component/subscribed_user_browser/subscribed_user_browser_table.class.php';
 /**
  * Tool to manage users in the course.
  */
