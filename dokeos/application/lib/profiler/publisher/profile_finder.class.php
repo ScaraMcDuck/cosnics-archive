103216900efab3741816ea8e992c71717ad34651
Hans De Bisschop <hans.de.bisschop@ehb.be>
2008-05-29 08:22:53 +0000

    Some small tweaks / bug fixes following the generalization of the ProfilerPublisher

--- dokeos/application/lib/profiler/publisher/profile_finder.class.php
+++ dokeos/application/lib/profiler/publisher/profile_finder.class.php
@@ -2,6 +2,7 @@
 /**
  * @package application.lib.profiler.publisher
  */
+ require_once Path :: get_application_library_path() . 'publisher/component/finder.class.php';
 require_once dirname(__FILE__).'/profile_browser.class.php';
 require_once Path :: get_library_path().'condition/and_condition.class.php';
 require_once Path :: get_library_path().'condition/or_condition.class.php';
@@ -11,62 +12,14 @@ require_once Path :: get_library_path().'html/formvalidator/FormValidator.class.
  * This class represents a profiler publisher component which can be used
  * to search for a certain learning object.
  */
-class ProfileFinder extends ProfileBrowser
+ 
+class ProfilePublisherFinderComponent extends PublisherFinderComponent
 {
-	/**
-	 * The search form
-	 */
-	private $form;
-	/**
-	 * The renderer for the search form
-	 */
-	private $renderer;
-	/**
-	 * Constructor.
-	 * @param LearningObjectPublisher $parent The creator of this object.
-	 */
-	function ProfileFinder($parent)
+	function ProfilePublisherFinderComponent($parent)
 	{
 		parent :: __construct($parent);
-		$this->form = new FormValidator('search', 'get','','',null,false);
-		$this->form->addElement('hidden', ProfilePublisher :: PARAM_ACTION);
-		$this->form->addElement('hidden', Profiler :: PARAM_ACTION);
-		$this->form->addElement('text', 'query', Translation :: get('Find'), 'size="40" class="search_query"');
-		$this->form->addElement('submit', 'submit', Translation :: get('Ok'));
-		$this->renderer = clone $this->form->defaultRenderer();
-		$this->renderer->setElementTemplate('<span>{element}</span> ');
-		$this->form->accept($this->renderer);
-	}
-
-	/*
-	 * Inherited
-	 */
-	function as_html()
-	{
-		$html = array();
-		$html[] = '<div class="lofinder_search_form" style="margin: 0 0 1em 0;">';
-		$html[] = $this->renderer->toHTML();
-		$html[] = '</div>';
-		if(strlen(trim($this->get_query())) > 0)
-		{
-			$html[] = parent::as_html();
-		}
-		return implode("\n",$html);
-	}
-	/*
-	 * Overriding
-	 */
-	protected function get_query()
-	{
-		if ($this->form->validate())
-		{
-			return $this->form->exportValue('query');
-		}
-		if ($_GET['query'])
-		{
-			return $_GET['query'];
-		}
-		return null;
+		$this->get_form()->addElement('hidden', Profiler :: PARAM_ACTION);
+		$this->get_form()->addElement('hidden', Application :: PARAM_APPLICATION);
 	}
 }
 ?>
