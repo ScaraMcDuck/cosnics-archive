0df8b6b219632e3e97523ccc63b821bf314d74b8
vanpouckesven <vanpouckesven@localhost>
2008-06-09 14:25:44 +0000

    Fixed a few installation bugs

--- dokeos/install/index.php
+++ dokeos/install/index.php
@@ -5,6 +5,8 @@ $cidReset = true;
 $this_section = 'install';
 
 ini_set('include_path',realpath(dirname(__FILE__).'/../plugin/pear'));
+ini_set("memory_limit"		,"3500M"	);	// Geen php-beperkingen voor geheugengebruik
+ini_set("max_execution_time"	,"72000");	// Twee uur moet voldoende zijn...
 
 require_once dirname(__FILE__).'/../common/filesystem/path.class.php';
 require_once Path :: get_library_path().'html/display.class.php';
