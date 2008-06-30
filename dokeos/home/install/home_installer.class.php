3d0aa7e071e19e719f34d764e12fe7d0bd642b91
Scara84 <Scara84@localhost>
2008-06-30 09:19:59 +0000

    Small bugfix in the installer.

--- dokeos/home/install/home_installer.class.php
+++ dokeos/home/install/home_installer.class.php
@@ -54,7 +54,7 @@ class HomeInstaller extends Installer
 		$column_news->set_title(Translation :: get('News'));
 		$column_news->set_sort('1');
 		$column_news->set_width('50');
-		$column_news->set_user('1');
+		$column_news->set_user('0');
 		if (!$column_news->create())
 		{
 			return false;
@@ -65,7 +65,7 @@ class HomeInstaller extends Installer
 		$block_test->set_title('Extra');
 		$block_test->set_application('personal_calendar');
 		$block_test->set_component('extra');
-		$block_test->set_user('1');
+		$block_test->set_user('0');
 		if (!$block_test->create())
 		{
 			return false;
@@ -76,7 +76,7 @@ class HomeInstaller extends Installer
 		$column_varia->set_title(Translation :: get('Various'));
 		$column_varia->set_sort('2');
 		$column_varia->set_width('23');
-		$column_varia->set_user('1');
+		$column_varia->set_user('0');
 		if (!$column_varia->create())
 		{
 			return false;
@@ -87,7 +87,7 @@ class HomeInstaller extends Installer
 		$block_user->set_title(Translation :: get('User'));
 		$block_user->set_application('user');
 		$block_user->set_component('login');
-		$block_user->set_user('1');
+		$block_user->set_user('0');
 		if (!$block_user->create())
 		{
 			return false;
@@ -98,7 +98,7 @@ class HomeInstaller extends Installer
 		$block_weblcms->set_title(Translation :: get('Weblcms'));
 		$block_weblcms->set_application('weblcms');
 		$block_weblcms->set_component('extra');
-		$block_weblcms->set_user('1');
+		$block_weblcms->set_user('0');
 		if (!$block_weblcms->create())
 		{
 			return false;
@@ -109,7 +109,7 @@ class HomeInstaller extends Installer
 		$block_search->set_title(Translation :: get('Search'));
 		$block_search->set_application('search_portal');
 		$block_search->set_component('extra');
-		$block_search->set_user('1');
+		$block_search->set_user('0');
 		if (!$block_search->create())
 		{
 			return false;
@@ -120,7 +120,7 @@ class HomeInstaller extends Installer
 		$column_extra->set_title(Translation :: get('Extra'));
 		$column_extra->set_sort('3');
 		$column_extra->set_width('25');
-		$column_extra->set_user('1');
+		$column_extra->set_user('0');
 		if (!$column_extra->create())
 		{
 			return false;
@@ -131,7 +131,7 @@ class HomeInstaller extends Installer
 		$block_user->set_title(Translation :: get('PersonalCalendar'));
 		$block_user->set_application('personal_calendar');
 		$block_user->set_component('month');
-		$block_user->set_user('1');
+		$block_user->set_user('0');
 		if (!$block_user->create())
 		{
 			return false;
