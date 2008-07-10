7d4aa8a078453b68a3b75017362c1b23c5cffbee
michaelkyndt <michaelkyndt@localhost>
2008-07-10 13:23:00 +0000

    Added search block

--- dokeos/application/lib/search_portal/block/search_portal_extra.class.php
+++ dokeos/application/lib/search_portal/block/search_portal_extra.class.php
@@ -23,7 +23,18 @@ class SearchPortalExtra extends SearchPortalBlock
 		$html = array();
 		
 		$html[] = $this->display_header();
-		$html[] = 'Search Portal test block ...';
+		//$html[] = 'Search Portal test block ...';
+		$form = new FormValidator('search_simple', 'get', 'run.php', '', null, false);
+		$form->addElement('text', 'query', '', 'size="30" class="search_query" id="inputString" onkeyup="lookup(this.value);"');
+		$form->addElement('submit', 'submit', Translation :: get('Search'));
+		$form->addElement('hidden','application','search_portal');
+		$html[] = '<div style="text-align: center; margin: 0 0 2em 0;">';
+		$renderer = clone $form->defaultRenderer();
+		$renderer->setElementTemplate('{label} {element} ');
+		$form->accept($renderer);
+		$form->setDefaults(array('application'=>'search_portal'));
+		$html[] = $renderer->toHTML();
+		$html[] = '</div>';
 		$html[] = $this->display_footer();
 		
 		return implode("\n", $html);
