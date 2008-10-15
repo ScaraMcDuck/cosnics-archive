f3cbefd05b264d6eea6ed3220ab0aa64637244a4
Hans De Bisschop <hans.de.bisschop@ehb.be>
2008-10-15 14:00:31 +0000

    Delete old language files.

--- dokeos/languages/oldlang/polish/scormbuilder.inc.php
+++
@@ -1,5 +0,0 @@
-<?php /*
-for more information: see languages.txt in the lang folder. 
-*/
-$langScormBuilder = "Narz&#281;dzie do budowy &#347;cie&#380;ek - narz&#281;dzie do budowy kursów w formacie Scorm";
-?>
