d655a67519b8548ff5a7579d606ff91dfc04e6fd
SSJMaglor <SSJMaglor@localhost>
2008-10-20 13:28:07 +0000

    Exercise tool toolbar update

--- dokeos/application/lib/weblcms/tool/exercise/exercise_tool_component.class.php
+++ dokeos/application/lib/weblcms/tool/exercise/exercise_tool_component.class.php
@@ -21,12 +21,32 @@ class ExerciseToolComponent extends ToolComponent
 		return parent :: factory('Exercise', $component_name, $exercise_tool);
 	}
 	
+	function get_toolbar() 
+	{
+		$action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
+		
+		$action_bar->set_search_url($this->get_url());
+		$action_bar->add_tool_action(
+			new ToolbarItem(
+				Translation :: get('Publish'), Theme :: get_common_img_path().'action_publish.png', $this->get_url(array(ExerciseTool :: PARAM_ACTION => ExerciseTool :: ACTION_PUBLISH)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+		
+		$action_bar->add_tool_action(
+			new ToolbarItem(
+				Translation :: get('Browse'), Theme :: get_common_img_path().'action_browser.png', $this->get_url(array(ExerciseTool :: PARAM_ACTION => ExerciseTool :: ACTION_VIEW_EXERCISES)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+		
+		return $action_bar;
+	}
+	
 	/**
 	 * Returns the actions that can be used on the left part of the action bar in exercise components.
 	 *
 	 * @return array: actions to be used in left part of the action bar.
 	 */
-	function get_left_actions() 
+	/*function get_left_actions() 
 	{
 		$publish = array();
 		if($this->is_allowed(ADD_RIGHT))
@@ -46,17 +66,17 @@ class ExerciseToolComponent extends ToolComponent
 		//'<a href="' . $this->get_url(array(ExerciseTool :: PARAM_ACTION => ExerciseTool :: ACTION_VIEW_EXERCISES), true) . '"><img src="'.Theme :: get_common_img_path().'action_browser.png" alt="'.Translation :: get('BrowserTitle').'" style="vertical-align:middle;"/> '.Translation :: get('BrowserTitle').'</a>';
 		
 		return array($browse,$publish);
-	}
+	}*/
 	
 	/**
 	 * Returns the actions that can be used on the middle part of the action bar in exercise components.
 	 *
 	 * @return array: actions to be used in middle part of the action bar.
 	 */
-	function get_right_actions()
+	/*function get_right_actions()
 	{
 		return array();
-	}
+	}*/
 }
 
 ?>
