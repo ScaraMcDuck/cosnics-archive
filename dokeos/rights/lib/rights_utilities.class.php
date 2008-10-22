2cde40b74bfcd0bece180551b505b8def6bf0af9
Hans De Bisschop <hans.de.bisschop@ehb.be>
2008-10-22 14:01:21 +0000

    New version of Pear::Tree + several bugfixes

--- dokeos/rights/lib/rights_utilities.class.php
+++ dokeos/rights/lib/rights_utilities.class.php
@@ -1,6 +1,8 @@
 <?php
 require_once Path :: get_rights_path() . 'lib/rights_data_manager.class.php';
 require_once Path :: get_rights_path() . 'lib/location.class.php';
+require_once Path :: get_library_path() . 'configuration/configuration.class.php';
+require_once 'Tree/Tree.php';
 
 /*
  * This should become the class which all applications use
@@ -18,24 +20,35 @@ class RightsUtilities
     
     function create_application_root_location($application)
     {
-		$location = new Location();
+		$configuration = Configuration :: get_instance();
+		$dsn = $configuration->get_parameter('database', 'connection_string');
+    	
+		$config = array(
+		    'type' => 'Nested',
+		    'storage' => array(
+		        'name' => 'MDB2',
+		        'dsn' => $dsn
+		        ,
+		        // 'connection' =>
+		    ),
+		    'options' => array(
+		        'table' => 'rights_location',
+		        'order' =>  'id',
+		        'fields' => array(
+		            'id' => array('type' => 'integer', 'name' => 'id'),
+		            'name' => array('type' => 'text', 'name' => 'location'),
+		            'left'      =>  array('type' => 'text', 'name' => 'left_value'),
+		            'right'     =>  array('type' => 'text', 'name' => 'right_value'),
+		            'parent_id'  =>  array('type' => 'integer', 'name' => 'parent')
+		        ),
+		    ),
+		);
 		
-		$location->set_location($application);
-		$location->set_application($application);
-		$location->set_type('root');
-		$location->set_identifier('0');
-		$location->set_left_value('1');
-		$location->set_right_value('2');
-		$location->set_parent('0');
+		$tree = Tree :: factoryDynamic($config);
 		
-		if ($location->create())
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		$tree->add( array("name"=>"c0"));
+		
+		return true;
     }
 }
 ?>
