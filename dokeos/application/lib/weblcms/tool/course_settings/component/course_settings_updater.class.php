531bb66b2d910d5bb87dc5620ee7daf67272edb6
vanpouckesven <vanpouckesven@users.noreply.github.com>
2008-10-23 10:50:07 +0000

    Refactored course settings

---
+++ dokeos/application/lib/weblcms/tool/course_settings/component/course_settings_updater.class.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * $Id: course_settings_tool.class.php 15449 2008-05-27 11:10:16Z Scara84 $
+ * Course settings tool
+ * @package application.weblcms.tool
+ * @subpackage course_settings
+ */
+require_once dirname(__FILE__).'/../course_settings_tool_component.class.php';
+require_once dirname(__FILE__).'/../../../course/course_form.class.php';
+
+class CourseSettingsToolUpdaterComponent extends CourseSettingsToolComponent
+{
+	function run()
+	{
+		$trail = new BreadcrumbTrail();
+		
+		if (!$this->get_course()->is_course_admin($this->get_parent()->get_user()))
+		{
+			$this->display_header($trail);
+			Display :: display_error_message(Translation :: get("NotAllowed"));
+			$this->display_footer();
+			exit;
+		}
+		
+		$form = new CourseForm(CourseForm :: TYPE_EDIT, $this->get_course(), $this->get_user(), $this->get_url(array(Tool :: PARAM_ACTION => CourseSettingsTool :: ACTION_UPDATE_COURSE_SETTINGS)));
+		
+		if($form->validate())
+		{
+			$success = $form->update_course();
+			$this->redirect(Weblcms :: ACTION_VIEW_WEBLCMS_HOME, Translation :: get($success ? 'CourseSettingsUpdated' : 'CourseSettingsUpdateFailed'), ($success ? false : true));
+		}
+		else
+		{			
+			$this->display_header($trail);
+			$form->display();
+			$this->display_footer();
+		}
+	}
+}
+?>
--- dokeos/application/lib/weblcms/tool/course_settings/course_settings_tool.class.php
+++ dokeos/application/lib/weblcms/tool/course_settings/course_settings_tool.class.php
@@ -1,40 +1,38 @@
 <?php
 /**
  * $Id$
- * Course settings tool
+ * Announcement tool
  * @package application.weblcms.tool
  * @subpackage course_settings
  */
-require_once dirname(__FILE__).'/../tool.class.php';
-require_once dirname(__FILE__).'/../../course/course_form.class.php';
 
+require_once dirname(__FILE__).'/course_settings_tool_component.class.php';
+/**
+ * This tool allows a user to publish course_settingss in his or her course.
+ */
 class CourseSettingsTool extends Tool
 {
+	const ACTION_UPDATE_COURSE_SETTINGS = 'update';
+	
+	/**
+	 * Inherited.
+	 */
 	function run()
 	{
-		$trail = new BreadcrumbTrail();
+		$action = $this->get_action();
+		$component = parent :: run();
 		
-		if (!$this->get_course()->is_course_admin($this->get_parent()->get_user()))
+		if($component) return;
+		
+		switch ($action)
 		{
-			$this->display_header($trail);
-			Display :: display_error_message(Translation :: get("NotAllowed"));
-			$this->display_footer();
-			exit;
+			case self :: ACTION_UPDATE_COURSE_SETTINGS :
+				$component = CourseSettingsToolComponent :: factory('Updater', $this);
+				break;
+			default :
+				$component = CourseSettingsToolComponent :: factory('Updater', $this);
 		}
-		
-		$form = new CourseForm(CourseForm :: TYPE_EDIT, $this->get_course(), $this->get_user(), $this->get_url());
-		
-		if($form->validate())
-		{
-			$success = $form->update_course();
-			$this->redirect(Weblcms :: ACTION_VIEW_WEBLCMS_HOME, Translation :: get($success ? 'CourseSettingsUpdated' : 'CourseSettingsUpdateFailed'), ($success ? false : true));
-		}
-		else
-		{			
-			$this->display_header($trail);
-			$form->display();
-			$this->display_footer();
-		}
+		$component->run();
 	}
 }
 ?>
