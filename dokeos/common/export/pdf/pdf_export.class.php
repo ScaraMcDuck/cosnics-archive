774b6d4d5220dc4d6ad80646a428e63235cc182e
vanpouckesven <vanpouckesven@localhost>
2008-11-05 13:54:43 +0000

    pdf tryout

--- dokeos/common/export/pdf/pdf_export.class.php
+++ dokeos/common/export/pdf/pdf_export.class.php
@@ -12,7 +12,7 @@ class PdfExport extends Export
 {
 	public function write_to_file($data)
 	{
-		$file = Filesystem::create_unique_name($this->get_path(SYS_ARCHIVE_PATH),$this->get_filename());
+		/*$file = Filesystem::create_unique_name($this->get_path(SYS_ARCHIVE_PATH),$this->get_filename());
 		$handle = fopen($file, 'a+');
 		foreach ($data as $index => $row)
 		{
@@ -20,7 +20,12 @@ class PdfExport extends Export
 		}
 		fclose($handle);
 		Filesystem :: file_send_for_download($file, true, $file);
-		exit;
+		exit;*/
+		
+		$pdf =& new Cezpdf();
+		$pdf->selectFont('./fonts/Helvetica.afm');
+		$pdf->ezTable($data, null, 'Users', array('fontSize' => 12));
+		$pdf->ezStream();
 	}
 }
 ?>
