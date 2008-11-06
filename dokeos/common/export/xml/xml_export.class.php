63b921f30d0edce910f0fa083606b4f43e529e0b
vanpouckesven <vanpouckesven@localhost>
2008-11-06 15:40:43 +0000

    Created export / import for groups

--- dokeos/common/export/xml/xml_export.class.php
+++ dokeos/common/export/xml/xml_export.class.php
@@ -10,23 +10,40 @@ require_once dirname(__FILE__).'/../export.class.php';
  */
 class XmlExport extends Export
 {
+	private $handle;
+	private $level = 0;
 	public function write_to_file($data)
 	{
 		$file = Filesystem::create_unique_name($this->get_path(SYS_ARCHIVE_PATH),$this->get_filename());
-		$handle = fopen($file, 'a+');
-		fwrite($handle, '<?xml version="1.0" encoding="ISO-8859-1"?>'."\n");
-		foreach ($data as $index => $row)
-		{
-			fwrite($handle, '<item>'."\n");
-			foreach ($row as $key => $value)
-			{
-				fwrite($handle, "\t\t".'<'.$key.'>'.$value.'</'.$key.'>'."\n");
-			}
-			fwrite($handle, "\t".'</item>'."\n");
-		}
-		fclose($handle);
+		$this->handle = fopen($file, 'a+');
+		fwrite($this->handle, '<?xml version="1.0" encoding="ISO-8859-1"?>'."\n");
+		$this->write_array($data);
+		fclose($this->handle);
 		Filesystem :: file_send_for_download($file, true, $file);
 		exit;
+	}
+	
+	public function write_array($row)
+	{
+		foreach ($row as $key => $value)
+		{
+				if(is_numeric($key))
+					$key = 'item';
+					
+				if(is_array($value))
+				{
+					fwrite($this->handle, str_repeat("\t", $this->level).'<'.$key.'>'."\n");
+					$this->level++;
+					$this->write_array($value);
+					$this->level--;
+					fwrite($this->handle, str_repeat("\t", $this->level).'</'.$key.'>'."\n");
+				}
+				else
+				{
+					fwrite($this->handle, str_repeat("\t", $this->level).'<'.$key.'>'.$value.'</'.$key.'>'."\n");
+				}
+				
+		}
 	}
 }
 ?>
--- dokeos/group/lib/group_manager/component/importer.class.php
+++ dokeos/group/lib/group_manager/component/importer.class.php
@@ -1,3 +1,73 @@
 <?php
+/**
+ * @package groups.lib.groupmanager.component
+ */
+require_once dirname(__FILE__).'/../group_manager.class.php';
+require_once dirname(__FILE__).'/../group_manager_component.class.php';
+require_once dirname(__FILE__).'/../../group_import_form.class.php';
+require_once Path :: get_admin_path() . 'lib/admin_manager/admin_manager.class.php';
 
+class GroupManagerImporterComponent extends GroupManagerComponent
+{	
+	/**
+	 * Runs this component and displays its output.
+	 */
+	function run()
+	{
+		$trail = new BreadcrumbTrail();
+		$admin = new AdminManager();
+		$trail->add(new Breadcrumb($admin->get_link(array(AdminManager :: PARAM_ACTION => AdminManager :: ACTION_ADMIN_BROWSER)), Translation :: get('PlatformAdmin')));
+		$trail->add(new Breadcrumb($this->get_url(), Translation :: get('GroupCreateCsv')));
+		
+		if (!$this->get_user()->is_platform_admin())
+		{
+			$this->display_header($trail);
+			Display :: display_error_message(Translation :: get("NotAllowed"));
+			$this->display_footer();
+			exit;
+		}
+		
+		$form = new GroupImportForm(GroupImportForm :: TYPE_IMPORT, $this->get_url(), $this->get_user());
+		
+		if($form->validate())
+		{
+			$success = $form->import_groups();
+			$this->redirect('url', Translation :: get($success ? 'GroupCreatedCsv' : 'GroupNotCreatedCsv'), ($success ? false : true), array(GroupManager :: PARAM_ACTION => GroupManager :: ACTION_BROWSE_GROUPS));
+		}
+		else
+		{
+			$this->display_header($trail);
+			$form->display();
+			$this->display_extra_information();
+			$this->display_footer();
+		}
+	}
+	
+	function display_extra_information()
+	{
+		$html = array();
+		$html[] = '<p>'. Translation :: get('XMLMustLookLike') .' ('. Translation :: get('MandatoryFields') .')</p>';
+		$html[] = '<blockquote>';
+		$html[] = '<pre>';
+		$html[] = '&lt;?xml version=&quot;1.0&quot; encoding=&quot;ISO-8859-1&quot;?&gt;';
+		$html[] = '';
+		$html[] = '&lt;Groups&gt;';
+		$html[] = '    &lt;Item&gt;';
+		$html[] = '        <b>&lt;Name&gt;xxx&lt;/Name&gt;</b>';
+		$html[] = '        <b>&lt;Description&gt;xxx&lt;/Description&gt;</b>';
+		$html[] = '        <b>&lt;Children&gt;</b>';
+		$html[] = '            &lt;Item&gt;';
+		$html[] = '                <b>&lt;Name&gt;xxx&lt;/Name&gt;</b>';
+		$html[] = '                <b>&lt;Description&gt;xxx&lt;/Description&gt;</b>';
+		$html[] = '                <b>&lt;Children&gt;xxx&lt;/Children&gt;</b>';
+		$html[] = '            &lt;/Item&gt;';
+		$html[] = '        <b>&lt;/Children&gt;</b>';
+		$html[] = '    &lt;/Item&gt;';
+		$html[] = '&lt;/Groups&gt;';
+		$html[] = '</pre>';
+		$html[] = '</blockquote>';
+		
+		echo implode($html, "\n");		
+	}
+}
 ?>
