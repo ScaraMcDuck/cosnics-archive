15a8b63edcee5d4af81fab5b890dc97c63534f51
vanpouckesven <vanpouckesven@users.noreply.github.com>
2008-11-06 13:58:06 +0000

    Added an exporter function (to xml) for groups

---
+++ dokeos/group/lib/group_export_form.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package groups.lib.groupmanager
+ */
+require_once Path :: get_library_path().'html/formvalidator/FormValidator.class.php';
+require_once dirname(__FILE__).'/group.class.php';
+require_once dirname(__FILE__).'/group_data_manager.class.php';
+
+class GroupExportForm extends FormValidator {
+
+	const TYPE_EXPORT = 1;
+
+	private $current_tag;
+	private $current_value;
+	private $group;
+	private $groups;
+
+
+	/**
+	 * Creates a new GroupImportForm
+	 * Used to export groups to a file
+	 */
+    function GroupExportForm($form_type, $action) {
+    	parent :: __construct('group_export', 'post', $action);
+
+		$this->form_type = $form_type;
+		$this->failedcsv = array();
+		if ($this->form_type == self :: TYPE_EXPORT)
+		{
+			$this->build_exporting_form();
+		}
+    }
+
+    function build_exporting_form()
+    {
+    	$this->addElement('select', 'file_type', Translation :: get('OutputFileType'),Export::get_supported_filetypes(array('ical', 'csv', 'pdf')));
+		$this->addElement('submit', 'group_export', Translation :: get('Ok'));
+		$this->setDefaults(array('file_type'=>'xml'));
+
+    }
+}
+?>
