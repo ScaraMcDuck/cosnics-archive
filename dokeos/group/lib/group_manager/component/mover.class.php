1266b4518802fd1aba09c1b918615b2ccfa04f96
vanpouckesven <vanpouckesven@users.noreply.github.com>
2008-11-07 12:22:03 +0000

    Added a move functionality for groups

---
+++ dokeos/group/lib/group_manager/component/mover.class.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package groups.groupsmanager
+ */
+require_once dirname(__FILE__).'/../group_manager.class.php';
+require_once dirname(__FILE__).'/../group_manager_component.class.php';
+require_once dirname(__FILE__).'/../group_move_form.class.php';
+require_once dirname(__FILE__).'/../../group_data_manager.class.php';
+require_once Path :: get_admin_path() . 'lib/admin_manager/admin_manager.class.php';
+
+class GroupManagerMoverComponent extends GroupManagerComponent
+{
+	/**
+	 * Runs this component and displays its output.
+	 */
+	function run()
+	{		
+		$trail = new BreadcrumbTrail();
+		$admin = new AdminManager();
+		$trail->add(new Breadcrumb($admin->get_link(array(AdminManager :: PARAM_ACTION => AdminManager :: ACTION_ADMIN_BROWSER)), Translation :: get('PlatformAdmin')));
+		$trail->add(new Breadcrumb($this->get_url(), Translation :: get('GroupMove')));
+
+		if (!$this->get_user()->is_platform_admin())
+		{
+			$this->display_header($trail);
+			Display :: display_warning_message(Translation :: get('NotAllowed'));
+			$this->display_footer();
+			exit;
+		}
+		$group = $this->retrieve_groups(new EqualityCondition(Group :: PROPERTY_ID, $_GET[GroupManager :: PARAM_GROUP_ID]))->next_result();
+
+		$form = new GroupMoveForm($group, $this->get_url(array(GroupManager :: PARAM_GROUP_ID => $_GET[GroupManager :: PARAM_GROUP_ID])), $this->get_user());
+		
+		if($form->validate())
+		{
+			$success = $form->move_group();
+			$parent = $form->get_new_parent();
+			$this->redirect('url', $success?Translation :: get('GroupMoved'):Translation :: get('GroupNotMoved'), $success?(false):true, array(GroupManager :: PARAM_ACTION => GroupManager :: ACTION_BROWSE_GROUPS, GroupManager :: PARAM_GROUP_ID => $parent));
+		}
+		else
+		{
+			$this->display_header($trail);
+			echo Translation :: get('Group') . ': ' . $group->get_name(); 
+			$form->display();
+			$this->display_footer();
+		}
+	}
+}
+?>
---
+++ dokeos/group/lib/group_manager/group_move_form.class.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @package groups.lib.groupmanager
+ */
+require_once Path :: get_library_path().'html/formvalidator/FormValidator.class.php';
+require_once dirname(__FILE__).'/../group.class.php';
+require_once dirname(__FILE__).'/../group_data_manager.class.php';
+
+class GroupMoveForm extends FormValidator 
+{
+    private $group;
+    private $locations = array();
+    private $level = 1;
+    private $gdm;
+    
+    function GroupMoveForm($group, $action, $user) 
+    {
+    	parent :: __construct('group_move', 'post', $action);
+    	$this->group = $group;
+    	
+    	$this->gdm = GroupDataManager :: get_instance();
+    	
+    	if($group->get_parent() != 0)
+    		$this->locations[0] = Translation :: get('Groups');
+    	$this->get_locations(0);
+    	
+		$this->build_form();
+    }
+
+    function build_form()
+    {
+    	$this->addElement('select', 'location', Translation :: get('NewLocation'),$this->locations);
+		$this->addElement('submit', 'group_export', Translation :: get('Ok'));
+    }
+    
+    function get_locations($parent)
+    {
+    	$conditions[] = new NotCondition(new EqualityCondition(Group :: PROPERTY_ID, $this->group->get_id()));
+    	$conditions[] = new NotCondition(new EqualityCondition(Group :: PROPERTY_ID, $this->group->get_parent()));
+    	$conditions[] = new EqualityCondition(Group :: PROPERTY_PARENT, $parent);
+    	$condition = new AndCondition($conditions);	
+    	
+    	$groups = $this->gdm->retrieve_groups($condition);
+    	while($group = $groups->next_result())
+    	{
+    		$this->locations[$group->get_id()] = str_repeat('--', $this->level) . ' ' . $group->get_name();
+    		$this->level++;
+    		$this->get_locations($group->get_id());
+    		$this->level--;
+    	}
+    }
+    
+    function move_group()
+    {
+    	$new_parent = $this->exportValue('location');
+    	$this->group->set_parent($new_parent);
+    	return $this->group->update();
+    }
+    
+    function get_new_parent()
+    {
+    	return $this->exportValue('location');
+    }
+}
+?>
