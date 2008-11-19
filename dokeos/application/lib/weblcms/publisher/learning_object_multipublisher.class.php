da0af84b9827b53f1f4b0c86019ebb4923cf239a
Scara84 <Scara84@localhost>
2008-11-19 12:50:15 +0000

    MultiPublisher finished for WebLCMS

--- dokeos/application/lib/weblcms/publisher/learning_object_multipublisher.class.php
+++ dokeos/application/lib/weblcms/publisher/learning_object_multipublisher.class.php
@@ -8,17 +8,41 @@ require_once Path :: get_repository_path(). 'lib/learning_object_display.class.p
 require_once dirname(__FILE__).'/../learning_object_publication_form.class.php';
 require_once Path :: get_library_path() . 'dokeos_utilities.class.php';
 
+require_once Path :: get_application_library_path() . 'publisher/component/publication_candidate_table/publication_candidate_table.class.php';
+
 /**
  * This class represents a profile publisher component which can be used
  * to preview a learning object in the learning object publisher.
  */
 class LearningObjectPublisherMultipublisherComponent extends PublisherMultipublisherComponent
 {
-	function get_publications_form($ids)
+	function get_publications_form()
 	{
+		$ids = $_POST[PublicationCandidateTable :: DEFAULT_NAME . ObjectTable :: CHECKBOX_NAME_SUFFIX];
+		
 		$html = array();
 		
-		$html[] = '// Learning object titles go here ...<br /><br />';
+		
+		if (count($ids) > 0)
+		{
+			$condition = new InCondition(LearningObject :: PROPERTY_ID, $ids);
+			$learning_objects = RepositoryDataManager :: get_instance()->retrieve_learning_objects(null, $condition);
+			//DokeosUtilities :: order_learning_objects_by_title($learning_objects);
+			
+			$html[] = '<div class="learning_object padding_10">';
+			$html[] = '<div class="title">'. Translation :: get('SelectedLearningObjects') .'</div>';
+			$html[] = '<div class="description">';
+			$html[] = '<ul class="attachments_list">';
+			
+			while($learning_object = $learning_objects->next_result())
+			{
+				$html[] = '<li><img src="'.Theme :: get_common_img_path().'treemenu_types/'.$learning_object->get_type().'.png" alt="'.htmlentities(Translation :: get(LearningObject :: type_to_class($learning_object->get_type()).'TypeName')).'"/> '.$learning_object->get_title().'</li>';
+			}
+			
+			$html[] = '</ul>';
+			$html[] = '</div>';
+			$html[] = '</div>';
+		}
 
 		$form = new LearningObjectPublicationForm(LearningObjectPublicationForm :: TYPE_MULTI, $ids, $this->get_parent(), $this->get_parent()->with_mail_option(), $this->get_parent()->get_course());
 		if ($form->validate())
@@ -37,7 +61,7 @@ class LearningObjectPublisherMultipublisherComponent extends PublisherMultipubli
 				$message = Translation :: get('ObjectPublished');
 			}
 			
-			//$this->redirect($message, (!$publication ? true : false), $parameters);
+			$this->redirect($message, (!$publication ? true : false), $parameters);
 		}
 		else
 		{
