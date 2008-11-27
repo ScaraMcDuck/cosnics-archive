4cccc4c2d994c161c6b8f07fdb6ceda7e791205b
SSJMaglor <SSJMaglor@cosnics>
2008-11-27 15:01:37 +0000

    Added an "upload" option as well as "select" to the document question when taking an assigment.

--- dokeos/application/lib/weblcms/tool/assessment/component/assessment_tester_form/question_types/document_question.class.php
+++ dokeos/application/lib/weblcms/tool/assessment/component/assessment_tester_form/question_types/document_question.class.php
@@ -12,11 +12,13 @@ class DocumentQuestionDisplay extends QuestionDisplay
 		$name = $this->get_clo_question()->get_ref().'_0';
 		$elements[] = $formvalidator->createElement('html', 'Select a document or upload a file:<br/>');
 		$elements[] = $formvalidator->createElement('select', null, Translation :: get('Select a document:'), $documents);
-		$elements[] = $formvalidator->createElement('html', '<br/><br/>');
+		$elements[] = $formvalidator->createElement('html', '<div style="display:block;" id="editor_html_content">');
 		$elements[] = $formvalidator->createElement('file', null, Translation :: get('Upload a file'));
+		$elements[] = $formvalidator->createElement('html', '</div>');
 		$formvalidator->addGroup($elements, $name, '<br/>');
+		//$formvalidator->addElement('upload_or_select', $name, Translation :: get('Upload or select a document'));
 		$formvalidator->addElement('html', '<br/>');
-		$formvalidator->addRule($name, Translation :: get('ThisFieldIsRequired'), 'required');
+		//$formvalidator->addRule($name, Translation :: get('ThisFieldIsRequired'), 'required');
 		$formvalidator->addElement('html', $this->display_footer());
 	}
 	
@@ -26,7 +28,7 @@ class DocumentQuestionDisplay extends QuestionDisplay
 		$documents = $dm->retrieve_learning_objects('document');
 		while ($document = $documents->next_result())
 		{
-			$user_documents[] = $document;
+			$user_documents[$document->get_id()] = $document;
 		}
 		return $user_documents;
 	}
