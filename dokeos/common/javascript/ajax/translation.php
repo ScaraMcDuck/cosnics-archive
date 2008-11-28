f51aa2b7c4db914e36d8923e82e7f67e2c3056b6
Hans De Bisschop <hans.de.bisschop@ehb.be>
2008-11-28 14:58:20 +0000

    Some experimenting with modal dialogs (e.g. http://www.ericmmartin.com/projects/simplemodal/)

---
+++ dokeos/common/javascript/ajax/translation.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(__FILE__).'/../../global.inc.php';
+
+$application	= $_POST['application'];
+$string			= $_POST['string'];
+
+Translation :: set_application($application);
+Theme :: set_application($application);
+
+echo Translation :: get($string);
+?>
