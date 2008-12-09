fc831e956f7f76b0c7b608decb8107aafa4dd822
vanpouckesven <vanpouckesven@users.noreply.github.com>
2008-12-09 11:47:27 +0000

    Added some changes for the repoviewer

--- dokeos/application/lib/weblcms/tool/announcement/component/announcement_publisher.class.php
+++ dokeos/application/lib/weblcms/tool/announcement/component/announcement_publisher.class.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__) . '/../announcement_tool.class.php';
 require_once dirname(__FILE__) . '/../announcement_tool_component.class.php';
-require_once dirname(__FILE__).'/../../../learning_object_publisher.class.php';
+require_once dirname(__FILE__).'/../../../learning_object_repo_viewer.class.php';
 
 class AnnouncementToolPublisherComponent extends AnnouncementToolComponent
 {
@@ -19,7 +19,7 @@ class AnnouncementToolPublisherComponent extends AnnouncementToolComponent
 		
 		if(!isset($object))
 		{
-			$pub = new LearningObjectPublisher($this, 'announcement', true);
+			$pub = new LearningObjectRepoViewer($this, 'announcement', true);
 			
 			$html[] = '<p><a href="' . $this->get_url(array(Tool :: PARAM_ACTION => AnnouncementTool :: ACTION_VIEW_ANNOUNCEMENTS), true) . '"><img src="'.Theme :: get_common_image_path().'action_browser.png" alt="'.Translation :: get('BrowserTitle').'" style="vertical-align:middle;"/> '.Translation :: get('BrowserTitle').'</a></p>';
 			$html[] =  $pub->as_html();
