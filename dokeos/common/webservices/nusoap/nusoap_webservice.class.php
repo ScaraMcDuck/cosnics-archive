1e464a5f1825139a4652910bd1623193c0d4e0a0
vanpouckesven <vanpouckesven@localhost>
2008-12-17 14:38:15 +0000

    Some more prepartion work for webservices

---
+++ dokeos/common/webservices/nusoap/nusoap_webservice.class.php
@@ -0,0 +1,41 @@
+<?php
+require_once Path :: get_plugin_path() . 'nusoap/nusoap.php';
+
+class NusoapWebservice
+{
+	private $webservice_handler;
+	
+	function NusoapWebservice($webservice)
+	{
+		$this->webservice = $webservice;
+	}
+	
+	function provide_webservice()
+	{
+		
+	}
+	
+	function call_webservice($wsdl, $functions)
+	{
+		$client = new nusoap_client($wsdl, 'wsdl');
+		
+		foreach($functions as $function)
+		{
+			$function_name = $function['name'];
+			$function_parameters = $function['parameters'];
+			$handler_function = $function['handler'];
+			$result = $client->call($function_name, $function_parameters);
+			call_user_func(array($this->webservice_handler), $handler_function, $result);
+		}
+	}
+	
+	function debug($result, $client)
+	{
+		dump($result);
+		
+		echo '<h2>Request</h2><pre>' . htmlspecialchars($client->request, ENT_QUOTES) . '</pre>';
+		echo '<h2>Response</h2><pre>' . htmlspecialchars($client->response, ENT_QUOTES) . '</pre>';
+		echo '<h2>Debug</h2><pre>' . htmlspecialchars($client->getDebug(), ENT_QUOTES) . '</pre>';
+	}
+}
+?>
