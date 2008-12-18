66d20e8623504b95e2dcb0cb446cf979baf333cf
vanpouckesven <vanpouckesven@localhost>
2008-12-18 10:05:24 +0000

    First draft of webservice provides

--- dokeos/common/webservices/nusoap/nusoap_webservice.class.php
+++ dokeos/common/webservices/nusoap/nusoap_webservice.class.php
@@ -13,30 +13,53 @@ class NusoapWebservice
 	function provide_webservice($functions)
 	{
 		$server = new soap_server();
+		$server->configureWSDL('Dokeos', 'http://www.dokeos.com');
 		
-		foreach($functions as $name => $function)
+		foreach($functions as $name => $objects)
 		{
+			$in = $objects['input'];
+			$input = array();
+			
+			foreach($in->get_default_property_names() as $property)
+			{
+				$input[$property] = 'xsd:string';
+			}
+			
+			$out = $objects['output'];			
+			$properties = array();
+				
+			foreach($out->get_default_property_names() as $property)
+			{
+				$properties[$property] = array('name' => $property, 'type' => 'xsd:string');
+			}
+			
 			$server->wsdl->addComplexType(
-			    'User',
+			    get_class($out),
 			    'complexType',
 			    'struct',
 			    'all',
 			    '',
-			    array(
-			        'datasource' => array('name' => 'datasource', 'type' => 'xsd:string'),
-			        'familyname' => array('name' => 'familyname', 'type' => 'xsd:string'),
-			        'givenname' => array('name' => 'givenname', 'type' => 'xsd:string'),
-			        'username' => array('name' => 'username', 'type' => 'xsd:string'),
-			        'emailaddress' => array('name' => 'emailaddress', 'type' => 'xsd:string'),
-			        'courseid' => array('name' => 'courseid', 'type' => 'xsd:string'),
-			        'coursetitle' => array('name' => 'coursetitle', 'type' => 'xsd:string'),
-			        'status' => array('name' => 'status', 'type' => 'xsd:string'),
-			        'startdate' => array('name' => 'startdate', 'type' => 'xsd:string'),
-			        'stopdate' => array('name' => 'stopdate', 'type' => 'xsd:string')
-			    )
+			    $properties
 			);
+			
+			$server->register('NusoapWebservice.' . $name, $input, array('return' => 'tns:' . get_class($out)),
+			       'http://www.dokeos.com', 'http://www.dokeos.com#' . $name, 'rpc', 'encoded', '');
+			
+			if (!isset($HTTP_RAW_POST_DATA)) $HTTP_RAW_POST_DATA = implode("\r\n", file('php://input'));
+			$server->service($HTTP_RAW_POST_DATA);
+			
 		}
-		
+	}
+	
+	function handle_webservice($input_array)
+	{
+		//dump($input_array);
+	}
+	
+	function get_user($input_array)
+	{
+		return array('name' => 'Sven', 'email' => 'a');
+		//dump($input_array);
 	}
 	
 	function call_webservice($wsdl, $functions)
@@ -50,19 +73,17 @@ class NusoapWebservice
 			$handler_function = $function['handler'];
 			$result = $client->call($function_name, $function_parameters);
 			$this->webservice_handler->{$handler_function}($result);
-			//call_user_func(array($this->webservice_handler, $handler_function), $result);
 			
-			//$this->debug($result, $client);
+			$this->debug($result, $client);
 		}
 	}
 	
 	function debug($result, $client)
-	{
-		dump($result);
-		
+	{	
 		echo '<h2>Request</h2><pre>' . htmlspecialchars($client->request, ENT_QUOTES) . '</pre>';
 		echo '<h2>Response</h2><pre>' . htmlspecialchars($client->response, ENT_QUOTES) . '</pre>';
 		echo '<h2>Debug</h2><pre>' . htmlspecialchars($client->getDebug(), ENT_QUOTES) . '</pre>';
 	}
 }
+
 ?>
--- dokeos/common/webservices/test/provider/output_user.class.php
+++ dokeos/common/webservices/test/provider/output_user.class.php
@@ -1,3 +1,60 @@
 <?php
 
+class OutputUser
+{
+	const PROPERTY_NAME = 'name';
+	const PROPERTY_EMAIL = 'email';
+	
+	private $defaultProperties;
+
+	function OutputUser($defaultProperties = array ())
+	{
+		$this->defaultProperties = $defaultProperties;
+	}
+	
+	function get_default_property($name)
+	{
+		return $this->defaultProperties[$name];
+	}
+	
+	function get_default_properties()
+	{
+		return $this->defaultProperties;
+	}
+	
+	function set_default_properties($defaultProperties)
+	{
+		$this->defaultProperties = $defaultProperties;
+	}
+	
+	static function get_default_property_names()
+	{
+		return array (self :: PROPERTY_NAME, self :: PROPERTY_EMAIL);
+	}
+
+	function set_default_property($name, $value)
+	{
+		$this->defaultProperties[$name] = $value;
+	}
+
+	function get_name()
+	{
+		return $this->get_default_property(self :: PROPERTY_NAME);
+	}
+	
+	function set_name($name)
+	{
+		$this->set_default_property(self :: PROPERTY_NAME, $name);
+	}	
+	
+	function get_email()
+	{
+		return $this->get_default_property(self :: PROPERTY_EMAIL);
+	}
+	
+	function set_email($email)
+	{
+		$this->set_default_property(self :: PROPERTY_EMAIL, $email);
+	}	
+}
 ?>
