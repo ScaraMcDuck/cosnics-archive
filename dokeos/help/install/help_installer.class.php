f0283ee233d83fe0c9f21fbd867564b0567769a6
vanpouckesven <vanpouckesven@localhost>
2009-01-13 09:51:02 +0000

    Adapted the install procedure to parse help pages from xml

--- dokeos/help/install/help_installer.class.php
+++ dokeos/help/install/help_installer.class.php
@@ -16,7 +16,60 @@ class HelpInstaller extends Installer
     function HelpInstaller($values)
     {
     	parent :: __construct($values, HelpDataManager :: get_instance());
+    	
+    	//$this->install_extra($values);
     }
+    
+    function install_extra()
+    {
+    	if (!$this->install_help_items())
+		{
+			return false;
+		}
+		else
+		{
+			$this->add_message(self :: TYPE_NORMAL, Translation :: get('HelpItemsInstalled'));
+		}
+		
+		return true;
+    }
+    
+    function install_help_items()
+   	{
+   		$file = dirname(__FILE__) . '/../help_items.xml';
+   		
+   		if (file_exists($file))
+		{			
+			$unserializer = &new XML_Unserializer();
+			$unserializer->setOption(XML_UNSERIALIZER_OPTION_COMPLEXTYPE, 'array');
+			$unserializer->setOption(XML_UNSERIALIZER_OPTION_ATTRIBUTES_PARSE, true);
+			$unserializer->setOption(XML_UNSERIALIZER_OPTION_RETURN_RESULT, true);
+			$unserializer->setOption(XML_UNSERIALIZER_OPTION_GUESS_TYPES, true);
+			$unserializer->setOption(XML_UNSERIALIZER_OPTION_FORCE_ENUM, array('location'));
+			
+			// userialize the document
+			$status = $unserializer->unserialize($file, true);    
+			if (PEAR::isError($status))
+			{
+				return false;
+			}
+			else
+			{
+				$data = $unserializer->getUnserializedData();
+				$help_items = $data['help_item'];
+				foreach($help_items as $help_item)
+				{
+					$item = new HelpItem();
+					$item->set_name($help_item['name']);
+					$item->set_url($help_item['url']);
+					$item->create();
+				}
+				
+				return true;
+			}
+		}
+		
+   	}
 	
 	function get_path()
 	{
