e7d63e989ba97d9ea2165ccb8403d485cafa2843
vanpouckesven <vanpouckesven@localhost>
2009-01-15 09:24:20 +0000

    Added a search + highlighter to the blog tool

--- dokeos/application/lib/weblcms/tool/blog/component/blog_viewer.class.php
+++ dokeos/application/lib/weblcms/tool/blog/component/blog_viewer.class.php
@@ -14,7 +14,7 @@ class BlogToolViewerComponent extends BlogToolComponent
 		}
 		$trail = new BreadcrumbTrail();
 		
-		$pid = $_GET['pid'];
+		$pid = Request :: get('pid');
 		if(!$pid)
 		{
 			$this->display_header($trail);
@@ -49,11 +49,33 @@ class BlogToolViewerComponent extends BlogToolComponent
 		$condition = new EqualityCondition(ComplexLearningObjectItem :: PROPERTY_PARENT, $root_object->get_id());
 		$children = $rdm->retrieve_complex_learning_object_items($condition, array(ComplexLearningObjectItem :: PROPERTY_DISPLAY_ORDER), array(SORT_DESC));
 		
+		$query = $this->action_bar->get_query();
+		$is_search = ($query && $query != ''); 
+		
 		while($child = $children->next_result())
 		{
 			$object_id = $child->get_ref();
 			$object = $rdm->retrieve_learning_object($object_id);
-			$html[] = $this->display_learning_object($object, $child->get_id());
+			
+			if($is_search)
+			{
+				$description = $object->get_description();
+				$title = $object->get_title();
+				
+				if(stripos($description, $query) !== false || stripos($title, $query) !== false)
+				{
+					$title = str_ireplace($query, '<span style="color:red; font-weight: bold;">' . $query . '</span>', $title);
+					$description = str_ireplace($query, '<span style="color:red; font-weight: bold;">' . $query . '</span>', $description);
+					
+					$object->set_title($title);
+					$object->set_description($description);
+					$html[] = $this->display_learning_object($object, $child->get_id());
+				}
+			}
+			else
+			{
+				$html[] = $this->display_learning_object($object, $child->get_id());
+			}
 		}
 		
 		return implode("\n", $html);
@@ -63,11 +85,19 @@ class BlogToolViewerComponent extends BlogToolComponent
 	{
 		$action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
 		
+		$action_bar->set_search_url($this->get_url(array(Tool :: PARAM_ACTION => BlogTool :: ACTION_VIEW_BLOG, Tool :: PARAM_PUBLICATION_ID => Request :: get('pid'))));
+		
 		if($this->is_allowed(ADD_RIGHT))
 		{
 			$action_bar->add_common_action(
 				new ToolbarItem(
 					Translation :: get('CreatePost'), Theme :: get_common_image_path().'action_create.png', $this->get_url(array(BlogTool :: PARAM_ACTION => Tool :: ACTION_CREATE_CLOI, 'parent' => $parent_id, 'type' => 'blog_item')), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+				)
+			);
+			
+			$action_bar->add_common_action(
+				new ToolbarItem(
+					Translation :: get('Showall'), Theme :: get_common_image_path().'action_browser.png', $this->get_url(array(BlogTool :: PARAM_ACTION => BlogTool :: ACTION_VIEW_BLOG, Tool :: PARAM_PUBLICATION_ID => Request :: get('pid'))), ToolbarItem :: DISPLAY_ICON_AND_LABEL
 				)
 			);
 		}
