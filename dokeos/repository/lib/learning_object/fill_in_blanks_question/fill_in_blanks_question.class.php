e60fe9da2c4bbefd8e23d73849f2e9ec7f03c9ff
SSJMaglor <SSJMaglor@cosnics>
2009-01-20 08:46:56 +0000

    Fixed question attempts install xml, changed fill in blanks question form

--- dokeos/repository/lib/learning_object/fill_in_blanks_question/fill_in_blanks_question.class.php
+++ dokeos/repository/lib/learning_object/fill_in_blanks_question/fill_in_blanks_question.class.php
@@ -5,18 +5,34 @@ require_once dirname(__FILE__) . '/../../learning_object.class.php';
  * @subpackage exercise
  */
 class FillInBlanksQuestion extends LearningObject {
-	const PROPERTY_ANSWER = 'answer';
-
-	function get_answer () {
-		return $this->get_additional_property(self :: PROPERTY_ANSWER);
+	const PROPERTY_ANSWERS = 'answers';
+	
+	public function add_answer($answer)
+	{
+		$answers = $this->get_answers();
+		$answers[] = $answer;
+		return $this->set_additional_property(self :: PROPERTY_ANSWERS, serialize($answers));
 	}
-	function set_answer ($answer) {
-		return $this->set_additional_property(self :: PROPERTY_ANSWER, $answer);
+	public function set_answers($answers)
+	{
+		return $this->set_additional_property(self :: PROPERTY_ANSWERS, serialize($answers));
+	}
+	public function get_answers()
+	{
+		if($result = unserialize($this->get_additional_property(self :: PROPERTY_ANSWERS)))
+		{
+			return $result;
+		}
+		return array();
+	}
+	public function get_number_of_answers()
+	{
+		return count($this->get_answers());
 	}
 	
 	static function get_additional_property_names()
 	{
-		return array (self :: PROPERTY_ANSWER);
+		return array (self :: PROPERTY_ANSWERS);
 	}
 }
 ?>
