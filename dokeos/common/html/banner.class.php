e63edce06f05b23170aecedea33f69591198f92c
vanpouckesven <vanpouckesven@localhost>
2009-01-22 09:47:13 +0000

    Added some cleanup and a button for the whois online list

--- dokeos/common/html/banner.class.php
+++ dokeos/common/html/banner.class.php
@@ -51,6 +51,9 @@ class Banner
 		$output[] = '-';
 		$output[] = '<a href="'.$this->get_setting('institution_url', 'admin').'" target="_top">'.$this->get_setting('institution', 'admin').'</a>';
 		$output[] = '</div>';
+		$output[] = '<div style="float:right;">';
+		$output[] = '<a href="' . $this->get_path(WEB_PATH). 'index_admin.php?go=whois_online">' . Translation :: get('WhoisOnline') . '</a>';
+		$output[] = '</div>';
 
 		//not to let the header disappear if there's nothing on the left
 		$output[] = '<div class="clear">&nbsp;</div>';
