2750cb49bb3c226ae8be5c2c372710838958acaf
sbil136 <sbil136@localhost>
2009-01-26 14:41:36 +0000

    More translations

--- dokeos/languages/dutch/user.inc.php
+++ dokeos/languages/dutch/user.inc.php
@@ -1,8 +1,88 @@
 <?php
-/*
- * Created on 27-feb-08
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
+$lang['user']['OutputFileType'] = 'Bestandsformaat van de output';
+$lang['user']['Ok'] = 'Oké';
+$lang['user']['FileName'] = 'Bestandsnaam';
+$lang['user']['OnlyXMLCSVAllowed'] = 'Enkel XML/CSV bestandtypes toegestaan';
+$lang['user']['Home'] = 'Start';
+$lang['user']['YZ'] = 'Y-Z';
+$lang['user']['List'] = 'Lijst';
+$lang['user']['Create'] = 'Aanmaken';
+$lang['user']['Export'] = 'Exporteer';
+$lang['user']['Import'] = 'Importeer';
+$lang['user']['Users'] = 'Gebruikers';
+$lang['user']['UserCreateCsv'] = 'Maak gebruikers CSV aan';
+$lang['user']['NotAllowed'] = 'U bent hier niet toegelaten';
+$lang['user']['CSVMustLookLike'] = 'Het CSV bestand moet er als volgt uitzien';
+$lang['user']['MandatoryFields'] = 'Verplichte velden';
+$lang['user']['XMLMustLookLike'] = 'Het XML bestand moet er als volgt uitzien';
+$lang['user']['UserCreateExport'] = 'Exporteer gebruiker';
+$lang['user']['UserRegister'] = 'Registreer gebruiker';
+$lang['user']['AlreadyRegistered'] = 'Gebruiker is reeds geregistreerd op het platform';
+$lang['user']['UserCreate'] = 'Voeg gebruiker toe';
+$lang['user']['NoObjectSelected'] = 'Er is geen object geselecteerd';
+$lang['user']['UserList'] = 'Gebruikerslijst';
+$lang['user']['SearchResults'] = 'Zoekresultaten';
+$lang['user']['CourseAdmin'] = 'Cursusbeheerder';
+$lang['user']['Student'] = 'Student';
+$lang['user']['PlatformAdmin'] = 'Platformbeheerder';
+$lang['user']['Edit'] = 'Wijzig';
+$lang['user']['VersionQuota'] = 'Versie quotum';
+$lang['user']['Delete'] = 'Verwijderen';
+$lang['user']['RemoveSelected'] = 'Verwijder geselecteerde';
+$lang['user']['UserUpdate'] = 'Wijzig gebruiker';
+$lang['user']['UserQuota'] = 'Gebruiker quotum';
+$lang['user']['ModifyProfile'] = 'Wijzig profiel';
+$lang['user']['LostPassword'] = 'Wachtwoord vergeten';
+$lang['user']['InvalidRequest'] = 'Aanvraag niet toegestaan';
+$lang['user']['Email'] = 'E-mail';
+$lang['user']['ThisFieldIsRequired'] = 'verplicht in te vullen veld';
+$lang['user']['WrongEmail'] = 'Dit is een verkeerde emailadres';
+$lang['user']['LoginRequest'] = 'Log-inverzoek';
+$lang['user']['YourAccountParam'] = 'Uw account';
+$lang['user']['UserName'] = 'Gebruikersnaam ('log-in')';
+$lang['user']['Pass'] = 'Wachtwoord';
+$lang['user']['LastName'] = 'Familienaam';
+$lang['user']['FirstName'] = 'Voornaam';
+$lang['user']['KeepPassword'] = 'Behou het huidige wachtwoord';
+$lang['user']['AutoGeneratePassword'] = 'Automatisch een nieuw wachtwoord genereren';
+$lang['user']['Password'] = 'wachtwoord';
+$lang['user']['OfficialCode'] = 'Officiële code';
+$lang['user']['AddPicture'] = 'Voeg foto toe';
+$lang['user']['OnlyImagesAllowed'] = 'Enkel afbeeldingen van het type PNG, JPG of GIF zijn toegestaan.';
+$lang['user']['PhoneNumber'] = 'Telefoonnummer';
+$lang['user']['Language'] = 'Taal';
+$lang['user']['PlatformDefault'] = 'Platform standaard';
+$lang['user']['Theme'] = 'Thema';
+$lang['user']['DiskQuota'] = 'Schijf quotum';
+$lang['user']['FieldMustBeNumeric'] = 'Het veld moet numerisch zijn';
+$lang['user']['DatabaseQuota'] = 'Databank quotum';
+$lang['user']['Status'] = 'Statuut';
+$lang['user']['LockOutWarningMessage'] = 'Waarschuwing! Sluit jezelf niet uit als beheerder';
+$lang['user']['Yes'] = 'Ja';
+$lang['user']['No'] = 'Nee';
+$lang['user']['SendMailToNewUser'] = 'Verzend een mail naar de nieuwe gebruiker';
+$lang['user']['YourReg'] = 'Uw inschrijving';
+$lang['user']['Dear'] = 'Beste';
+$lang['user']['YouAreReg'] = 'U bent ingeschreven';
+$lang['user']['Settings'] = 'Instellingen';
+$lang['user']['Address'] = 'Adres';
+$lang['user']['Is'] = 'Is';
+$lang['user']['Problem'] = 'Probleem';
+$lang['user']['Formula'] = 'Formule';
+$lang['user']['Manager'] = 'Beheerder';
+$lang['user']['EmailWrong'] = 'Emailadres is verkeerd';
+$lang['user']['UsernameWrong'] = 'De gebruikersnaam mag enkel letters, cijfers en _.- bevatten.';
+$lang['user']['UserTaken'] = 'De gebruikersnaam is al in gebruik';
+$lang['user']['Enter2passToChange'] = 'Voer uw wachtwoord tweemaal in om deze te wijzigen';
+$lang['user']['Confirmation'] = 'Bevestiging';
+$lang['user']['PassTwo'] = 'Voer wachtwoord opnieuw in';
+$lang['user']['UpdateImage'] = 'Wijzig profielfoto';
+$lang['user']['AddImage'] = 'Voeg profielfoto toe';
+$lang['user']['DelImage'] = 'Verwijder profielfoto';
+$lang['user']['Find'] = 'Zoek';
+$lang['user']['MenuUser'] = 'Gebruikersmenu';
+$lang['user']['Reg'] = 'Inschrijving';
+$lang['user']['InvalidId'] = 'Ongeldig ID';
+$lang['user']['InvalidForSelfRegistration'] = 'Ongeldig ID';
+$lang['user']['AdminAccountCreated'] = 'Beheerders-account aangemaakt';
 ?>
