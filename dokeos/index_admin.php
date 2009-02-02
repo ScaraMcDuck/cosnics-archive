3c8d2ce69c3c9973e70f48e734a9fcb013a9c8b6
vanpouckesven <vanpouckesven@localhost>
2009-02-02 14:12:16 +0000

    WhoisOnline fix

--- dokeos/index_admin.php
+++ dokeos/index_admin.php
@@ -7,10 +7,10 @@ require_once Path :: get_user_path().'lib/user_manager/user_manager.class.php';
 Translation :: set_application($this_section);
 Theme :: set_application($this_section);
 
-if (!Authentication :: is_valid())
+/*if (!Authentication :: is_valid())
 {
 	Display :: not_allowed();
-}
+}*/
 
 $usermgr = new UserManager(Session :: get_user_id());
 $user = $usermgr->retrieve_user(Session :: get_user_id());
