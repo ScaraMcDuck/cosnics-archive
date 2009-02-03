587b30d473124f5c439cd051cda6f5659636474c
joellel <joellel@localhost>
2009-02-03 11:06:58 +0000

    Forum update

--- dokeos/repository/lib/learning_object/forum/forum.class.php
+++ dokeos/repository/lib/learning_object/forum/forum.class.php
@@ -9,87 +9,26 @@ require_once dirname(__FILE__).'/../../learning_object.class.php';
  */
 class Forum extends LearningObject
 {
-	/**
-	 * Gets the number of topics in this forum
-	 * @return int The number of topics
-	 */
-	function get_topic_count()
+	const PROPERTY_LOCKED = 'locked';
+	
+	function get_locked()
 	{
-		$datamanager = RepositoryDataManager::get_instance();
-		$count = $datamanager->count_learning_objects('forum_topic',new EqualityCondition(self::PROPERTY_PARENT_ID, $this->get_id()));
-		return $count;
+		return $this->get_additional_properties(self :: PROPERTY_LOCKED);
+	}
+	 
+	function set_locked($locked)
+	{
+		return $this->set_additional_properties(self :: PROPERTY_LOCKED, $locked);
 	}
 	
-	/**
-	 * Gets the number of posts in this forum
-	 * @return int The number of posts
-	 */
-	function get_post_count()
+	static function get_additional_property_names()
 	{
-		//TODO: implement this in a more efficient way
-		$topics = $this->get_forum_topics();
-		$count = 0;
-		while($topic = $topics->next_result())
-		{
-			$count += $topic->get_post_count();
-		}
-		return $count;
+		return array(self :: PROPERTY_LOCKED);
 	}
 	
-	/**
-	 * Gets the most recent post in this forum
-	 * @return null|ForumPost If no posts in this forum, null will be returned.
-	 * Else the most recent post.
-	 */
-	function get_last_post()
+	function get_allowed_types()
 	{
-		//TODO: implement this in a more efficient way
-		$topics = $this->get_forum_topics();
-		$last_post = null;
-		while($topic = $topics->next_result())
-		{
-			$last_topic_post = $topic->get_last_post();
-			if($last_post == null || $last_topic_post->get_creation_date() > $last_post->get_creation_date())
-			{
-				$last_post = $last_topic_post;
-			}
-		}
-		return $last_post;
-	}
-	
-	/**
-	 * Gets the list of topics in this forum
-	 * @return ResultSet The set of topics
-	 */
-	function get_forum_topics()
-	{
-		$datamanager = RepositoryDataManager::get_instance();
-		$topics = $datamanager->retrieve_learning_objects('forum_topic',new EqualityCondition(self::PROPERTY_PARENT_ID, $this->get_id()));
-		return $topics;
-	}
-	
-	/**
-	 * Gets a topic in this forum
-	 * @param int $topic_id The id of the requested topic
-	 * @return ForumTopic The topic
-	 */
-	function get_forum_topic($topic_id)
-	{
-		$datamanager = RepositoryDataManager::get_instance();
-		$topic = $datamanager->retrieve_learning_object($topic_id,'forum_topic');
-		return $topic;
-	}
-	
-	function get_forum_post($post_id)
-	{
-		$datamanager = RepositoryDataManager::get_instance();
-		$post = $datamanager->retrieve_learning_object($post_id,'forum_post');
-		return $post;
-	}
-
-	function is_versionable()
-	{
-		return false;
+		return array('forum_topic');
 	}
 }
 ?>
--- dokeos/repository/lib/learning_object/forum/forum_form.class.php
+++ dokeos/repository/lib/learning_object/forum/forum_form.class.php
@@ -7,19 +7,55 @@ require_once dirname(__FILE__).'/forum.class.php';
  */
 class ForumForm extends LearningObjectForm
 {
+	
+	function create_learning_object()
+	{
+		$object = new Forum();
+		$object->set_locked($this->exportValue(Forum :: PROPERTY_LOCKED));
+		$this->set_learning_object($object);
+		return parent :: create_learning_object();
+	}
+	
+	function update_learning_object()
+	{
+		$object = $this->get_learning_object();
+		$object->set_locked($this->exportValue(Forum :: PROPERTY_LOCKED));
+		return parent :: update_learning_object();
+	}
+	
+	function build_creation_form()
+	{
+		parent :: build_creation_form();
+		$this->addElement('category', Translation :: get(get_class($this) .'Properties'));
+		$this->addElement('checkbox','locked', Translation :: get('Locked'));
+		$this->addElement('category');
+	}
+	
+	function build_editing_form()
+	{
+		parent :: build_editing_form();
+		$this->addElement('category', Translation :: get(get_class($this) .'Properties'));
+		$this->addElement('checkbox','locked', Translation :: get('Locked'));
+		$this->addElement('category');
+	}
+	
 	function set_csv_values($valuearray)
 	{
 		$defaults[LearningObject :: PROPERTY_TITLE] = $valuearray[0];
 		$defaults[LearningObject :: PROPERTY_PARENT_ID] = $valuearray[1];
-		$defaults[LearningObject :: PROPERTY_DESCRIPTION] = $valuearray[2];	
+		$defaults[LearningObject :: PROPERTY_DESCRIPTION] = $valuearray[2];
+		$defaults[LearningObject :: PROPERTY_LOCKED] = $valuearray[3];	
 		parent :: set_values($defaults);			
 	}
-
-	function create_learning_object()
+	
+	/*function setDefaults($defaults = array())
 	{
-		$object = new Forum();
-		$this->set_learning_object($object);
-		return parent :: create_learning_object();
-	}
+		$object = $this->get_learning_object();
+		if($object != null){
+			$defaults[Forum :: PROPERTY_LOCKED] = $object->get_locked();
+		}
+		parent :: setDefaults($defaults);
+	}*/
+	
 }
 ?>
