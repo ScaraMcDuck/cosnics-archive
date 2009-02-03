35f0db93d91d7e2c64225478f64c21abd503c986
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-02-03 11:41:05 +0000

    Bugfixes learning object publication form

--- dokeos/application/lib/weblcms/learning_object_publication_form.class.php
+++ dokeos/application/lib/weblcms/learning_object_publication_form.class.php
@@ -448,7 +448,7 @@ class LearningObjectPublicationForm extends FormValidator
 				$adm = AdminDataManager :: get_instance();
 				$site_name_setting = PlatformSetting :: get('site_name');
 				
-				$subject = '['.$site_name_setting->get_value().'] '.$learning_object->get_title();
+				$subject = '['.$site_name_setting.'] '.$learning_object->get_title();
 				$body = new html2text($display->get_full_html());
 				// TODO: send email to correct users/course_groups. For testing, the email is sent now to the repo_viewer.
 				$user = $this->user;
