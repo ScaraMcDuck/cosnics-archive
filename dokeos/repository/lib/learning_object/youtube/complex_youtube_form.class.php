ecb185ecb3481d62c0e6a9a64f5d1736216a94dd
Scara84 <Scara84@localhost>
2009-02-18 10:53:23 +0000

    YouTube-object added.

---
+++ dokeos/repository/lib/learning_object/youtube/complex_youtube_form.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package repository.learningobject
+ * @subpackage youtube
+ */
+require_once dirname(__FILE__) . '/../../complex_learning_object_item_form.class.php';
+require_once dirname(__FILE__) . '/complex_youtube.class.php';
+
+class ComplexYoutubeForm extends ComplexLearningObjectItemForm
+{
+}
+
+?>
