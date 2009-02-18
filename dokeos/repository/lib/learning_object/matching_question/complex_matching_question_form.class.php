7a4a149ee07949ec5405477b88483f0718cf7954
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-02-18 09:46:45 +0000

    Changed the assessment tool to work with the new builder

--- dokeos/repository/lib/learning_object/matching_question/complex_matching_question_form.class.php
+++ dokeos/repository/lib/learning_object/matching_question/complex_matching_question_form.class.php
@@ -10,27 +10,48 @@ require_once dirname(__FILE__) . '/complex_matching_question.class.php';
  */
 class ComplexMatchingQuestionForm extends ComplexLearningObjectItemForm
 {
-   	protected function build_creation_form()
+	protected function build_creation_form()
     {
     	parent :: build_creation_form();
-    	$this->addElement('text', ComplexMatchingQuestion :: PROPERTY_WEIGHT, Translation :: get('Weight'), array("size" => "50"));
+    	$elements = $this->get_elements();
+    	foreach($elements as $element)
+    	{
+    		$this->addElement($element);
+    	}
     }
     // Inherited
     protected function build_editing_form()
     {
 		parent :: build_editing_form();
-    	$this->addElement('text', ComplexMatchingQuestion :: PROPERTY_WEIGHT, Translation :: get('Weight'), array("size" => "50"));
+    	$elements = $this->get_elements();
+    	foreach($elements as $element)
+    	{
+    		$this->addElement($element);
+    	}
 	}
-	// Inherited
+	
+	public function get_elements()
+	{
+		$elements[] = $this->createElement('text', ComplexMatchingQuestion :: PROPERTY_WEIGHT, Translation :: get('Weight'), array("size" => "50"));
+		return $elements;
+	}
+	
 	function setDefaults($defaults = array ())
+	{
+		$defaults = array_merge($defaults, $this->get_default_values());
+		parent :: setDefaults($defaults);
+	}
+	
+	function get_default_values()
 	{
 		$cloi = $this->get_complex_learning_object_item();
 	
 		if (isset ($cloi))
 		{
-			$defaults[ComplexMatchingQuestion :: PROPERTY_WEIGHT] = $cloi->get_weight();
+			$defaults[ComplexMatchingQuestion :: PROPERTY_WEIGHT] = $cloi->get_weight() ? $cloi->get_weight() : 0;
 		}
-		parent :: setDefaults($defaults);
+		
+		return $defaults;
 	}
 
 	// Inherited
@@ -41,6 +62,14 @@ class ComplexMatchingQuestionForm extends ComplexLearningObjectItemForm
 		$cloi->set_weight($values[ComplexMatchingQuestion :: PROPERTY_WEIGHT]); 
 		return parent :: create_complex_learning_object_item();
 	}
+	
+	function create_cloi_from_values($values)
+	{
+		$cloi = $this->get_complex_learning_object_item();
+		$cloi->set_weight($values[ComplexMatchingQuestion :: PROPERTY_WEIGHT]); 
+		return parent :: create_complex_learning_object_item();
+	}
+	
 	// Inherited
 	function update_complex_learning_object_item()
 	{
