a74a889b2a52816f729d7a2df981abb0e3b189ba
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-02-20 12:31:23 +0000

    Fixed the display of a document

--- dokeos/repository/lib/learning_object/document/document_display.class.php
+++ dokeos/repository/lib/learning_object/document/document_display.class.php
@@ -14,8 +14,13 @@ class DocumentDisplay extends LearningObjectDisplay
 	{	
 		$html = parent :: get_full_html();
 		$object = $this->get_learning_object();
+		$name = $object->get_filename();
+		if(substr($name, -3) == 'jpg' || substr($name, -4) == 'jpeg' || substr($name, -3) == 'bmp' || substr($name, -3) == 'png')
+			$html = preg_replace('|</div>\s*$|s', '<a href="'.htmlentities($object->get_url()).'"><img style="max-width: 100%" src="' . $object->get_url() . '" /></a></div>' , $html);
+		else
+			$html = preg_replace('|</div>\s*$|s', '<div class="document_link" style="margin-top: 1em;"><a href="'.htmlentities($object->get_url()).'">'.htmlentities($object->get_filename()).'</a> ('.Filesystem::format_file_size($object->get_filesize()).')</div></div>', $html);
 		
-		return preg_replace('|</div>\s*$|s', '<div class="document_link" style="margin-top: 1em;"><a href="'.htmlentities($object->get_url()).'">'.htmlentities($object->get_filename()).'</a> ('.Filesystem::format_file_size($object->get_filesize()).')</div></div>', $html);
+		return $html;
 	}
 	//Inherited
 	function get_short_html()
