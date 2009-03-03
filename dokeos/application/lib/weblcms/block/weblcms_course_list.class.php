c13f7ed370a6c4acbc7972c5f5b3c447e04e69f2
vanpouckesven <vanpouckesven@localhost>
2009-03-03 10:57:04 +0000

    Bugfixes

--- dokeos/application/lib/weblcms/block/weblcms_course_list.class.php
+++ dokeos/application/lib/weblcms/block/weblcms_course_list.class.php
@@ -4,6 +4,7 @@
  */
 require_once dirname(__FILE__).'/../weblcms_block.class.php';
 require_once dirname(__FILE__).'/../course/course_user_category.class.php';
+require_once dirname(__FILE__).'/../tool/tool.class.php';
 /**
  * This class represents a calendar repo_viewer component which can be used
  * to browse through the possible learning objects to publish.
@@ -36,15 +37,41 @@ class WeblcmsCourseList extends WeblcmsBlock
 	function display_course_digest($courses)
 	{
 		$html = array();
+		$wdm = WeblcmsDataManager :: get_instance();
+		
 		if($courses->size() > 0)
 		{
 			$html[] = '<ul style="padding: 0px; margin: 0px 0px 0px 15px;">';
 			while ($course = $courses->next_result())
 			{
+				$tools = $wdm->get_course_modules($course->get_id());
 				$weblcms = $this->get_parent();
+				$weblcms->set_course($course);
 				$html[] = '<li><a href="'. $weblcms->get_course_viewing_link($course, true) .'">'.$course->get_name().'</a>';
 				//$html[] = '<br />'. $course->get_id() .' - '. $course->get_titular();
+		
+				foreach($tools as $index => $tool)
+				{					  
+					require_once dirname(__FILE__).'/../tool/'.$tool->name.'/'.$tool->name.'_tool.class.php';
+					
+					if($tool->visible && $weblcms->tool_has_new_publications($tool->name))
+					{
+						$params[Weblcms::PARAM_TOOL] = $tool->name;
+						$params[Weblcms::PARAM_COURSE] = $course->get_id();
+						$params[Weblcms::PARAM_ACTION] = Weblcms::ACTION_VIEW_COURSE;
+						$url = 'run.php?application=weblcms';
+						
+						foreach($params as $key => $param)
+						{
+							$url .= '&' . $key . '=' . $param;
+						}
+						
+						$html[] = '<a href="'.$url.'"><img src="'. Theme :: get_image_path('weblcms'). 'tool_' . $tool->name.'_new.png" alt="'.Translation :: get('New').'"/></a>';
+					}
+				}
+				
 				$html[] = '</li>';
+				
 			}
 			$html[] = '</ul>';
 		}
