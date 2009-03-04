50a728beed2abca2c260dcdb08ea466c5a22fbc0
SSJMaglor <SSJMaglor@localhost>
2009-03-04 10:06:31 +0000

    Fixed learning path question attempt tracker xml

--- dokeos/repository/lib/export/qti/question/question_types/multiple_answer_exporter.class.php
+++ dokeos/repository/lib/export/qti/question/question_types/multiple_answer_exporter.class.php
@@ -8,14 +8,10 @@ class MultipleAnswerQuestionQtiExport extends QuestionQtiExport
 	{
 		$rdm = RepositoryDataManager :: get_instance();
 		$question = $this->get_learning_object();
-		//$condition = new EqualityCondition(ComplexLearningObjectItem :: PROPERTY_PARENT, $question->get_id());
-		//$clo_answers = $rdm->retrieve_complex_learning_object_items($condition);
-		//while ($clo_answer = $clo_answers->next_result())
 		$q_answers = $question->get_options();
 		foreach($q_answers as $q_answer)
 		{
-			//$answer = $rdm->retrieve_learning_object($clo_answer->get_ref(), 'answer');
-			$answers[] = array('answer' => $q_answer->get_value(), 'score' => $q_answer->get_weight());
+			$answers[] = array('answer' => $q_answer->get_value(), 'score' => $q_answer->get_weight(), 'feedback' => $q_answer->get_comment());
 		}
 		
 		$item_xml[] = '<assessmentItem xmlns="http://www.imsglobal.org/xsd/imsqti_v2p1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsglobal.org/xsd/imsqti_v2p1    http://www.imsglobal.org/xsd/imsqti_v2p1.xsd" identifier="q'.$question->get_id().'" title="'.$question->get_title().'" adaptive="false" timeDependent="false">';
@@ -24,6 +20,7 @@ class MultipleAnswerQuestionQtiExport extends QuestionQtiExport
 		$item_xml[] = '</responseDeclaration>';
 
 		$item_xml[] = '<outcomeDeclaration identifier="SCORE" cardinality="single" baseType="float" />';
+		$item_xml[] = '<outcomeDeclaration identifier="FEEDBACK" cardinality="multiple" baseType="identifier">';
 		$item_xml[] = $this->get_interaction_xml($answers);
 		$item_xml[] = '<responseProcessing template="http://www.imsglobal.org/question/qti_v2p1/rptemplates/map_response" />';
 		$item_xml[] = '</assessmentItem>';
@@ -56,7 +53,9 @@ class MultipleAnswerQuestionQtiExport extends QuestionQtiExport
 		$interaction_xml[] = '<prompt>'.htmlspecialchars($this->get_learning_object()->get_description()).'</prompt>';
 		foreach ($answers as $i => $answer)
 		{
-			$interaction_xml[] = '<simpleChoice identifier="c'.$i.'" fixed="false">'.htmlspecialchars($answer['answer']).'</simpleChoice>';
+			$interaction_xml[] = '<simpleChoice identifier="c'.$i.'" fixed="false">'.htmlspecialchars($answer['answer']);
+			$interaction_xml[] = '<feedbackInline outcomeIdentifier="FEEDBACK" identifier="c'.$i.'" showHide="show">'.htmlspecialchars($answer['comment']).'</feedbackInline>';
+			$interaction_xml[] = '</simpleChoice>';
 		}
 		$interaction_xml[] = '</choiceInteraction>';
 		$interaction_xml[] = '</itemBody>';
