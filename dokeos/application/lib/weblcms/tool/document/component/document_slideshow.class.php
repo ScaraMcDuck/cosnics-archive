a71a547e35af053194c628f39bd8131468d66c3f
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-03-06 13:38:38 +0000

    Added the actionbar to the document slideshow viewer

--- dokeos/application/lib/weblcms/tool/document/component/document_slideshow.class.php
+++ dokeos/application/lib/weblcms/tool/document/component/document_slideshow.class.php
@@ -18,11 +18,32 @@ class DocumentToolSlideshowComponent extends DocumentToolComponent
 		}
 		
 		$browser = new DocumentSlideshowBrowser($this);
+		$this->action_bar = $this->get_action_bar();
+		
 		$trail = new BreadcrumbTrail();
 		
 		$this->display_header($trail);
+		echo $this->action_bar->as_html();
 		echo $browser->as_html();
 		$this->display_footer();
+	}
+	
+	function get_action_bar()
+	{
+		$action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
+		
+		if($_GET['thumbnails'])
+		{
+			$action_bar->add_common_action(new ToolbarItem(Translation :: get('Slideshow'), Theme :: get_common_image_path().'action_slideshow.png', $this->get_url(array('tool_action' => 'slideshow', 'thumbnails'=>null)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+		}
+		else
+		{
+			$action_bar->add_common_action(new ToolbarItem(Translation :: get('Thumbnails'), Theme :: get_common_image_path().'action_slideshow_thumbnail.png', $this->get_url(array('tool_action' => 'slideshow','thumbnails'=>1)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+		}
+		
+		$action_bar->add_tool_action(HelpManager :: get_tool_bar_help_item('document tool'));
+		
+		return $action_bar;
 	}
 }
 ?>
