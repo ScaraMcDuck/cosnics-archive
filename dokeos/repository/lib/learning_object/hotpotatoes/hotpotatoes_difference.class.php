7665ab61852e27655f3bae192b74773a3e1728fb
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-03-09 14:33:49 +0000

    Added the hotpotatoes learning object

---
+++ dokeos/repository/lib/learning_object/hotpotatoes/hotpotatoes_difference.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package repository.learningobject
+ * @subpackage openquestion
+ */
+/**
+ * This class can be used to get the difference between open question
+ */
+class HotpotatoesDifference extends LearningObjectDifference
+{
+	function get_difference()
+	{
+		$object = $this->get_object();
+		$version = $this->get_version();
+		
+		$object_string = $object->get_path();
+        $object_string = explode("\n", strip_tags($object_string));
+           	
+        $version_string = $version->get_path();
+		$version_string = explode("\n", strip_tags($version_string));
+		
+		$td = new Difference_Engine($object_string, $version_string);
+		
+		return array_merge(parent :: get_difference(), $td->getDiff());
+	}
+}
+?>
