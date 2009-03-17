7b402af48ac1f2342128618be65d960f39e37be8
Samumon <Samumon@localhost>
2009-03-17 08:30:28 +0000

    Whirlpool addition to the hashing framework

---
+++ dokeos/common/hashing/whirlpool/whirlpool_hashing.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Class that defines whirlpool hashing
+ * @author Samumon
+ *
+ */
+class WhirlpoolHashing extends Hashing
+{
+	function create_hash($value)
+	{
+		return hash('whirlpool', $value);
+	}
+	
+	function create_file_hash($file)
+	{
+		return hash_file('whirlpool', $file);
+	}
+	
+}
+?>
