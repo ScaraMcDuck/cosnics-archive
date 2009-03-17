0f2f4008cc9a9cdff83e9454cdeac97152479bd2
SSJMaglor <SSJMaglor@cosnics>
2009-03-17 12:56:38 +0000

    Temp fix for http://www.dokeosplanet.org/forum/viewtopic.php?f=38&t=337

---
+++ dokeos/application/lib/weblcms/tool/assessment/component/assessment_deleter.class.php
@@ -0,0 +1,64 @@
+<?php
+require_once Path :: get_application_path().'lib/weblcms/trackers/weblcms_assessment_attempts_tracker.class.php';
+
+class AssessmentToolDeleterComponent extends AssessmentToolComponent
+{
+	function run()
+	{
+		if($this->is_allowed(DELETE_RIGHT))
+		{ 
+			if(isset($_GET[Tool :: PARAM_PUBLICATION_ID]))
+				$publication_ids = $_GET[Tool :: PARAM_PUBLICATION_ID]; 
+			else
+				$publication_ids = $_POST[Tool :: PARAM_PUBLICATION_ID]; 
+			
+			if (!is_array($publication_ids))
+			{
+				$publication_ids = array ($publication_ids);
+			}
+			
+			$this->delete_publications($publication_ids);
+		}
+	}
+	
+	function delete_publications($publication_ids)
+	{
+		$datamanager = WeblcmsDataManager :: get_instance();
+		$success = true;
+		foreach($publication_ids as $index => $pid)
+		{
+			$track = new WeblcmsAssessmentAttemptsTracker();
+			$condition = new EqualityCondition(WeblcmsAssessmentAttemptsTracker :: PROPERTY_ASSESSMENT_ID, $pid);
+			$items = $track->retrieve_tracker_items();
+			
+			if (count($items) == 0)
+			{
+				$publication = $datamanager->retrieve_learning_object_publication($pid); 
+				$publication->delete();
+			}
+			else
+			{
+				$success = false;
+			}
+		}
+		
+		if(count($publication_ids) > 1)
+		{
+			if ($success)
+				$message = htmlentities(Translation :: get('LearningObjectPublicationsDeleted'));
+			else
+				$message = htmlentities(Translation :: get('SomePublicationsCouldNotBeDeleted'));
+		}
+		else
+		{
+			if ($success)
+				$message = htmlentities(Translation :: get('LearningObjectPublicationDeleted'));
+			else
+				$message = htmlentities(Translation :: get('LearningObjectPublicationNotDeleted'));
+				
+		}
+		
+		$this->redirect(null, $message, (!$success), array('pid' => null));
+	}
+}
+?>
