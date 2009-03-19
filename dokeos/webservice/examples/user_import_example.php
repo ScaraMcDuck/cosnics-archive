867f165aaea1a588bf49b8d7d1b6f7e2cb8e1e3b
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-03-19 09:20:26 +0000

    Added first parsing method for example

--- dokeos/webservice/examples/user_import_example.php
+++ dokeos/webservice/examples/user_import_example.php
@@ -1,11 +1,33 @@
 <?php
 
-$file = 'user_import.csv';
-parse_csv();
+$file = dirname(__FILE__) . '/user_import.csv';
+$users = parse_csv($file);
+dump($users);
 
-function parse_csv()
+function parse_csv($file)
 {
+	if(file_exists($file) && $fp = fopen($file, "r"))
+	{
+		$keys = fgetcsv($fp, 1000, ";");
+		$users = array();
+		
+		while($user_data = fgetcsv($fp, 1000, ";"))
+		{
+			$user = array();
+			foreach($keys as $index => $key)
+			{
+				$user[$key] = trim($user_data[$index]);	
+			}
+			$users[] = $user;
+		}
+		fclose($fp);
+	}
+	else
+	{
+		echo("FOUT: Kan het bestand niet openen ($file)");
+	}
 	
+	return $users;
 }
 
 function create_user($user)
@@ -21,6 +43,13 @@ function update_user($user)
 function delete_user($user)
 {
 	
+}
+
+function dump($value)
+{
+	echo '<pre>';
+	print_r($value);
+	echo '</pre>';
 }
 
 ?>
