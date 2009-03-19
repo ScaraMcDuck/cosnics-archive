a988d5020860851c19aace510f81edf067b207a1
inmemoryblue <inmemoryblue@cosnics>
2009-03-19 10:24:16 +0000

    made changes to the login webservice, debugging

--- dokeos/user/webservices/test_call_local_webservice.class.php
+++ dokeos/user/webservices/test_call_local_webservice.class.php
@@ -60,7 +60,7 @@ class TestCallLocalWebservice
 		{
 			$functions[] = array(
 				'name' => 'LoginWebservice.login',
-				'parameters' => array('username'=>'Soliber','password'=>'8856ffce09dad0fd33bfe3ae803cd97cc4540a78'),
+				'parameters' => array('username'=>'Soliber','password'=>'94390bf841d79d92d1eb48cbc6623f3be2713ad6'),
 				'handler' => 'handle_webservice'
 			);
 		}
