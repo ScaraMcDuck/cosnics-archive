79c87a20a17fc34b02f2e27a1de4749ad0f07be6
Samumon <Samumon@localhost>
2009-03-25 12:47:02 +0000

    script to get outside IP of a computer

--- dokeos/webservice/security/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-include 'remote_addr.php'; // produce output
-$var = get_ip();
-echo $var;
-?>
