75f0129d37b481ac7696b00efbd3db134bb69681
SSJMaglor <SSJMaglor@localhost>
2009-03-30 12:44:56 +0000

    Fixed assignment results viewer

--- dokeos/application/lib/weblcms/tool/assessment/component/assessment_results_viewer/assignment_results_viewer.class.php
+++ dokeos/application/lib/weblcms/tool/assessment/component/assessment_results_viewer/assignment_results_viewer.class.php
@@ -20,16 +20,45 @@ class AssignmentResultsViewer extends ResultsViewer
 		$uaid = parent :: get_user_assessment()->get_id();
 		$dm = RepositoryDataManager :: get_instance();
 		$db = WeblcmsDataManager :: get_instance();
-		$publication = WeblcmsDataManager :: get_instance()->retrieve_learning_object_publication(parent :: get_user_assessment()->get_assessment_id());
-		$condition = new EqualityCondition(ComplexLearningObjectItem :: PROPERTY_PARENT, $publication->get_learning_object()->get_id());
+		$user_assessment = parent :: get_user_assessment();
+		
+		//dump($assessment);
+		//dump($user_assessment);
+		if (get_class($user_assessment) == 'WeblcmsAssessmentAttemptsTracker')
+		{
+			$publication = WeblcmsDataManager :: get_instance()->retrieve_learning_object_publication(parent :: get_user_assessment()->get_assessment_id());
+			$condition = new EqualityCondition(ComplexLearningObjectItem :: PROPERTY_PARENT, $publication->get_learning_object()->get_id());
+		}
+		else
+		{
+			$condition = new EqualityCondition(ComplexLearningObjectItem :: PROPERTY_PARENT, $user_assessment->get_assessment_id());
+		}
 		$clo_questions = $dm->retrieve_complex_learning_object_items($condition);
 		while($clo_question = $clo_questions->next_result())
 		{
 			$question = $dm->retrieve_learning_object($clo_question->get_ref());
-			$track = new WeblcmsQuestionAttemptsTracker();
-			$condition_ass = new EqualityCondition(WeblcmsQuestionAttemptsTracker :: PROPERTY_ASSESSMENT_ATTEMPT_ID, $this->get_user_assessment()->get_id());
-			$condition_question = new EqualityCondition(WeblcmsQuestionAttemptsTracker :: PROPERTY_QUESTION_ID, $clo_question->get_id());
-			$condition = new AndCondition(array($condition_ass, $condition_question));
+			
+			if (get_class($user_assessment) == 'WeblcmsAssessmentAttemptsTracker')
+			{
+				$track = new WeblcmsQuestionAttemptsTracker();
+				$condition_ass = new EqualityCondition(WeblcmsQuestionAttemptsTracker :: PROPERTY_ASSESSMENT_ATTEMPT_ID, $this->get_user_assessment()->get_id());
+				$condition_question = new EqualityCondition(WeblcmsQuestionAttemptsTracker :: PROPERTY_QUESTION_ID, $clo_question->get_id());
+				$condition = new AndCondition(array($condition_ass, $condition_question));		
+			}
+			else
+			{
+				$track = new WeblcmsLearningPathQuestionAttemptsTracker();
+				$condition_ass = new EqualityCondition(WeblcmsLearningPathQuestionAttemptsTracker :: PROPERTY_LEARNING_PATH_ASSESSMENT_ATTEMPT_ID, $this->get_user_assessment()->get_id());
+				$condition_question = new EqualityCondition(WeblcmsLearningPathQuestionAttemptsTracker :: PROPERTY_QUESTION_ID, $clo_question->get_id());
+				$condition = new AndCondition(array($condition_ass, $condition_question));	
+				//dump($condition);
+			}
+			
+			
+			//$track = new WeblcmsQuestionAttemptsTracker();
+			//$condition_ass = new EqualityCondition(WeblcmsQuestionAttemptsTracker :: PROPERTY_ASSESSMENT_ATTEMPT_ID, $this->get_user_assessment()->get_id());
+			//$condition_question = new EqualityCondition(WeblcmsQuestionAttemptsTracker :: PROPERTY_QUESTION_ID, $clo_question->get_id());
+			//$condition = new AndCondition(array($condition_ass, $condition_question));
 			$q_results = $track->retrieve_tracker_items($condition);
 			$question_result = QuestionResult :: create_question_result($this, $clo_question, $q_results, $this->get_edit_rights(), $count, parent :: get_user_assessment()->get_id());
 			$count++;
@@ -43,9 +72,10 @@ class AssignmentResultsViewer extends ResultsViewer
 			$this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
 		}
 		$max_total_score = $assessment->get_maximum_score();
-		$score = round(parent :: get_user_assessment()->get_score());
+		$score = round(parent :: get_user_assessment()->get_total_score());
 		$pct_score = round(($score / $max_total_score) * 100, 2);
-		$this->addElement('html', '<h3>'.Translation :: get('TotalScore').': '.$score."/".$max_total_score.' ('.$pct_score.'%)</h3><br />');		
+		$this->addElement('html', '<h3>'.Translation :: get('TotalScore').': '.$score."/".$max_total_score.' ('.$pct_score.'%)</h3><br />');
+		
 	}
 }
 ?>
