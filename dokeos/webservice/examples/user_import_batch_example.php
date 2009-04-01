cab506aa3cc0f562469864df4eab66e4d7703a0d
inmemoryblue <inmemoryblue@cosnics>
2009-04-01 13:40:38 +0000

    changed most example scripts changed server settings in most webservices

--- dokeos/webservice/examples/user_import_batch_example.php
+++ dokeos/webservice/examples/user_import_batch_example.php
@@ -1,6 +1,7 @@
 <?php
 require_once dirname(__FILE__) . '/../../plugin/nusoap/nusoap.php';
-ini_set('max_execution_time', 7200);
+ini_set('max_execution_time', -1);
+ini_set('memory_limit',-1);
 $time_start = microtime(true);
 
 $file = dirname(__FILE__) . '/user_import.csv';
