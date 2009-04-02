82ab54491b307fc06bc8234da5cd077449c53daf
MichaelKyndt <MichaelKyndt@localhost>
2009-04-02 09:11:37 +0000

    - added reporting block users per role, roles, number of roles, groups per role
    reinstall required

---
+++ dokeos/rights/reporting/templates/rights_data_reporting_template.class.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @author Michael Kyndt
+ */
+require_once Path :: get_reporting_path(). 'lib/reporting_template.class.php';
+class RightsDataReportingTemplate extends ReportingTemplate
+{
+	function RightsDataReportingTemplate($parent=null)
+	{
+        $this->parent = $parent;
+        $this->add_reporting_block(ReportingDataManager :: get_instance()->retrieve_reporting_block_by_name("RightsUsersPerRole"),
+            array(ReportingTemplate :: PARAM_VISIBLE => ReportingTemplate :: REPORTING_BLOCK_VISIBLE, ReportingTemplate :: PARAM_DIMENSIONS => ReportingTemplate :: REPORTING_BLOCK_USE_BLOCK_DIMENSIONS));
+        $this->add_reporting_block(ReportingDataManager :: get_instance()->retrieve_reporting_block_by_name("RightsNoOfRoles"),
+            array(ReportingTemplate :: PARAM_VISIBLE => ReportingTemplate :: REPORTING_BLOCK_VISIBLE, ReportingTemplate :: PARAM_DIMENSIONS => ReportingTemplate :: REPORTING_BLOCK_USE_BLOCK_DIMENSIONS));
+        $this->add_reporting_block(ReportingDataManager :: get_instance()->retrieve_reporting_block_by_name("RightsGroupsPerRole"),
+            array(ReportingTemplate :: PARAM_VISIBLE => ReportingTemplate :: REPORTING_BLOCK_VISIBLE, ReportingTemplate :: PARAM_DIMENSIONS => ReportingTemplate :: REPORTING_BLOCK_USE_BLOCK_DIMENSIONS));
+	}
+
+    /**
+     * @see ReportingTemplate -> get_properties()
+     */
+    public static function get_properties()
+    {
+        $properties[ReportingTemplateRegistration :: PROPERTY_TITLE] = 'RightsDataReportingTemplateTitle';
+        $properties[ReportingTemplateRegistration :: PROPERTY_PLATFORM] = 1;
+        $properties[ReportingTemplateRegistration :: PROPERTY_DESCRIPTION] = 'RightsDataReportingTemplateDescription';
+
+        return $properties;
+    }
+
+    /**
+     * @see ReportingTemplate -> to_html()
+     */
+    function to_html()
+    {
+    	//template header
+        $html[] = $this->get_header();
+
+        //template menu
+        //$html[] = $this->get_menu();
+
+        //show visible blocks
+        $html[] = $this->get_visible_reporting_blocks();
+
+    	//template footer
+        $html[] = $this->get_footer();
+
+    	return implode("\n", $html);
+    }
+}
+?>
