8c02ee95a57ae55d578a931236edc68bccd55132
MichaelKyndt <MichaelKyndt@localhost>
2009-04-02 08:27:07 +0000

    - Added an admin reporting template - added reporting blocks to the template & their functions

--- dokeos/admin/reporting/reporting_admin.class.php
+++ dokeos/admin/reporting/reporting_admin.class.php
@@ -9,7 +9,16 @@ class ReportingAdmin {
 
     public static function getNoOfApplications()
     {
+        require_once Path :: get_admin_path().'lib/admin_manager/admin_manager.class.php';
+        $user = UserDataManager :: get_instance()->retrieve_user(Session :: get_user_id());
+        $adm = new AdminManager($user);
+        $arr[Translation :: get('NumberOfApplications')][0] = 0;
+        foreach ($adm->get_application_platform_admin_links() as $application_links)
+        {
+            $arr[Translation :: get('NumberOfApplications')][0]++;
+        }
 
+        return Reporting::getSerieArray($arr);
     }
 
     public static function getMostUsedApplications()
