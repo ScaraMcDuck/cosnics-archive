c16e1ed3495b4e5329508ba675798a0f6cdba856
Samumon <Samumon@cosnics>
2009-04-06 12:38:28 +0000

    Busy with creating wiki pages

--- dokeos/application/lib/weblcms/tool/wiki/component/wiki_page_creator.class.php
+++ dokeos/application/lib/weblcms/tool/wiki/component/wiki_page_creator.class.php
@@ -6,6 +6,7 @@ require_once Path::get_repository_path().'/lib/complex_learning_object_item.clas
 
 class WikiToolPageCreatorComponent extends WikiToolComponent
 {
+    private $pub;
 	function run()
 	{
 		if (!$this->is_allowed(ADD_RIGHT))
@@ -16,18 +17,18 @@ class WikiToolPageCreatorComponent extends WikiToolComponent
 		$trail = new BreadcrumbTrail();
         $this->display_header($trail);
 		$object = $_GET['object'];
-		$pub = new LearningObjectRepoViewer($this, 'wiki_page', true,RepoViewer :: SELECT_MULTIPLE, WikiTool ::ACTION_CREATE_PAGE);
-        $pub->set_parameter('wiki_id', $_GET['wiki_id']);
+		$this->pub = new LearningObjectRepoViewer($this, 'wiki_page', true,RepoViewer :: SELECT_MULTIPLE, WikiTool ::ACTION_CREATE_PAGE);
+        $this->pub->set_parameter('wiki_id', $_GET['wiki_id']);
 
 		if(!isset($object))
 		{
 			$html[] = '<p><a href="' . $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_BROWSE_WIKIS), true) . '"><img src="'.Theme :: get_common_image_path().'action_browser.png" alt="'.Translation :: get('BrowserTitle').'" style="vertical-align:middle;"/> '.Translation :: get('BrowserTitle').'</a></p>';
-			$html[] =  $pub->as_html();
+			$html[] =  $this->pub->as_html();
             
         echo implode("\n",$html);
 		}
 		else
-        {   $cloi = new ComplexLearningObjectItem(array(ComplexLearningObjectItem :: PROPERTY_REF => $_GET['object'], ComplexLearningObjectItem ::PROPERTY_PARENT => $pub->get_parameter('wiki_id'), ComplexLearningObjectItem ::PROPERTY_USER_ID => $pub->get_user_id(), ComplexLearningObjectItem ::PROPERTY_DISPLAY_ORDER => '1'));
+        {   $cloi = new ComplexLearningObjectItem(array(ComplexLearningObjectItem :: PROPERTY_REF => $_GET['object'], ComplexLearningObjectItem ::PROPERTY_PARENT => $this->pub->get_parameter('wiki_id'), ComplexLearningObjectItem ::PROPERTY_USER_ID => $this->pub->get_user_id(), ComplexLearningObjectItem ::PROPERTY_DISPLAY_ORDER => '1'));
             $cloi->create();
             $action_bar = $this->get_toolbar();
             echo '<br />' . $action_bar->as_html();
@@ -44,7 +45,7 @@ class WikiToolPageCreatorComponent extends WikiToolComponent
 		$action_bar->set_search_url($this->get_url());
 		$action_bar->add_common_action(
 			new ToolbarItem(
-				Translation :: get('Create'), Theme :: get_common_image_path().'action_create.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_CREATE_PAGE, 'wiki_id' => $this->object_id)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+				Translation :: get('Create'), Theme :: get_common_image_path().'action_create.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_CREATE_PAGE, 'wiki_id' => $this->pub->get_parameter('wiki_id'))), ToolbarItem :: DISPLAY_ICON_AND_LABEL
 			)
 		);
 
