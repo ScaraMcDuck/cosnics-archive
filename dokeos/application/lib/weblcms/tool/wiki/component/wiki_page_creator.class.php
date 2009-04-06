16ab53ba6f438e63a5b90caab3fcd2771a7c3b19
Samumon <Samumon@localhost>
2009-04-06 12:29:45 +0000

    Busy with creating wiki pages

--- dokeos/application/lib/weblcms/tool/wiki/component/wiki_page_creator.class.php
+++ dokeos/application/lib/weblcms/tool/wiki/component/wiki_page_creator.class.php
@@ -1,6 +1,7 @@
 <?php
 require_once dirname(__FILE__).'/../../../learning_object_repo_viewer.class.php';
 require_once Path::get_library_path().'/html/action_bar/action_bar_renderer.class.php';
+require_once dirname(__FILE__) . '/../../../publisher/learning_object_publisher.class.php';
 require_once Path::get_repository_path().'/lib/complex_learning_object_item.class.php';
 
 class WikiToolPageCreatorComponent extends WikiToolComponent
@@ -13,24 +14,52 @@ class WikiToolPageCreatorComponent extends WikiToolComponent
 			return;
 		}
 		$trail = new BreadcrumbTrail();
+        $this->display_header($trail);
 		$object = $_GET['object'];
 		$pub = new LearningObjectRepoViewer($this, 'wiki_page', true,RepoViewer :: SELECT_MULTIPLE, WikiTool ::ACTION_CREATE_PAGE);
+        $pub->set_parameter('wiki_id', $_GET['wiki_id']);
 
 		if(!isset($object))
 		{
 			$html[] = '<p><a href="' . $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_BROWSE_WIKIS), true) . '"><img src="'.Theme :: get_common_image_path().'action_browser.png" alt="'.Translation :: get('BrowserTitle').'" style="vertical-align:middle;"/> '.Translation :: get('BrowserTitle').'</a></p>';
 			$html[] =  $pub->as_html();
+            
+        echo implode("\n",$html);
 		}
 		else
-		{
-			$cloi = new ComplexLearningObjectItem(array(ComplexLearningObjectItem :: PROPERTY_REF,$object));
+        {   $cloi = new ComplexLearningObjectItem(array(ComplexLearningObjectItem :: PROPERTY_REF => $_GET['object'], ComplexLearningObjectItem ::PROPERTY_PARENT => $pub->get_parameter('wiki_id'), ComplexLearningObjectItem ::PROPERTY_USER_ID => $pub->get_user_id(), ComplexLearningObjectItem ::PROPERTY_DISPLAY_ORDER => '1'));
+            $cloi->create();
+            $action_bar = $this->get_toolbar();
+            echo '<br />' . $action_bar->as_html();
+            echo '<p>Page created</p>';
 		}
 
-		$this->display_header($trail);
-        //$page = $pub->get_default_learning_object('wiki_page');
-        //dump($page);
-		echo implode("\n",$html);
+		
 		$this->display_footer();
+	}
+
+    function get_toolbar()
+	{
+		$action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
+		$action_bar->set_search_url($this->get_url());
+		$action_bar->add_common_action(
+			new ToolbarItem(
+				Translation :: get('Create'), Theme :: get_common_image_path().'action_create.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_CREATE_PAGE, 'wiki_id' => $this->object_id)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+
+		$action_bar->add_common_action(
+			new ToolbarItem(
+				Translation :: get('Browse'), Theme :: get_common_image_path().'action_browser.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_BROWSE_WIKIS)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+
+		if(!$this->introduction_text && PlatformSetting :: get('enable_introduction', 'weblcms'))
+		{
+			$action_bar->add_common_action(new ToolbarItem(Translation :: get('PublishIntroductionText'), Theme :: get_common_image_path().'action_publish.png', $this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_PUBLISH_INTRODUCTION)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+		}
+		$action_bar->add_tool_action(HelpManager :: get_tool_bar_help_item('wiki tool'));
+		return $action_bar;
 	}
 }
 ?>
