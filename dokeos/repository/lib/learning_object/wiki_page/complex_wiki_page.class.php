634d5ce27be311b0a3e82f713376cb2fab6b7647
Samumon <Samumon@localhost>
2009-04-07 07:28:07 +0000

    Changes to allow for a wiki homepage designation in the db

--- dokeos/repository/lib/learning_object/wiki_page/complex_wiki_page.class.php
+++ dokeos/repository/lib/learning_object/wiki_page/complex_wiki_page.class.php
@@ -7,6 +7,21 @@ require_once dirname(__FILE__) . '/../../complex_learning_object_item.class.php'
 
 class ComplexWikiPage extends ComplexLearningObjectItem
 {
+    const PROPERTY_IS_HOMEPAGE = 'is_homepage';
 
+	static function get_additional_property_names()
+	{
+		return array(self :: PROPERTY_IS_HOMEPAGE);
+	}
+
+	function get_is_homepage()
+	{
+		return $this->get_additional_property(self :: PROPERTY_IS_HOMEPAGE);
+	}
+
+	function set_is_homepage($value)
+	{
+		$this->set_additional_property(self :: PROPERTY_IS_HOMEPAGE, $value);
+	}
 }
 ?>
