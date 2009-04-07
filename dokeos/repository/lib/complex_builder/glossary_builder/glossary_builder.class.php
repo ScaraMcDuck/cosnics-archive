396560eeba0b637ab5c81aa356dbc57334b031f7
vanpouckesven <vanpouckesven@localhost>
2009-04-07 07:26:08 +0000

    Added some empty builders that work with the standard builder system

---
+++ dokeos/repository/lib/complex_builder/glossary_builder/glossary_builder.class.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once dirname(__FILE__) . '/../complex_builder.class.php';
+require_once dirname(__FILE__) . '/glossary_builder_component.class.php';
+
+class GlossaryBuilder extends ComplexBuilder
+{
+	
+}
+
+?>
