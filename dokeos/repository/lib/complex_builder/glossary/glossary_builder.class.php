34b24ea983656a52ba2d4d727fe076ed37f13d9a
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-04-08 10:34:18 +0000

    Fixed the complex builder for the glossary

--- dokeos/repository/lib/complex_builder/glossary/glossary_builder.class.php
+++ dokeos/repository/lib/complex_builder/glossary/glossary_builder.class.php
@@ -5,7 +5,22 @@ require_once dirname(__FILE__) . '/glossary_builder_component.class.php';
 
 class GlossaryBuilder extends ComplexBuilder
 {
-	
+	function run()
+	{
+		$action = $this->get_action();
+		
+		switch($action)
+		{
+			case ComplexBuilder :: ACTION_BROWSE_CLO :
+				$component = GlossaryBuilderComponent :: factory('Browser', $this); 
+				break;
+		}
+		
+		if(!$component)
+			parent :: run();
+		else
+			$component->run();
+	}
 }
 
 ?>
--- dokeos/repository/lib/repository_manager/component/complex_browser/complex_browser_table_cell_renderer.class.php
+++ dokeos/repository/lib/repository_manager/component/complex_browser/complex_browser_table_cell_renderer.class.php
@@ -107,7 +107,7 @@ class ComplexBrowserTableCellRenderer extends DefaultLearningObjectTableCellRend
 		$toolbar_data = array();
 		
 		$edit_url = $this->browser->get_complex_learning_object_item_edit_url($cloi, $this->browser->get_root());
-		if($cloi->is_extended() || get_parent_class($browser) == 'ComplexBuilder')
+		if($cloi->is_extended() || get_parent_class($this->browser) == 'ComplexBuilder')
 		{
 			$toolbar_data[] = array(
 				'href' => $edit_url,
