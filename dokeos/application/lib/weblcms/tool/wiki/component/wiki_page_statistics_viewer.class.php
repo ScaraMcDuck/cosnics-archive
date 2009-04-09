ee07abfec3e1f87060ebf591243e7d3da81c0f09
Samumon <Samumon@localhost>
2009-04-09 12:48:34 +0000

    To make statistics for most active user within a wiki page

---
+++ dokeos/application/lib/weblcms/tool/wiki/component/wiki_page_statistics_viewer.class.php
@@ -0,0 +1,103 @@
+<?php
+
+require_once dirname(__FILE__) . '/../wiki_tool.class.php';
+require_once dirname(__FILE__) . '/../wiki_tool_component.class.php';
+require_once dirname(__FILE__).'/wiki_page_table/wiki_page_table.class.php';
+require_once Path :: get_library_path() . '/html/action_bar/action_bar_renderer.class.php';
+
+class WikiToolPageStatisticsViewerComponent extends WikiToolComponent
+{
+	private $action_bar;
+    private $wiki_page_id;
+    private $cid;
+
+
+	function run()
+	{
+		if(!$this->is_allowed(VIEW_RIGHT))
+		{
+			Display :: not_allowed();
+			return;
+		}
+
+        $this->wiki_page_id = Request :: get('wiki_page_id');
+       
+        $dm = RepositoryDataManager :: get_instance();
+        $wiki_page = $dm->retrieve_learning_object($this->wiki_page_id);
+        $versions = $dm->retrieve_learning_object_versions($wiki_page);
+        
+
+        $this->display_header(new BreadcrumbTrail());
+
+        $this->action_bar = $this->get_toolbar();
+        echo '<br />' . $this->action_bar->as_html();
+        dump($this->get_most_active_user($versions));
+
+        
+        $this->display_footer();
+	}
+
+    function get_toolbar()
+	{
+		$action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
+
+		$action_bar->set_search_url($this->get_url());
+
+        $action_bar->add_common_action(
+			new ToolbarItem(
+				Translation :: get('Create Wiki Page'), Theme :: get_common_image_path().'action_create.png', $this->get_url(array(Tool :: PARAM_ACTION => WikiTool :: ACTION_CREATE_PAGE, 'wiki_id' => $this->wiki_id)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+		$action_bar->add_common_action(
+			new ToolbarItem(
+				Translation :: get('Edit'), Theme :: get_common_image_path().'action_edit.png', $this->get_url(array(Tool :: PARAM_ACTION => WikiTool :: ACTION_EDIT_PAGE, 'cid' => $this->cid)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+
+        $action_bar->add_common_action(
+			new ToolbarItem(
+				Translation :: get('Delete'), Theme :: get_common_image_path().'action_delete.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_DELETE, 'cid' => $this->cid)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+
+        $action_bar->add_tool_action(
+			new ToolbarItem(
+				Translation :: get('Discuss'), Theme :: get_common_image_path().'action_edit.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_DISCUSS, 'wiki_page_id' => $this->wiki_page_id, 'wiki_id' => $this->wiki_id)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+
+        $action_bar->add_tool_action(
+			new ToolbarItem(
+				Translation :: get('History'), Theme :: get_common_image_path().'action_edit.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_HISTORY, 'wiki_page_id' => $this->wiki_page_id, 'wiki_id' => $this->wiki_id)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+
+        $action_bar->add_tool_action(
+			new ToolbarItem(
+				Translation :: get('Notify Changes'), Theme :: get_common_image_path().'action_edit.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_HISTORY, 'wiki_page_id' => $this->wiki_page_id)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+
+        $action_bar->add_tool_action(
+			new ToolbarItem(
+				Translation :: get('Statistics'), Theme :: get_common_image_path().'action_statistics.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_PAGE_STATISTICS, 'wiki_page_id' => $this->wiki_page_id)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
+			)
+		);
+
+
+		return $action_bar;
+	}
+
+    function get_most_active_user($versions)
+    {
+        $users = array();
+        foreach($versions as $version)
+        {
+            $users[$version->get_default_property('owner')]++;
+        }
+        arsort($users);
+        dump($users);
+        dump(array_pop(array_keys(($users))));
+    }
+}
+?>
