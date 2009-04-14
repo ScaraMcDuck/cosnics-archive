f5352eadafc821977d8eb9f919ba30d5e6d840cd
Scara84 <Scara84@localhost>
2009-04-14 13:51:17 +0000

    Implementing nested trees for groups.

--- dokeos/group/lib/data_manager/database.class.php
+++ dokeos/group/lib/data_manager/database.class.php
@@ -216,11 +216,245 @@ class DatabaseGroupDataManager extends GroupDataManager
 		if($group_id)
 		{
 			$conditions = array();
-			$conditions[] = new EqualityCondition(Group :: PROPERTY_NAME,$username);
+			$conditions[] = new EqualityCondition(Group :: PROPERTY_NAME,$groupname);
 			$conditions = new EqualityCondition(Group :: PROPERTY_ID, $group_id);
 			$condition = new AndCondition($conditions);
 		}
 		return !($this->database->count_objects(Group :: get_table_name(), $condition) == 1);
+	}
+	
+	function add_nested_values($group, $previous_visited, $number_of_elements = 1)
+	{
+		// Update all necessary left-values
+		$condition = new InequalityCondition(Group :: PROPERTY_LEFT_VALUE, InequalityCondition :: GREATER_THAN, $previous_visited);
+		
+		$query = 'UPDATE '. $this->database->escape_table_name('group') .' SET '. $this->database->escape_column_name(Group :: PROPERTY_LEFT_VALUE) . '=' . $this->database->escape_column_name(Group :: PROPERTY_LEFT_VALUE) . ' + ?';
+
+		$params = array ();
+		$params[] = $number_of_elements * 2;
+		
+		if (isset ($condition))
+		{
+			$translator = new ConditionTranslator($this->database, $params, false);
+			$translator->translate($condition);
+			$query .= $translator->render_query();
+			$params = $translator->get_parameters();
+		}
+		
+		$statement = $this->database->get_connection()->prepare($query);
+		// TODO: Some error-handling please !
+		$res = $statement->execute($params);
+		
+		// Update all necessary right-values
+		$condition = new InequalityCondition(Group :: PROPERTY_RIGHT_VALUE, InequalityCondition :: GREATER_THAN, $previous_visited);
+		
+		$query = 'UPDATE '. $this->database->escape_table_name('group') .' SET '. $this->database->escape_column_name(Group :: PROPERTY_RIGHT_VALUE) . '=' . $this->database->escape_column_name(Group :: PROPERTY_RIGHT_VALUE) . ' + ?';
+
+		$params = array ();
+		$params[] = $number_of_elements * 2;
+		
+		if (isset ($condition))
+		{
+			$translator = new ConditionTranslator($this->database, $params, false);
+			$translator->translate($condition);
+			$query .= $translator->render_query();
+			$params = $translator->get_parameters();
+		}
+		
+		$statement = $this->database->get_connection()->prepare($query);
+		// TODO: Some error-handling please !
+		$res = $statement->execute($params);
+		
+		// TODO: For now we just return true ...
+        return true;
+	}
+	
+	function delete_nested_values($group)
+	{
+        $delta = $group->get_right_value() - $group->get_left_value() + 1;
+        
+		// Update all necessary nested-values
+		$condition = new InequalityCondition(Group :: PROPERTY_LEFT_VALUE, InequalityCondition :: GREATER_THAN, $group->get_left_value());
+		
+		$query  = 'UPDATE '. $this->escape_table_name('group');
+		$query .= ' SET '. $this->escape_column_name(Group :: PROPERTY_LEFT_VALUE) . '=' . $this->escape_column_name(Group :: PROPERTY_LEFT_VALUE) . ' - ?,';
+		$query .= $this->escape_column_name(Group :: PROPERTY_RIGHT_VALUE) . '=' . $this->escape_column_name(Group :: PROPERTY_RIGHT_VALUE) . ' - ?';
+
+		$params = array ();
+		$params[] = $delta;
+		$params[] = $delta;
+		
+		if (isset ($condition))
+		{
+			$translator = new ConditionTranslator($this, $params, true);
+			$translator->translate($condition);
+			$query .= $translator->render_query();
+			$params = $translator->get_parameters();
+		}
+		
+		$statement = $this->connection->prepare($query);
+		// TODO: Some error-handling please !
+		$statement->execute($params);
+		
+		// Update some more nested-values
+		$conditions = array();
+		$conditions[] = new InequalityCondition(Group :: PROPERTY_LEFT_VALUE, InequalityCondition :: LESS_THAN, $group->get_left_value());
+		$conditions[] = new InequalityCondition(Group :: PROPERTY_RIGHT_VALUE, InequalityCondition :: GREATER_THAN, $group->get_right_value());
+		$condition = new AndCondition($conditions);
+		
+		$query  = 'UPDATE '. $this->escape_table_name('group');
+		$query .= ' SET '. $this->escape_column_name(Group :: PROPERTY_RIGHT_VALUE) . '=' . $this->escape_column_name(Group :: PROPERTY_RIGHT_VALUE) . ' - ?';
+
+		$params = array ();
+		$params[] = $delta;
+		
+		if (isset ($condition))
+		{
+			$translator = new ConditionTranslator($this, $params, true);
+			$translator->translate($condition);
+			$query .= $translator->render_query();
+			$params = $translator->get_parameters();
+		}
+		
+		$statement = $this->connection->prepare($query);
+		// TODO: Some error-handling please !
+		$statement->execute($params);
+		
+        return true;
+	}
+	
+	function move_group($group, $new_parent_id, $new_previous_id = 0)
+	{
+        // Check some things first to avoid trouble
+        if ($new_previous_id)
+        {
+            // Don't let people move an element behind itself
+            // No need to spawn an error, since we're just not doing anything
+            if ($new_previous_id == $group->get_id())
+            {
+                return true;
+            }
+            
+            $new_previous = $this->retrieve_group($new_previous_id);
+            // TODO: What if group $new_previous_id doesn't exist ? Return error.
+            $new_parent_id = $new_previous->get_parent();
+        }
+        else
+        {
+        	// No parent ID was set ... problem !
+            if ($new_parent_id == 0)
+            {
+                return false;
+            }
+            // Move the group underneath one of it's children ?
+            // I think not ... Return error
+            if ($group->is_child_of($new_parent_id))
+            {
+            	return false;
+            }
+            // Move an element underneath itself ?
+            // No can do ... just ignore and return true
+            if ($new_parent_id == $group->get_id())
+            {
+                return true;
+            }
+            // Try to retrieve the data of the parent element
+            $new_parent = $this->retrieve_group($new_parent_id);
+            // TODO: What if this is an invalid group ? Return error. 
+        }
+
+        $number_of_elements = ($group->get_right_value() - $group->get_left_value() + 1) / 2;
+        $previous_visited = $new_previous_id ? $new_previous->get_right_value() : $new_parent->get_left_value();
+        
+        // Update the nested values so we can actually add the element
+        // Return false if this failed
+        if (!$this->add_nested_values($group, $previous_visited, $number_of_elements))
+        {
+        	return false;
+        }
+        
+        // Now we can update the actual parent_id
+        // Return false if this failed
+        $group->set_parent($new_parent_id);
+        if (!$group->update())
+        {
+        	return false;
+        }
+
+        // Update the left/right values of those elements that are being moved
+
+        // First get the offset we need to add to the left/right values
+        // if $newPrevId is given we need to get the right value,
+        // otherwise the left since the left/right has changed
+        // because we already updated it up there. We need to get them again.
+        // We have to do that anyway, to have the proper new left/right values
+        if ($new_previous_id)
+        {
+        	$temp = $this->retrieve_group($new_previous_id);
+        	// TODO: What if $temp doesn't exist ? Return error.
+        	$calculate_width = $temp->get_right_value();
+        }
+        else
+        {
+        	$temp = $this->retrieve_group($new_parent_id);
+        	// TODO: What if $temp doesn't exist ? Return error.
+        	$calculate_width = $temp->get_left_value();
+        }
+
+        // Get the element that is being moved again, since the left and
+        // right might have changed by the add-call
+        
+        $group = $this->retrieve_group($group->get_id());
+        // TODO: What if $group doesn't exist ? Return error.
+        
+        // Calculate the offset of the element to to the spot where it should go
+        // correct the offset by one, since it needs to go inbetween!
+        $offset = $calculate_width - $group->get_left_value() + 1;
+        
+        // Do the actual update
+		$conditions = array();
+		$conditions[] = new InequalityCondition(Group :: PROPERTY_LEFT_VALUE, InequalityCondition :: GREATER_THAN, ($group->get_left_value() - 1));
+		$conditions[] = new InequalityCondition(group :: PROPERTY_RIGHT_VALUE, InequalityCondition :: LESS_THAN, ($group->get_right_value() + 1));
+		$condition = new AndCondition($conditions);
+		
+		$query  = 'UPDATE '. $this->escape_table_name('group');
+		$query .= ' SET '. $this->escape_column_name(Group :: PROPERTY_LEFT_VALUE) . '=' . $this->escape_column_name(Group :: PROPERTY_LEFT_VALUE) . ' + ?,';
+		$query .= $this->escape_column_name(Group :: PROPERTY_RIGHT_VALUE) . '=' . $this->escape_column_name(Group :: PROPERTY_RIGHT_VALUE) . ' + ?';
+
+		$params = array ();
+		$params[] = $offset;
+		$params[] = $offset;
+		
+		if (isset ($condition))
+		{
+			$translator = new ConditionTranslator($this, $params, true);
+			$translator->translate($condition);
+			$query .= $translator->render_query();
+			$params = $translator->get_parameters();
+		}
+		
+		$statement = $this->connection->prepare($query);
+		// TODO: Some error-handling please !
+		$statement->execute($params);
+		
+		// Remove the subtree where the group was before
+		if (!$this->delete_nested_values($group))
+		{
+			return false;
+		}
+
+        return true;
+	}
+	
+	/**
+	 * Checks whether the given column name is the name of a column that
+	 * contains a date value, and hence should be formatted as such.
+	 * @param string $name The column name.
+	 * @return boolean True if the column is a date column, false otherwise.
+	 */
+	static function is_date_column($name)
+	{
+		return false;
 	}
 }
 ?>
