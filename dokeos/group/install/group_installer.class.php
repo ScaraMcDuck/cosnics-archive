9cbfabc6233e77d1cecb8962f33a4521974961ce
Scara84 <Scara84@localhost>
2009-04-15 09:43:14 +0000

    Implementing nested trees for groups.

--- dokeos/group/install/group_installer.class.php
+++ dokeos/group/install/group_installer.class.php
@@ -3,6 +3,7 @@
  * @package group.install
  */
 require_once dirname(__FILE__).'/../lib/group_data_manager.class.php';
+require_once dirname(__FILE__).'/../lib/group.class.php';
 require_once Path :: get_library_path().'installer.class.php';
 /**
  * This installer can be used to create the storage structure for the
@@ -17,6 +18,31 @@ class GroupInstaller extends Installer
     {
     	parent :: __construct($values, GroupDataManager :: get_instance());
     }
+    
+	/**
+	 * Additional installation steps.
+	 */
+	function install_extra()
+	{
+		if (!$this->create_root_group())
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	function create_root_group()
+	{
+		$values = $this->get_form_values();
+		
+		$group = new Group();
+		$group->set_name($values['organization_name']);
+		$group->set_parent(0);
+		$group->create();
+		
+		return true;
+	}
 	
 	function get_path()
 	{
