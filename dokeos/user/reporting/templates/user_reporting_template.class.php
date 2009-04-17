52640e536fa4225386d4394e21d4a12f421cf374
MichaelKyndt <MichaelKyndt@localhost>
2009-04-17 09:00:50 +0000

    - Added a link to the course reporting from the administration panel - Added a link to the user reporting from the administration panel - Changed some reporting block names to represent more what they contain - Added a user reporting template, which shows user information
    reinstall required (if reporting is used)

---
+++ dokeos/user/reporting/templates/user_reporting_template.class.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @author Michael Kyndt
+ * @todo:
+ * Template configuration:
+ * Able to change name, description etc
+ * 2 listboxes: one with available reporting blocks for the app, one with
+ * reporting blocks already in template.
+ */
+require_once Path :: get_reporting_path(). 'lib/reporting_template.class.php';
+class UserReportingTemplate extends ReportingTemplate
+{
+	function UserReportingTemplate($parent=null)
+	{
+        $this->parent = $parent;
+        $this->add_reporting_block(ReportingDataManager :: get_instance()->retrieve_reporting_block_by_name("UserInformation"),
+            array(ReportingTemplate :: PARAM_VISIBLE => ReportingTemplate :: REPORTING_BLOCK_VISIBLE, ReportingTemplate :: PARAM_DIMENSIONS => ReportingTemplate :: REPORTING_BLOCK_USE_BLOCK_DIMENSIONS));
+        $this->add_reporting_block(ReportingDataManager :: get_instance()->retrieve_reporting_block_by_name("UserPlatformStatistics"),
+            array(ReportingTemplate :: PARAM_VISIBLE => ReportingTemplate :: REPORTING_BLOCK_VISIBLE, ReportingTemplate :: PARAM_DIMENSIONS => ReportingTemplate :: REPORTING_BLOCK_USE_BLOCK_DIMENSIONS));
+	}
+
+    /**
+     * @see ReportingTemplate -> get_properties()
+     */
+    public static function get_properties()
+    {
+        $properties[ReportingTemplateRegistration :: PROPERTY_TITLE] = 'UserReportingTemplateTitle';
+        $properties[ReportingTemplateRegistration :: PROPERTY_PLATFORM] = 0;
+        $properties[ReportingTemplateRegistration :: PROPERTY_DESCRIPTION] = 'UserReportingTemplateDescription';
+
+        return $properties;
+    }
+
+    /**
+     * @see ReportingTemplate -> to_html()
+     */
+    function to_html()
+    {
+    	//template header
+        $html[] = $this->get_header();
+
+        $html[] = '<div class="reporting_template_container">';
+        $html[] = '<div class="reporting_template_con_left">';
+        $html[] = $this->get_reporting_block_html('UserUserInformation');
+        $html[] = '</div>';
+        $html[] = '<div class="reporting_template_con_right">';
+        $html[] = $this->get_reporting_block_html('UserCourseStatistics');
+        $html[] = '</div><div class="clear">&nbsp;</div>';
+        $html[] = '</div>';
+
+        //show visible blocks
+        //$html[] = $this->get_visible_reporting_blocks();
+
+    	//template footer
+        $html[] = $this->get_footer();
+
+    	return implode("\n", $html);
+    }
+}
+?>
