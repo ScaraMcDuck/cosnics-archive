c1baa71f3ea389513d01c1a552074ee0d51933de
sbil136 <sbil136@localhost>
2009-04-20 14:49:47 +0000

    New serie of translations (thanks to Michael)

--- dokeos/languages/english/myportfolio.inc.php
+++ dokeos/languages/english/myportfolio.inc.php
@@ -20,54 +20,22 @@ $lang['myportfolio']['NoPublicationSelected'] = 'No publication selected';
 $lang['myportfolio']['PublishPortfolio'] = 'Publish portfolio';
 $lang['myportfolio']['Save'] = 'Save';
 $lang['myportfolio']['Reset'] = 'Reset';
-$lang['myportfolio']['MyAccount'] = 'My account';
-$lang['myportfolio']['Repository'] = 'Repository';
-$lang['myportfolio']['Administration'] = 'Administration';
-$lang['myportfolio']['Logout'] = 'Logout';
-
-$lang['myportfolio']['Myportfolio'] = "My Portfolio";
-$lang['myportfolio']['MyPortfolio'] = "My Portfolio";
-$lang['myportfolio']['PortfolioOf'] = "Portfolio of ";
-$lang['myportfolio']['Pf_item_viewTitle'] = "View";
-$lang['myportfolio']['Pf_create_childTitle'] = "Add";
-$lang['myportfolio']['Pf_edit_childTitle'] = "Edit";
-$lang['myportfolio']['Pf_propsTitle'] = "Properties";
-$lang['myportfolio']['Pf_pubsTitle'] = "View Publications";
-$lang['myportfolio']['Pf_projTitle'] = "View Projects";
-$lang['myportfolio']['Pf_thesTitle'] = "View Theses";
-$lang['myportfolio']['PublicationcreatorTitle'] = "New";
-$lang['myportfolio']['BrowserTitle'] = "Browse";
-$lang['myportfolio']['FinderTitle'] = "Search";
-$lang['myportfolio']['CategoryTypeName'] = "Category";
-$lang['myportfolio']['Attachments'] = "Attachments";
-$lang['myportfolio']['AddAttachments'] = "Add attachments";
-$lang['myportfolio']['ObjectCreated'] = "Object Created";
-$lang['myportfolio']['CreateAsNewVersion'] = "Create as new version";
-
-$lang['myportfolio']['ViewPortfolio'] = "View Portfolio";
-$lang['myportfolio']['PublishPortfolio'] = "Create Portfoliopage";
-
-$lang['myportfolio']['Pf_intro_text'] = "Welcome to your portfolio, click Edit to create your own portfolio.";
-
-$lang['myportfolio']['MyResearch'] = "My Research";
-$lang['myportfolio']['Mypubs'] = "Publications";
-$lang['myportfolio']['Myproj'] = "Projects";
-$lang['myportfolio']['Mythes'] = "Theses";
-
-$lang['myportfolio']['Pf_Visibility'] = "Portfolio Visibility";
-$lang['myportfolio']['Pf_movepage'] = "Move page and subpages to:";
-$lang['myportfolio']['Pf_deletepage'] = "Delete this page";
-$lang['myportfolio']['Pf_public'] = "Open for the world";
-$lang['myportfolio']['Pf_platform'] = "Open to people inside the platform";
-$lang['myportfolio']['Pf_private'] = "Private";
-
-$lang['myportfolio']['Username'] = "Username";
-$lang['myportfolio']['Lastname'] = "Lastname";
-$lang['myportfolio']['Firstname'] = "Firstname";
-$lang['myportfolio']['Portfolio_item'] = "Portfolio";
-
-$lang['myportfolio']['GeneralProperties'] = "General Properties";
-$lang['myportfolio']['PortfolioItemFormTitle'] = "Title";
-$lang['myportfolio']['PortfolioItemFormDescription'] = "Description";
-$lang['myportfolio']['Update'] = "Update";
+$lang['myportfolio']['PortfolioOf'] = ' ';
+$lang['myportfolio']['Pf_intro_text'] = ' ';
+$lang['myportfolio']['Mypubs'] = ' ';
+$lang['myportfolio']['MyResearch'] = ' ';
+$lang['myportfolio']['Myproj'] = ' ';
+$lang['myportfolio']['Mythes'] = ' ';
+$lang['myportfolio']['Search'] = 'Search';
+$lang['myportfolio']['Pf_public'] = ' ';
+$lang['myportfolio']['Pf_platform'] = ' ';
+$lang['myportfolio']['Pf_private'] = ' ';
+$lang['myportfolio']['Pf_Visibility'] = ' ';
+$lang['myportfolio']['Pf_movepage'] = ' ';
+$lang['myportfolio']['Pf_deletepage'] = ' ';
+$lang['myportfolio']['dateFormatShort'] = '%b %d, %y';
+$lang['myportfolio']['timeNoSecFormat'] = '%I:%M %p';
+$lang['myportfolio']['Edit'] = ' ';
+$lang['myportfolio']['EditNotAllowed'] = ' ';
+$lang['myportfolio']['NoObjectSelected'] = ' ';
 ?>
