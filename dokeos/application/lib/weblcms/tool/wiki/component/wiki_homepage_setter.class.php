140b25d1326cc9d5a598cc09c56e76aa13c7fb98
Samumon <Samumon@localhost>
2009-04-20 12:11:02 +0000

    Fixed homepage setter to work with cid's and pid's

--- dokeos/application/lib/weblcms/tool/wiki/component/wiki_homepage_setter.class.php
+++ dokeos/application/lib/weblcms/tool/wiki/component/wiki_homepage_setter.class.php
@@ -20,15 +20,13 @@ class WikiToolHomepageSetterComponent extends WikiToolComponent
 		$trail = new BreadcrumbTrail();
 
         $dm = RepositoryDataManager :: get_instance();
-        $conditions[] = new EqualityCondition(ComplexWikipage ::PROPERTY_REF, Request :: get('ref'));
-        $conditions[] = new EqualityCondition(ComplexWikipage ::PROPERTY_PARENT, Request :: get('parent'));
-        $page = $dm->retrieve_complex_learning_object_items(new AndCondition($conditions))->as_array();
-        if(!empty($page[0]))
+        $page = $dm->retrieve_complex_learning_object_item(Request :: get('id'));
+        if(!empty($page))
         {
-            $page[0]->set_is_homepage(true);
-            $page[0]->update();
+            $page->set_is_homepage(true);
+            $page->update();
         }
-        $this->redirect(null, null, '', array(Tool :: PARAM_ACTION => WikiTool ::ACTION_VIEW_WIKI, WikiTool :: PARAM_PUBLICATION_ID => Request :: get('parent')));
+        $this->redirect(null, null, '', array(Tool :: PARAM_ACTION => WikiTool ::ACTION_VIEW_WIKI, 'cid' => Request :: get('id')));
         
     }
 }
