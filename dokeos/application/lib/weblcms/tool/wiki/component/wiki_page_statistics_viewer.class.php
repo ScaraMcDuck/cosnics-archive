133817e20709b07806e4ae5d3e94f243f405ab0d
Samumon <Samumon@cosnics>
2009-04-22 12:28:45 +0000

    Fixed some bugs with the statistics and redirection

--- dokeos/application/lib/weblcms/tool/wiki/component/wiki_page_statistics_viewer.class.php
+++ dokeos/application/lib/weblcms/tool/wiki/component/wiki_page_statistics_viewer.class.php
@@ -24,72 +24,8 @@ class WikiToolPageStatisticsViewerComponent extends WikiToolComponent
         $params[ReportingManager :: PARAM_COURSE_ID] = $this->get_course_id();
         $params['pid'] = Request :: get('pid');
         $params['cid'] = Request :: get('cid');
-        $this->display_header(new BreadcrumbTrail());
-        $this->action_bar = $this->get_toolbar();
-        echo '<br />' . $this->action_bar->as_html();
         $url = ReportingManager :: get_reporting_template_registration_url('WikiPageReportingTemplate',$params);
         header('location: '.$url);
-
-
     }
-
-    function get_toolbar()
-	{
-		$action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
-
-		$action_bar->set_search_url($this->get_url());
-
-
-        $action_bar->add_common_action(
-			new ToolbarItem(
-				Translation :: get('CreateWikiPage'), Theme :: get_common_image_path().'action_create.png', $this->get_url(array(Tool :: PARAM_ACTION => WikiTool :: ACTION_CREATE_PAGE, 'pid' => $this->publication_id)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
-			)
-		);
-
-        $action_bar->add_common_action(
-			new ToolbarItem(
-				Translation :: get('BrowseWiki'), Theme :: get_common_image_path().'action_browser.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool ::ACTION_VIEW_WIKI, 'pid' => $this->publication_id)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
-			)
-		);
-
-        $action_bar->add_common_action(
-			new ToolbarItem(
-				Translation :: get('Edit'), Theme :: get_common_image_path().'action_edit.png', $this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_EDIT_CLOI, 'pid' => $this->publication_id, 'cid' => $this->cid)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
-			)
-		);
-
-        $action_bar->add_common_action(
-			new ToolbarItem(
-				Translation :: get('Delete'), Theme :: get_common_image_path().'action_delete.png', $this->get_url(array(WikiTool :: PARAM_ACTION => Tool:: ACTION_DELETE_CLOI, 'pid' => $this->publication_id,'cid' => $this->cid)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
-			)
-		);
-
-        $action_bar->add_tool_action(
-			new ToolbarItem(
-				Translation :: get('Discuss'), Theme :: get_common_image_path().'action_users.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_DISCUSS, 'pid' => $this->publication_id, 'cid' => $this->cid)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
-			)
-		);
-
-        $action_bar->add_tool_action(
-			new ToolbarItem(
-				Translation :: get('History'), Theme :: get_common_image_path().'action_versions.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_HISTORY, 'pid' => $this->publication_id, 'cid' => $this->cid)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
-			)
-		);
-
-        /*$action_bar->add_tool_action(
-			new ToolbarItem(
-				Translation :: get('NotifyChanges'), Theme :: get_common_image_path().'action_subscribe.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_HISTORY, 'pid' => $this->publication_id, 'cid' => $this->cid)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
-			)
-		);*/
-
-        $action_bar->add_tool_action(
-			new ToolbarItem(
-				Translation :: get('Statistics'), Theme :: get_common_image_path().'action_reporting.png', $this->get_url(array(WikiTool :: PARAM_ACTION => WikiTool :: ACTION_PAGE_STATISTICS, 'pid' => $this->publication_id, 'cid' => $this->cid)), ToolbarItem :: DISPLAY_ICON_AND_LABEL
-			)
-		);
-
-
-		return $action_bar;
-	}
 }
 ?>
