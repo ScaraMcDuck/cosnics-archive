e854a0a15572d9bc4af57441224669f8a94a7988
Samumon <Samumon@localhost>
2009-05-04 10:55:26 +0000

    Changed the wiki form so that the current value for "locked" is displayed Added extra textarea to save links for the customisable wikitoolbox

--- dokeos/repository/lib/learning_object/wiki/wiki.class.php
+++ dokeos/repository/lib/learning_object/wiki/wiki.class.php
@@ -7,6 +7,7 @@ require_once dirname(__FILE__) . '/../../learning_object.class.php';
 class Wiki extends LearningObject 
 {
     const PROPERTY_LOCKED = 'locked';
+    const PROPERTY_LINKS = 'links';
     
 	function get_allowed_types()
 	{
@@ -23,9 +24,19 @@ class Wiki extends LearningObject
 		return $this->set_additional_property(self :: PROPERTY_LOCKED, $locked);
 	}
 
+    function get_links()
+	{
+		return $this->get_additional_property(self :: PROPERTY_LINKS);
+	}
+
+    function set_links($links)
+	{
+		return $this->set_additional_property(self :: PROPERTY_LINKS, $links);
+	}
+
     static function get_additional_property_names()
 	{
-		return array(self :: PROPERTY_LOCKED);
+		return array(self :: PROPERTY_LOCKED, self :: PROPERTY_LINKS);
 	}
 }
 ?>
