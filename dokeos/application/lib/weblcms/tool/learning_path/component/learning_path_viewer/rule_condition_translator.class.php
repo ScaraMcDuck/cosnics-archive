7eed23604d674fb6a601345b008752e5e75347ed
vanpouckesven <vanpouckesven@localhost>
2009-05-14 08:40:44 +0000

    Fixed implementation of example CM-03a

--- dokeos/application/lib/weblcms/tool/learning_path/component/learning_path_viewer/rule_condition_translator.class.php
+++ dokeos/application/lib/weblcms/tool/learning_path/component/learning_path_viewer/rule_condition_translator.class.php
@@ -52,37 +52,87 @@ class RuleConditionTranslator
 		$pre_condition_rules = $rules->get_precondition_rules();
 		foreach($pre_condition_rules as $pre_condition_rule)
 		{
-			//$action = $pre_condition_rule->get_action();
 			$rules = $pre_condition_rule->get_conditions();
-
+			$action = $pre_condition_rule->get_action();
+			$operator = $pre_condition_rule->get_conditions_operator();
+			$status = true;
+			
 			foreach($rules as $rule)
 			{
 				switch($rule->get_condition())
 				{
 					case "satisfied":
-						if(is_array($objective_trackers))
-						{
-							foreach($objective_trackers as $objective_tracker)
-							{
-								if($objective_tracker->get_status() == 'completed')
-									return 'skip';
-							}
-						}
-						else
-						{ 
-							foreach($tracker_data['trackers'] as $tracker)
-							{ 
-								if($tracker->get_status() == 'completed')
-									return 'skip';
-							}
-						}
+						$condition_status = $this->check_for_satisfied($objective_trackers, $tracker_data); break;
+					case "attempted":
+						$condition_status = $this->check_for_attempted($tracker_data); break;
+					case "completed":
+						$condition_status = $this->check_for_completed($tracker_data); break;
 				}
+
+				if($rule->get_not_condition())
+					$condition_status = !$condition_status;
+				
+				switch($operator)
+				{
+					case "all":
+						$status &= $condition_status; break;
+					case "any":
+						$status |= $condition_status; break;
+				} 
+			}
+			
+			if($status)
+			{
+				if($action == 'stopFowardTraversal')
+				{
+					$this->stop_forward_traversing = true;
+					return 'enabled';
+				}
+				
+				return $action;
 			}
 			
 		}
 		
 		return 'enabled';
 		
+	}
+	
+	private function check_for_satisfied($objective_trackers, $tracker_data)
+	{
+		if(is_array($objective_trackers))
+		{
+			foreach($objective_trackers as $objective_tracker)
+			{
+				if($objective_tracker->get_status() == 'completed')
+				{
+					return true;
+				}
+			}
+		}
+		else
+		{ 
+			foreach($tracker_data['trackers'] as $tracker)
+			{ 
+				if($tracker->get_status() == 'completed')
+				{
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	private function check_for_attempted($tracker_data)
+	{
+		echo (count($tracker_data['trackers']) > 0);
+		return count($tracker_data['trackers']) > 0;
+	}
+	
+	private function check_for_completed($tracker_data)
+	{
+		return ($tracker_data['completed'] == 1);
 	}
 }
 
