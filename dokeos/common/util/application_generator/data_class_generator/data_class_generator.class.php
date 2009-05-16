2ddfddd6b48caa0c00a64d9be26596bd429522ee
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-05-16 17:15:17 +0000

    Started working on an application generator which will make it much easier for an developer to start programming in dokeos 2.0

---
+++ dokeos/common/util/application_generator/data_class_generator/data_class_generator.class.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Dataclass generator used to generate dataclasses with given properties
+ * @author Sven Vanpoucke
+ */
+class DataClassGenerator 
+{
+    private $template;
+    private $location;
+    private $classname;
+    private $properties;
+    private $package;
+    private $description;
+    private $author;
+    
+    /**
+     * Constructor
+     * @param string $location - The location of the class
+     * @param string $classname the classname
+     * @param array of strings $properties the properties
+     * @param string $package the package
+     * @param string $description the description
+     * @param string $author, the author
+     */
+    function DataClassGenerator($location, $classname, $properties, $package, $description, $author) 
+    {
+    	$this->template = new MyTemplate();
+    	$this->template->set_rootdir(dirname(__FILE__));
+    	
+    	$this->location = $location;
+    	$this->classname = $classname;
+    	$this->properties = $properties;
+    	$this->package = $package;
+    	$this->description = $description;
+    	$this->author = $author;
+    	
+    	$this->generate_data_class();
+    }
+    
+    /**
+     * Generate a dataclass with the given info
+     */
+    function generate_data_class()
+    {	
+    	if(!is_dir($this->location))
+    		mkdir($this->location, 0777, true);
+    	 
+    	$file = fopen($this->location . strtolower($this->classname) . '.class.php', 'w+');
+    	
+    	if($file)
+    	{
+    		$this->template->set_filenames(array(
+				'dataclass' => 'data_class.template')
+				);
+			
+			$property_names = array();
+			
+			$this->template->assign_vars(array(
+				'PACKAGE' => $this->package,
+				'DESCRIPTION' => $this->description,
+				'AUTHOR' => $this->author,
+				'CLASSNAME' => $this->classname
+			));
+			
+			foreach($this->properties as $property)
+			{
+				$property_const = 'PROPERTY_' . strtoupper($property);
+				$property_names[] = 'self :: ' . $property_const;
+				
+				$this->template->assign_block_vars("CONSTS", array(
+					'PROPERTY_CONST' => $property_const,
+					'PROPERTY_NAME' => $property
+				));
+				
+				$this->template->assign_block_vars("PROPERTY", array(
+					'PROPERTY_CONST' => $property_const,
+					'PROPERTY_NAME' => $property
+				));
+			}
+			
+			$this->template->assign_vars(array(
+				'DEFAULT_PROPERTY_NAMES' => implode(', ', $property_names)
+			));
+			
+			$string = "<?php \n" . $this->template->pparse_return('dataclass') . "\n?>";
+			fwrite($file, $string);
+			fclose($file);
+    	}
+    }
+}
+
+?>
---
+++ dokeos/common/util/application_generator/index.php
@@ -0,0 +1,74 @@
+<?php
+ini_set('include_path',realpath(dirname(__FILE__).'/../../../plugin/pear'));
+require_once dirname(__FILE__).'/../../global.inc.php';
+include(dirname(__FILE__) . '/settings.inc.php');
+include(dirname(__FILE__) . '/../my_template.php');
+include(dirname(__FILE__) . '/data_class_generator/data_class_generator.class.php');
+
+$location = $application['location'];
+$name = $application['name'];
+
+create_folders($location, $name);
+
+$files = FileSystem :: get_directory_content($location, FileSystem :: LIST_FILES);
+foreach($files as $file)
+{
+	$new_path = move_file($file); 
+	
+	$properties = retrieve_properties_from_xml_file($file);
+	$classname = DokeosUtilities :: underscores_to_camelcase(str_replace('.xml', '', basename($file)));
+	$description = 'This class describes a ' . $classname . ' data object';
+	
+	$generator = new DataClassGenerator($location, $classname , $properties, $name, $description, $application['author']);
+}
+
+/**
+ * Create folders for the application
+ *
+ * @param String $location - The location of the application
+ * @param String $name - The name of the application
+ */
+function create_folders($location, $name)
+{
+	$folders = array('data_manager', 'forms', 'install', $name . '_manager', 'rights');
+	foreach($folders as $folder)
+	{
+		FileSystem :: create_dir($location . $folder);
+	}
+}
+
+/**
+ * Move a file from the root to the install folder
+ *
+ * @param String $file - Path of the file
+ * @return String $new_file - New path of the file
+ */
+function move_file($file)
+{
+	$new_file = dirname($file) . '/install/' . basename($file);
+	FileSystem :: copy_file($file, $new_file);
+	return $new_file;
+}
+
+/**
+ * Retrieves the properties from a data xml file
+ *
+ * @param String $file - The xml file
+ * @return Array of String - The properties
+ */
+function retrieve_properties_from_xml_file($file)
+{
+	$properties = array();
+	
+	$options[] = array(XML_UNSERIALIZER_OPTION_FORCE_ENUM => array('property'));
+	$array = DokeosUtilities :: extract_xml_file($file, $options);
+	
+	foreach($array['properties']['property'] as $property)
+	{
+		$properties[] = $property['name'];
+	}
+	
+	return $properties;
+}
+
+?>
