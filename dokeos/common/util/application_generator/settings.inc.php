431219f54c3bb7741909f87a60a6ac7a4167fb21
vanpouckesven <vanpouckesven@localhost>
2009-05-16 17:15:17 +0000

    Started working on an application generator which will make it much easier for an developer to start programming in dokeos 2.0

---
+++ dokeos/common/util/application_generator/settings.inc.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Settings for dataclass generator
+ */
+$application['location'] = dirname(__FILE__) . '/examples/linker/';
+$application['name'] = 'linker';
+$application['author'] = 'Sven Vanpoucke';
+?>
