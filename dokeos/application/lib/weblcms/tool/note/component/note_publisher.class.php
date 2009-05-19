e65668af1443942aae37112b0d9c1bb56efe0c6b
Samumon <Samumon@cosnics>
2009-05-19 14:56:00 +0000

    Fixed breadcrumbs in Notes in Courses

--- dokeos/application/lib/weblcms/tool/note/component/note_publisher.class.php
+++ dokeos/application/lib/weblcms/tool/note/component/note_publisher.class.php
@@ -15,6 +15,8 @@ class NoteToolPublisherComponent extends NoteToolComponent
 			return;
 		}
 		$trail = new BreadcrumbTrail();
+        $trail->add(new BreadCrumb($this->get_url(array(Tool :: PARAM_ACTION => 'publish')), Translation :: get('Publish')));
+
 		
 		$object = $_GET['object'];
 		$pub = new LearningObjectRepoViewer($this, 'note', true);
