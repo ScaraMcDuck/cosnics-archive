e3cb96eaadc31fa1f53933535e1922b50a128a95
sbil136 <sbil136@localhost>
2009-05-27 13:50:12 +0000

    Added two new language packs

---
+++ dokeos/languages/english/linker.inc.php
@@ -0,0 +1,19 @@
+<?php
+$lang['linker']['Name'] = 'Name';
+$lang['linker']['ThisFieldIsRequired'] = 'required field';
+$lang['linker']['Description'] = 'Description';
+$lang['linker']['Url'] = 'Url';
+$lang['linker']['Update'] = 'Update';
+$lang['linker']['Reset'] = 'Reset';
+$lang['linker']['Create'] = 'Create';
+$lang['linker']['Links'] = 'Links';
+$lang['linker']['Title'] = 'Title';
+$lang['linker']['Delete'] = 'Delete';
+$lang['linker']['CreateLink'] = 'Create link';
+$lang['linker']['LinkCreated'] = 'Link created';
+$lang['linker']['LinkNotCreated'] = 'Link not created';
+$lang['linker']['UpdateLink'] = 'Update link';
+$lang['linker']['LinkUpdated'] = 'Link updated';
+$lang['linker']['LinkNotUpdated'] = 'Link not updated';
+$lang['linker']['NoLinksSelected'] = 'No link selected';
+?>
