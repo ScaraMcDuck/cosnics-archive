ba79bb56d1c1d758795336d7a3e0900fa1bd95ef
scara84 <scara84@localhost>
2009-06-02 13:47:21 +0000

    Added some jQuery functionality to allow paging of the platformmenu tabs.

--- dokeos/plugin/jquery/jquery.tabula.js
+++ dokeos/plugin/jquery/jquery.tabula.js
@@ -1,3 +1,26 @@
+/*
+Copyright (c) 2008, http://seyfertdesign.com/jquery/ui-tabs-paging.html
+Copyright (c) 2009, Hans De Bisschop, conversion to seperate (non ui-tabs based) plugin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 (function($){
 	$.fn.extend({ 
 		//plugin name - animatemenu
@@ -22,7 +45,7 @@
 			
 			function init()
 			{
-				self.lis = $('ul', self);
+				self.lis = $('ul:not(li > ul)', self);
 				
 				allTabsWidth = 0, currentPage = 0, maxPageWidth = 0, buttonWidth = 0,
 				pages = new Array(), tabWidths = new Array(), selectedTabWidths = new Array();
@@ -30,32 +53,28 @@
 				containerWidth = $(self).width();
 				
 				// create next button			
-				$li = $('<ul><li></li></ul>')
-					.addClass('menu-tabs-next')
-				//$a = $('<a href="#"></a>')
+				$li = $('<ul><li><a href="#">'+ opts.nextButton +'</a></li></ul>')
 					.click(function() { page('next'); return false; })
-					.html(opts.nextButton);
-				//$li.append($a);
+					.addClass('menu-tabs-next');
 				
 				self.lis.eq($(self.lis).length-1).after($li);
-				buttonWidth = $li.outerWidth({ margin: true });
+				
+				buttonWidth = $li.width();
 				
 				// create prev button
-				$li = $('<ul><li></li></ul>')
-					.addClass('menu-tabs-prev')
-				//$a = $('<a href="#"></a>')
+				$li = $('<ul><li><a href="#">'+ opts.prevButton +'</a></li></ul>')
 					.click(function() { page('prev'); return false; })
-					.html(opts.prevButton);
-				//$li.append($a);
+					.addClass('menu-tabs-prev');
 				self.lis.eq(0).before($li);
-				buttonWidth += $li.outerWidth({ margin: true });
-				buttonWidth += 8;
+				
+				buttonWidth += $li.width();
+				buttonWidth += 70;
 				
 				// loops through LIs, get width of each tab when selected and unselected.
 				self.lis.each(function(i) {
 					var listElement = $("li:first", this);
-					tabWidths[i] = $(listElement).outerWidth({ margin: true });
-					selectedTabWidths[i] = $(listElement).outerWidth({ margin: true });
+					tabWidths[i] = $(this).width();
+					selectedTabWidths[i] = $(this).width();
 					allTabsWidth += selectedTabWidths[i];
 					
 					if($(listElement).hasClass("current"))
@@ -87,17 +106,27 @@
 						}
 						pages[pageIndex].end = i+1;
 						pageWidth += tabWidths[i];
-						if (i == currentTab) currentPage = pageIndex;
+						if (i == currentTab)
+						{
+							currentPage = pageIndex;
+						}
 					}
-					if ((pageWidth + maxTabPadding) > maxPageWidth)	
+					if ((pageWidth + maxTabPadding) > maxPageWidth)
+					{
 						maxPageWidth = (pageWidth + maxTabPadding);
-	
+					}
+					
 				    // hide all tabs then show tabs for current page
 					self.lis.hide().slice(pages[currentPage].start, pages[currentPage].end).show();
+				    
 					if (currentPage == (pages.length - 1) && !opts.cycle) 
-						disableButton('next');			
-					if (currentPage == 0 && !opts.cycle) 
+					{
+						disableButton('next');
+					}
+					if (currentPage == 0 && !opts.cycle)
+					{
 						disableButton('prev');
+					}
 					
 					// calculate the right padding for the next button
 					buttonPadding = containerWidth - maxPageWidth - buttonWidth - 8;
@@ -105,43 +134,54 @@
 					//	$('.ui-tabs-paging-next', self.element).css({ paddingRight: buttonPadding + 'px' });
 					
 					initialized = true;
-				} else {
+				}
+				else
+				{
 					destroy();
 				}
 				
 				$(window).bind('resize', handleResize);
 			}
 			
-			function page(direction) {					
+			function page(direction)
+			{
 				currentPage = currentPage + (direction == 'prev'?-1:1);
 				
 				if (direction == 'prev' && currentPage < 0 && opts.cycle)
+				{
 					currentPage = pages.length - 1;
-				else if ((direction == 'prev' && currentPage < 0) || 
-				         (direction == 'next' && currentPage >= pages.length))
+				}
+				else if ((direction == 'prev' && currentPage < 0) || (direction == 'next' && currentPage >= pages.length))
+				{
 					currentPage = 0;
+				}
 				
 				var start = pages[currentPage].start;
 				var end = pages[currentPage].end;
 				self.lis.hide().slice(pages[currentPage].start, pages[currentPage].end).show();
 				
-				if (direction == 'prev') {
+				if (direction == 'prev')
+				{
 					enableButton('next');
 					if (opts.follow && (currentTab < start || currentTab > (end-1))) self.select(end-1);
 					if (!opts.cycle && start <= 0) disableButton('prev');
-				} else {
+				}
+				else
+				{
 					enableButton('prev');
 					if (opts.follow && (currentTab < start || currentTab > (end-1))) self.select(start);
 					if (!opts.cycle && end >= $(self.lis).length) disableButton('next');
 				}
 			}
 			
-			function disableButton(direction) {
-				//$('.menu-tabs-'+direction, self.lis).hide();
+			function disableButton(direction)
+			{
+				$('.menu-tabs-' + direction, self).hide();
 			}
 			
-			function enableButton(direction) {
-				$('.menu-tabs-'+direction, self.lis).show();
+			function enableButton(direction)
+			{
+				$('.menu-tabs-' + direction, self).show();
 			}
 			
 			// special function defined to handle IE6 and IE7 resize issues
