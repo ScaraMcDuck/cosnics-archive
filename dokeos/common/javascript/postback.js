7b45924cba250c44a5bc4a51cf790facc61abe73
vanpouckesven <vanpouckesven@localhost>
2009-06-02 09:24:12 +0000

    Fixed the auto submit of the filter form

--- dokeos/common/javascript/postback.js
+++ dokeos/common/javascript/postback.js
@@ -2,13 +2,17 @@
 {
 	var handle_postback = function() 
 	{ 
-	   var form = $(this).closest("form"); alert(form.attr("id"));
-	   form.submit();
+	   var form = $(this).closest("form");
+	   $("button:submit", form).click();
 	} 
 
 	$(document).ready( function() 
 	{
 		$(".postback").change(handle_postback);
+		$(".postback").each(function(i){
+				 var form = $(this).closest("form");
+				 $("button:submit", form).hide();
+		});
 
 	});
 	
