2651f61d90d7aa097de6c33bf2765299be5f20bf
Hans De Bisschop <hans.de.bisschop@ehb.be>
2009-06-10 08:14:45 +0000

    Implemented new "receiver" selection element for system announcements.

--- dokeos/admin/lib/data_manager/database.class.php
+++ dokeos/admin/lib/data_manager/database.class.php
@@ -19,196 +19,269 @@ require_once 'MDB2.php';
 class DatabaseAdminDataManager extends AdminDataManager
 {
 	private $database;
-	
+
 	function initialize()
 	{
 		$this->database = new Database(array('admin_category' => 'cat', 'language' => 'lang', 'setting' => 'setting', 'registration' => 'reg', 'system_announcement_publication' => 'sa'));
-		$this->database->set_prefix('admin_'); 
+		$this->database->set_prefix('admin_');
 	}
-	
+
     function retrieve_languages($condition = null, $orderBy = array (), $orderDir = array (), $offset = 0, $maxObjects = -1)
 	{
 		return $this->database->retrieve_objects(Language :: get_table_name(), $condition, $offset, $maxObjects, $orderBy, $orderDir);
 	}
-	
+
     function retrieve_settings($condition = null, $orderBy = array (), $orderDir = array (), $offset = 0, $maxObjects = -1)
 	{
 		return $this->database->retrieve_objects(Setting :: get_table_name(), $condition, $offset, $maxObjects, $orderBy, $orderDir);
 	}
-	
+
 	function retrieve_registrations($condition = null, $orderBy = array (), $orderDir = array (), $offset = 0, $maxObjects = -1)
 	{
 		return $this->database->retrieve_objects(Registration :: get_table_name(), $condition, $offset, $maxObjects, $orderBy, $orderDir);
 	}
-	
+
 	function retrieve_language_from_english_name($english_name)
 	{
 		$condition = new EqualityCondition(Language :: PROPERTY_ENGLISH_NAME, $english_name);
 		return $this->database->retrieve_object(Language :: get_table_name(), $condition);
 	}
-	
+
 	function retrieve_setting_from_variable_name($variable, $application = 'admin')
 	{
 		$conditions[] = new EqualityCondition(Setting :: PROPERTY_APPLICATION, $application);
 		$conditions[] = new EqualityCondition(Setting :: PROPERTY_VARIABLE, $variable);
 		$condition = new AndCondition($conditions);
-	
+
 		return $this->database->retrieve_object(Setting :: get_table_name(), $condition);
 	}
-	
+
 	function update_setting($setting)
 	{
 		$condition = new EqualityCondition(Setting :: PROPERTY_ID, $setting->get_id());
 		return $this->database->update($setting, $condition);
 	}
-	
+
 	function update_registration($registration)
 	{
 		$condition = new EqualityCondition(Registration :: PROPERTY_ID, $registration->get_id());
 		return $this->database->update($registration, $condition);
 	}
-	
+
 	function update_system_announcement_publication($system_announcement_publication)
 	{
+		// Delete existing target users and groups
+		$parameters['id'] = $system_announcement_publication->get_id();
+		$query = 'DELETE FROM '.$this->database->escape_table_name('system_announcement_publication_user').' WHERE system_announcement_publication = ?';
+		$statement = $this->database->get_connection()->prepare($query);
+		$statement->execute($parameters['id']);
+		$query = 'DELETE FROM '.$this->database->escape_table_name('system_announcement_publication_group').' WHERE system_announcement_publication = ?';
+		$statement = $this->database->get_connection()->prepare($query);
+		$statement->execute($parameters['id']);
+
+		// Add updated target users and course_groups
+		$users = $system_announcement_publication->get_target_users();
+		$this->database->get_connection()->loadModule('Extended');
+		foreach($users as $user_id)
+		{
+			$props = array();
+			$props[$this->database->escape_column_name('system_announcement_publication')] = $system_announcement_publication->get_id();
+			$props[$this->database->escape_column_name('user')] = $user_id;
+			$this->database->get_connection()->extended->autoExecute($this->database->get_table_name('system_announcement_publication_user'), $props, MDB2_AUTOQUERY_INSERT);
+		}
+		$groups = $system_announcement_publication->get_target_groups();
+		foreach($groups as $group_id)
+		{
+			$props = array();
+			$props[$this->database->escape_column_name('system_announcement_publication')] = $system_announcement_publication->get_id();
+			$props[$this->database->escape_column_name('group_id')] = $group_id;
+			$this->database->get_connection()->extended->autoExecute($this->database->get_table_name('system_announcement_publication_group'), $props, MDB2_AUTOQUERY_INSERT);
+		}
+
+
 		$condition = new EqualityCondition(SystemAnnouncementPublication :: PROPERTY_ID, $system_announcement_publication->get_id());
 		return $this->database->update($system_announcement_publication, $condition);
 	}
-	
+
 	function delete_registration($registration)
 	{
 		$condition = new EqualityCondition(Registration :: PROPERTY_ID, $registration->get_id());
 		return $this->database->delete($registration->get_table_name(), $condition);
 	}
-	
+
 	function delete_system_announcement_publication($system_announcement_publication)
 	{
 		$condition = new EqualityCondition(SystemAnnouncementPublication :: PROPERTY_ID, $system_announcement_publication->get_id());
 		return $this->database->delete($system_announcement_publication->get_table_name(), $condition);
 	}
-	
+
 	// Inherited.
 	function get_next_language_id()
 	{
 		return $this->database->get_next_id(Language :: get_table_name());
 	}
-	
+
 	// Inherited.
 	function get_next_registration_id()
 	{
 		return $this->database->get_next_id(Registration :: get_table_name());
 	}
-	
+
 	// Inherited.
 	function get_next_setting_id()
 	{
 		return $this->database->get_next_id(Setting :: get_table_name());
 	}
-	
+
 	function get_next_system_announcement_publication_id()
 	{
 		return $this->database->get_next_id(SystemAnnouncementPublication :: get_table_name());
 	}
-	
+
 	function create_language($language)
 	{
 		return $this->database->create($language);
 	}
-	
+
 	function create_registration($registration)
 	{
 		return $this->database->create($registration);
 	}
-	
+
 	function create_setting($setting)
 	{
 		return $this->database->create($setting);
 	}
-	
+
 	function create_system_announcement_publication($system_announcement_publication)
 	{
-		return $this->database->create($system_announcement_publication);
+		if ($this->database->create($system_announcement_publication))
+		{
+			$users = $system_announcement_publication->get_target_users();
+    		foreach($users as $user_id)
+    		{
+    			$props = array();
+    			$props[$this->database->escape_column_name('system_announcement_publication')] = $system_announcement_publication->get_id();
+    			$props[$this->database->escape_column_name('user')] = $user_id;
+    			$this->database->get_connection()->extended->autoExecute($this->database->get_table_name('system_announcement_publication_user'), $props, MDB2_AUTOQUERY_INSERT);
+    		}
+    		$groups = $system_announcement_publication->get_target_groups();
+    		foreach($groups as $group_id)
+    		{
+    			$props = array();
+    			$props[$this->database->escape_column_name('system_announcement_publication')] = $system_announcement_publication->get_id();
+    			$props[$this->database->escape_column_name('group_id')] = $group_id;
+    			$this->database->get_connection()->extended->autoExecute($this->database->get_table_name('system_announcement_publication_group'), $props, MDB2_AUTOQUERY_INSERT);
+    		}
+
+    		return true;
+		}
+		else
+		{
+		    return false;
+		}
 	}
-	
+
 	function create_storage_unit($name, $properties, $indexes)
 	{
 		return $this->database->create_storage_unit($name, $properties, $indexes);
 	}
-	
+
 	function count_system_announcement_publications($condition = null)
 	{
 		return $this->database->count_objects(SystemAnnouncementPublication :: get_table_name(), $condition);
 	}
-	
+
 	function retrieve_system_announcement_publication($id)
 	{
 		$condition = new EqualityCondition(SystemAnnouncementPublication :: PROPERTY_ID, $id);
 		return $this->database->retrieve_object(SystemAnnouncementPublication :: get_table_name(), $condition);
-	}	
-	
+	}
+
 	function retrieve_system_announcement_publications($condition = null, $orderBy = array (), $orderDir = array (), $offset = 0, $maxObjects = -1)
 	{
 		return $this->database->retrieve_objects(SystemAnnouncementPublication :: get_table_name(), $condition, $offset, $maxObjects, $orderBy, $orderDir);
 	}
-	
+
 	function retrieve_system_announcement_publication_target_groups($system_announcement_publication)
 	{
-		return array();
+		$query = 'SELECT * FROM '.$this->database->escape_table_name('system_announcement_publication_group').' WHERE system_announcement_publication = ?';
+		$sth = $this->database->get_connection()->prepare($query);
+		$res = $sth->execute($system_announcement_publication->get_id());
+
+		$groups = array();
+		while($target_group = $res->fetchRow(MDB2_FETCHMODE_ASSOC))
+		{
+			$groups[] = $target_group['group_id'];
+		}
+
+		return $groups;
 	}
-	
+
 	function retrieve_system_announcement_publication_target_users($system_announcement_publication)
 	{
-		return array();
+		$query = 'SELECT * FROM '.$this->database->escape_table_name('system_announcement_publication_user').' WHERE system_announcement_publication = ?';
+		$sth = $this->database->get_connection()->prepare($query);
+		$res = $sth->execute($system_announcement_publication->get_id());
+
+		$users = array();
+		while($target_user = $res->fetchRow(MDB2_FETCHMODE_ASSOC))
+		{
+			$users[] = $target_user['user'];
+		}
+
+		return $users;
 	}
-	
+
 	function get_next_category_id()
 	{
 		return $this->database->get_next_id('admin_category');
 	}
-	
+
 	function delete_category($category)
 	{
 		$condition = new EqualityCondition(AdminCategory :: PROPERTY_ID, $category->get_id());
 		$succes = $this->database->delete('admin_category', $condition);
-		
+
 		$query = 'UPDATE '.$this->database->escape_table_name('admin_category').' SET '.
 				 $this->database->escape_column_name(AdminCategory :: PROPERTY_DISPLAY_ORDER).'='.
 				 $this->database->escape_column_name(AdminCategory :: PROPERTY_DISPLAY_ORDER).'-1 WHERE '.
 				 $this->database->escape_column_name(AdminCategory :: PROPERTY_DISPLAY_ORDER).'>? AND ' .
 				 $this->database->escape_column_name(AdminCategory :: PROPERTY_PARENT) . '=?';
-		$statement = $this->database->get_connection()->prepare($query); 
+		$statement = $this->database->get_connection()->prepare($query);
 		$statement->execute(array($category->get_display_order(), $category->get_parent()));
-		
+
 		return $succes;
 	}
-	
+
 	function update_category($category)
-	{ 
+	{
 		$condition = new EqualityCondition(AdminCategory :: PROPERTY_ID, $category->get_id());
 		return $this->database->update($category, $condition);
 	}
-	
+
 	function create_category($category)
 	{
 		return $this->database->create($category);
 	}
-	
+
 	function count_categories($conditions = null)
 	{
 		return $this->database->count_objects('admin_category', $conditions);
 	}
-	
+
 	function retrieve_categories($condition = null, $offset = null, $count = null, $order_property = null, $order_direction = null)
 	{
 		return $this->database->retrieve_objects('admin_category', $condition, $offset, $count, $order_property, $order_direction);
 	}
-	
+
 	function select_next_display_order($parent_category_id)
 	{
-		$query = 'SELECT MAX(' . AdminCategory :: PROPERTY_DISPLAY_ORDER . ') AS do FROM ' . 
+		$query = 'SELECT MAX(' . AdminCategory :: PROPERTY_DISPLAY_ORDER . ') AS do FROM ' .
 		$this->database->escape_table_name('admin_category');
-	
+
 		$condition = new EqualityCondition(AdminCategory :: PROPERTY_PARENT, $parent_category_id);
-		
+
 		$params = array ();
 		if (isset ($condition))
 		{
@@ -217,15 +290,15 @@ class DatabaseAdminDataManager extends AdminDataManager
 			$query .= $translator->render_query();
 			$params = $translator->get_parameters();
 		}
-		
+
 		$sth = $this->database->get_connection()->prepare($query);
 		$res = $sth->execute($params);
 		$record = $res->fetchRow(MDB2_FETCHMODE_ORDERED);
 		$res->free();
-	
+
 		return $record[0] + 1;
 	}
-	
+
 	public function get_learning_object_publication_attributes($object_id, $type = null, $offset = null, $count = null, $order_property = null, $order_direction = null)
 	{
 		if (isset($type))
@@ -282,7 +355,7 @@ class DatabaseAdminDataManager extends AdminDataManager
 		}
 		return $publication_attr;
 	}
-	
+
 	public function get_learning_object_publication_attribute($publication_id)
 	{
 		$condition = new EqualityCondition('id',$publication_id);
@@ -300,19 +373,19 @@ class DatabaseAdminDataManager extends AdminDataManager
 		$info->set_publication_object_id($record->get_learning_object());
 		return $info;
 	}
-	
+
 	public function any_learning_object_is_published($object_ids)
 	{
 		$condition = new InCondition(SystemAnnouncementPublication :: PROPERTY_LEARNING_OBJECT_ID,$object_ids);
 		return $this->database->count_objects('system_announcement_publication',$condition)>=1;
 	}
-	
+
 	public function count_publication_attributes($type = null, $condition = null)
 	{
 		$condition = new EqualityCondition('publisher', Session :: get_user_id());
 		return $this->database->count_objects('system_announcement_publication', $condition);
 	}
-	
+
 	public function delete_learning_object_publications($object_id)
 	{
 		$condition = new EqualityCondition(SystemAnnouncementPublication :: PROPERTY_LEARNING_OBJECT_ID,$object_id);
--- dokeos/admin/lib/system_announcement_publication_form.class.php
+++ dokeos/admin/lib/system_announcement_publication_form.class.php
@@ -18,10 +18,10 @@ class SystemAnnouncementPublicationForm extends FormValidator
    /**#@+
     * Constant defining a form parameter
  	*/
- 	
+
 	const TYPE_SINGLE = 1;
 	const TYPE_MULTI = 2;
- 	
+
 	const PARAM_FOREVER = 'forever';
 	const PARAM_FROM_DATE = 'from_date';
 	const PARAM_TO_DATE = 'to_date';
@@ -30,6 +30,10 @@ class SystemAnnouncementPublicationForm extends FormValidator
 	const PARAM_TARGETS_TO = 'to';
 	const PARAM_TARGET_USER_PREFIX = 'user';
 	const PARAM_TARGET_GROUP_PREFIX = 'group';
+
+	const PARAM_TARGET = 'target_users_and_groups';
+	const PARAM_TARGET_ELEMENTS = 'target_users_and_groups_elements';
+	const PARAM_TARGET_OPTION = 'target_users_and_groups_option';
 
 	/**#@-*/
 	/**
@@ -41,9 +45,9 @@ class SystemAnnouncementPublicationForm extends FormValidator
 	 * publication)
 	 */
 	private $form_user;
-	
+
 	private $form_type;
-	
+
 	private $system_announcement_publication;
 
 	/**
@@ -59,7 +63,7 @@ class SystemAnnouncementPublicationForm extends FormValidator
 		$this->form_type = $form_type;
 		$this->learning_object = $learning_object;
 		$this->form_user = $form_user;
-		
+
 		switch($this->form_type)
 		{
 			case self :: TYPE_SINGLE:
@@ -82,22 +86,22 @@ class SystemAnnouncementPublicationForm extends FormValidator
     function setDefaults()
     {
     	$defaults = array();
-    	$defaults[self :: PARAM_TARGETS][self :: PARAM_RECEIVERS] = 0;
+    	$defaults[self :: PARAM_TARGET_OPTION] = 0;
 		$defaults[self :: PARAM_FOREVER] = 1;
 		parent :: setDefaults($defaults);
     }
-    
+
     function build_single_form()
     {
     	$this->build_form();
     }
-    
+
     function build_multi_form()
     {
-    	$this->build_form();    	
+    	$this->build_form();
     	$this->addElement('hidden', 'ids', serialize($this->learning_object));
     }
-    
+
     function add_footer()
     {
 		$buttons = array();
@@ -105,15 +109,23 @@ class SystemAnnouncementPublicationForm extends FormValidator
 		$buttons[] = $this->createElement('style_reset_button', 'reset', Translation :: get('Reset'), array('class' => 'normal empty'));
 		$this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
     }
-    
+
 	/**
 	 * Builds the form by adding the necessary form elements.
 	 */
     function build_form()
     {
-		$receiver_options = $this->get_receiver_options();
-		$attributes = array(self :: PARAM_RECEIVERS => $receiver_options);
-		$this->addElement('receivers', self :: PARAM_TARGETS, Translation :: get('PublishFor'), $attributes);
+		$attributes = array();
+		$attributes['search_url'] = Path :: get(WEB_PATH).'admin/xml_user_group_feed.php';
+		$locale = array ();
+		$locale['Display'] = Translation :: get('PublishFor');
+		$locale['Searching'] = Translation :: get('Searching');
+		$locale['NoResults'] = Translation :: get('NoResults');
+		$locale['Error'] = Translation :: get('Error');
+		$attributes['locale'] = $locale;
+		$attributes['exclude'] = array('user_' . $this->form_user->get_id());
+		$attributes['defaults'] = array();
+		$this->add_receivers(self :: PARAM_TARGET, Translation :: get('PublishFor'), $attributes);
 
 		$this->add_forever_or_timewindow();
 		$this->addElement('checkbox', SystemAnnouncementPublication :: PROPERTY_HIDDEN, Translation :: get('Hidden'));
@@ -126,7 +138,7 @@ class SystemAnnouncementPublicationForm extends FormValidator
     function create_learning_object_publication()
     {
 		$values = $this->exportValues();
-		
+
 		if ($values[self :: PARAM_FOREVER] != 0)
 		{
 			$from = $to = 0;
@@ -137,22 +149,9 @@ class SystemAnnouncementPublicationForm extends FormValidator
 			$to = DokeosUtilities :: time_from_datepicker($values[self :: PARAM_TO_DATE]);
 		}
 		$hidden = ($values[SystemAnnouncementPublication :: PROPERTY_HIDDEN] ? 1 : 0);
-		
-		if($values[self :: PARAM_TARGETS][self :: PARAM_RECEIVERS] == 1)
-		{
-			foreach($values[self::PARAM_TARGETS][self :: PARAM_TARGETS_TO] as $index => $target)
-			{
-				list($type,$id) = explode('-',$target);
-				if($type == self :: PARAM_TARGET_GROUP_PREFIX)
-				{
-					$groups[] = $id;
-				}
-				elseif($type == self :: PARAM_TARGET_USER_PREFIX)
-				{
-					$users[] = $id;
-				}
-			}
-		}
+
+		$users = $values[self :: PARAM_TARGET_ELEMENTS]['user'];
+		$groups = $values[self :: PARAM_TARGET_ELEMENTS]['group'];
 
 		$pub = new SystemAnnouncementPublication();
 		$pub->set_learning_object_id($this->learning_object->get_id());
@@ -174,33 +173,14 @@ class SystemAnnouncementPublicationForm extends FormValidator
 			return false;
 		}
     }
-    
-    function get_receiver_options()
-    {
-    	$receiver_options = array();
-    	
-    	$groups = GroupDataManager :: get_instance()->retrieve_groups();
-    	
-    	while ($group = $groups->next_result())
-    	{
-    		$receiver_choices[self :: PARAM_TARGET_GROUP_PREFIX.'-'.$group->get_id()] = Translation :: get('Group').': '.$group->get_name();
-    	}
-    	
-    	$users = UserDataManager :: get_instance()->retrieve_users();
-    	
-    	while ($user = $users->next_result())
-    	{
-    		$receiver_options[self :: PARAM_TARGET_USER_PREFIX.'-'.$user->get_id()] = $user->get_fullname();
-    	}
-    	
-    	return $receiver_options;
-    }
-    
+
     function set_system_announcement_publication($system_announcement_publication)
     {
     	$this->system_announcement_publication = $system_announcement_publication;
-		$this->addElement('hidden','said');
+
+    	$this->addElement('hidden','said');
 		$this->addElement('hidden','action');
+
 		$defaults['action'] = 'edit';
 		$defaults['said'] = $system_announcement_publication->get_id();
 		$defaults[SystemAnnouncementPublication :: PROPERTY_FROM_DATE] = $system_announcement_publication->get_from_date();
@@ -210,29 +190,54 @@ class SystemAnnouncementPublicationForm extends FormValidator
 			$defaults[self :: PARAM_FOREVER] = 0;
 		}
 		$defaults[SystemAnnouncementPublication :: PROPERTY_HIDDEN] = $system_announcement_publication->is_hidden();
-		$users = $system_announcement_publication->get_target_users();
-		foreach($users as $user)
+
+		$udm = UserDataManager :: get_instance();
+		$gdm = GroupDataManager :: get_instance();
+
+		$target_groups = $this->system_announcement_publication->get_target_groups();
+		$target_users = $this->system_announcement_publication->get_target_users();
+
+		$defaults[self :: PARAM_TARGET_ELEMENTS] = array();
+		foreach($target_groups as $target_group)
 		{
-			$defaults[self :: PARAM_TARGETS][self :: PARAM_TARGETS_TO][] = self :: PARAM_TARGET_USER_PREFIX . '-'.$user;
+		    $group = $gdm->retrieve_group($target_group);
+
+		    $selected_group = array ();
+            $selected_group['id'] = 'group_' . $group->get_id();
+            $selected_group['class'] = 'type type_group';
+            $selected_group['title'] = $group->get_name();
+            $selected_group['description'] = $group->get_description();
+
+            $defaults[self :: PARAM_TARGET_ELEMENTS][$selected_group['id']] = $selected_group;
 		}
-		$groups = $system_announcement_publication->get_target_groups();
-		foreach($groups as $index => $group)
+    	foreach($target_users as $target_user)
 		{
-			$defaults[self :: PARAM_TARGETS][self :: PARAM_TARGETS_TO][] = self :: PARAM_TARGET_GROUP_PREFIX . '-'.$group;
+		    $user = $udm->retrieve_user($target_user);
+
+		    $selected_user = array ();
+            $selected_user['id'] = 'user_' . $user->get_id();
+            $selected_user['class'] = 'type type_user';
+            $selected_user['title'] = $user->get_fullname();
+            $selected_user['description'] = $user->get_username();
+
+            $defaults[self :: PARAM_TARGET_ELEMENTS][$selected_user['id']] = $selected_user;
 		}
-		
-		if (count($users) > 0 || count($groups) > 0)
+
+		if (count($defaults[self :: PARAM_TARGET_ELEMENTS]) > 0)
 		{
-			$defaults[self :: PARAM_TARGETS][self :: PARAM_RECEIVERS] = '1';
+            $defaults[self :: PARAM_TARGET_OPTION] = '1';
 		}
-		
+
+		$active = $this->getElement(self :: PARAM_TARGET_ELEMENTS);
+		$active->_elements[0]->setValue(serialize($defaults[self :: PARAM_TARGET_ELEMENTS]));
+
 		parent::setDefaults($defaults);
     }
-    
+
     function update_learning_object_publication()
     {
 		$values = $this->exportValues();
-		
+
 		if ($values[self :: PARAM_FOREVER] != 0)
 		{
 			$from = $to = 0;
@@ -243,22 +248,9 @@ class SystemAnnouncementPublicationForm extends FormValidator
 			$to = DokeosUtilities :: time_from_datepicker($values[self :: PARAM_TO_DATE]);
 		}
 		$hidden = ($values[SystemAnnouncementPublication :: PROPERTY_HIDDEN] ? 1 : 0);
-		
-		if($values[self :: PARAM_TARGETS][self :: PARAM_RECEIVERS] == 1)
-		{
-			foreach($values[self::PARAM_TARGETS][self :: PARAM_TARGETS_TO] as $index => $target)
-			{
-				list($type,$id) = explode('-',$target);
-				if($type == self :: PARAM_TARGET_GROUP_PREFIX)
-				{
-					$groups[] = $id;
-				}
-				elseif($type == self :: PARAM_TARGET_USER_PREFIX)
-				{
-					$users[] = $id;
-				}
-			}
-		}
+
+		$users = $values[self :: PARAM_TARGET_ELEMENTS]['user'];
+		$groups = $values[self :: PARAM_TARGET_ELEMENTS]['group'];
 
 		$pub = $this->system_announcement_publication;
 		$pub->set_modified(time());
@@ -277,13 +269,13 @@ class SystemAnnouncementPublicationForm extends FormValidator
 			return false;
 		}
     }
-    
+
     function create_learning_object_publications()
     {
 		$values = $this->exportValues();
 
     	$ids = unserialize($values['ids']);
-    	
+
     	foreach($ids as $id)
     	{
 			if ($values[self :: PARAM_FOREVER] != 0)
@@ -296,23 +288,10 @@ class SystemAnnouncementPublicationForm extends FormValidator
 				$to = DokeosUtilities :: time_from_datepicker($values[self :: PARAM_TO_DATE]);
 			}
 			$hidden = ($values[SystemAnnouncementPublication :: PROPERTY_HIDDEN] ? 1 : 0);
-			
-			if($values[self :: PARAM_TARGETS][self :: PARAM_RECEIVERS] == 1)
-			{
-				foreach($values[self::PARAM_TARGETS][self :: PARAM_TARGETS_TO] as $index => $target)
-				{
-					list($type,$id) = explode('-',$target);
-					if($type == self :: PARAM_TARGET_GROUP_PREFIX)
-					{
-						$groups[] = $id;
-					}
-					elseif($type == self :: PARAM_TARGET_USER_PREFIX)
-					{
-						$users[] = $id;
-					}
-				}
-			}
-	
+
+			$users = $values[self :: PARAM_TARGET_ELEMENTS]['user'];
+			$groups = $values[self :: PARAM_TARGET_ELEMENTS]['group'];
+
 			$pub = new SystemAnnouncementPublication();
 			$pub->set_learning_object_id($id);
 			$pub->set_publisher($this->form_user->get_id());
@@ -323,7 +302,7 @@ class SystemAnnouncementPublicationForm extends FormValidator
 			$pub->set_to_date($to);
 			$pub->set_target_groups($groups);
 			$pub->set_target_users($users);
-	
+
 			if (!$pub->create())
 			{
 				return false;
