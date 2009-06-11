2761c9c52989a2f8b408beadd5f83b3129399b44
scara84 <scara84@localhost>
2009-06-11 13:39:48 +0000

    Optimizing the way assessments are displayed.

--- dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/question_display.class.php
+++ dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/question_display.class.php
@@ -1,82 +1,99 @@
 <?php
 
-abstract class QuestionDisplay 
+abstract class QuestionDisplay
 {
 	private $clo_question;
 	private $question_nr;
 	private $formvalidator;
-	
+
 	function QuestionDisplay($formvalidator, $clo_question, $question_nr)
 	{
 		$this->formvalidator = $formvalidator;
+
 		$this->clo_question = $clo_question;
 		$this->question_nr = $question_nr;
 	}
-	
+
 	function get_clo_question()
 	{
 		return $this->clo_question;
 	}
-	
+
 	function display()
 	{
 		$this->add_header();
 		$this->add_question_form($this->formvalidator);
 		$this->add_footer();
 	}
-	
-	abstract function add_question_form($formvalidator); 
+
+	abstract function add_question_form($formvalidator);
 
 	function add_header()
 	{
 		$formvalidator = $this->formvalidator;
 		$clo_question = $this->get_clo_question();
 		$learning_object = RepositoryDataManager :: get_instance()->retrieve_learning_object($clo_question->get_ref());
-		
+
+		$number_of_questions = $formvalidator->get_number_of_questions();
+		$current_question = $this->question_nr;
+
 		$html[] = '<div class="question">';
-		$html[] = '<div class="title" style="padding: 5px 5px 5px 35px; border:1px solid grey; background: #e6e6e6 url('. Theme :: get_common_image_path(). 'learning_object/' .$learning_object->get_icon_name().'.png) no-repeat; background-position: 5px 2px; height: 16px;">';
-		$html[] = Translation :: get('Question') . ' ' . $this->question_nr . ': ' . $learning_object->get_title();
+		$html[] = '<div class="title">';
+//		$html[] = '<div class="number">';
+//		$html[] = '<div class="bevel">';
+//		$html[] = $this->question_nr . '.';
+//		$html[] = '<img src="'. Theme :: get_common_image_path(). 'treemenu_types/' .$learning_object->get_icon_name().'.png">';
+//		$html[] = '</div>';
+//		$html[] = '</div>';
+		$html[] = '<div class="text">';
+		$html[] = '<div class="bevel">';
+		$html[] = '<img src="'. Theme :: get_common_image_path(). 'treemenu_types/' .$learning_object->get_icon_name().'.png">';
+		$html[] = Translation :: get('Question') . ' ' . $current_question . '/' . $number_of_questions . '&nbsp;' . $learning_object->get_title();
+		//$html[] = $learning_object->get_title();
 		$html[] = '</div>';
-		$html[] = '<div class="description" style="padding-left: 35px;">';
+		$html[] = '</div>';
+		$html[] = '<div class="clear"></div>';
+		$html[] = '</div>';
+		$html[] = '<div class="answer">';
+		$html[] = '<div class="description">';
 		$description = $learning_object->get_description();
 
 		if($description != '<p>&#160;</p>' && count($description) > 0 )
 		{
-			$html[] = '<div style="font-style: italic; ">' . $description . '</div>';
+			$html[] = $description;
 		}
-		else
-		{
-			$html[] = '<br />';
-		}
-		
+
+		$html[] = '</div>';
+		$html[] = '<div class="clear"></div>';
+
 		$header = implode("\n", $html);
 		$formvalidator->addElement('html', $header);
 	}
-	
+
 	function add_footer($formvalidator)
 	{
 		$formvalidator = $this->formvalidator;
-		$html[] = '<br/><br /></div>';
 		$html[] = '</div>';
-		
+		$html[] = '</div>';
+
 		$footer = implode("\n", $html);
 		$formvalidator->addElement('html', $footer);
 	}
 
-	static function factory($formvalidator, $clo_question, $question_nr) 
+	static function factory($formvalidator, $clo_question, $question_nr)
 	{
 		$question = RepositoryDataManager :: get_instance()->retrieve_learning_object($clo_question->get_ref());
 		$type = $question->get_type();
-		
+
 		$file = dirname(__FILE__) . '/question_display/' . $type . '.class.php';
-		
+
 		if(!file_exists($file))
 		{
 			die('file does not exist: ' . $file);
 		}
-		
+
 		require_once $file;
-		
+
 		$class = DokeosUtilities :: underscores_to_camelcase($type) . 'Display';
 		$question_display = new $class($formvalidator, $clo_question, $question_nr);
 		return $question_display;
