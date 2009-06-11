0df1a5c348e7d63b8ef8f53fccda9aff501f6c53
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-06-11 11:06:06 +0000

    Fixed the fill in the blanks automatic layout

--- dokeos/repository/lib/learning_object/fill_in_blanks_question/fill_in_blanks_question_form.class.php
+++ dokeos/repository/lib/learning_object/fill_in_blanks_question/fill_in_blanks_question_form.class.php
@@ -6,22 +6,27 @@
 require_once dirname(__FILE__).'/../../learning_object_form.class.php';
 require_once dirname(__FILE__).'/fill_in_blanks_question.class.php';
 require_once dirname(__FILE__).'/fill_in_blanks_answer.class.php';
+
 class FillInBlanksQuestionForm extends LearningObjectForm
 {
 	protected function build_creation_form()
 	{
 		parent :: build_creation_form();
-		$this->addElement('category', Translation :: get(get_class($this) .'Properties'));
-		//$this->add_options();
+		$this->addElement('category', Translation :: get('AnswerOptions'));
+		$this->addElement('textarea', 'answer', Translation :: get('answer'), 'rows="10" cols="79" class="answer"');
+		$this->addElement('html', ResourceManager :: get_instance()->get_resource_html(Path :: get(WEB_PATH) . 'common/javascript/fill_in_the_blanks.js'));
+		$this->add_options();
 		$this->addElement('category');
 	}
+	
 	protected function build_editing_form()
 	{
 		parent :: build_editing_form();
 		$this->addElement('category', Translation :: get(get_class($this) .'Properties'));
-		//$this->add_options();
+		$this->add_options();
 		$this->addElement('category');
 	}
+	
 	function setDefaults($defaults = array ())
 	{
 		if(!$this->isSubmitted())
@@ -32,23 +37,27 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 				$options = $object->get_answers();
 				foreach($options as $index => $option)
 				{
-					$defaults['option'][$index] = $option->get_value();
-					$defaults['option_weight'][$index] = $option->get_weight();
+					$defaults['weight'][$index] = $option->get_weight();
 					$defaults['comment'][$index] = $option->get_comment();
 				}
 			}
-			else
-			{
-				$number_of_options = intval($_SESSION['mc_number_of_options']);
-		
-				for($option_number = 0; $option_number <$number_of_options ; $option_number++)
-				{
-					$defaults['option_weight'][$option_number] = 1;
-				}
-			}
+			
+			parent :: setDefaults($defaults);
+			return;
 		}
-		parent :: setDefaults($defaults);
+		
+		if(!$this->validate())
+		{
+			for($option_number = 0; $option_number < count($defaults['match']) ; $option_number++)
+			{
+				$defaults['weight'][$option_number] = 1;
+				$defaults['comment'][$option_number] = '';
+			}
+	
+			parent :: setConstants($defaults);
+		}
 	}
+	
 	function create_learning_object()
 	{
 		$object = new FillInBlanksQuestion();
@@ -56,11 +65,13 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 		$this->add_options_to_object();
 		return parent :: create_learning_object();
 	}
+	
 	function update_learning_object()
 	{
 		$this->add_options_to_object();
 		return parent :: update_learning_object();
 	}
+	
 	private function add_options_to_object()
 	{
 		$object = $this->get_learning_object();
@@ -79,7 +90,7 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 	
 	function validate()
 	{
-		if(isset($_POST['add']) || isset($_POST['remove']) || isset($_POST['change_answer_type']))
+		if(isset($_POST['add']))
 		{
 			return false;
 		}
@@ -91,61 +102,61 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 	 */
 	private function add_options()
 	{
-		if(!$this->isSubmitted())
-		{
-			unset($_SESSION['mc_number_of_options']);
-			unset($_SESSION['mc_skip_options']);
-		}
-		if(!isset($_SESSION['mc_number_of_options']))
-		{
-			$_SESSION['mc_number_of_options'] = 3;
-		}
-		if(!isset($_SESSION['mc_skip_options']))
-		{
-			$_SESSION['mc_skip_options'] = array();
-		}
-		if(isset($_POST['add']))
-		{
-			$_SESSION['mc_number_of_options'] = $_SESSION['mc_number_of_options']+1;
-		}
-		if(isset($_POST['remove']))
-		{
-			$indexes = array_keys($_POST['remove']);
-			$_SESSION['mc_skip_options'][] = $indexes[0];
-		}
-		$object = $this->get_learning_object();
-		if(!$this->isSubmitted() && !is_null($object))
-		{
-			$_SESSION['mc_number_of_options'] = $object->get_number_of_answers();
-			//$_SESSION['mc_answer_type'] = $object->get_answer_type();
-		}
-		$number_of_options = intval($_SESSION['mc_number_of_options']);
-		$show_label = true; $count = 1;
-		for($option_number = 0; $option_number <$number_of_options ; $option_number++)
-		{
-			if(!in_array($option_number,$_SESSION['mc_skip_options']))
-			{
-				$group = array();
-				
-				$this->addElement('category', Translation :: get('Answer') . ' ' . ($count));
+		$values = $this->exportValues();
+		
+		$matches = array();
+		preg_match_all('/\[[a-zA-Z0-9_-]*\]/', $values['answer'], $matches);
+		$matches = $matches[0];
+		
+		$this->addElement('image','add[]',Theme :: get_common_image_path().'action_list_add.png', 'class="add_matches"');
+		
+		if(count($matches) == 0) 
+			$visible = 'display: none; ';
 
-				$this->add_html_editor('option['.$option_number.']', Translation :: get('Answer'), true);
-				$this->add_html_editor('comment['.$option_number.']', Translation :: get('Comment'), false);
-				$this->addElement('text','option_weight['.$option_number.']', Translation :: get('Weight'), 'size="2"  class="input_numeric"');
-				$this->addRule('option_weight['.$option_number.']', Translation :: get('ThisFieldIsRequired'), 'required');
-				$this->addRule('option_weight['.$option_number.']', Translation :: get('ValueShouldBeNumeric'), 'numeric');
-				
-				if($number_of_options - count($_SESSION['mc_skip_options']) > 1)
-				{
-					$this->addElement('image','remove['.$option_number.']',Theme :: get_common_image_path().'action_list_remove.png');
-				}
-				$this->addElement('category');
-				$count++;
-			}
+		$renderer = $this->defaultRenderer();
+		
+		$table_header = array();
+        $table_header[] = '<table class="data_table" style="' . $visible . 'width: 655px; margin:auto; left: -30px; position: relative;">';
+        $table_header[] = '<thead>';
+        $table_header[] = '<tr>';
+        $table_header[] = '<th></th>';
+        $table_header[] = '<th>Comment</th>';
+        $table_header[] = '<th style="width: 50px;">Weight</th>';
+        $table_header[] = '</tr>';
+        $table_header[] = '</thead>';
+        $table_header[] = '<tbody>';
+        $this->addElement('html', implode("\n", $table_header));
+		
+        $html_editor_options = array();
+        $html_editor_options['width'] = '100%';
+        $html_editor_options['height'] = '65';
+        $html_editor_options['show_toolbar'] = false;
+        $html_editor_options['show_tags'] = false;
+        $html_editor_options['toolbar_set'] = 'RepositoryQuestion';
+        
+		for($option_number = 0; $option_number < count($matches) ; $option_number++)
+		{
+			$group = array();
+
+			$element = $this->createElement('text','match['.$option_number.']', Translation :: get('Match'), 'style="width: 90%;" ');
+			$element->freeze();
+			$group[] = $element;
+			$group[] = $this->create_html_editor('comment[' . $option_number . ']', Translation :: get('Comment'), $html_editor_options);
+			$group[] = $this->createElement('text','weight['.$option_number.']', Translation :: get('Weight'), 'size="2"');
+
+			$this->addGroup($group, 'option_' . $option_number, null, '', false);
+			
+			$renderer->setElementTemplate('<tr>{element}</tr>', 'option_' . $option_number);
+   			$renderer->setGroupElementTemplate('<td>{element}</td>', 'option_' . $option_number);
+   			
+   			$defaults['match'][$option_number] = $matches[$option_number];
 		}
-		//Notice: The [] are added to this element name so we don't have to deal with the _x and _y suffixes added when clicking an image button
-		$this->addElement('image','add[]',Theme :: get_common_image_path().'action_list_add.png');
-		$this->setDefaults();
+		
+		$table_footer[] = '</tbody>';
+        $table_footer[] = '</table>';
+        $this->addElement('html', implode("\n", $table_footer));
+
+		$this->setDefaults($defaults);
 	}
 }
 ?>
