4dd5dfb15ace95118cc7dd4f0673a959b67b3bec
Hans De Bisschop <hans.de.bisschop@ehb.be>
2009-06-12 08:43:33 +0000

    Fixing some issues with the "fill in the blanks" question (in progress)

--- dokeos/repository/lib/learning_object/fill_in_blanks_question/fill_in_blanks_question_form.class.php
+++ dokeos/repository/lib/learning_object/fill_in_blanks_question/fill_in_blanks_question_form.class.php
@@ -10,28 +10,28 @@ require_once dirname(__FILE__).'/fill_in_blanks_answer.class.php';
 class FillInBlanksQuestionForm extends LearningObjectForm
 {
 	const DEFAULT_SIZE = 20;
-	
+
 	protected function build_creation_form()
 	{
 		parent :: build_creation_form();
 		$this->addElement('category', Translation :: get('AnswerOptions'));
-		$this->addElement('textarea', 'answer', Translation :: get('answer'), 'rows="10" cols="79" class="answer"');
+		$this->addElement('textarea', 'answer', Translation :: get('QuestionText'), 'rows="10" cols="79" class="answer"');
 		$this->addElement('html', ResourceManager :: get_instance()->get_resource_html(Path :: get(WEB_PATH) . 'common/javascript/fill_in_the_blanks.js'));
 		$this->add_options();
-		$this->addElement('category'); 
+		$this->addElement('category');
 	}
-	
+
 	protected function build_editing_form()
 	{
 		parent :: build_editing_form();
 		$this->addElement('category', Translation :: get('AnswerOptions'));
-		$this->addElement('textarea', 'answer', Translation :: get('answer'), 'rows="10" cols="79" class="answer"');
+		$this->addElement('textarea', 'answer', Translation :: get('QuestionText'), 'rows="10" cols="79" class="answer"');
 		$this->addElement('html', ResourceManager :: get_instance()->get_resource_html(Path :: get(WEB_PATH) . 'common/javascript/fill_in_the_blanks.js'));
 		$this->setDefaults();
 		$this->add_options();
-		$this->addElement('category'); 
+		$this->addElement('category');
 	}
-	
+
 	function setDefaults($defaults = array ())
 	{
 		if(!$this->isSubmitted())
@@ -48,11 +48,11 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 				}
 				$defaults['answer'] = $object->get_answer_text();
 			}
-	
+
 			parent :: setDefaults($defaults);
 			return;
 		}
-		
+
 		if(!$this->validate())
 		{
 			for($option_number = 0; $option_number < count($defaults['match']) ; $option_number++)
@@ -61,11 +61,11 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 				$defaults['comment'][$option_number] = '';
 				$defaults['size'][self :: DEFAULT_SIZE];
 			}
-	
+
 			parent :: setConstants($defaults);
 		}
 	}
-	
+
 	function create_learning_object()
 	{
 		$object = new FillInBlanksQuestion();
@@ -74,7 +74,7 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 		$this->add_options_to_object();
 		return parent :: create_learning_object();
 	}
-	
+
 	function update_learning_object()
 	{
 		$object = $this->get_learning_object();
@@ -82,12 +82,12 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 		$this->add_options_to_object();
 		return parent :: update_learning_object();
 	}
-	
+
 	private function add_options_to_object()
 	{
 		$object = $this->get_learning_object();
 		$values = $this->exportValues();
-		
+
 		$count = count($values['match']);
 		for($i = 0; $i < $count; $i++)
 		{
@@ -95,12 +95,12 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 			$comment = $values['comment'][$i];
 			$size = $values['size'][$i];
 			$value = substr($values['match'][$i], 1, strlen($values['match'][$i]) - 2);
-			
+
 			$options[] = new FillInBlanksQuestionAnswer($value, $weight, $comment, $size);
 		}
 		$object->set_answers($options);
 	}
-	
+
 	function validate()
 	{
 		if(isset($_POST['add']))
@@ -116,38 +116,43 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 	private function add_options()
 	{
 		$values = $this->exportValues();
-		
-		$matches = array();
-		preg_match_all('/\[[a-zA-Z0-9_-]*\]/', $values['answer'], $matches);
-		$matches = $matches[0];
-		
-		$this->addElement('image','add[]',Theme :: get_common_image_path().'action_list_add.png', 'class="add_matches"');
-		
-		if(count($matches) == 0) 
-			$visible = 'display: none; ';
-
 		$renderer = $this->defaultRenderer();
-		
+
+		$matches = array();
+		preg_match_all('/\[[a-zA-Z0-9_\s\-]*\]/', $values['answer'], $matches);
+		$matches = $matches[0];
+
+		//$this->addElement('image','add[]',Theme :: get_common_image_path().'action_list_add.png', 'class="add_matches"');
+
+		$visible = (count($matches) == 0) ? 'display: none;' : '';
+
 		$table_header = array();
-        $table_header[] = '<table class="data_table" style="' . $visible . 'width: 654px; margin:auto; left: -29px; position: relative;">';
+		$table_header[] = '<div id="answers_table" class="row" style="' . $visible . '">';
+		$table_header[] = '<div class="label">';
+        $table_header[] = Translation :: get('Answers');
+        $table_header[] = '</div>';
+        $table_header[] = '<div class="formw">';
+        $table_header[] = '<div class="element">';
+
+        $table_header[] = '<table class="data_table" style="width: 661px;">';
         $table_header[] = '<thead>';
         $table_header[] = '<tr>';
         $table_header[] = '<th></th>';
         $table_header[] = '<th>' . Translation :: get('Feedback') . '</th>';
-        $table_header[] = '<th style="width: 50px;">' . Translation :: get('Weight') . '</th>';
-        $table_header[] = '<th style="width: 50px;">' . Translation :: get('Size') . '</th>';
+        $table_header[] = '<th class="numeric">' . Translation :: get('Score') . '</th>';
+        $table_header[] = '<th class="numeric">' . Translation :: get('Size') . '</th>';
         $table_header[] = '</tr>';
         $table_header[] = '</thead>';
         $table_header[] = '<tbody>';
         $this->addElement('html', implode("\n", $table_header));
-		
+
         $html_editor_options = array();
         $html_editor_options['width'] = '100%';
         $html_editor_options['height'] = '65';
         $html_editor_options['show_toolbar'] = false;
         $html_editor_options['show_tags'] = false;
         $html_editor_options['toolbar_set'] = 'RepositoryQuestion';
-        
+
 		for($option_number = 0; $option_number < count($matches) ; $option_number++)
 		{
 			$group = array();
@@ -158,9 +163,9 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 			$group[] = $this->create_html_editor('comment[' . $option_number . ']', Translation :: get('Comment'), $html_editor_options);
 			$group[] = $this->createElement('text','match_weight['.$option_number.']', Translation :: get('Weight'), 'size="2"');
 			$group[] = $this->createElement('text','size['.$option_number.']', Translation :: get('Size'), 'size="2"');
-			
+
 			$this->addGroup($group, 'option_' . $option_number, null, '', false);
-			
+
 			$this->addGroupRule('option_'.$option_number,
 					array(
 						'match_weight['.$option_number.']' =>
@@ -183,15 +188,20 @@ class FillInBlanksQuestionForm extends LearningObjectForm
 							)
 					)
 				);
-			
+
 			$renderer->setElementTemplate('<tr>{element}</tr>', 'option_' . $option_number);
    			$renderer->setGroupElementTemplate('<td>{element}</td>', 'option_' . $option_number);
-   			
+
    			$defaults['match'][$option_number] = $matches[$option_number];
 		}
-		
+
 		$table_footer[] = '</tbody>';
         $table_footer[] = '</table>';
+        $table_footer[] = '</div>';
+        $table_footer[] = '<div class="form_feedback"></div></div>';
+        $table_footer[] = '<div class="clear">&nbsp;</div>';
+        $table_footer[] = '</div>';
+
         $this->addElement('html', implode("\n", $table_footer));
 
 		$this->setDefaults($defaults);
