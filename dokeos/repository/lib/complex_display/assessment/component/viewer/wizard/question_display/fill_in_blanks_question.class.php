f740808828fcd0d15294483461483c3d9b432540
vanpouckesven <vanpouckesven@localhost>
2009-06-12 13:48:10 +0000

    Changed fill in the blanks

--- dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/question_display/fill_in_blanks_question.class.php
+++ dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/question_display/fill_in_blanks_question.class.php
@@ -8,13 +8,20 @@ class FillInBlanksQuestionDisplay extends QuestionDisplay
 	{
 		$clo_question = $this->get_clo_question();
 		$question = RepositoryDataManager :: get_instance()->retrieve_learning_object($clo_question->get_ref());
-		$answers = $question->get_answers();
+		$answer_text = $question->get_answer_text();
+		$answer_text = str_replace("\n", '<br />', $answer_text);
 		
-		foreach($answers as $i => $answer)
+		$matches = array();
+		preg_match_all('/\[[a-zA-Z0-9_-\s]*\]/', $answer_text, $matches);
+		$matches = $matches[0];
+		foreach($matches as $i => $match)
 		{
 			$name = $clo_question->get_id().'_'.$i;
-			$formvalidator->addElement('text', $name, '('.$i.')');
+			$element = $formvalidator->createElement('text', $name, '('.$i.')');
+			$answer_text = str_replace($match, $element->toHtml(), $answer_text);
 		}
+		
+		$formvalidator->addElement('static', null, null, $answer_text);
 	}
 }
 ?>
