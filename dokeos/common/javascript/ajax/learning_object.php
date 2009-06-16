eb7efa7cb6ce832ea3b4221721d81d79420bd4c1
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-06-16 13:45:15 +0000

    Added a modal form to show the selected learning object

---
+++ dokeos/common/javascript/ajax/learning_object.php
@@ -0,0 +1,10 @@
+<?php
+require_once dirname(__FILE__).'/../../global.inc.php';
+require_once Path :: get_repository_path() . 'lib/repository_data_manager.class.php';
+
+$object = Request :: post('object');
+$object = RepositoryDataManager :: get_instance()->retrieve_learning_object($object);
+
+echo $object->get_title();
+
+?>
