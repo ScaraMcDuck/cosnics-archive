3a85799e07edd91134a9645676922af2381aa2e4
vanpouckesven <vanpouckesven@localhost>
2009-06-18 11:06:35 +0000

    added score calculator for assessment tool

---
+++ dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/inc/score_calculator/multiple_choice_score_calculator.class.php
@@ -0,0 +1,28 @@
+<?php
+require_once dirname(__FILE__).'/../score_calculator.class.php';
+
+class MultipleChoiceScoreCalculator extends ScoreCalculator
+{
+	
+	function get_score()
+	{
+		$question = $this->get_question();
+		if ($question->get_answer_type() == 'radio')
+		{
+			$answers = $question->get_options();
+			$selected = $answers[$this->get_answer()];
+			if ($selected->is_correct())
+				return $selected->get_weight();
+			else
+				return 0;
+		}
+		else
+		{
+
+			$answers = $question->get_options();
+			$answer = $answers[$this->get_answer_num()-1];
+			return $answer->get_weight();
+		}
+	}
+}
+?>
