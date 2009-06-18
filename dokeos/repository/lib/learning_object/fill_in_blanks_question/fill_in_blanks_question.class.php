cd86c4b25c191595fd5f8acae1d91133c9e9b7b7
scara84 <scara84@localhost>
2009-06-18 14:52:35 +0000

    Extending the "fill in the blanks" question type.

--- dokeos/repository/lib/learning_object/fill_in_blanks_question/fill_in_blanks_question.class.php
+++ dokeos/repository/lib/learning_object/fill_in_blanks_question/fill_in_blanks_question.class.php
@@ -9,19 +9,20 @@ class FillInBlanksQuestion extends LearningObject
 {
 	const PROPERTY_ANSWERS = 'answers';
 	const PROPERTY_ANSWER_TEXT = 'answer_text';
-	
+	const PROPERTY_QUESTION_TYPE = 'question_type';
+
 	public function add_answer($answer)
 	{
 		$answers = $this->get_answers();
 		$answers[] = $answer;
 		return $this->set_additional_property(self :: PROPERTY_ANSWERS, serialize($answers));
 	}
-	
+
 	public function set_answers($answers)
 	{
 		return $this->set_additional_property(self :: PROPERTY_ANSWERS, serialize($answers));
 	}
-	
+
 	public function get_answers()
 	{
 		if($result = unserialize($this->get_additional_property(self :: PROPERTY_ANSWERS)))
@@ -30,25 +31,35 @@ class FillInBlanksQuestion extends LearningObject
 		}
 		return array();
 	}
-	
+
 	public function get_number_of_answers()
 	{
 		return count($this->get_answers());
 	}
-	
+
 	public function get_answer_text()
 	{
 		return $this->get_additional_property(self :: PROPERTY_ANSWER_TEXT);
 	}
-	
+
 	public function set_answer_text($answer_text)
 	{
 		$this->set_additional_property(self :: PROPERTY_ANSWER_TEXT, $answer_text);
 	}
-	
+
+	public function get_question_type()
+	{
+		return $this->get_additional_property(self :: PROPERTY_QUESTION_TYPE);
+	}
+
+	public function set_question_type($question_type)
+	{
+		$this->set_additional_property(self :: PROPERTY_QUESTION_TYPE, $question_type);
+	}
+
 	static function get_additional_property_names()
 	{
-		return array (self :: PROPERTY_ANSWERS, self :: PROPERTY_ANSWER_TEXT);
+		return array (self :: PROPERTY_ANSWERS, self :: PROPERTY_ANSWER_TEXT, self :: PROPERTY_QUESTION_TYPE);
 	}
 }
 ?>
