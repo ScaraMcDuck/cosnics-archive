58c4253da416a4eba8897a47f8900c114ea2bb7d
sbil136 <sbil136@localhost>
2009-06-25 09:51:20 +0000

    Working on Webconferencing:
    * implemented action bar
    * added more checks for passwords, passcodes
    * added returnUrl
    * added help items for new application

--- dokeos/application/lib/webconferencing/webconferencing_manager/component/webconferences_browser.class.php
+++ dokeos/application/lib/webconferencing/webconferencing_manager/component/webconferences_browser.class.php
@@ -13,17 +13,24 @@ require_once dirname(__FILE__).'/webconference_browser/webconference_browser_tab
  */
 class WebconferencingManagerWebconferencesBrowserComponent extends WebconferencingManagerComponent
 {
+	
+	private $action_bar;
 
 	function run()
 	{
 		$trail = new BreadcrumbTrail();
+		$trail->add(new Breadcrumb($this->get_url(), Translation :: get('Webconferencing')));
 		$trail->add(new Breadcrumb($this->get_url(), Translation :: get('BrowseWebconferences')));
+		$trail->add_help('webconferencing general');
+		$this->action_bar = $this->get_action_bar();
+		
+		$toolbar = $this->get_action_bar();
 
 		$this->display_header($trail);
-
-		echo '<a href="' . $this->get_create_webconference_url() . '">' . Translation :: get('CreateWebconference') . '</a>';
-		echo '<br /><br />';
+		echo $toolbar->as_html();
+		echo '<div id="action_bar_browser">';
 		echo $this->get_table();
+		echo '</div>';
 		$this->display_footer();
 	}
 
@@ -31,6 +38,33 @@ class WebconferencingManagerWebconferencesBrowserComponent extends Webconferenci
 	{
 		$table = new WebconferenceBrowserTable($this, array(Application :: PARAM_APPLICATION => 'webconferencing', Application :: PARAM_ACTION => WebconferencingManager :: ACTION_BROWSE_WEBCONFERENCES), null);
 		return $table->as_html();
+	}
+	
+	function add_actionbar_item($item)
+	{
+		$this->action_bar->add_tool_action($item);
+	}
+
+	function get_action_bar()
+	{
+		$action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
+
+		$action_bar->set_search_url($this->get_url());
+		$action_bar->add_common_action(new ToolbarItem(Translation :: get('CreateWebconference'), Theme :: get_common_image_path().'action_publish.png', $this->get_create_webconference_url(), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+		$action_bar->add_common_action(new ToolbarItem(Translation :: get('ShowAll'), Theme :: get_common_image_path().'action_browser.png', $this->get_url(), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+		return $action_bar;
+	}
+	
+	function get_condition()
+	{
+		$condition = null;
+		$user = $this->get_user();
+
+		if (!$user->is_platform_admin())
+		{
+		}
+
+		return $condition;
 	}
 
 }
