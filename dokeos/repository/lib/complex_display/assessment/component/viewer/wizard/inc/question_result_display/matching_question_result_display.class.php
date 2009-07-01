687ebf1c7c9c47d3f4e700a3bc6544f3da583def
vanpouckesven <vanpouckesven@localhost>
2009-07-01 13:59:10 +0000

    Some additions to the matching result display

--- dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/inc/question_result_display/matching_question_result_display.class.php
+++ dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/inc/question_result_display/matching_question_result_display.class.php
@@ -9,16 +9,47 @@ class MatchingQuestionResultDisplay extends QuestionResultDisplay
         $html[] = '<table class="data_table take_assessment">';
         $html[] = '<thead>';
         $html[] = '<tr>';
-        $html[] = '<th class="list">' . Translation :: get('PossibleMatches') . '</th>';
+        $html[] = '<th class="list"></th>';
+        $html[] = '<th>' . Translation :: get('PossibleMatches') . '</th>';
         $html[] = '</tr>';
         $html[] = '</thead>';
         $html[] = '<tbody>';
 		
+        $label = 'A';
         $matches = $this->get_question()->get_matches();
         foreach($matches as $i => $match)
         {
         	$html[] = '<tr class="' . ($i % 2 == 0 ? 'row_even' : 'row_odd') . '">';
+        	$html[] = '<td>' . $label . '</td>';
         	$html[] = '<td>' . $match . '</td>';
+        	$html[] = '</tr>';
+        	$label++;
+        }
+        
+        $html[] = '</tbody>';
+        $html[] = '</table>';
+        
+        $html[] = '<table class="data_table take_assessment">';
+        $html[] = '<thead>';
+        $html[] = '<tr>';
+        $html[] = '<th>' . Translation :: get('YourMatch') . '</th>';
+        $html[] = '<th>' . Translation :: get('Correct') . '</th>';
+        $html[] = '<th>' . Translation :: get('Option') . '</th>';
+        $html[] = '<th>' . Translation :: get('Feedback') . '</th>';
+        $html[] = '</tr>';
+        $html[] = '</thead>';
+        $html[] = '<tbody>';
+		
+        $answers = $this->get_answers();
+        
+        $options = $this->get_question()->get_options();
+        foreach($options as $i => $option)
+        {
+        	$html[] = '<tr class="' . ($i % 2 == 0 ? 'row_even' : 'row_odd') . '">';
+        	$html[] = '<td>' . $answers[$i] . '</td>';
+        	$html[] = '<td>' . $option->get_match() . '</td>';
+        	$html[] = '<td>' . $option->get_value() . '</td>';
+        	$html[] = '<td>' . $option->get_comment() . '</td>';
         	$html[] = '</tr>';
         }
         
