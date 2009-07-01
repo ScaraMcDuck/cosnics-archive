f9b35e2adaa58811a1245e7037660b53cb506b4e
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-07-01 12:46:34 +0000

    Fixed result display for question types multiple choice, multiple answer, open question, rating question, percentage question

--- dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/inc/question_result_display/multiple_choice_question_result_display.class.php
+++ dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/inc/question_result_display/multiple_choice_question_result_display.class.php
@@ -4,6 +4,73 @@ require_once dirname(__FILE__).'/../question_result_display.class.php';
 
 class MultipleChoiceQuestionResultDisplay extends QuestionResultDisplay
 {
-	
+	function display_question_result()
+	{		
+        $html[] = '<table class="data_table">';
+        $html[] = '<thead>';
+        $html[] = '<tr>';
+        $html[] = '<th>' . Translation :: get('Choice') . '</th>';
+        $html[] = '<th>' . Translation :: get('Correct') . '</th>';
+        $html[] = '<th>' . Translation :: get('Answer') . '</th>';
+        $html[] = '<th>' . Translation :: get('Feedback') . '</th>';
+        $html[] = '</tr>';
+        $html[] = '</thead>';
+        $html[] = '<tbody>';
+		
+        $answers = $this->get_answers();
+        $options = $this->get_question()->get_options();
+        $type = $this->get_question()->get_answer_type();
+      
+        foreach($options as $i => $option)
+        {
+       		$html[] = '<tr>';
+       		
+       		if(array_key_exists($i + 1, $answers))
+       		{
+       			$selected = " checked ";
+       		}
+       		else
+       		{
+       			$selected = "";
+       		}
+       		
+       		if($type == 'radio')
+       		{	
+       			$html[] = '<td>' . '<input type="radio" name="yourchoice" value="' . $i . '" disabled' . $selected . '/>' . '</td>';
+       		}
+       		else
+       		{
+       			$html[] = '<td>' . '<input type="checkbox" name="correctchoice_' . $i . '" disabled' . $selected . '/>' . '</td>';
+       		}
+       		
+       		if($option->is_correct())
+       		{
+       			$selected = " checked ";
+       		}
+       		else
+       		{
+       			$selected = "";
+       		}
+
+       		if($type == 'radio')
+       		{
+       			$html[] = '<td>' . '<input type="radio" name="correctchoice" value="' . $i . '" disabled' . $selected . '/>' . '</td>';
+       		}
+       		else 
+       		{
+       			$html[] = '<td>' . '<input type="checkbox" name="correctchoice_' . $i . '" disabled' . $selected . '/>' . '</td>';
+       		}
+       		
+       		
+       		$html[] = '<td>' . $option->get_value() . '</td>';
+       		$html[] = '<td>' . $option->get_comment() . '</td>';
+       		$html[] = '</tr>';
+        }
+        
+        $html[] = '</tbody>';
+        $html[] = '</table>';
+        
+		echo implode("\n", $html);
+	}	
 }
 ?>
--- dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/inc/question_result_display/open_question_result_display.class.php
+++ dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/inc/question_result_display/open_question_result_display.class.php
@@ -4,6 +4,18 @@ require_once dirname(__FILE__).'/../question_result_display.class.php';
 
 class OpenQuestionResultDisplay extends QuestionResultDisplay
 {
+	function display_question_result()
+	{
+		$answers = $this->get_answers();
+		$html[] = $answers[0];
+		$html[] = '<div class="warning-message">' . Translation :: get('NotYetRatedWarning') . '</div>';
+		
+		echo implode("\n", $html);
+	}
 	
+	function add_borders()
+	{
+		return true;
+	}
 }
 ?>
