a5d46fc3201a43ffc0e86038359be7ed2e2e12c2
vanpouckesven <vanpouckesven@localhost>
2009-07-03 08:02:15 +0000

    Finished the result viewer for the match and ordering question

--- dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/inc/score_calculator/ordering_score_calculator.class.php
+++ dokeos/repository/lib/complex_display/assessment/component/viewer/wizard/inc/score_calculator/ordering_score_calculator.class.php
@@ -5,7 +5,22 @@ class OrderingScoreCalculator extends ScoreCalculator
 {
     function calculate_score()
     {
-        return 0;
+        $user_answers = $this->get_answer();
+    	
+    	$answers = $this->get_question()->get_options();
+    	
+    	$score = 0;
+    	
+    	foreach($answers as $i => $answer)
+    	{
+    		if($user_answers[$i + 1] == $answer->get_order())
+    		{
+    			$score++;
+    			//$score += $answer->get_weight();	    	
+    		}
+    	}
+    	
+    	return $score;
     }
 }
 ?>
