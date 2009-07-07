363c294c138b189255d213cc432a443734c4d3a6
scara84 <scara84@localhost>
2009-07-07 09:37:28 +0000

    Continuing work on the package manager ( + a small html editor tweak).

--- dokeos/plugin/html_editor/fckeditor/fcktemplates.xml.php
+++ dokeos/plugin/html_editor/fckeditor/fcktemplates.xml.php
@@ -14,32 +14,50 @@ $html = array();
 
 if (Authentication :: is_valid())
 {
-	$rdm = RepositoryDataManager :: get_instance();
-	
-	$conditions = array();
-	$conditions[] = new EqualityCondition(LearningObject :: PROPERTY_OWNER_ID, Session :: get_user_id());
-	$conditions[] = new EqualityCondition(LearningObject :: PROPERTY_TYPE, 'template');
-	$condition = new AndCondition($conditions);
-	
-	$templates = $rdm->retrieve_learning_objects(null, $condition, array(LearningObject :: PROPERTY_TITLE), array(SORT_ASC));
-	
-	$html[] = '<?xml version="1.0" encoding="utf-8" ?>';
-	$html[] = '<Templates>';
-	
-	while ($template = $templates->next_result())
-	{
-		//$html[] = '<Template title="'. $template->get_title() .'" image="'. Theme :: get_common_image_path() .'status_error.png">';
-		$html[] = '<Template title="'. $template->get_title() .'">';
-		$html[] = '<Description>'. $template->get_description() .'</Description>';
-		$html[] = '<Html>';
-		$html[] = '<![CDATA[';
-		$html[] = $template->get_design();
-		$html[] = ']]>';
-		$html[] = '</Html>';
-		$html[] = '</Template>';
-	}
-	
-	$html[] = '</Templates>';
+    $adm = AdminDataManager :: get_instance();
+
+    $conditions = array();
+    $conditions[] = new EqualityCondition(Registration :: PROPERTY_TYPE, Registration :: TYPE_LEARNING_OBJECT);
+    $conditions[] = new EqualityCondition(Registration :: PROPERTY_NAME, 'template');
+    $condition = new AndCondition($conditions);
+
+    $template_object_exists = $adm->count_registrations($condition);
+
+    if (!$template_object_exists)
+    {
+    	$html[] = '<?xml version="1.0" encoding="utf-8" ?>';
+    	$html[] = '<Templates>';
+    	$html[] = '</Templates>';
+    }
+    else
+    {
+	    $rdm = RepositoryDataManager :: get_instance();
+
+    	$conditions = array();
+    	$conditions[] = new EqualityCondition(LearningObject :: PROPERTY_OWNER_ID, Session :: get_user_id());
+    	$conditions[] = new EqualityCondition(LearningObject :: PROPERTY_TYPE, 'template');
+    	$condition = new AndCondition($conditions);
+
+    	$templates = $rdm->retrieve_learning_objects(null, $condition, array(LearningObject :: PROPERTY_TITLE), array(SORT_ASC));
+
+    	$html[] = '<?xml version="1.0" encoding="utf-8" ?>';
+    	$html[] = '<Templates>';
+
+    	while ($template = $templates->next_result())
+    	{
+    		//$html[] = '<Template title="'. $template->get_title() .'" image="'. Theme :: get_common_image_path() .'status_error.png">';
+    		$html[] = '<Template title="'. $template->get_title() .'">';
+    		$html[] = '<Description>'. $template->get_description() .'</Description>';
+    		$html[] = '<Html>';
+    		$html[] = '<![CDATA[';
+    		$html[] = $template->get_design();
+    		$html[] = ']]>';
+    		$html[] = '</Html>';
+    		$html[] = '</Template>';
+    	}
+
+    	$html[] = '</Templates>';
+    }
 }
 
 echo implode("\n", $html);
