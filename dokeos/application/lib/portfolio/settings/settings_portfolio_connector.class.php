68846c23c23c87ecc8cdd7ef17a2f3f907186900
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-07-09 08:02:32 +0000

    Added settings to the portfolio for picture and first page

---
+++ dokeos/application/lib/portfolio/settings/settings_portfolio_connector.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Simple connector class to facilitate rendering settings forms by
+ * preprocessing data from the datamanagers to a simple array format.
+ * @author Sven Vanpoucke
+ */
+
+class SettingsPortfolioConnector
+{
+
+}
+?>
