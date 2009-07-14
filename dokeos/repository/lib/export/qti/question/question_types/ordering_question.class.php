b528b291243acc3b12dc9d9cb3b47a84cfe9e736
vanpouckesven <vanpouckesven@localhost>
2009-07-14 10:53:10 +0000

    First bugfixes in qti export, needs to be refactored more to support new question types as well

---
+++ dokeos/repository/lib/export/qti/question/question_types/ordering_question.class.php
@@ -0,0 +1,19 @@
+<?php
+require_once dirname(__FILE__).'/../question_qti_export.class.php';
+
+class OrderingQuestionQtiExport extends QuestionQtiExport
+{
+	
+	function export_learning_object()
+	{
+	}
+	
+	function get_outcome_xml()
+	{
+	}
+	
+	function get_interaction_xml($high, $low)
+	{
+	}
+}
+?>
