e3f8e9a2de4d3e1ae424885bdf388df991a11ada
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-07-16 08:53:18 +0000

    Fixed some more import problems and added the fill in the blanks answer text

--- dokeos/repository/lib/import/qti/question/question_types/fill_in_blanks_import.class.php
+++ dokeos/repository/lib/import/qti/question/question_types/fill_in_blanks_import.class.php
@@ -24,6 +24,21 @@ class FillInBlanksQuestionQtiImport extends QuestionQtiImport
 		$question->set_title($title);
 		$question->set_description($description);
 		
+		$answer_text = parent :: get_tag_content('itemBody');
+		$pos = strpos($answer_text, '</prompt>') + 9;
+		
+		$answer_text = substr($answer_text, $pos, strlen($answer_text) - $pos);
+		$answers = $data['responseDeclaration'];
+		
+		foreach($answers as $answer)
+		{
+			$value = '[' . $answer['correctResponse']['value'] . ']';
+			$answer_text = preg_replace('/\<textentryinteraction(.*)textentryinteraction\>/i', $value, $answer_text, 1);
+		}
+		
+		$question->set_answer_text($answer_text);
+		$question->set_question_type(0);
+		
 		$this->create_answers($data, $question);
 		parent :: create_question($question);
 		return $question->get_id();
@@ -33,16 +48,19 @@ class FillInBlanksQuestionQtiImport extends QuestionQtiImport
 	{
 		$answers = $data['responseDeclaration'];
 		
-		foreach ($answers as $answer)
+		foreach ($answers as $i => $answer)
 		{
 			//$answer_list[$answer['identifier']] = $answer['correctResponse']['value'];
 			$value = $answer['correctResponse']['value'];
 			$weight = $answer['correctResponse']['mapping']['mapEntry']['mappedValue'];
 			if ($weight = '')
 				$weight = 1;
+		
+			$comment = parent :: get_tag_content('feedbackInline', array('outcomeIdentifier' => 'FEEDBACK' . $i));
+
 			//$answer_lo = $this->create_answer($answer['correctResponse']['value']);
 			//$this->create_complex_answer($question, $answer_lo, 1);
-			$fib_ans = new FillInBlanksQuestionAnswer($value, $weight);
+			$fib_ans = new FillInBlanksQuestionAnswer('[' . $value . ']', $weight, $comment, 20);
 			$question->add_answer($fib_ans);
 		}
 	}
