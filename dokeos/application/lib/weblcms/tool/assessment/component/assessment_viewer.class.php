4449e0721127a49b12806866b5e85a07d6707dc0
vanpouckesven <vanpouckesven@localhost>
2009-07-16 12:35:30 +0000

    Fixed a bug in the assessment module

--- dokeos/application/lib/weblcms/tool/assessment/component/assessment_viewer.class.php
+++ dokeos/application/lib/weblcms/tool/assessment/component/assessment_viewer.class.php
@@ -64,41 +64,11 @@ class AssessmentToolViewerComponent extends AssessmentToolComponent
 
 		$this->display_footer();
 	}
-
-	/*function display_introduction_text()
+	
+	function get_condition()
 	{
-		$html = array();
-
-		$introduction_text = $this->introduction_text;
-
-		if($introduction_text)
-		{
-
-			$tb_data[] = array(
-				'href' => $this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_EDIT, Tool :: PARAM_PUBLICATION_ID => $introduction_text->get_id())),
-				'label' => Translation :: get('Edit'),
-				'img' => Theme :: get_common_image_path() . 'action_edit.png',
-				'display' => DokeosUtilities :: TOOLBAR_DISPLAY_ICON
-			);
-
-			$tb_data[] = array(
-				'href' => $this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_DELETE, Tool :: PARAM_PUBLICATION_ID => $introduction_text->get_id())),
-				'label' => Translation :: get('Delete'),
-				'img' => Theme :: get_common_image_path() . 'action_delete.png',
-				'display' => DokeosUtilities :: TOOLBAR_DISPLAY_ICON
-			);
-
-			$html[] = '<div class="learning_object">';
-			$html[] = '<div class="description">';
-			$html[] = $introduction_text->get_learning_object()->get_description();
-			$html[] = '</div>';
-			$html[] = DokeosUtilities :: build_toolbar($tb_data) . '<div class="clear"></div>';
-			$html[] = '</div>';
-			$html[] = '<br />';
-		}
-
-		return implode("\n",$html);
-	}*/
+		return null;
+	}
 
 	function get_toolbar($search)
 	{
