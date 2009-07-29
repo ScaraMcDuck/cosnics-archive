a8ef01e751bb46b4a1a3162fd60f87ee049c139b
vanpouckesven <vanpouckesven@localhost>
2009-07-29 12:31:15 +0000

    Fixed the glossary student view

--- dokeos/application/lib/weblcms/tool/glossary/component/glossary_browser.class.php
+++ dokeos/application/lib/weblcms/tool/glossary/component/glossary_browser.class.php
@@ -60,12 +60,16 @@ class GlossaryToolBrowserComponent extends GlossaryToolComponent
 		if(!Request :: get('pid'))
 		{
 			$action_bar->set_search_url($this->get_url());
-			$action_bar->add_common_action(new ToolbarItem(Translation :: get('Publish'), Theme :: get_common_image_path().'action_publish.png', $this->get_url(array(GlossaryTool :: PARAM_ACTION => GlossaryTool :: ACTION_PUBLISH)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+			
+			if($this->is_allowed(ADD_RIGHT))
+			{
+				$action_bar->add_common_action(new ToolbarItem(Translation :: get('Publish'), Theme :: get_common_image_path().'action_publish.png', $this->get_url(array(GlossaryTool :: PARAM_ACTION => GlossaryTool :: ACTION_PUBLISH)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
+			}
 		}
 
 		$action_bar->add_common_action(new ToolbarItem(Translation :: get('ShowAll'), Theme :: get_common_image_path().'action_browser.png', $this->get_url(), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
 
-		if(!$this->introduction_text && PlatformSetting :: get('enable_introduction', 'weblcms'))
+		if(!$this->introduction_text && PlatformSetting :: get('enable_introduction', 'weblcms') && $this->is_allowed(EDIT_RIGHT))
 		{
 			$action_bar->add_common_action(new ToolbarItem(Translation :: get('PublishIntroductionText'), Theme :: get_common_image_path().'action_introduce.png', $this->get_url(array(AnnouncementTool :: PARAM_ACTION => Tool :: ACTION_PUBLISH_INTRODUCTION)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
 		}
