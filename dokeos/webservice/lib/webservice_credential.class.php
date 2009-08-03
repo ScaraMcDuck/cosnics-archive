f35f87da2cf6bab7e73394759bf2de365792793c
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-08-03 11:50:39 +0000

    Fixed the webservice dataclass refactoring

--- dokeos/webservice/lib/webservice_credential.class.php
+++ dokeos/webservice/lib/webservice_credential.class.php
@@ -1,5 +1,6 @@
 <?php
 require_once dirname(__FILE__).'/webservice_data_manager.class.php';
+require_once Path :: get_common_path() . 'data_class.class.php';
 
 /**
  * @package webservice
@@ -8,7 +9,7 @@ require_once dirname(__FILE__).'/webservice_data_manager.class.php';
  *	@author Stefan Billiet
  */
 
-class WebserviceCredential
+class WebserviceCredential extends DataClass
 {
 	const CLASS_NAME = __CLASS__;	
 	const PROPERTY_USER_ID = 'user_id';
@@ -16,45 +17,6 @@ class WebserviceCredential
 	const PROPERTY_IP = 'ip';
 	const PROPERTY_TIME_CREATED = 'time_created';
 	const PROPERTY_END_TIME = 'end_time';
-	
-	/**
-	 * Default properties of the webservice_category object, stored in an associative
-	 * array.
-	 */
-	private $defaultProperties;
-	
-	/**
-	 * Creates a new webservice_credential object.
-	 * @param array $defaultProperties The default properties of the webservice_category
-	 *                                 object. Associative array.
-	 */
-	function WebserviceCredential($defaultProperties = array ())
-	{
-		$this->defaultProperties = $defaultProperties;
-	}
-
-	/**
-	 * Gets a default property of this webservice_credential object by name.
-	 * @param string $name The name of the property.
-	 */
-	function get_default_property($name)
-	{
-		return $this->defaultProperties[$name];
-	}
-
-	/**
-	 * Gets the default properties of this webservice_credential.
-	 * @return array An associative array containing the properties.
-	 */
-	function get_default_properties()
-	{
-		return $this->defaultProperties;
-	}
-	
-	function set_default_properties($defaultProperties)
-	{
-		$this->defaultProperties = $defaultProperties;
-	}
 	
 	/**
 	 * Get the default properties of all webservice_credentials.
@@ -66,27 +28,13 @@ class WebserviceCredential
 	}
 	
 	/**
-	 * Sets a default property of this webservice_credential by name.
-	 * @param string $name The name of the property.
-	 * @param mixed $value The new value for the property.
+	 * inherited
 	 */
-	function set_default_property($name, $value)
+	function get_data_manager()
 	{
-		$this->defaultProperties[$name] = $value;
+		return WebserviceDataManager :: get_instance();	
 	}
 	
-	/**
-	 * Checks if the given identifier is the name of a default webservice_credential
-	 * property.
-	 * @param string $name The identifier.
-	 * @return boolean True if the identifier is a property name, false
-	 *                 otherwise.
-	 */
-	static function is_default_property_name($name)
-	{
-		return in_array($name, self :: get_default_property_names());
-	}
-
 	/**
 	 * Returns the id of this webservice_credential.
 	 * @return int The id.
@@ -176,12 +124,6 @@ class WebserviceCredential
 	{
 		$wdm = WebserviceDataManager :: get_instance();
 		return $wdm->create_webservice_credential($this);
-	}
-	
-	function delete()
-	{
-		$wdm = WebserviceDataManager :: get_instance();
-		return $wdm->delete_webservice_credential($this);
 	}
 }
 ?>
