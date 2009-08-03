a17b83c80c4a54c49780da3c5d8c15e4730be560
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-08-03 11:57:48 +0000

    fixed the platform category dataclass

--- dokeos/application/common/category_manager/platform_category.class.php
+++ dokeos/application/common/category_manager/platform_category.class.php
@@ -1,6 +1,7 @@
 <?php
 
 //require_once dirname(__FILE__).'/reservations_data_manager.class.php';
+require_once Path :: get_common_path() . 'data_class.class.php';
 
 /**
  * @package category
@@ -9,54 +10,11 @@
  *	@author Sven Vanpoucke
  */
 
-abstract class PlatformCategory
+abstract class PlatformCategory extends DataClass
 {
-	const PROPERTY_ID = 'id';
 	const PROPERTY_NAME = 'name';
 	const PROPERTY_PARENT = 'parent';
 	const PROPERTY_DISPLAY_ORDER = 'display_order';
-	
-	/**
-	 * Default properties of the contribution object, stored in an associative
-	 * array.
-	 */
-	private $defaultProperties;
-
-	/**
-	 * Creates a new contribution object.
-	 * @param int $id The numeric ID of the contribution object. May be omitted
-	 *                if creating a new object.
-	 * @param array $defaultProperties The default properties of the contribution
-	 *                                 object. Associative array.
-	 */
-	 
-	function PlatformCategory($defaultProperties = array ())
-	{
-		$this->defaultProperties = $defaultProperties;
-	}
-	
-	/**
-	 * Gets a default property of this contribution object by title.
-	 * @param string $title The title of the property.
-	 */
-	function get_default_property($title)
-	{
-		return $this->defaultProperties[$title];
-	}
-	
-	/**
-	 * Gets the default properties of this contribution.
-	 * @return array An associative array containing the properties.
-	 */
-	function get_default_properties()
-	{
-		return $this->defaultProperties;
-	}
-	
-	function set_default_properties($defaultProperties)
-	{
-		$this->defaultProperties = $defaultProperties;
-	}
 	
 	/**
 	 * Get the default properties of all contributions.
@@ -64,48 +22,16 @@ abstract class PlatformCategory
 	 */
 	static function get_default_property_names()
 	{
-		return array (self :: PROPERTY_ID, self :: PROPERTY_NAME, self :: PROPERTY_PARENT, self :: PROPERTY_DISPLAY_ORDER);
-	}
-		
-	/**
-	 * Sets a default property of this contribution by title.
-	 * @param string $title The title of the property.
-	 * @param mixed $value The new value for the property.
-	 */
-	function set_default_property($title, $value)
-	{
-		$this->defaultProperties[$title] = $value;
+		return parent :: get_default_property_names(array (self :: PROPERTY_NAME, self :: PROPERTY_PARENT, self :: PROPERTY_DISPLAY_ORDER));
 	}
 	
 	/**
-	 * Checks if the given identifier is the title of a default contribution
-	 * property.
-	 * @param string $title The identifier.
-	 * @return boolean True if the identifier is a property title, false
-	 *                 otherwise.
+	 * inherited
 	 */
-	static function is_default_property_name($title)
+	function get_data_manager()
 	{
-		return in_array($title, self :: get_default_property_names());
+		return null;
 	}
-
-	/**
-	 * Returns the id of this contribution.
-	 * @return int The id.
-	 */
-	function get_id()
-	{
-		return $this->get_default_property(self :: PROPERTY_ID);
-	}
-	
-	/**
-	 * Sets the name of this contribution.
-	 * @param int $name The name.
-	 */
-	function set_id($id)
-	{
-		$this->set_default_property(self :: PROPERTY_ID, $id);
-	}	
 	
 	function get_name()
 	{
@@ -137,9 +63,9 @@ abstract class PlatformCategory
 		$this->set_default_property(self :: PROPERTY_DISPLAY_ORDER, $display_order);
 	}
 	
-	abstract function create();
+	/*abstract function create();
 	abstract function update();
-	abstract function delete();
+	abstract function delete();*/
 	/*function create()
 	{
 		$rdm = ReservationsDataManager :: get_instance();
