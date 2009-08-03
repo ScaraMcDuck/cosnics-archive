84aca9d423cc96bf34b608d46fdd4c6a847e3d17
vanpouckesven <vanpouckesven@localhost>
2009-08-03 12:09:22 +0000

    Finished refactoring dataclassess of alexia

--- dokeos/application/lib/alexia/alexia_publication_group.class.php
+++ dokeos/application/lib/alexia/alexia_publication_group.class.php
@@ -3,12 +3,14 @@
  * portfolio
  */
 
+require_once Path :: get_common_path() . 'data_class.class.php';
+
 /**
  * This class describes a AlexiaPublicationGroup data object
  *
  * @author Sven Vanpoucke
  */
-class AlexiaPublicationGroup
+class AlexiaPublicationGroup extends DataClass
 {
 	const CLASS_NAME = __CLASS__;
 	const TABLE_NAME = 'publication_group';
@@ -20,38 +22,6 @@ class AlexiaPublicationGroup
 	const PROPERTY_GROUP_ID = 'group_id';
 
 	/**
-	 * Default properties stored in an associative array.
-	 */
-	private $defaultProperties;
-
-	/**
-	 * Creates a new AlexiaPublicationGroup object
-	 * @param array $defaultProperties The default properties
-	 */
-	function AlexiaPublicationGroup($defaultProperties = array ())
-	{
-		$this->defaultProperties = $defaultProperties;
-	}
-
-	/**
-	 * Gets a default property by name.
-	 * @param string $name The name of the property.
-	 */
-	function get_default_property($name)
-	{
-		return $this->defaultProperties[$name];
-	}
-
-	/**
-	 * Gets the default properties
-	 * @return array An associative array containing the properties.
-	 */
-	function get_default_properties()
-	{
-		return $this->defaultProperties;
-	}
-
-	/**
 	 * Get the default properties
 	 * @return array The property names.
 	 */
@@ -59,23 +29,13 @@ class AlexiaPublicationGroup
 	{
 		return array (self :: PROPERTY_PUBLICATION, self :: PROPERTY_GROUP_ID);
 	}
-
+	
 	/**
-	 * Sets a default property by name.
-	 * @param string $name The name of the property.
-	 * @param mixed $value The new value for the property.
+	 * inherited
 	 */
-	function set_default_property($name, $value)
+	function get_data_manager()
 	{
-		$this->defaultProperties[$name] = $value;
-	}
-
-	/**
-	 * Sets the default properties of this class
-	 */
-	function set_default_properties($defaultProperties)
-	{
-		$this->defaultProperties = $defaultProperties;
+		return AlexiaDataManager :: get_instance();	
 	}
 
 	/**
@@ -113,24 +73,12 @@ class AlexiaPublicationGroup
 		$this->set_default_property(self :: PROPERTY_GROUP_ID, $group_id);
 	}
 
-	function delete()
-	{
-		$dm = AlexiaDataManager :: get_instance();
-		return $dm->delete_alexia_publication_group($this);
-	}
-
 	function create()
 	{
 		$dm = AlexiaDataManager :: get_instance();
        	return $dm->create_alexia_publication_group($this);
 	}
-
-	function update()
-	{
-		$dm = AlexiaDataManager :: get_instance();
-		return $dm->update_alexia_publication_group($this);
-	}
-
+	
 	static function get_table_name()
 	{
 		return self :: TABLE_NAME;
