33ba82ba34ac8c476f3324623644c68532e8ccfc
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-08-03 09:55:22 +0000

    added refactoring for the rights application fixed a few small bugs

--- dokeos/rights/lib/role_right_location.class.php
+++ dokeos/rights/lib/role_right_location.class.php
@@ -7,49 +7,15 @@
  *	@author Dieter De Neef
  */
 
-class RoleRightLocation
+require_once Path :: get_common_path() . 'data_class.class.php';
+
+class RoleRightLocation extends DataClass
 {
+	const CLASS_NAME = __CLASS__;
 	const PROPERTY_RIGHT_ID = 'right_id';
 	const PROPERTY_LOCATION_ID = 'location_id';
 	const PROPERTY_ROLE_ID = 'role_id';
 	const PROPERTY_VALUE = 'value';
-	
-	private $defaultProperties;
-
-	function update() 
-	{
-		$rdm = RightsDataManager :: get_instance();
-		$success = $rdm->update_rolerightlocation($this);
-		if (!$success)
-		{
-			return false;
-		}
-
-		return true;
-	}
-	
-	function RoleRightLocation($defaultProperties = array ())
-	{
-		$this->defaultProperties = $defaultProperties;
-	}
-	
-	/**
-	 * Gets a default property of this user object by name.
-	 * @param string $name The name of the property.
-	 */
-	function get_default_property($name)
-	{
-		return $this->defaultProperties[$name];
-	}
-	
-	/**
-	 * Gets the default properties of this user.
-	 * @return array An associative array containing the properties.
-	 */
-	function get_default_properties()
-	{
-		return $this->defaultProperties;
-	}
 	
 	/**
 	 * Get the default properties of all users.
@@ -59,27 +25,13 @@ class RoleRightLocation
 	{
 		return array (self :: PROPERTY_RIGHT_ID, self :: PROPERTY_ROLE_ID, self :: PROPERTY_LOCATION_ID, self :: PROPERTY_VALUE);
 	}
-		
-	/**
-	 * Sets a default property of this user by name.
-	 * @param string $name The name of the property.
-	 * @param mixed $value The new value for the property.
-	 */
-	function set_default_property($name, $value)
-	{
-		$this->defaultProperties[$name] = $value;
-	}
 	
 	/**
-	 * Checks if the given identifier is the name of a default user
-	 * property.
-	 * @param string $name The identifier.
-	 * @return boolean True if the identifier is a property name, false
-	 *                 otherwise.
+	 * inherited
 	 */
-	static function is_default_property_name($name)
+	function get_data_manager()
 	{
-		return in_array($name, self :: get_default_property_names());
+		return RightsDataManager :: get_instance();	
 	}
 	
 	function get_right_id()
@@ -121,16 +73,7 @@ class RoleRightLocation
 	{
 		$this->set_default_property(self :: PROPERTY_VALUE, $value);
 	}
-	
-	/**
-	 * Instructs the Datamanager to delete this user.
-	 * @return boolean True if success, false otherwise.
-	 */
-	function delete()
-	{
-		return UserDataManager :: get_instance()->delete_rolerightlocation($this);
-	}
-	
+
 	function create()
 	{
 		$rdm = RightsDataManager :: get_instance();
@@ -141,6 +84,11 @@ class RoleRightLocation
 	{
 		$value = $this->get_value();
 		$this->set_value(!$value);
+	}
+	
+	static function get_table_name()
+	{
+		return DokeosUtilities :: camelcase_to_underscores(self :: CLASS_NAME);
 	}
 }
 ?>
