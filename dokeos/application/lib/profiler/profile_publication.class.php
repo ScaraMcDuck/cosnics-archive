fc7df9ecffd896484bde9bc3d4ea1dee35237323
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-08-03 12:57:06 +0000

    fixed the profiler dataclass refactoring

--- dokeos/application/lib/profiler/profile_publication.class.php
+++ dokeos/application/lib/profiler/profile_publication.class.php
@@ -5,6 +5,7 @@
 require_once Path :: get_repository_path() . 'lib/repository_data_manager.class.php';
 require_once Path :: get_user_path() . 'lib/user_data_manager.class.php';
 require_once Path :: get_application_path(). 'lib/profiler/profiler_data_manager.class.php';
+require_once Path :: get_common_path() . 'data_class.class.php';
 
 /**
  *	This class represents a ProfilePublication.
@@ -17,94 +18,33 @@ require_once Path :: get_application_path(). 'lib/profiler/profiler_data_manager
  *	@author Hans de Bisschop
  *	@author Dieter De Neef
  */
-class ProfilePublication
+class ProfilePublication extends DataClass
 {
     const CLASS_NAME = __CLASS__;
     const TABLE_NAME = 'publication';
     
-    const PROPERTY_ID = 'id';
     const PROPERTY_PROFILE = 'profile';
     const PROPERTY_PUBLISHER = 'publisher';
     const PROPERTY_PUBLISHED = 'published';
     const PROPERTY_CATEGORY = 'category';
-    
-    private $defaultProperties;
-
-    /**
-     * Creates a new profile object.
-     * @param int $id The numeric ID of the ProfilePublication object. May be omitted
-     *                if creating a new object.
-     * @param array $defaultProperties The default properties of the ProfilePublication
-     *                                 object. Associative array.
-     */
-    function ProfilePublication($defaultProperties = array ())
-    {
-        $this->defaultProperties = $defaultProperties;
-    }
-
-    /**
-     * Gets a default property of this ProfilePublication object by name.
-     * @param string $name The name of the property.
-     */
-    function get_default_property($name)
-    {
-        return $this->defaultProperties[$name];
-    }
-
-    /**
-     * Gets the default properties of this ProfilePublication.
-     * @return array An associative array containing the properties.
-     */
-    function get_default_properties()
-    {
-        return $this->defaultProperties;
-    }
-
-    function set_default_properties($defaultProperties)
-    {
-        $this->defaultProperties = $defaultProperties;
-    }
-
+ 
     /**
      * Get the default properties of all ProfilePublications.
      * @return array The property names.
      */
     static function get_default_property_names()
     {
-        return array(self :: PROPERTY_ID, self :: PROPERTY_CATEGORY, self :: PROPERTY_PROFILE, self :: PROPERTY_PUBLISHER, self :: PROPERTY_PUBLISHED);
+        return parent :: get_default_property_names(array(self :: PROPERTY_CATEGORY, self :: PROPERTY_PROFILE, self :: PROPERTY_PUBLISHER, self :: PROPERTY_PUBLISHED));
     }
 
-    /**
-     * Sets a default property of this ProfilePublication by name.
-     * @param string $name The name of the property.
-     * @param mixed $value The new value for the property.
-     */
-    function set_default_property($name, $value)
-    {
-        $this->defaultProperties[$name] = $value;
-    }
-
-    /**
-     * Checks if the given identifier is the name of a default profiler
-     * property.
-     * @param string $name The identifier.
-     * @return boolean True if the identifier is a property name, false
-     *                 otherwise.
-     */
-    static function is_default_property_name($name)
-    {
-        return in_array($name, self :: get_default_property_names());
-    }
-
-    /**
-     * Returns the id of this ProfilePublication.
-     * @return int The ProfilePublication id.
-     */
-    function get_id()
-    {
-        return $this->get_default_property(self :: PROPERTY_ID);
-    }
-
+	/**
+	 * inherited
+	 */
+	function get_data_manager()
+	{
+		return ProfilerDataManager :: get_instance();	
+	}
+    
     /**
      * Returns the learning object id from this ProfilePublication object
      * @return int The Profile ID
@@ -135,15 +75,6 @@ class ProfilePublication
     function get_category()
     {
         return $this->get_default_property(self :: PROPERTY_CATEGORY);
-    }
-
-    /**
-     * Sets the id of this ProfilePublication.
-     * @param int $pm_id The ProfilePublication id.
-     */
-    function set_id($id)
-    {
-        $this->set_default_property(self :: PROPERTY_ID, $id);
     }
 
     /**
@@ -204,24 +135,6 @@ class ProfilePublication
         $id = $pmdm->get_next_profile_publication_id();
         $this->set_id($id);
         return $pmdm->create_profile_publication($this);
-    }
-
-    /**
-     * Deletes this publication from persistent storage
-     * @see ProfilerDataManager::delete_profile_publication()
-     */
-    function delete()
-    {
-        return ProfilerDataManager :: get_instance()->delete_profile_publication($this);
-    }
-
-    /**
-     * Updates this publication in persistent storage
-     * @see ProfilerDataManager::update_profile_publication()
-     */
-    function update()
-    {
-        return ProfilerDataManager :: get_instance()->update_profile_publication($this);
     }
 
     static function get_table_name()
