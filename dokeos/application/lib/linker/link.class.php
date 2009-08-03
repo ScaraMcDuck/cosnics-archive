30e32172a45d885713cb7ebc6483f5fb8218c2ce
vanpouckesven <vanpouckesven@localhost>
2009-08-03 12:32:29 +0000

    Fixed the dataclass of the linker application

--- dokeos/application/lib/linker/link.class.php
+++ dokeos/application/lib/linker/link.class.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once dirname(__FILE__).'/linker_data_manager.class.php';
+require_once Path :: get_common_path() . 'data_class.class.php';
 
 /**
  * @package link
@@ -9,55 +10,13 @@ require_once dirname(__FILE__).'/linker_data_manager.class.php';
  *  @author Sven Vanpoucke
  */
 
-class Link
+class Link extends DataClass
 {
 	const CLASS_NAME = __CLASS__;
 	
-	const PROPERTY_ID = 'id';
 	const PROPERTY_NAME = 'name';
 	const PROPERTY_DESCRIPTION = 'description';
 	const PROPERTY_URL = 'url';
-	
-	/**
-	 * Default properties of the link object, stored in an associative
-	 * array.
-	 */
-	private $defaultProperties;
-
-	/**
-	 * Creates a new link object.
-	 * @param int $id The numeric ID of the link object. May be omitted
-	 *                if creating a new object.
-	 * @param array $defaultProperties The default properties of the link
-	 *                                 object. Associative array.
-	 */
-	function Link($defaultProperties = array ())
-	{
-		$this->defaultProperties = $defaultProperties;
-	}
-	
-	/**
-	 * Gets a default property of this link object by name.
-	 * @param string $name The name of the property.
-	 */
-	function get_default_property($name)
-	{
-		return $this->defaultProperties[$name];
-	}
-	
-	/**
-	 * Gets the default properties of this link.
-	 * @return array An associative array containing the properties.
-	 */
-	function get_default_properties()
-	{
-		return $this->defaultProperties;
-	}
-	
-	function set_default_properties($defaultProperties)
-	{
-		$this->defaultProperties = $defaultProperties;
-	}
 	
 	/**
 	 * Get the default properties of all links.
@@ -67,36 +26,13 @@ class Link
 	{
 		return array (self :: PROPERTY_ID, self :: PROPERTY_NAME, self :: PROPERTY_DESCRIPTION, self :: PROPERTY_URL);
 	}
-		
-	/**
-	 * Sets a default property of this link by name.
-	 * @param string $name The name of the property.
-	 * @param mixed $value The new value for the property.
-	 */
-	function set_default_property($name, $value)
-	{
-		$this->defaultProperties[$name] = $value;
-	}
 	
 	/**
-	 * Checks if the given identifier is the name of a default link
-	 * property.
-	 * @param string $name The identifier.
-	 * @return boolean True if the identifier is a property name, false
-	 *                 otherwise.
+	 * inherited
 	 */
-	static function is_default_property_name($name)
+	function get_data_manager()
 	{
-		return in_array($name, self :: get_default_property_names());
-	}
-
-	/**
-	 * Returns the id of this link.
-	 * @return int The id.
-	 */
-	function get_id()
-	{
-		return $this->get_default_property(self :: PROPERTY_ID);
+		return LinkerDataManager :: get_instance();	
 	}
 	
 	/**
@@ -127,15 +63,6 @@ class Link
 	}
 	
 	/**
-	 * Sets the link_id of this link.
-	 * @param int $link_id The link_id.
-	 */
-	function set_id($id)
-	{
-		$this->set_default_property(self :: PROPERTY_ID, $id);
-	}		
-	
-	/**
 	 * Sets the name of this link.
 	 * @param String $name the name.
 	 */
@@ -160,25 +87,6 @@ class Link
 	function set_url($url)
 	{
 		$this->set_default_property(self :: PROPERTY_URL, $url);
-	}
-	
-	function delete()
-	{
-		$ldm = LinkerDataManager :: get_instance();
-		return $ldm->delete_link($this);
-	}
-	
-	function create()
-	{
-		$ldm = LinkerDataManager :: get_instance();
-		$this->set_id($ldm->get_next_link_id());
-       	return $ldm->create_link($this);
-	}
-	
-	function update() 
-	{
-		$ldm = LinkerDataManager :: get_instance();
-		return $ldm->update_link($this);
 	}
 	
 	static function get_table_name()
