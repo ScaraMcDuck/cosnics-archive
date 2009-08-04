43120b3052425a08de2198f4963b5acaa3718bb9
alaxos <alaxos@localhost>
2009-08-04 10:32:13 +0000

    Added some functions

--- dokeos/common/xml/xml_tool.class.php
+++ dokeos/common/xml/xml_tool.class.php
@@ -47,8 +47,69 @@ class XMLTool
     }
     
     
-    
+    /**
+     * Returns the first element found by the XPATH query. 
+     * The first subnode is searched by using a XPATH query relative to the document containing the $node.
+     * 
+     * @param DOMNode $node
+     * @param string $xpath_query
+     * @return DOMNode
+     */
     public static function get_first_element_by_xpath($node, $xpath_query)
+    {
+//        $dom = new DOMDocument();
+//        $imported_node = $dom->importNode($node, true);
+//        $dom->appendChild($imported_node);
+        
+        $xpath = new DOMXPath($node->ownerDocument);
+		$node_list = $xpath->query($xpath_query);
+		
+		if($node_list->length > 0)
+		{
+		    return $node_list->item(0);
+		}
+		else
+		{
+		    return null;
+		}
+    }
+    
+    
+    /**
+     * Returns the value of the first element found by the XPATH query. 
+     * The first subnode is searched by using a XPATH query relative to the document containing the $node.
+     * 
+     * @param DOMNode $node
+     * @param string $xpath_query
+     * @return string
+     */
+    public static function get_first_element_value_by_xpath($node, $xpath_query)
+    {
+        $node = self :: get_first_element_by_xpath($node, $xpath_query);
+        
+        if(isset($node))
+        {
+            return $node->nodeValue;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    
+    
+    /**
+     * Returns the first element found by the XPATH query. 
+     * The first subnode is searched by using a XPATH query relative to the given $node.
+     * 
+     * NOTE: this function can not be used to retrieve a node that needs to be updated 
+     *       as the returned node is a copy of the original one, and thus is not the same object reference  
+     * 
+     * @param DOMNode $node
+     * @param string $xpath_query
+     * @return DOMNode
+     */
+    public static function get_first_element_by_relative_xpath($node, $xpath_query)
     {
         $dom = new DOMDocument();
         $imported_node = $dom->importNode($node, true);
@@ -68,16 +129,16 @@ class XMLTool
     }
     
     /**
-     * Returns the first $subnode occurence of a $node 
-     * The subnode is searched by using a XPATH query relative to the $node.
+     * Returns the value of the first element found by the XPATH query. 
+     * The first subnode is searched by using a XPATH query relative to the given $node.
      * 
      * @param DOMNode $node
      * @param string $xpath_query
      * @return string
      */
-    public static function get_first_element_value_by_xpath($node, $xpath_query)
+    public static function get_first_element_value_by_relative_xpath($node, $xpath_query)
     {
-        $node = self :: get_first_element_by_xpath($node, $xpath_query);
+        $node = self :: get_first_element_by_relative_xpath($node, $xpath_query);
         
         if(isset($node))
         {
@@ -115,6 +176,7 @@ class XMLTool
 		return $values;
     }
     
+    
     /**
      * Returns a nodes list under a given node.  
      * The subnodes are searched by using a XPATH query relative to the document containing the $node.
@@ -125,25 +187,19 @@ class XMLTool
      */
     public static function get_all_element_by_xpath($node, $xpath_query)
     {
-//        $dom = new DOMDocument();
-//        $imported_node = $dom->importNode($node, true);
-//        $dom->appendChild($imported_node);
-        
-//        $xpath = new DOMXPath($dom);
-//		  $node_list = $xpath->query($xpath_query);
-		
         $xpath = new DOMXPath($node->ownerDocument);
         $node_list = $xpath->query($xpath_query);
         
 		return $node_list;
     }
     
+    
     /**
      * Get an attribute value, of the default value if the attribute is null or empty
      * 
-     * @param $node The node to search the attribute on
-     * @param $attribute_name The name of the attribute to get the value from
-     * @param $default_value A default value if the attribute doesn't exist or is empty
+     * @param DOMNode $node The node to search the attribute on
+     * @param string $attribute_name The name of the attribute to get the value from
+     * @param string $default_value A default value if the attribute doesn't exist or is empty
      */
     public static function get_attribute($node, $attribute_name, $default_value = null)
     {
@@ -156,7 +212,27 @@ class XMLTool
         
         return $value;
     }   
-     
+    
+    /**
+     * Delete all the nodes from a DOMDocument that are found with the given xpath query
+     * 
+     * @param DOMDocument $dom_document The DOMDocument from which nodes must be removed
+     * @param string $xpath_query
+     */
+    public static function delete_element_by_xpath($dom_document, $xpath_query)
+    {
+        $xpath = new DOMXPath($dom_document);
+        $node_list = $xpath->query($xpath_query);
+        
+        if($node_list->length > 0)
+	    {
+	        foreach ($node_list as $node_to_delete) 
+	        {
+	        	$node_to_delete->parentNode->removeChild($node_to_delete);
+	        }
+	    }
+    }
+    
 }
 
 ?>
