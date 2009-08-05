22505fe762cbcd7f6ded97294628c66cad6a72cd
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-08-05 13:05:41 +0000

    Added reporting to the assessment application, the view of summary results and the view of assessment attempts (still needs some deletion, detailed reporting, ...)

--- dokeos/application/lib/assessment/assessment_manager/component/assessment_publication_results_viewer.class.php
+++ dokeos/application/lib/assessment/assessment_manager/component/assessment_publication_results_viewer.class.php
@@ -1,3 +1,64 @@
 <?php
+/**
+ * @package application.assessment.assessment.component
+ */
+require_once dirname(__FILE__).'/../assessment_manager.class.php';
+require_once dirname(__FILE__).'/../assessment_manager_component.class.php';
 
+/**
+ * Component to create a new assessment_publication object
+ * @author Sven Vanpoucke
+ * @author 
+ */
+class AssessmentManagerAssessmentPublicationResultsViewerComponent extends AssessmentManagerComponent
+{
+	/**
+	 * Runs this component and displays its output.
+	 */
+	function run()
+	{
+		$trail = new BreadcrumbTrail();
+		$trail->add(new Breadcrumb($this->get_url(array(AssessmentManager :: PARAM_ACTION => AssessmentManager :: ACTION_BROWSE_ASSESSMENT_PUBLICATIONS)), Translation :: get('BrowseAssessmentPublications')));
+		$trail->add(new Breadcrumb($this->get_url(), Translation :: get('ViewResults')));
+
+		$pid = Request :: get(AssessmentManager :: PARAM_ASSESSMENT_PUBLICATION);
+		
+		if(!$pid)
+		{
+			$html = $this->display_summary_results();
+		}
+		else 
+		{
+			$trail->add(new Breadcrumb($this->get_url(array(AssessmentManager :: PARAM_ASSESSMENT_PUBLICATION => $pid)), Translation :: get('ViewAssessmentResults')));
+			$html = $this->display_assessment_results($pid);
+		}
+		
+		$this->display_header($trail);
+		echo $html;
+		$this->display_footer();
+	}
+	
+	function display_summary_results()
+	{
+		require_once(Path :: get_application_path() . 'lib/assessment/reporting/templates/assessment_attempts_summary_template.class.php');
+		
+		$current_category = Request :: get('category');
+		$current_category = $current_category ? $current_category : 0;
+		
+		$parameters = array('category'  => $current_category);
+		$template = new AssessmentAttemptsSummaryTemplate($this, 0, $parameters, null);
+		$template->set_reporting_blocks_function_parameters($parameters);
+		return $template->to_html();
+	}
+	
+	function display_assessment_results($pid)
+	{
+		require_once(Path :: get_application_path() . 'lib/assessment/reporting/templates/assessment_attempts_template.class.php');
+		
+		$parameters = array(AssessmentManager :: PARAM_ASSESSMENT_PUBLICATION => $pid);
+		$template = new AssessmentAttemptsTemplate($this, 0, $parameters, null, $pid);
+		$template->set_reporting_blocks_function_parameters($parameters);
+		return $template->to_html();
+	}
+}
 ?>
---
+++ dokeos/application/lib/assessment/reporting/templates/assessment_attempts_template.class.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @author Sven Vanpoucke
+ */
+require_once Path :: get_reporting_path(). 'lib/reporting_template.class.php';
+require_once Path :: get_reporting_path().'lib/reporting_manager/reporting_manager.class.php';
+require_once dirname(__FILE__) . '/../../trackers/assessment_assessment_attempts_tracker.class.php';
+
+class AssessmentAttemptsTemplate extends ReportingTemplate
+{
+	private $pid;
+	
+	function AssessmentAttemptsTemplate($parent=null,$id,$params,$trail, $pid)
+	{
+		$this->pid = $pid;
+		
+		$this->add_reporting_block(ReportingDataManager :: get_instance()->retrieve_reporting_block_by_name("AssessmentAttempts"),
+            array(ReportingTemplate :: PARAM_VISIBLE => ReportingTemplate :: REPORTING_BLOCK_VISIBLE, ReportingTemplate :: PARAM_DIMENSIONS => ReportingTemplate :: REPORTING_BLOCK_USE_CONTAINER_DIMENSIONS));
+
+        parent :: __construct($parent,$id,$params,$trail);
+	}
+
+    /**
+     * @see ReportingTemplate -> get_properties()
+     */
+    public static function get_properties()
+    {
+        $properties[ReportingTemplateRegistration :: PROPERTY_TITLE] = 'AssessmentAttemptsTemplateTitle';
+        $properties[ReportingTemplateRegistration :: PROPERTY_PLATFORM] = 0;
+        $properties[ReportingTemplateRegistration :: PROPERTY_DESCRIPTION] = 'AssessmentAttemptsTemplateDescription';
+
+        return $properties;
+    }
+
+    /**
+     * @see ReportingTemplate -> to_html()
+     */
+    function to_html()
+    {
+    	//template header
+        $html[] = $this->get_header();
+        //$html[] = '<div class="reporting_center">';
+        //show visible blocks
+        
+        $html[] = $this->get_learning_object_data();
+        
+        $html[] = $this->get_visible_reporting_blocks();
+        //$html[] = '</div>';
+    	//template footer
+        $html[] = $this->get_footer();
+
+    	return implode("\n", $html);
+    }
+    
+    function get_learning_object_data()
+    {
+    	$pub = AssessmentDataManager :: get_instance()->retrieve_assessment_publication($this->pid);
+    	$assessment = $pub->get_publication_object();
+    	
+    	$html[] = '<div class="learning_object" style="background-image: url('. Theme :: get_common_image_path(). 'learning_object/assessment.png);">';
+		$html[] = '<div class="title">';
+		$html[] = $assessment->get_title();
+		$html[] = '</div>';
+		$html[] = $assessment->get_description();
+		$html[] = '<div class="title">';
+		$html[] = Translation :: get('Statistics');
+		$html[] = '</div>';
+		$track = new AssessmentAssessmentAttemptsTracker();
+		
+		$avg = $track->get_average_score($pub);
+		if (!isset($avg))
+		{
+			$avg_line = 'No results';
+		}
+		else
+		{
+			$avg_line = $avg . '%';
+		}
+		$html[] = Translation :: get('AverageScore').': '.$avg_line;
+		$html[] = '<br/>'.Translation :: get('TimesTaken').': '.$track->get_times_taken($pub);
+		$html[] = '</div>';
+		
+		return implode("\n", $html);
+    }
+}
+?>
