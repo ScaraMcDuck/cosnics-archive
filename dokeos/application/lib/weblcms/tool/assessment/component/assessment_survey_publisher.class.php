7b8b0984f71549dad4e2dbe70dd23dfb2ed320af
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-08-06 12:40:30 +0000

    fixed the actionbar in the assessment tool to remove unneccessary items

--- dokeos/application/lib/weblcms/tool/assessment/component/assessment_survey_publisher.class.php
+++ dokeos/application/lib/weblcms/tool/assessment/component/assessment_survey_publisher.class.php
@@ -14,17 +14,17 @@ class AssessmentToolSurveyPublisherComponent extends AssessmentToolComponent
 	
 	function get_toolbar()
 	{
-		$toolbar = parent :: get_toolbar();
+		$toolbar = new ActionbarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
 		
 		if ($this->is_allowed(EDIT_RIGHT))
 		{
-			$toolbar->add_tool_action(
+			$toolbar->add_common_action(
 				new ToolbarItem(
 					Translation :: get('ViewInvitedUsers'), Theme :: get_common_image_path().'action_visible.png', $this->get_url(array(AssessmentTool :: PARAM_ACTION => AssessmentTool :: ACTION_PUBLISH_SURVEY, AssessmentTool :: PARAM_PUBLICATION_ACTION => AssessmentTool :: ACTION_VIEW, Tool :: PARAM_PUBLICATION_ID => Request :: get(Tool :: PARAM_PUBLICATION_ID))), ToolbarItem :: DISPLAY_ICON_AND_LABEL
 				)
 			);
 			
-			$toolbar->add_tool_action(
+			$toolbar->add_common_action(
 				new ToolbarItem(
 					Translation :: get('PublishSurvey'), Theme :: get_common_image_path().'action_publish.png', $this->get_url(array(AssessmentTool :: PARAM_ACTION => AssessmentTool :: ACTION_PUBLISH_SURVEY, Tool :: PARAM_PUBLICATION_ID => Request :: get(Tool :: PARAM_PUBLICATION_ID))), ToolbarItem :: DISPLAY_ICON_AND_LABEL
 				)
