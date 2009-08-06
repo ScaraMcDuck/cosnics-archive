55e5a95cb078907d08d200d4d12ac3c017df2bea
vanpouckesven <vanpouckesven@localhost>
2009-08-06 10:09:45 +0000

    Fixed the save of the hotpotatoes in the assessment module now with ajax instead of old code

--- dokeos/application/lib/assessment/assessment_manager/component/assessment_publication_viewer.class.php
+++ dokeos/application/lib/assessment/assessment_manager/component/assessment_publication_viewer.class.php
@@ -71,14 +71,17 @@ class AssessmentManagerAssessmentPublicationViewerComponent extends AssessmentMa
 			$this->active_tracker = $this->create_tracker();
 		}
 
+        $trail = new BreadcrumbTrail();
+        $trail->add(new Breadcrumb($this->get_url(array(AssessmentManager :: PARAM_ACTION => AssessmentManager :: ACTION_BROWSE_ASSESSMENT_PUBLICATIONS)), Translation :: get('BrowseAssessmentPublications')));
+        $trail->add(new Breadcrumb($this->get_url(array(AssessmentManager :: PARAM_ASSESSMENT_PUBLICATION => $this->pid)), Translation :: get('TakeAssessment')));
+		
 		// Executing assessment
 		
 		if($this->assessment->get_assessment_type() == 'hotpotatoes')
 		{
-			$this->display_header(new BreadcrumbTrail());
+			$this->display_header($trail);
 
-			$this->assessment->add_javascript($this->get_course_id());
-			$path = $this->assessment->get_test_path();
+			$path = $this->add_javascript();
 			echo '<iframe src="' . $path . '" width="100%" height="600">
   				 <p>Your browser does not support iframes.</p>
 				 </iframe>';
@@ -90,11 +93,7 @@ class AssessmentManagerAssessmentPublicationViewerComponent extends AssessmentMa
 		{
 			$display = ComplexDisplay :: factory($this, $this->assessment->get_type());
 	        $display->set_root_lo($this->assessment);
-	        
-	        $trail = new BreadcrumbTrail();
-	        $trail->add(new Breadcrumb($this->get_url(array(AssessmentManager :: PARAM_ACTION => AssessmentManager :: ACTION_BROWSE_ASSESSMENT_PUBLICATIONS)), Translation :: get('BrowseAssessmentPublications')));
-	        $trail->add(new Breadcrumb($this->get_url(array(AssessmentManager :: PARAM_ASSESSMENT_PUBLICATION => $this->pid)), Translation :: get('TakeAssessment')));
-	        
+
 			$this->display_header($trail);
 			$display->run();
 			$this->display_footer();
@@ -150,6 +149,86 @@ class AssessmentManagerAssessmentPublicationViewerComponent extends AssessmentMa
 	function get_current_attempt_id()
 	{
 		return $this->active_tracker->get_id();
+	}
+	
+	//Add javascript
+	
+	function add_javascript($course)
+	{
+		$content = $this->read_file_content();
+		$js_content = $this->replace_javascript($content, $course);
+		$path = $this->write_file_content($js_content);
+		
+		return $path;
+	}
+	
+	private function read_file_content()
+	{
+		$full_file_path = Path :: get(SYS_REPO_PATH) . $this->assessment->get_path();
+		
+		if(is_file($full_file_path)) 
+		{
+			if (!($fp = fopen(urldecode($full_file_path), "r"))) 
+			{
+				return "";
+			}
+			$contents = fread($fp, filesize($full_file_path));
+			fclose($fp);
+			return $contents;
+	  	}
+	}
+	
+	private function write_file_content($content)
+	{
+		$full_file_path = Path :: get(SYS_REPO_PATH) . substr($this->assessment->get_path(), 0, strlen($this->assessment->get_path()) - 4) . '.' . Session :: get_user_id() . '.t.htm';
+		$full_web_path = Path :: get(WEB_REPO_PATH) . substr($this->assessment->get_path(), 0, strlen($this->assessment->get_path()) - 4) . '.' . Session :: get_user_id() . '.t.htm';
+		Filesystem::remove($full_file_path);
+		
+		if (($fp = fopen(urldecode($full_file_path), "w"))) 
+		{
+			fwrite($fp,$content);
+			fclose($fp);
+		}
+		
+		return $full_web_path;
+	}
+	
+	private function replace_javascript($content)
+	{
+		$mit = "function Finish(){";
+		$js_content = "var SaveScoreVariable = 0; // This variable included by Dokeos System\n".
+					"function mySaveScore() // This function included by Dokeos System\n".
+					"{\n".
+					"   if (SaveScoreVariable==0)\n".
+					"		{\n".
+					"			SaveScoreVariable = 1;\n".
+					"			var result=jQuery.ajax({type: 'POST', url:'".Path ::get(WEB_PATH)."application/lib/assessment/ajax/hotpotatoes_save_score.php', data: {id: " . $this->active_tracker->get_id() . ", score: Score}, async: false}).responseText;\n" .
+				//	"			alert(result);" .
+					"			if (C.ie)\n".
+					"			{\n".
+				//	"				window.alert(Score);\n".
+					"				document.parent.location.href=\"" . $this->get_browse_assessment_publications_url() . "\"\n".
+					"			}\n".
+					"			else\n".
+					"			{\n".
+				//	"				window.alert(Score);\n".
+					"				window.parent.location.href=\"" . $this->get_browse_assessment_publications_url() . "\"\n".
+					"			}\n".
+					"		}\n".
+					"}\n".
+					"// Must be included \n".
+					"function Finish(){\n".
+					" mySaveScore();";
+		$newcontent = str_replace($mit,$js_content,$content);
+		$prehref="<!-- BeginTopNavButtons -->";
+		$posthref="<!-- BeginTopNavButtons --><!-- edited by Dokeos -->";
+		$newcontent = str_replace($prehref,$posthref,$newcontent);
+		
+		$jquery_content = "<head>\n<script src='" . Path :: get(WEB_PATH) . "plugin/jquery/jquery.min.js' type='text/javascript'></script>";
+		$add_to = '<head>';
+		$newcontent = str_replace($add_to,$jquery_content,$newcontent);
+		
+		return $newcontent;
 	}
 }
 ?>
