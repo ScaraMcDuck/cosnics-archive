701ee4df4b04406b29bddb64645542c2b91925ab
vanpouckesven <vanpouckesven@localhost>
2009-08-07 08:46:57 +0000

    Creative friday: prepared the physical location object so it can be installed

--- dokeos/repository/lib/learning_object/physical_location/physical_location_difference.class.php
+++ dokeos/repository/lib/learning_object/physical_location/physical_location_difference.class.php
@@ -11,5 +11,20 @@
  */
 class PhysicalLocationDifference extends LearningObjectDifference
 {
+	function get_difference()
+	{
+		$object = $this->get_object();
+		$version = $this->get_version();
+		
+		$object_string = $object->get_location();
+        $object_string = explode("\n", strip_tags($object_string));
+           	
+        $version_string = $version->get_location();
+		$version_string = explode("\n", strip_tags($version_string));
+		
+		$td = new Difference_Engine($version_string, $object_string);
+		
+		return array_merge(parent :: get_difference(), $td->getDiff());
+	}
 }
 ?>
--- dokeos/repository/lib/learning_object/physical_location/physical_location_form.class.php
+++ dokeos/repository/lib/learning_object/physical_location/physical_location_form.class.php
@@ -15,16 +15,50 @@ class PhysicalLocationForm extends LearningObjectForm
 	function create_learning_object()
 	{
 		$object = new PhysicalLocation();
+		$object->set_location($this->exportValue(PhysicalLocation :: PROPERTY_LOCATION));
 		$this->set_learning_object($object);
 		return parent :: create_learning_object();
 	}
 
+	function update_learning_object()
+	{
+		$object = $this->get_learning_object();
+		$object->set_location($this->exportValue(PhysicalLocation :: PROPERTY_LOCATION));
+		return parent :: update_learning_object();
+	}
+	
 	function set_csv_values($valuearray)
 	{
 		$defaults[LearningObject :: PROPERTY_TITLE] = $valuearray[0];
 		$defaults[LearningObject :: PROPERTY_PARENT_ID] = $valuearray[1];
-		$defaults[LearningObject :: PROPERTY_DESCRIPTION] = $valuearray[2];	
+		$defaults[LearningObject :: PROPERTY_DESCRIPTION] = $valuearray[2];
+		$defaults[PhysicalLocation :: PROPERTY_LOCATION] = $valuearray[3];		
 		parent :: set_values($defaults);			
 	}	
+
+	protected function build_creation_form()
+	{
+		parent :: build_creation_form();
+		$this->addElement('category', Translation :: get(get_class($this) .'Properties'));
+		$this->add_textfield(PhysicalLocation :: PROPERTY_LOCATION, Translation :: get('Location'), true, array('size' => '100'));
+		$this->addElement('category');
+	}
+	protected function build_editing_form()
+	{
+		parent :: build_editing_form();
+		$this->addElement('category', Translation :: get(get_class($this) .'Properties'));
+		$this->add_textfield(PhysicalLocation :: PROPERTY_LOCATION, Translation :: get('Location'), true, array('size' => '100'));
+		$this->addElement('category');
+	}
+
+	function setDefaults($defaults = array ())
+	{
+		$lo = $this->get_learning_object();
+		if (isset($lo))
+		{
+			$defaults[PhysicalLocation :: PROPERTY_LOCATION] = $lo->get_location();
+		}
+		parent :: setDefaults($defaults);
+	}
 }
 ?>
