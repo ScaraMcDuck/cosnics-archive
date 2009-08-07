b3b9d37cda2210967c7b6a41892f2eeb374cd4cc
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-08-07 08:34:53 +0000

    Creative friday: added a new physical location learning object

---
+++ dokeos/repository/lib/learning_object/physical_location/physical_location_difference.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package repository.learningobject
+ * @subpackage physical_location
+ * 
+ *  @author Hans De Bisschop
+ *  @author Dieter De Neef
+ */
+/**
+ * This class can be used to get the difference between physical_locations
+ */
+class PhysicalLocationDifference extends LearningObjectDifference
+{
+}
+?>
---
+++ dokeos/repository/lib/learning_object/physical_location/physical_location_difference_display.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package repository.learningobject
+ * @subpackage physical_location
+ * 
+ *  @author Hans De Bisschop
+ *  @author Dieter De Neef
+ */
+/**
+ * This class can be used to display the difference between physical_locations
+ */
+class PhysicalLocationDifferenceDisplay extends LearningObjectDifferenceDisplay
+{
+}
+?>
