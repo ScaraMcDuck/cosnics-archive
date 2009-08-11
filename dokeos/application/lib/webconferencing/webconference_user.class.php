a426831e3932d1d9d8900e3a4af97334218e3993
MichaelKyndt <MichaelKyndt@localhost>
2009-08-11 11:28:10 +0000

    updated webconference

---
+++ dokeos/application/lib/webconferencing/webconference_user.class.php
@@ -0,0 +1,79 @@
+<?php 
+require_once Path :: get_common_path() . 'data_class.class.php';
+
+/**
+ * This class describes a WebconferenceUser data object
+ * @author Michael Kyndt
+ */
+class WebconferenceUser extends DataClass
+{
+	const CLASS_NAME = __CLASS__;
+	const TABLE_NAME = 'publication_user';
+
+	/**
+	 * WebconferenceUser properties
+	 */
+	const PROPERTY_WEBCONFERENCE = 'webconference';
+	const PROPERTY_USER = 'user';
+
+	/**
+	 * Get the default properties
+	 * @return array The property names.
+	 */
+	static function get_default_property_names()
+	{
+		return array (self :: PROPERTY_WEBCONFERENCE, self :: PROPERTY_USER);
+	}
+
+	function get_data_manager()
+	{
+		return WebconferencingDataManager :: get_instance();
+	}
+
+	/**
+	 * Returns the webconference of this WebconferenceUser.
+	 * @return the webconference.
+	 */
+	function get_webconference()
+	{
+		return $this->get_default_property(self :: PROPERTY_WEBCONFERENCE);
+	}
+
+	/**
+	 * Sets the webconference of this WebconferenceUser.
+	 * @param webconference
+	 */
+	function set_webconference($webconference)
+	{
+		$this->set_default_property(self :: PROPERTY_WEBCONFERENCE, $webconference);
+	}
+
+	/**
+	 * Returns the user of this WebconferenceUser.
+	 * @return the user.
+	 */
+	function get_user()
+	{
+		return $this->get_default_property(self :: PROPERTY_USER);
+	}
+
+	/**
+	 * Sets the user of this WebconferenceUser.
+	 * @param user
+	 */
+	function set_user($user)
+	{
+		$this->set_default_property(self :: PROPERTY_USER, $user);
+	}
+
+	function create()
+	{
+            return $this->get_data_manager()->create_webconference_user($this);
+	}
+
+	static function get_table_name()
+	{
+		return self :: TABLE_NAME;
+	}
+}
+?>
