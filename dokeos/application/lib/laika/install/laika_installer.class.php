8ce7eb38f98754e9cececb3d27c0540357ee94bc
Hans De Bisschop <hans.de.bisschop@ehb.be>
2009-08-11 12:52:54 +0000

    Small fix for Laika.

--- dokeos/application/lib/laika/install/laika_installer.class.php
+++ dokeos/application/lib/laika/install/laika_installer.class.php
@@ -14,23 +14,23 @@ class LaikaInstaller extends Installer
     
     function install_extra()
     {
-    	$sql_file = Path :: get_application_path() . 'lib/laika/install/dump.sql';
-    	$query = file_get_contents($sql_file);
-    	$queries = explode('@@@@', $query);
-    	
-    	$dm = $this->get_data_manager();
-    	$database = $dm->get_database();
-    	$connection = $database->get_connection();
-    	
-    	foreach($queries as $query)
-    	{
-			$statement = $connection->query(trim($query));
-			
-			if (MDB2 :: isError($statement))
-			{
-				return false;	
-			}
-    	}
+//    	$sql_file = Path :: get_application_path() . 'lib/laika/install/dump.sql';
+//    	$query = file_get_contents($sql_file);
+//    	$queries = explode('@@@@', $query);
+//    	
+//    	$dm = $this->get_data_manager();
+//    	$database = $dm->get_database();
+//    	$connection = $database->get_connection();
+//    	
+//    	foreach($queries as $query)
+//    	{
+//			$statement = $connection->query(trim($query));
+//			
+//			if (MDB2 :: isError($statement))
+//			{
+//				return false;	
+//			}
+//    	}
 		
 		return true;
     }
