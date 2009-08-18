e9c6f14181b45ee46736ea286a97bad2851ea407
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-08-18 12:57:43 +0000

    Added new language files

--- dokeos/languages/english/webservice.inc.php
+++ dokeos/languages/english/webservice.inc.php
@@ -1,26 +1,8 @@
 <?php
 $lang['webservice']['WebserviceCategory'] = 'Webservice category';
-$lang['webservice']['Manage'] = 'Manage';
-$lang['webservice']['Webservice'] = 'Webservice';
-$lang['webservice']['Activate'] = 'Activate';
 $lang['webservice']['Webservices'] = 'Webservices';
 $lang['webservice']['BrowseWebservices'] = 'Browse webservices';
-$lang['webservice']['NotAllowed'] = 'You are not allowed here. ';
-$lang['webservice']['Add'] = 'Add';
-$lang['webservice']['Name'] = 'Name';
-$lang['webservice']['Description'] = 'Description';
-$lang['webservice']['MyAccount'] = 'My account';
-$lang['webservice']['Logout'] = 'Logout';
-$lang['webservice']['Administration'] = 'Administration';
-$lang['webservice']['Repository'] = 'Repository';
-$lang['webservice']['List'] = 'List';
-$lang['webservice']['ListDescription'] = 'List description';
-$lang['webservice']['LockedFalse'] = 'Locked false';
-$lang['webservice']['LocationNoInherit'] = 'Location no inherit';
-$lang['webservice']['LocationInherit'] = 'Location inherit';
 $lang['webservice']['ManageWebservices'] = 'Manage webservices';
 $lang['webservice']['EditRightsForAllProvidedWebservices'] = 'Edit rights for all provided webservices';
 $lang['webservice']['SetRightsForThe'] = 'Set rights for the';
-$lang['webservice']['Category'] = 'Category';
-$lang['webservice']['LockedTrue'] = 'Locked';
 ?>
