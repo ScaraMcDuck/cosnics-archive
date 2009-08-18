1da75e94336ad003192996f99377af3827a2b4c1
vanpouckesven <vanpouckesven@localhost>
2009-08-18 13:18:35 +0000

    cleanup

---
+++ dokeos/languages/english/assessment.inc.php
@@ -0,0 +1,17 @@
+<?php
+$lang['assessment']['DeleteResults'] = 'Delete results';
+$lang['assessment']['Action'] = 'Action';
+$lang['assessment']['BrowseAssessmentPublications'] = 'Browse assessment publications';
+$lang['assessment']['AssessmentAttemptsDeleted'] = 'Assessment attempts deleted';
+$lang['assessment']['ViewAssessmentResults'] = 'View assessment results';
+$lang['assessment']['ViewAssessmentDetails'] = 'View assessment details';
+$lang['assessment']['CreateAssessmentPublication'] = 'Create assessment publication';
+$lang['assessment']['UpdateAssessmentPublication'] = 'Update assessment publication';
+$lang['assessment']['CategoriesMoved'] = 'Categories moved';
+$lang['assessment']['MoveAssessmentPublications'] = 'Move assessment publications';
+$lang['assessment']['SelectCategory'] = 'Select category';
+$lang['assessment']['QtiImported'] = 'Qti imported';
+$lang['assessment']['UsersInvited'] = 'Users invited';
+$lang['assessment']['NoAssessmentPublicationsSelected'] = 'No assessment publications selected';
+$lang['assessment']['Assessments'] = 'Assessments';
+?>
