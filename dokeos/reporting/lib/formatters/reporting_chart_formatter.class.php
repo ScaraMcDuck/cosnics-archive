8c0f1d313f1a91655e2a8fd51e7785a0a1690722
MichaelKyndt <michaelkyndt@cosnics>
2009-08-19 08:08:30 +0000

    updated the reporting

--- dokeos/reporting/lib/formatters/reporting_chart_formatter.class.php
+++ dokeos/reporting/lib/formatters/reporting_chart_formatter.class.php
@@ -3,40 +3,41 @@
  * 
  * @author: Michael Kyndt
  */
-class ReportingChartFormatter extends ReportingFormatter {
-	private $instance;
-	private $reporting_block;
-
-     /**
-     * @see Reporting Formatter -> to_html
-     */
-	public function to_html()
-	{
-		return $this->get_chart_instance()->to_html();
-	} //to_html
+class ReportingChartFormatter extends ReportingFormatter
+{
+    private $instance;
+    private $reporting_block;
 
     /**
-    * Returns the link to the image without html
-    */
+     * @see Reporting Formatter -> to_html
+     */
+    public function to_html()
+    {
+        return $this->get_chart_instance()->to_html();
+    } //to_html
+
+    /**
+     * Returns the link to the image without html
+     */
     public function to_link()
     {
         return $this->get_chart_instance()->to_link();
     }
 
-	public function ReportingChartFormatter(&$reporting_block)
-	{
-		$this->reporting_block = $reporting_block;
-	} //ReportingChartFormatter
+    public function ReportingChartFormatter(&$reporting_block)
+    {
+        $this->reporting_block = $reporting_block;
+    } //ReportingChartFormatter
 
-	public function get_chart_instance()
-	{
-		//if (!isset ($this->instance)) {
-			$chartformatter = 'Pchart';
-			require_once dirname(__FILE__) . '/'.strtolower($chartformatter).'/'.strtolower($chartformatter).'_reporting_chart_formatter.class.php';
-			$class = $chartformatter.'ReportingChartFormatter';
-			$this->instance = new $class ($this->reporting_block);
-		//}
-		return $this->instance;
-	} //get_instance
+    public function get_chart_instance()
+    {
+    //if (!isset ($this->instance)) {
+        $chartformatter = 'Pchart';
+        require_once dirname(__FILE__) . '/'.strtolower($chartformatter).'/'.strtolower($chartformatter).'_reporting_chart_formatter.class.php';
+        $class = $chartformatter.'ReportingChartFormatter';
+        $this->instance = new $class ($this->reporting_block);
+        //}
+        return $this->instance;
+    } //get_instance
 } //ReportingChartFormatter
 ?>
