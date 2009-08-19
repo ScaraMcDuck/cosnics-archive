dc94f40a28ae67d5a1eb4bb60cadff45d1fce329
MichaelKyndt <michaelkyndt@cosnics>
2009-08-19 08:26:41 +0000

    updated the reporting

--- dokeos/reporting/lib/forms/reporting_template_registration_form.class.php
+++ dokeos/reporting/lib/forms/reporting_template_registration_form.class.php
@@ -6,36 +6,38 @@ require_once Path :: get_library_path() . 'html/formvalidator/FormValidator.clas
 require_once Path :: get_reporting_path() . 'lib/reporting_template_registration.class.php';
 require_once Path :: get_reporting_path() . 'lib/reporting_data_manager.class.php';
 
-class ReportingTemplateRegistrationForm extends FormValidator {
+class ReportingTemplateRegistrationForm extends FormValidator
+{
 
-	const TYPE_CREATE = 1;
-	const TYPE_EDIT = 2;
-	const RESULT_SUCCESS = 'ReportingTemplateRegistrationUpdated';
-	const RESULT_ERROR = 'ReportingTemplateRegistrationUpdateFailed';
+    const TYPE_CREATE = 1;
+    const TYPE_EDIT = 2;
+    const RESULT_SUCCESS = 'ReportingTemplateRegistrationUpdated';
+    const RESULT_ERROR = 'ReportingTemplateRegistrationUpdateFailed';
 
-	private $parent;
-	private $reporting_template_registration;
+    private $parent;
+    private $reporting_template_registration;
 
-	/**
-	 * Creates a new UserForm
-	 * Used by the admin to create/update a user
-	 */
-    function ReportingTemplateRegistrationForm($form_type, $reporting_template_registration, $action) {
-    	parent :: __construct('reportingtemplateregistration_edit', 'post', $action);
-    	
-    	$this->reporting_template_registration = $reporting_template_registration;
+    /**
+     * Creates a new UserForm
+     * Used by the admin to create/update a user
+     */
+    function ReportingTemplateRegistrationForm($form_type, $reporting_template_registration, $action)
+    {
+        parent :: __construct('reportingtemplateregistration_edit', 'post', $action);
 
-		$this->form_type = $form_type;
-		if ($this->form_type == self :: TYPE_EDIT)
-		{
-			$this->build_editing_form();
-		}
-		elseif ($this->form_type == self :: TYPE_CREATE)
-		{
-			$this->build_creation_form();
-		}
+        $this->reporting_template_registration = $reporting_template_registration;
 
-		$this->setDefaults();
+        $this->form_type = $form_type;
+        if ($this->form_type == self :: TYPE_EDIT)
+        {
+            $this->build_editing_form();
+        }
+        elseif ($this->form_type == self :: TYPE_CREATE)
+        {
+            $this->build_creation_form();
+        }
+
+        $this->setDefaults();
     }
 
     /**
@@ -43,10 +45,10 @@ class ReportingTemplateRegistrationForm extends FormValidator {
      */
     function build_basic_form()
     {
-		$this->addElement('text', ReportingTemplateRegistration :: PROPERTY_TITLE, Translation :: get('Title'), array("size" => "50"));
-		$this->addRule(ReportingTemplateRegistration :: PROPERTY_TITLE, Translation :: get('ThisFieldIsRequired'), 'required');
-		
-		$this->add_html_editor(ReportingTemplateRegistration :: PROPERTY_DESCRIPTION, Translation :: get('Description'), true);
+        $this->addElement('text', ReportingTemplateRegistration :: PROPERTY_TITLE, Translation :: get('Title'), array("size" => "50"));
+        $this->addRule(ReportingTemplateRegistration :: PROPERTY_TITLE, Translation :: get('ThisFieldIsRequired'), 'required');
+
+        $this->add_html_editor(ReportingTemplateRegistration :: PROPERTY_DESCRIPTION, Translation :: get('Description'), true);
     }
 
     /**
@@ -54,16 +56,16 @@ class ReportingTemplateRegistrationForm extends FormValidator {
      */
     function build_editing_form()
     {
-    	$user = $this->user;
-    	$parent = $this->parent;
+        $user = $this->user;
+        $parent = $this->parent;
 
-    	$this->build_basic_form();
+        $this->build_basic_form();
 
-    	$this->addElement('hidden', ReportingTemplateRegistration :: PROPERTY_ID);
-    	
-		$buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Update'), array('class' => 'positive update'));
-		$buttons[] = $this->createElement('style_reset_button', 'reset', Translation :: get('Reset'), array('class' => 'normal empty'));
-		$this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
+        $this->addElement('hidden', ReportingTemplateRegistration :: PROPERTY_ID);
+
+        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Update'), array('class' => 'positive update'));
+        $buttons[] = $this->createElement('style_reset_button', 'reset', Translation :: get('Reset'), array('class' => 'normal empty'));
+        $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
     }
 
     /**
@@ -71,11 +73,11 @@ class ReportingTemplateRegistrationForm extends FormValidator {
      */
     function build_creation_form()
     {
-    	$this->build_basic_form();
-    	
-		$buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Create'), array('class' => 'positive'));
-		$buttons[] = $this->createElement('style_reset_button', 'reset', Translation :: get('Reset'), array('class' => 'normal empty'));
-		$this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
+        $this->build_basic_form();
+
+        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Create'), array('class' => 'positive'));
+        $buttons[] = $this->createElement('style_reset_button', 'reset', Translation :: get('Reset'), array('class' => 'normal empty'));
+        $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
     }
 
     /**
@@ -83,21 +85,21 @@ class ReportingTemplateRegistrationForm extends FormValidator {
      */
     function update_reporting_template_registration()
     {
-    	$reporting_template_registration = $this->reporting_template_registration;
-    	$values = $this->exportValues();
-    	
-    	$reporting_template_registration->set_title($values[ReportingTemplateRegistration :: PROPERTY_TITLE]);
-    	$reporting_template_registration->set_description($values[ReportingTemplateRegistration :: PROPERTY_DESCRIPTION]);
+        $reporting_template_registration = $this->reporting_template_registration;
+        $values = $this->exportValues();
 
-//        return $reporting_template_registration->update();
-   		if (!$reporting_template_registration->update())
-   		{
-   			return false;
-   		}
-   		else
-   		{
-   			return true;
-   		}
+        $reporting_template_registration->set_title($values[ReportingTemplateRegistration :: PROPERTY_TITLE]);
+        $reporting_template_registration->set_description($values[ReportingTemplateRegistration :: PROPERTY_DESCRIPTION]);
+
+        //        return $reporting_template_registration->update();
+        if (!$reporting_template_registration->update())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
 
@@ -106,35 +108,35 @@ class ReportingTemplateRegistrationForm extends FormValidator {
      */
     function create_reporting_template_registration()
     {
-    	$reporting_template_registration = $this->reporting_template_registration;
-    	$values = $this->exportValues();
-    	
-    	$reporting_template_registration->set_title($values[Role :: PROPERTY_TITLE]);
-    	$reporting_template_registration->set_description($values[Role :: PROPERTY_DESCRIPTION]);
+        $reporting_template_registration = $this->reporting_template_registration;
+        $values = $this->exportValues();
 
-//        return $reporting_template_registration->create();
-   		if (!$reporting_template_registration->create())
-   		{
-   			return false;
-   		}
-   		else
-   		{
-   			return true;
-   		}
+        $reporting_template_registration->set_title($values[Role :: PROPERTY_TITLE]);
+        $reporting_template_registration->set_description($values[Role :: PROPERTY_DESCRIPTION]);
+
+        //        return $reporting_template_registration->create();
+        if (!$reporting_template_registration->create())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
-	/**
-	 * Sets default values.
-	 * @param array $defaults Default values for this form's parameters.
-	 */
-	function setDefaults($defaults = array ())
-	{
-		$reporting_template_registration = $this->reporting_template_registration;
-		
-		$defaults[ReportingTemplateRegistration :: PROPERTY_TITLE] = $reporting_template_registration->get_title();
-		$defaults[ReportingTemplateRegistration :: PROPERTY_DESCRIPTION] = $reporting_template_registration->get_description();
-			
-		parent :: setDefaults($defaults);
-	}
+    /**
+     * Sets default values.
+     * @param array $defaults Default values for this form's parameters.
+     */
+    function setDefaults($defaults = array ())
+    {
+        $reporting_template_registration = $this->reporting_template_registration;
+
+        $defaults[ReportingTemplateRegistration :: PROPERTY_TITLE] = $reporting_template_registration->get_title();
+        $defaults[ReportingTemplateRegistration :: PROPERTY_DESCRIPTION] = $reporting_template_registration->get_description();
+
+        parent :: setDefaults($defaults);
+    }
 }
 ?>
