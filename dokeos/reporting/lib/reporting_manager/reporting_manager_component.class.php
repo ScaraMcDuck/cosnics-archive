970891cc79f5c4478d7d853a131d089ef1937a7c
MichaelKyndt <michaelkyndt@cosnics>
2009-08-19 09:08:45 +0000

    updated the reporting

--- dokeos/reporting/lib/reporting_manager/reporting_manager_component.class.php
+++ dokeos/reporting/lib/reporting_manager/reporting_manager_component.class.php
@@ -7,31 +7,31 @@
 
 require_once Path :: get_library_path() . 'core_application_component.class.php';
 
- /**
+/**
  * @author Michael Kyndt
  */
 
-abstract class ReportingManagerComponent extends CoreApplicationComponent  
+abstract class ReportingManagerComponent extends CoreApplicationComponent
 {
-	/**
-	 * Constructor
-	 * @param ReportingManager $reporting_manager The reporting manager which
-	 * provides this component
-	 */
-    function ReportingManagerComponent($reporting_manager) 
+/**
+ * Constructor
+ * @param ReportingManager $reporting_manager The reporting manager which
+ * provides this component
+ */
+    function ReportingManagerComponent($reporting_manager)
     {
-    	parent :: __construct($reporting_manager);
+        parent :: __construct($reporting_manager);
     }
-    
+
     function count_reporting_template_registrations($condition = null)
-	{
+    {
         return $this->get_parent()->count_reporting_template_registrations($condition);
-	}
+    }
 
     function retrieve_reporting_template_registrations($condition = null, $offset = null, $count = null, $order_property = null, $order_direction = null)
-	{
+    {
         return $this->get_parent()->retrieve_reporting_template_registrations($condition, $offset, $count, $order_property, $order_direction);
-	}
+    }
 
     function retrieve_reporting_template_registration($reporting_template_registration_id)
     {
@@ -39,9 +39,9 @@ abstract class ReportingManagerComponent extends CoreApplicationComponent
     }
 
     function get_reporting_template_registration_viewing_url($reporting_template_registration)
-	{
-		return $this->get_parent()->get_reporting_template_registration_viewing_url($reporting_template_registration);
-	}
+    {
+        return $this->get_parent()->get_reporting_template_registration_viewing_url($reporting_template_registration);
+    }
 
     function get_reporting_template_registration_editing_url($reporting_template_registration)
     {
