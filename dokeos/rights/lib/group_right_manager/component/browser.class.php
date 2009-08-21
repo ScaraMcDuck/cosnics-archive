a383c350680fee0a345b3161cd8520e9555006cf
Scara84 <Scara84@localhost>
2009-08-21 18:31:14 +0000

    Some more cleanup.

--- dokeos/rights/lib/group_right_manager/component/browser.class.php
+++ dokeos/rights/lib/group_right_manager/component/browser.class.php
@@ -49,78 +49,65 @@ class GroupRightManagerBrowserComponent extends GroupRightManagerComponent
 		if (!isset($this->application))
 		{
 			$this->application = 'admin';
-//			$this->display_header($trail);
-//			echo $this->get_applications();
-//			$this->display_warning_message(Translation :: get('SelectApplication'));
-//			$this->display_footer();
-//			exit;
 		}
-//		else
+		
+	    $conditions = array();
+   		$conditions[] = new EqualityCondition(Location :: PROPERTY_PARENT, 0);
+   		$conditions[] = new EqualityCondition(Location :: PROPERTY_APPLICATION, $this->application);
+   		$condition = new AndCondition($conditions);
+   		$root = RightsDataManager :: get_instance()->retrieve_locations($condition, null, 1, array(new ObjectTableOrder(Location :: PROPERTY_LOCATION)))->next_result();
+
+		if (isset($location))
+		{
+		    $this->location = $this->retrieve_location($location);
+		}
+		else
+		{
+		    $this->location = $root;
+		}
+
+//		$parent_conditions = array();
+//		$parent_conditions[] = new InequalityCondition(Location :: PROPERTY_LEFT_VALUE, InequalityCondition :: LESS_THAN_OR_EQUAL, $this->location->get_left_value());
+//		$parent_conditions[] = new InequalityCondition(Location :: PROPERTY_RIGHT_VALUE, InequalityCondition :: GREATER_THAN_OR_EQUAL, $this->location->get_right_value());
+//		$parent_conditions[] = new EqualityCondition(Location :: PROPERTY_APPLICATION, $this->application);
+//
+//		$parent_condition = new AndCondition($parent_conditions);
+//		$order = array(new ObjectTableOrder(Location :: PROPERTY_LEFT_VALUE));
+//		$order_direction = array(SORT_ASC);
+//
+//		$parents = $this->retrieve_locations($parent_condition, null, null, $order, $order_direction);
+//
+//		while($parent = $parents->next_result())
 //		{
-		    $conditions = array();
-   			$conditions[] = new EqualityCondition(Location :: PROPERTY_PARENT, 0);
-   			$conditions[] = new EqualityCondition(Location :: PROPERTY_APPLICATION, $this->application);
-   			$condition = new AndCondition($conditions);
-   			$root = RightsDataManager :: get_instance()->retrieve_locations($condition, null, 1, array(new ObjectTableOrder(Location :: PROPERTY_LOCATION)))->next_result();
-
-			if (isset($location))
-			{
-			    $this->location = $this->retrieve_location($location);
-			}
-			else
-			{
-			    $this->location = $root;
-			}
-
-//			$parent_conditions = array();
-//			$parent_conditions[] = new InequalityCondition(Location :: PROPERTY_LEFT_VALUE, InequalityCondition :: LESS_THAN_OR_EQUAL, $this->location->get_left_value());
-//			$parent_conditions[] = new InequalityCondition(Location :: PROPERTY_RIGHT_VALUE, InequalityCondition :: GREATER_THAN_OR_EQUAL, $this->location->get_right_value());
-//			$parent_conditions[] = new EqualityCondition(Location :: PROPERTY_APPLICATION, $this->application);
-//
-//			$parent_condition = new AndCondition($parent_conditions);
-//			$order = array(new ObjectTableOrder(Location :: PROPERTY_LEFT_VALUE));
-//			$order_direction = array(SORT_ASC);
-//
-//			$parents = $this->retrieve_locations($parent_condition, null, null, $order, $order_direction);
-//
-//			while($parent = $parents->next_result())
-//			{
-//				$trail->add(new Breadcrumb($this->get_url(array('location' => $parent->get_id())), $parent->get_location()));
-//			}
-
-			$this->action_bar = $this->get_action_bar();
-
-			$this->display_header($trail);
-
-			$html = array();
-			$html[] = $this->get_applications();
-			$html[] = $this->action_bar->as_html() . '<br />';
-
-			$url_format = $this->get_url(array(Application :: PARAM_ACTION => RightsManager :: ACTION_MANAGE_GROUP_RIGHTS, GroupRightManager :: PARAM_GROUP_RIGHT_ACTION => GroupRightManager :: ACTION_BROWSE_GROUP_RIGHTS, GroupRightManager :: PARAM_GROUP => $this->group->get_id(), GroupRightManager :: PARAM_SOURCE => $this->application, GroupRightManager :: PARAM_LOCATION => '%s'));
-			$url_format = str_replace('=%25s', '=%s', $url_format);
-    		$location_menu = new LocationMenu($root->get_id(), $this->location->get_id(), $url_format);
-    		$html[] = '<div style="float: left; width: 18%; overflow: auto; height: 500px;">';
-    		$html[] = $location_menu->render_as_tree();
-    		$html[] = '</div>';
-
-    		$table = new GroupLocationBrowserTable($this, $this->get_parameters(), $this->get_condition($location));
-
-    		$html[] = '<div style="float: right; width: 80%;">';
-
-//    		if ($this->location->get_parent() == 0)
-//    		{
-//    		    $html[] = $this->get_root_rights_table();
-//    		}
-
-    		$html[] = $table->as_html();
-    		$html[] = RightsUtilities :: get_rights_legend();
-    		$html[] = '</div>';
-    		$html[] = ResourceManager :: get_instance()->get_resource_html(Path :: get(WEB_PATH) . 'rights/javascript/configure_group.js');
-
-    		echo implode("\n", $html);
-
-			$this->display_footer();
+//			$trail->add(new Breadcrumb($this->get_url(array('location' => $parent->get_id())), $parent->get_location()));
 //		}
+
+		$this->action_bar = $this->get_action_bar();
+
+		$this->display_header($trail);
+
+		$html = array();
+		$html[] = $this->get_applications();
+		$html[] = $this->action_bar->as_html() . '<br />';
+
+		$url_format = $this->get_url(array(Application :: PARAM_ACTION => RightsManager :: ACTION_MANAGE_GROUP_RIGHTS, GroupRightManager :: PARAM_GROUP_RIGHT_ACTION => GroupRightManager :: ACTION_BROWSE_GROUP_RIGHTS, GroupRightManager :: PARAM_GROUP => $this->group->get_id(), GroupRightManager :: PARAM_SOURCE => $this->application, GroupRightManager :: PARAM_LOCATION => '%s'));
+		$url_format = str_replace('=%25s', '=%s', $url_format);
+    	$location_menu = new LocationMenu($root->get_id(), $this->location->get_id(), $url_format);
+    	$html[] = '<div style="float: left; width: 18%; overflow: auto; height: 500px;">';
+    	$html[] = $location_menu->render_as_tree();
+    	$html[] = '</div>';
+
+    	$table = new GroupLocationBrowserTable($this, $this->get_parameters(), $this->get_condition($location));
+
+    	$html[] = '<div style="float: right; width: 80%;">';
+    	$html[] = $table->as_html();
+    	$html[] = RightsUtilities :: get_rights_legend();
+    	$html[] = '</div>';
+    	$html[] = ResourceManager :: get_instance()->get_resource_html(Path :: get(WEB_PATH) . 'rights/javascript/configure_group.js');
+
+    	echo implode("\n", $html);
+
+		$this->display_footer();
 	}
 
 	function get_root_rights_table()
