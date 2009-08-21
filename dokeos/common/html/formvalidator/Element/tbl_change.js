012e857e3f6ce4ae5541ddcddccbc38150831209
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-08-21 12:08:52 +0000

    fixed the datepicker for the calendar

--- dokeos/common/html/formvalidator/Element/tbl_change.js
+++ dokeos/common/html/formvalidator/Element/tbl_change.js
@@ -1,7 +1,3 @@
-<?php
-// $Id$
-require ('../../../global.inc.php');
-?>
 var day;
 var month;
 var year;
@@ -28,7 +24,7 @@ function openCalendar(form, field) {
 			datevalues[dateindex++] = forminputs[i].value;
 		}
 	}
-    window.open('html/formvalidator/Element/calendar_popup.php', 'calendar', 'width=220,height=200,status=no');
+    window.open(path + 'html/formvalidator/Element/calendar_popup.php', 'calendar', 'width=220,height=200,status=no');
 	day = datevalues[0];
 	month = datevalues[1];
 	year = datevalues[2];
@@ -105,55 +101,90 @@ function initCalendar() {
     str = "";
 
     //heading table
-    str += '
-<table class="calendar">
-	<tr>
-		<th class="monthyear" width="50%">'; str += '<a
-			href="javascript:month--; initCalendar();">&laquo;</a> '; str +=
-		month_names[month]; str += ' <a
-			href="javascript:month++; initCalendar();">&raquo;</a>'; str += '</th>
-		<th class="monthyear" width="50%">'; str += '<a
-			href="javascript:year--; initCalendar();">&laquo;</a> '; str += year;
-		str += ' <a href="javascript:year++; initCalendar();">&raquo;</a>';
-		str += '</th>
-	</tr>
-</table>
-'; str += '
-<table class="calendar">
-	<tr>
-		'; for (i = 0; i < 7; i++) { str += "
-		<th class='daynames'>" + day_names[i] + "</th>
-		"; } str += "
-	</tr>
-	"; var firstDay = new Date(year, month, 1).getDay(); var lastDay = new
-	Date(year, month + 1, 0).getDate(); str += "
-	<tr>
-		"; dayInWeek = 0; for (i = 0; i < firstDay; i++) { str += "
-		<td>&nbsp;</td>
-		"; dayInWeek++; } for (i = 1; i <= lastDay; i++) { if (dayInWeek == 7)
-		{ str += "
-	</tr>
-	<tr>
-		"; dayInWeek = 0; } dispmonth = 1 + month; actVal = formatNum4(year) +
-		"-" + formatNum2(dispmonth, 'month') + "-" + formatNum2(i, 'day'); if
-		(i == day) { style = ' class="selected"'; } else { style = ''; } str
-		+= "
-		<td" + style + "><a href=\"javascript:returnDate(" + i +","+month+","+year + ");\">"
-		+ i + "</a></td>
-		"; dayInWeek++; } for (i = dayInWeek; i < 7; i++) { str += "
-		<td>&nbsp;</td>
-		"; } str += "
-	</tr>
-</table>
-"; cnt.innerHTML = str; } /** * Returns date from calendar. * * @param
-string date text */ function returnDate(d,m,y) { formblock=
-window.opener.document.getElementById(window.opener.formName);
-forminputs = formblock.getElementsByTagName('select'); var datevalues =
-new Array(); var dateindex = 0; for (i = 0; i < forminputs.length; i++)
-{ // regex here to check name attribute var regex = new
-RegExp(window.opener.fieldName, "i"); if
-(regex.test(forminputs[i].getAttribute('name'))) {
-datevalues[dateindex++] = forminputs[i]; } } datevalues[0].selectedIndex
-= (d-1) ; datevalues[1].selectedIndex = m; date = new Date(); year =
-date.getFullYear()-1; datevalues[2].selectedIndex = (y-year);
-window.close(); }
+    str += '<table class="calendar"><tr><th class="monthyear" width="50%">'; 
+    str += '<a href="javascript:month--; initCalendar();">&laquo;</a> '; 
+    str += month_names[month]; 
+    str += ' <a href="javascript:month++; initCalendar();">&raquo;</a>'; 
+    str += '</th><th class="monthyear" width="50%">'; str += '<a href="javascript:year--; initCalendar();">&laquo;</a> '; 
+    str += year;
+	str += ' <a href="javascript:year++; initCalendar();">&raquo;</a>';
+	str += '</th></tr></table>';
+	str += '<table class="calendar"><tr>'; 
+	
+	for (i = 0; i < 7; i++) 
+	{ 
+		str += "<th class='daynames'>" + day_names[i] + "</th>"; 
+	} 
+	
+	str += "</tr>"; 
+	
+	var firstDay = new Date(year, month, 1).getDay(); 
+	var lastDay = new Date(year, month + 1, 0).getDate(); 
+	
+	str += "<tr>";
+	
+	dayInWeek = 0; 
+	
+	for (i = 0; i < firstDay; i++) 
+	{ 
+		str += "<td>&nbsp;</td>"; 
+	    dayInWeek++;
+	}
+	
+	for (i = 1; i <= lastDay; i++) 
+	{ 
+		if (dayInWeek == 7)
+		{ 
+			str += "</tr><tr>"; 
+			dayInWeek = 0; 
+		} 
+		
+		dispmonth = 1 + month; 
+		actVal = formatNum4(year) + "-" + formatNum2(dispmonth, 'month') + "-" + formatNum2(i, 'day');
+		
+		if(i == day) 
+		{ 
+			style = ' class="selected"'; 
+		} 
+		else 
+		{ 
+			style = ''; 
+		} 
+		
+		str += "<td" + style + "><a href=\"javascript:returnDate(" + i +","+month+","+year + ");\">" + i + "</a></td>"; 
+		dayInWeek++; 
+	} 
+	
+	for (i = dayInWeek; i < 7; i++) 
+	{ 
+		str += "<td>&nbsp;</td>"; 
+	} 
+	
+	str += "</tr></table>"; 
+	
+	cnt.innerHTML = str; 
+} /** * Returns date from calendar. * * @param string date text */ 
+
+function returnDate(d,m,y) 
+{ 
+	formblock = window.opener.document.getElementById(window.opener.formName);
+	forminputs = formblock.getElementsByTagName('select'); 
+	var datevalues = new Array(); 
+	var dateindex = 0; 
+	
+	for (i = 0; i < forminputs.length; i++)
+	{
+		var regex = new RegExp(window.opener.fieldName, "i"); 
+		if(regex.test(forminputs[i].getAttribute('name'))) 
+		{
+			datevalues[dateindex++] = forminputs[i]; 
+		} 
+	} 
+	
+	datevalues[0].selectedIndex = (d-1); 
+	datevalues[1].selectedIndex = m; 
+	date = new Date(); 
+	year = date.getFullYear()-1; 
+	datevalues[2].selectedIndex = (y-year);
+	window.close(); 
+}
