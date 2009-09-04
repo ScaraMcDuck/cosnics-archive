eaafeec48f858bda42a605ef75ecaccddc502d29
vanpouckesven <vanpouckesven@localhost>
2009-09-04 12:30:13 +0000

    added a javascript warning message if you type a title that is already in your repository

---
+++ dokeos/repository/ajax/title_exists.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../common/global.inc.php';
+require_once Path :: get_repository_path() . 'lib/repository_data_manager.class.php';
+
+if (Authentication :: is_valid())
+{
+	$title = Request :: post('title');
+	
+	$conditions = new EqualityCondition(LearningObject :: PROPERTY_TITLE, $title);
+	$conditions = new EqualityCondition(LearningObject :: PROPERTY_OWNER_ID, Session :: get_user_id());
+	$condition = new AndCondition($conditions);
+	
+	$count = RepositoryDataManager :: get_instance()->count_learning_objects(null, $condition);
+	if($count > 0)
+	{
+		echo '<div class="warning-message">' . Translation :: get('TitleExists') . '</div>';
+	}
+}
+
+?>
