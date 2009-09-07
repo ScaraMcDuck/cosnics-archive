20657a19e9728fc6cda12186c0fdfc892f8de544
vanpouckesven <vanpouckesven@localhost>
2009-09-07 06:49:41 +0000

    fixed a minor bug in the name checking method

--- dokeos/repository/ajax/title_exists.php
+++ dokeos/repository/ajax/title_exists.php
@@ -7,11 +7,11 @@ if (Authentication :: is_valid())
 {
 	$title = Request :: post('title');
 	
-	$conditions = new EqualityCondition(LearningObject :: PROPERTY_TITLE, $title);
-	$conditions = new EqualityCondition(LearningObject :: PROPERTY_OWNER_ID, Session :: get_user_id());
+	$conditions[] = new EqualityCondition(LearningObject :: PROPERTY_TITLE, $title);
+	$conditions[] = new EqualityCondition(LearningObject :: PROPERTY_OWNER_ID, Session :: get_user_id());
 	$condition = new AndCondition($conditions);
 	
-	$count = RepositoryDataManager :: get_instance()->count_learning_objects(null, $condition);
+	$count = RepositoryDataManager :: get_instance()->count_learning_objects(null, $condition); 
 	if($count > 0)
 	{
 		echo '<div class="warning-message">' . Translation :: get('TitleExists') . '</div>';
