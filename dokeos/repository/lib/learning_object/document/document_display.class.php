a1a35a20482a2307fba45e35394aa5e0b49863c3
alaxos <alaxos@localhost>
2009-09-18 13:11:47 +0000

    uses the download script to show images in the get_short_html() method as well

--- dokeos/repository/lib/learning_object/document/document_display.class.php
+++ dokeos/repository/lib/learning_object/document/document_display.class.php
@@ -24,16 +24,19 @@ class DocumentDisplay extends LearningObjectDisplay
 		}
 		else
 		{
-			$html = preg_replace('|</div>\s*$|s', '<div class="document_link" style="margin-top: 1em;"><a href="'.htmlentities($url).'">'.htmlentities($object->get_filename()).'</a> ('.Filesystem::format_file_size($object->get_filesize()).')</div></div>', $html);
+			$html = preg_replace('|</div>\s*$|s', '<div class="document_link" style="margin-top: 1em;"><a href="'.htmlentities($url).'">'.htmlentities($name).'</a> ('.Filesystem::format_file_size($object->get_filesize()).')</div></div>', $html);
 		}
 		
 		return $html;
 	}
+	
 	//Inherited
 	function get_short_html()
 	{
 		$object = $this->get_learning_object();
-		return '<span class="learning_object"><a href="'.htmlentities($object->get_url()).'">'.htmlentities($object->get_title()).'</a></span>';
+		$url = RepositoryManager :: get_document_downloader_url($object->get_id());
+		
+		return '<span class="learning_object"><a href="'.htmlentities($url).'">'.htmlentities($object->get_title()).'</a></span>';
 	}
 }
 ?>
