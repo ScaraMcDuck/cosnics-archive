92be596522ac9fbd1b9e28114b8db6159f1c2085
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-09-22 13:25:31 +0000

    Added repetition to the export of ical

--- dokeos/repository/lib/export/ical/ical_export.class.php
+++ dokeos/repository/lib/export/ical/ical_export.class.php
@@ -30,8 +30,8 @@ class IcalExport extends LearningObjectExport
         $vevent = new vevent();
         $vevent->setProperty('summary', mb_convert_encoding($learning_object->get_title(), 'UTF-8'));
 
-        $vevent->setProperty('dtstart', $this->get_date_as_array($learning_object->get_start_date()));
-        $vevent->setProperty('dtend', $this->get_date_as_array($learning_object->get_end_date()));
+        $vevent->setProperty('dtstart', $this->get_date_in_ical_format($learning_object->get_start_date()));
+        $vevent->setProperty('dtend', $this->get_date_in_ical_format($learning_object->get_end_date()));
         
         //$vevent->setProperty('LOCATION', $learning_object->get_location());
         $vevent->setProperty('description', mb_convert_encoding($learning_object->get_description(), 'UTF-8'));
@@ -41,8 +41,14 @@ class IcalExport extends LearningObjectExport
         $vevent->setProperty('organizer', $owner->get_email());
         $vevent->setProperty('attendee', $owner->get_email());
 
-        $ical->setComponent($vevent);
+        //TODO: Add repetition
+
+        if($learning_object->repeats())
+        {
+        	$vevent->setProperty( 'rrule', $this->get_rrule());
+        }
         
+        $ical->setComponent($vevent);
         $calendar = $ical->createCalendar();
 
         $handle = fopen($file, 'w+');
@@ -52,7 +58,51 @@ class IcalExport extends LearningObjectExport
       	return $file;
 	}
 	
-	function get_date_as_array($date)
+	function get_rrule()
+	{
+		$rrule = array();
+		
+		$learning_object = $this->get_learning_object();
+		$repeat = $learning_object->get_repeat_type();
+		
+		switch ($repeat)
+		{
+			case CalendarEvent :: REPEAT_TYPE_DAY :
+				$rrule['FREQ'] =  'DAILY';
+				break;
+			case CalendarEvent :: REPEAT_TYPE_WEEK :
+				$rrule['FREQ'] =  'WEEKLY';
+				break;
+			case CalendarEvent :: REPEAT_TYPE_MONTH :
+				$rrule['FREQ'] =  'MONTHLY';
+				break;
+			case CalendarEvent :: REPEAT_TYPE_YEAR :
+				$rrule['FREQ'] =  'YEARLY';
+				break;
+			case CalendarEvent :: REPEAT_TYPE_BIWEEK :
+				$rrule['FREQ'] = 'WEEKLY';
+				$rrule['INTERVAL'] = '2';
+				break;
+			case CalendarEvent :: REPEAT_TYPE_WEEKDAYS :
+				$rrule['FREQ'] = 'DAILY';
+				$rrule['BYDAY'] = array(array('DAY' => 'MO'),
+										array('DAY' => 'TU'),
+										array('DAY' => 'WE'),
+										array('DAY' => 'TH'),
+										array('DAY' => 'FR'));
+				break;
+		}
+		
+		if(!$learning_object->repeats_indefinately())
+		{
+			$rrule['UNTIL'] = $this->get_date_in_ical_format($learning_object->get_repeat_to());
+		}
+		
+		return $rrule;
+		
+	}
+	
+	function get_date_in_ical_format($date)
 	{
 		$y = date('Y', $date);
         $m = date('m', $date);
@@ -61,7 +111,7 @@ class IcalExport extends LearningObjectExport
         $M = date('i', $date);
         $s = date('s', $date);
         
-        return array('year' => $y, 'month' => $m, 'day' => $d, 'hour' => $h, 'min' => $M, 'sec' => $s);
+        return $y . $m . $d . 'T' . $h . $M . $s;
 	}
 
 }
