8d0c46c971ccd04c013406e7d7ba77d392334ec2
vanpouckesven <vanpouckesven@users.noreply.github.com>
2009-09-25 11:45:42 +0000

    fixed the size of the creation form (http://www.dokeosplanet.org/forum/viewtopic.php?f=33&t=767)

--- dokeos/common/html/formvalidator/Element/upload_or_create.php
+++ dokeos/common/html/formvalidator/Element/upload_or_create.php
@@ -59,7 +59,7 @@ class HTML_QuickForm_upload_or_create extends HTML_QuickForm_group
         $this->_elements[0]->setChecked(true);
         $this->_elements[1] = new HTML_QuickForm_file('file', '');
         $this->_elements[2] = new HTML_QuickForm_Radio('choice', '', Translation :: get('Create'), '1', array('onclick' => 'javascript:editor_show(\'editor_html_content\'); javascript:editor_hide(\'uncompress\')'));
-        $this->_elements[3] = new HTML_QuickForm_fckeditor_html_editor('html_content', '');
+        $this->_elements[3] = new HTML_QuickForm_fckeditor_html_editor('html_content', '', null, array('height' => '500'));
         $this->_elements[4] = new HTML_QuickForm_checkbox('uncompress','', Translation :: get('Uncompress'), array('id' => 'uncompress'));
         $this->_elements[3]->fullPage = true;
     }
