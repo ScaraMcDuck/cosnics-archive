a871a2faa6fbcc4d0ffd43a2ad59f3fc9aa80089
Hans De Bisschop <hans.de.bisschop@ehb.be>
2009-10-06 10:16:37 +0000

    Partial fix of the PHPMailer Class + small extension of the Mailing framework

--- dokeos/common/mail/phpmailer/phpmailer.conf.php
+++ dokeos/common/mail/phpmailer/phpmailer.conf.php
@@ -6,9 +6,9 @@
 /**
  * Change these values if you want to use phpmailer to send emails
  */
-$phpmailer_config['SMTP_FROM_EMAIL'] = $administrator["email"];
-$phpmailer_config['SMTP_FROM_NAME'] = $administrator["name"];
-$phpmailer_config['SMTP_HOST'] = 'localhost';
+$phpmailer_config['SMTP_FROM_EMAIL'] = '';
+$phpmailer_config['SMTP_FROM_NAME'] = '';
+$phpmailer_config['SMTP_HOST'] = '';
 $phpmailer_config['SMTP_PORT'] = 25;
 $phpmailer_config['SMTP_MAILER'] = 'smtp'; //mail, sendmail or smtp
 $phpmailer_config['SMTP_AUTH'] = 0;
